2e779bc890108628c25e4b0101f8d239be6ce4d3
최준호 <junho5336@gmail.com>
2023-12-21 17:27:52 +0900

    refactor: 반환값 accessToken임을 명시

--- src/main/java/in/koreatech/koin/dto/UserLoginResponse.java
+++ src/main/java/in/koreatech/koin/dto/UserLoginResponse.java
@@ -1,20 +1,19 @@
 package in.koreatech.koin.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor(access = lombok.AccessLevel.PRIVATE)
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserLoginResponse {
 
-    private String token;
+    @JsonProperty("token")
+    private String accessToken;
 
     @JsonProperty("refresh_token")
     private String refreshToken;
+
     @JsonProperty("user_type")
     private String userType;
 
