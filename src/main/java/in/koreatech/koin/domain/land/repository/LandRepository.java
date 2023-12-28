1b535c54bb06f1e8a79650ac7e0d0da121e2a4bb
daheeParkk <qkrekgml7414@naver.com>
2023-12-28 21:24:55 +0900

    feat : 전체 조회 repository 생성

--- src/main/java/in/koreatech/koin/domain/land/repository/LandRepository.java
+++ src/main/java/in/koreatech/koin/domain/land/repository/LandRepository.java
@@ -1,2 +1,12 @@
-package in.koreatech.koin.domain.land.repository;public class LandRepository {
+package in.koreatech.koin.domain.land.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.Repository;
+
+import in.koreatech.koin.domain.land.domain.Land;
+
+public interface LandRepository extends Repository<Land, Long> {
+
+    List<Land> findAll();
 }
