24eeed1aec3df48dfc83d2f455aee0324fe30410
daheeParkk <qkrekgml7414@naver.com>
2023-12-28 20:48:09 +0900

    faet : controller 구현

--- src/main/java/in/koreatech/koin/domain/land/controller/LandController.java
+++ src/main/java/in/koreatech/koin/domain/land/controller/LandController.java
@@ -1,2 +1,24 @@
-package in.koreatech.koin.domain.land.controller;public class LandController {
+package in.koreatech.koin.domain.land.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.koreatech.koin.domain.land.dto.LandResponse;
+import in.koreatech.koin.domain.land.service.LandService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class LandController {
+
+    private final LandService landService;
+
+    @GetMapping("/lands")
+    public ResponseEntity<List<LandResponse>> getLands() {
+        List<LandResponse> responses = landService.getLands();
+        return ResponseEntity.ok(responses);
+    }
 }
