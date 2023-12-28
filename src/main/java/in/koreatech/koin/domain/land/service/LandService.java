da387f41905cfe554873c290d9a10f1141c8d3eb
daheeParkk <qkrekgml7414@naver.com>
2023-12-28 21:24:30 +0900

    feat : 전체 조회 service 구현

--- src/main/java/in/koreatech/koin/domain/land/service/LandService.java
+++ src/main/java/in/koreatech/koin/domain/land/service/LandService.java
@@ -1,2 +1,27 @@
-package in.koreatech.koin.domain.land.service;public class LandService {
+package in.koreatech.koin.domain.land.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import in.koreatech.koin.domain.land.dto.LandResponse;
+
+import in.koreatech.koin.domain.land.repository.LandRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class LandService {
+
+    private final LandRepository landRepository;
+
+    @Transactional
+    public List<LandResponse> getLands() {
+        return landRepository.findAll()
+            .stream()
+            .map(LandResponse::from)
+            .toList();
+    }
 }
