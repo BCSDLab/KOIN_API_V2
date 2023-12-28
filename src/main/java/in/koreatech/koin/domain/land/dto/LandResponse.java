2b8be493ab7acc9b86cbe7b9827638bf5c11fb5e
daheeParkk <qkrekgml7414@naver.com>
2023-12-28 21:23:08 +0900

    feat : response DTO 생성

--- src/main/java/in/koreatech/koin/domain/land/dto/LandResponse.java
+++ src/main/java/in/koreatech/koin/domain/land/dto/LandResponse.java
@@ -1,2 +1,29 @@
-package in.koreatech.koin.domain.land.dto;public class LandResponse {
+package in.koreatech.koin.domain.land.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import in.koreatech.koin.domain.land.domain.Land;
+
+public record LandResponse(
+    @JsonProperty("internal_name") String internalName,
+    @JsonProperty("monthly_fee") String monthlyFee,
+    @JsonProperty("latitude") String latitude,
+    @JsonProperty("charter_fee") String charterFee,
+    @JsonProperty("name") String name,
+    @JsonProperty("id") Long id,
+    @JsonProperty("longitude") String longitude,
+    @JsonProperty("room_type") String roomType) {
+
+    public static LandResponse from(Land land) {
+        return new LandResponse(
+            land.getInternalName(),
+            land.getMonthlyFee(),
+            land.getLatitude(),
+            land.getCharterFee(),
+            land.getName(),
+            land.getId(),
+            land.getLongitude(),
+            land.getRoomType()
+        );
+    }
 }
