c405f12c858d67b48e3b90e163eaa24ed8074e8e
최준호 <junho5336@gmail.com>
2024-01-03 16:36:05 +0900

    refactor: 미사용 생성자 제거

--- src/main/java/in/koreatech/koin/domain/user/exception/UserNotFoundException.java
+++ src/main/java/in/koreatech/koin/domain/user/exception/UserNotFoundException.java
@@ -4,9 +4,6 @@ public class UserNotFoundException extends RuntimeException {
 
     private static final String DEFAULT_MESSAGE = "존재하지 않는 사용자입니다.";
 
-    public UserNotFoundException() {
-    }
-
     public UserNotFoundException(String message) {
         super(message);
     }
