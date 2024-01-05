4242b9a96a55f62c48d34f492cc3d3ea22c96bca
최준호 <junho5336@gmail.com>
2024-01-05 21:07:21 +0900

    feat: 사용자 인증 구현 (#149)
    * feat: 사용자 인증 구현
    
    * refactor: ArgumentResolver 이름 변경
    
    * refactor: CustomException 적용
    
    * refactor: 예외상황 명시하도록 순서 변경
    
    * refactor: 상수추출
    
    * refactor: 미사용 생성자 제거
    
    * chore: 패키지 변경
    
    * refactor: 미사용 어노테이션 제거
    
    * refactor: 오타수정
    
    * refactor: 오류메시지 구체화
    
    * refactor: 미사용 메서드 제거

---
+++ src/main/java/in/koreatech/koin/domain/user/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package in.koreatech.koin.domain.user.repository;
+
+import in.koreatech.koin.domain.user.model.Student;
+import java.util.Optional;
+import org.springframework.data.repository.Repository;
+
+public interface StudentRepository extends Repository<Student, Long> {
+
+    Student save(Student student);
+
+    Optional<Student> findById(Long id);
+}
