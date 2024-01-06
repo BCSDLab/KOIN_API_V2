0398cbd89fd3b101f58a9da91201cf965187e63d
songsunkook <songsunkook@gmail.com>
2024-01-06 20:00:46 +0900

    style: enum 개행 조정

--- src/main/java/in/koreatech/koin/domain/dept/model/Dept.java
+++ src/main/java/in/koreatech/koin/domain/dept/model/Dept.java
@@ -8,15 +8,33 @@ import lombok.Getter;
 
 @Getter
 public enum Dept {
-    ARCHITECTURAL_ENGINEERING("건축공학부", List.of(72L), "https://cms3.koreatech.ac.kr/arch/1083/subview.do"),
-    EMPLOYMENT_SERVICE_POLICY_DEPARTMENT("고용서비스정책학과", List.of(85L), "https://www.koreatech.ac.kr/kor/CMS/UnivOrganMgr/subMain.do?mCode=MN451"),
-    MECHANICAL_ENGINEERING("기계공학부", List.of(20L), "https://cms3.koreatech.ac.kr/me/795/subview.do"),
-    DESIGN_ENGINEERING("디자인공학부", List.of(51L), "https://cms3.koreatech.ac.kr/ide/1047/subview.do"),
-    MECHATRONICS_ENGINEERING("메카트로닉스공학부", List.of(40L), "https://www.koreatech.ac.kr/kor/CMS/UnivOrganMgr/subMain.do?mCode=MN076"),
-    INDUSTRIAL_MANAGEMENT("산업경영학부", List.of(80L), "https://cms3.koreatech.ac.kr/sim/1167/subview.do"),
-    NEW_ENERGY_MATERIALS_CHEMICAL_ENGINEERING("에너지신소재화학공학부", List.of(74L), "https://cms3.koreatech.ac.kr/ace/992/subview.do"),
-    ELECTRICAL_AND_ELECTRONIC_COMMUNICATION_ENGINEERING("전기전자통신공학부", List.of(61L), "https://cms3.koreatech.ac.kr/ite/842/subview.do"),
-    COMPUTER_SCIENCE("컴퓨터공학부", List.of(35L, 36L), "https://cse.koreatech.ac.kr/page_izgw21"),
+    ARCHITECTURAL_ENGINEERING(
+        "건축공학부", List.of(72L),
+        "https://cms3.koreatech.ac.kr/arch/1083/subview.do"),
+    EMPLOYMENT_SERVICE_POLICY_DEPARTMENT(
+        "고용서비스정책학과", List.of(85L),
+        "https://www.koreatech.ac.kr/kor/CMS/UnivOrganMgr/subMain.do?mCode=MN451"),
+    MECHANICAL_ENGINEERING(
+        "기계공학부", List.of(20L),
+        "https://cms3.koreatech.ac.kr/me/795/subview.do"),
+    DESIGN_ENGINEERING(
+        "디자인공학부", List.of(51L),
+        "https://cms3.koreatech.ac.kr/ide/1047/subview.do"),
+    MECHATRONICS_ENGINEERING(
+        "메카트로닉스공학부", List.of(40L),
+        "https://www.koreatech.ac.kr/kor/CMS/UnivOrganMgr/subMain.do?mCode=MN076"),
+    INDUSTRIAL_MANAGEMENT(
+        "산업경영학부", List.of(80L),
+        "https://cms3.koreatech.ac.kr/sim/1167/subview.do"),
+    NEW_ENERGY_MATERIALS_CHEMICAL_ENGINEERING(
+        "에너지신소재화학공학부", List.of(74L),
+        "https://cms3.koreatech.ac.kr/ace/992/subview.do"),
+    ELECTRICAL_AND_ELECTRONIC_COMMUNICATION_ENGINEERING(
+        "전기전자통신공학부", List.of(61L),
+        "https://cms3.koreatech.ac.kr/ite/842/subview.do"),
+    COMPUTER_SCIENCE(
+        "컴퓨터공학부", List.of(35L, 36L),
+        "https://cse.koreatech.ac.kr/page_izgw21"),
     ;
 
     private final String name;
