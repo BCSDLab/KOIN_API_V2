8d6a74c8b6cbb816a38e2c428af6d15d8dbd9477
daheeParkk <qkrekgml7414@naver.com>
2024-01-09 16:26:19 +0900

    feat : service에 조회 기능 구현

--- src/main/java/in/koreatech/koin/domain/land/service/LandService.java
+++ src/main/java/in/koreatech/koin/domain/land/service/LandService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import in.koreatech.koin.domain.land.dto.LandListItemResponse;
+import in.koreatech.koin.domain.land.dto.LandResponse;
+import in.koreatech.koin.domain.land.model.Land;
 import in.koreatech.koin.domain.land.repository.LandRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -21,5 +23,12 @@ public class LandService {
             .stream()
             .map(LandListItemResponse::from)
             .toList();
+    }
+
+    public LandResponse getLand(Long id) {
+        Land land = landRepository.findById(id)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 복덕방입니다."));
+
+        return LandResponse.from(land);
     }
 }
