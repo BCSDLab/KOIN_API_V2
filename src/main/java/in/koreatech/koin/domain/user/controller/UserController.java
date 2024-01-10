2f0dacca72c0c0e831a86bf1f0eb56d251af3e24
최준호 <junho5336@gmail.com>
2024-01-10 11:29:24 +0900

    feat: logout 구현 (#154)

--- src/main/java/in/koreatech/koin/domain/user/controller/UserController.java
+++ src/main/java/in/koreatech/koin/domain/user/controller/UserController.java
@@ -1,17 +1,17 @@
 package in.koreatech.koin.domain.user.controller;
 
+import in.koreatech.koin.domain.auth.UserAuth;
+import in.koreatech.koin.domain.user.dto.UserLoginRequest;
+import in.koreatech.koin.domain.user.dto.UserLoginResponse;
+import in.koreatech.koin.domain.user.model.User;
+import in.koreatech.koin.domain.user.service.UserService;
+import jakarta.validation.Valid;
 import java.net.URI;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import in.koreatech.koin.domain.user.dto.UserLoginRequest;
-import in.koreatech.koin.domain.user.dto.UserLoginResponse;
-import in.koreatech.koin.domain.user.service.UserService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,11 @@ public class UserController {
         UserLoginResponse response = userService.login(request);
         return ResponseEntity.created(URI.create("/"))
             .body(response);
+    }
+
+    @PostMapping("/user/logout")
+    public ResponseEntity<Void> logout(@UserAuth User user) {
+        userService.logout(user);
+        return ResponseEntity.ok().build();
     }
 }
