e78149243418f44f4b3581f0a8a825ab1577f7af
songsunkook <songsunkook@gmail.com>
2024-01-11 16:49:58 +0900

    fix: 최신글부터 조회하도록 수정

--- src/main/java/in/koreatech/koin/domain/community/service/CommunityService.java
+++ src/main/java/in/koreatech/koin/domain/community/service/CommunityService.java
@@ -2,6 +2,7 @@ package in.koreatech.koin.domain.community.service;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,12 +23,14 @@ public class CommunityService {
     private final ArticleRepository articleRepository;
     private final BoardRepository boardRepository;
 
+    public static final Sort SORT_ORDER_BY = Sort.by(Sort.Direction.DESC, "id");
+
     public ArticlesResponse getArticles(Long boardId, Long page, Long limit) {
         Criteria criteria = Criteria.of(page, limit);
         Board board = boardRepository.findById(boardId)
             .orElseThrow(() -> ArticleNotFoundException.withDetail(
                 "boardId: " + boardId + ", page: " + criteria.getPage() + ", limit: " + criteria.getLimit()));
-        PageRequest pageRequest = PageRequest.of(criteria.getPage(), criteria.getLimit());
+        PageRequest pageRequest = PageRequest.of(criteria.getPage(), criteria.getLimit(), SORT_ORDER_BY);
         Page<Article> articles = articleRepository.findByBoardId(boardId, pageRequest);
         if (articles.getContent().isEmpty()) {
             throw ArticleNotFoundException.withDetail(
