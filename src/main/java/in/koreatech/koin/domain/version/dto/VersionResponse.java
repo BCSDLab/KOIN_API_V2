dee5de046cd1c24a77c3197f0a3154520cd9a957
Tony <xhdtn8070@naver.com>
2024-01-13 21:59:47 +0900

    feat: response dto 작성

---
+++ src/main/java/in/koreatech/koin/domain/version/dto/VersionResponse.java
@@ -0,0 +1,29 @@
+package in.koreatech.koin.domain.version.dto;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import in.koreatech.koin.domain.version.model.Version;
+
+@JsonNaming(value = SnakeCaseStrategy.class)
+public record VersionResponse(
+    @JsonProperty("id") Long id,
+    @JsonProperty("version") String version,
+    @JsonProperty("type") String type,
+    @JsonProperty("created_at") LocalDateTime createdAt,
+    @JsonProperty("updated_at") LocalDateTime updatedAt) {
+
+    public static VersionResponse from(Version version) {
+        return new VersionResponse(
+            version.getId(),
+            version.getVersion(),
+            version.getType(),
+            version.getCreatedAt(),
+            version.getUpdatedAt()
+        );
+    }
+}
