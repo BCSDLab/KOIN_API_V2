4680ddd91f8bafb6bfdee169d1e72e03afcb63a6
songsunkook <songsunkook@gmail.com>
2024-01-23 17:58:51 +0900

    refactor: 유저 인증 정보 전달 방식 수정
    기존: 컨트롤러단에서 @RequestHeader로 받아 전달
    변경: 서비스단에서 RequestContextHolder로 습득

--- src/main/java/in/koreatech/koin/domain/community/controller/CommunityController.java
+++ src/main/java/in/koreatech/koin/domain/community/controller/CommunityController.java
@@ -3,7 +3,6 @@ package in.koreatech.koin.domain.community.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +15,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CommunityController {
 
-    private static final String AUTHORIZATION = "Authorization";
-
     private final CommunityService communityService;
 
     @GetMapping("/articles/{id}")
-    public ResponseEntity<ArticleResponse> getArticle(@PathVariable Long id,
-        @RequestHeader(name = AUTHORIZATION, required = false) String loginToken) {
-        ArticleResponse foundArticle = communityService.getArticle(id, loginToken);
+    public ResponseEntity<ArticleResponse> getArticle(@PathVariable Long id) {
+        ArticleResponse foundArticle = communityService.getArticle(id);
         return ResponseEntity.ok().body(foundArticle);
     }
 
--- src/main/java/in/koreatech/koin/domain/community/service/CommunityService.java
+++ src/main/java/in/koreatech/koin/domain/community/service/CommunityService.java
@@ -7,6 +7,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import in.koreatech.koin.domain.auth.JwtProvider;
 import in.koreatech.koin.domain.auth.exception.AuthException;
@@ -19,6 +21,7 @@ import in.koreatech.koin.domain.community.model.Criteria;
 import in.koreatech.koin.domain.community.repository.ArticleRepository;
 import in.koreatech.koin.domain.community.repository.BoardRepository;
 import in.koreatech.koin.domain.community.repository.CommentRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -26,24 +29,30 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class CommunityService {
 
+    private static final String AUTHORIZATION = "Authorization";
+    public static final Sort SORT_ORDER_BY = Sort.by(Sort.Direction.DESC, "id");
+
     private final JwtProvider jwtProvider;
     private final ArticleRepository articleRepository;
     private final BoardRepository boardRepository;
     private final CommentRepository commentRepository;
 
-    public static final Sort SORT_ORDER_BY = Sort.by(Sort.Direction.DESC, "id");
 
-    public ArticleResponse getArticle(Long articleId, String loginToken) {
+    public ArticleResponse getArticle(Long articleId) {
         Article article = articleRepository.getById(articleId);
         Board board = boardRepository.getById(article.getBoardId());
+        // board.increaseHit();
         List<Comment> comments = commentRepository.findAllByArticleId(articleId);
-        comments.forEach(comment -> comment.updateAuthority(getUserId(loginToken)));
+        Long userId = getUserId();
+        comments.forEach(comment -> comment.updateAuthority(userId));
         return ArticleResponse.of(article, board, comments);
     }
 
-    private Long getUserId(String loginToken) {
+    private Long getUserId() {
         try {
-            return jwtProvider.getUserId(loginToken);
+            HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+            return jwtProvider.getUserId(request.getHeader(AUTHORIZATION));
         } catch (AuthException e) {
             return null;
         }
