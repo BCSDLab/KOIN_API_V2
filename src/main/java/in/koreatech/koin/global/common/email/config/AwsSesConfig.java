0a4f2930d89f353da23bc8b9d66879c263e0ce4f
daheeParkk <qkrekgml7414@naver.com>
2024-01-30 16:34:08 +0900

    feat : AwsSesConfig 작성

---
+++ src/main/java/in/koreatech/koin/global/common/email/config/AwsSesConfig.java
@@ -0,0 +1,31 @@
+package in.koreatech.koin.global.common.email.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsync;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsyncClient;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${AWS_ACCESS_KEY_ID}")
+    private String AWS_ACCESS_KEY_ID;
+
+    @Value("${AWS_SECRET_KEY}")
+    private String AWS_SECRET_KEY;
+
+    @Bean
+    public AmazonSimpleEmailServiceAsync amazonSimpleEmailServiceAsync() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY_ID, AWS_SECRET_KEY);
+
+        return AmazonSimpleEmailServiceAsyncClient.asyncBuilder()
+            .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+            .withRegion(Regions.US_WEST_2)
+            .build();
+    }
+}
