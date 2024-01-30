5abaf1e4c05605abf663d9b43098338505f9bb1c
송선권 <songsunkook@gmail.com>
2024-01-30 19:28:35 +0900

    feat: 게시글 단일 조회 (#173)
    * feat: 골격 작성
    
    * feat: Repository 메서드 추가
    
    * feat: 응답 객체 작성
    
    * feat: Comment 도메인 엔티티 작성
    
    * feat: 초안 완성
    
    * fix: 로그인된 사용자 정보 습득 절차 수정
    
    1. 비로그인 유저의 요청도 받도록 수정
    2. 올바르지 않은 토큰 입력 시 권한 없음으로 진행되도록 수정
    
    * rename: 응답 객체 변수명 수정
    
    * refactor: 내용 요약 정보 최신화 시기 변경
    
    기존: getter 호출 시마다 로직 수행하여 추출
    변경: jpa 엔티티 로드 후 1회 로직 수행
    
    * refactor: 유저 인증 정보 전달 방식 수정
    
    기존: 컨트롤러단에서 @RequestHeader로 받아 전달
    변경: 서비스단에서 RequestContextHolder로 습득
    
    * feat: 게시글 조회 기록 저장 및 조회수 변동 구현
    
    유저의 게시글 조회가 조회수 증가로 이어지는 간격을 1시간으로 설정
    
    * fix: 게시글 조회 관련 버그 수정
    
    기존에 게시글 조회 이력이 남아있는데 만료 시간이 지난 경우, 기존 내역을 업데이트해줘야 하나 새로 저장을 시도하여 구문 에러가 발생함.
    위 상황에는 기존 내역을 업데이트하는 로직을 추가함으로써 문제 해결
    
    * fix: 게시글 목록 조회 버그 수정
    
    문제: 최대 페이지 초과 시 404 에러가 발생하지 않음.
    원인: 예외처리 로직을 별도 함수로 분리했으나 기존 함수를 사용중이었음
    해결: 예외처리가 적용된 함수로 호출문 변경
    
    * fix: 조회 기록 Builder에 만료시간 기입
    
    * test: 신규 테스트 추가 및 기존 테스트 수정
    
    * other: 학과 조회 자료형 수정
    
    학과 코드: Long -> String
    
    * refactor: 코드스타일 개선
    
    * rename: 메서드 네이밍 수정
    
    * refactor: 빈 게시글 목록 조회 예외처리 제거
    
    * refactor: IP 주소 습득 로직 분리
    
    * test: 예외 제거에 따른 테스트 수정
    
    * refactor: 연관관계 방식으로 로직 및 테스트 수정
    
    * refactor: ArticleViewLog의 FK를 연관관계로 매핑

---
+++ src/main/java/in/koreatech/koin/domain/community/dto/ArticleResponse.java
@@ -0,0 +1,116 @@
+package in.koreatech.koin.domain.community.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import in.koreatech.koin.domain.community.model.Article;
+import in.koreatech.koin.domain.community.model.Board;
+import in.koreatech.koin.domain.community.model.Comment;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record ArticleResponse(
+    Long id,
+    Long boardId,
+    String title,
+    String content,
+    String nickname,
+    Boolean isSolved,
+    Boolean isNotice,
+    @JsonProperty("contentSummary") String contentSummary,
+    Long hit,
+    Byte commentCount,
+    InnerBoardResponse board,
+    List<InnerCommentResponse> comments,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt
+) {
+
+    public static ArticleResponse of(Article article) {
+        return new ArticleResponse(
+            article.getId(),
+            article.getBoard().getId(),
+            article.getTitle(),
+            article.getContent(),
+            article.getNickname(),
+            article.getIsSolved(),
+            article.getIsNotice(),
+            article.getContentSummary(),
+            article.getHit(),
+            article.getCommentCount(),
+            InnerBoardResponse.from(article.getBoard()),
+            article.getComment().stream().map(InnerCommentResponse::from).toList(),
+            article.getCreatedAt(),
+            article.getUpdatedAt()
+        );
+    }
+
+    @JsonNaming(value = SnakeCaseStrategy.class)
+    private record InnerBoardResponse(
+        Long id,
+        String tag,
+        String name,
+        Boolean isAnonymous,
+        Long articleCount,
+        Boolean isDeleted,
+        Boolean isNotice,
+        Long parentId,
+        Long seq,
+        List<InnerBoardResponse> children,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt
+    ) {
+
+        public static InnerBoardResponse from(Board board) {
+            return new InnerBoardResponse(
+                board.getId(),
+                board.getTag(),
+                board.getName(),
+                board.getIsAnonymous(),
+                board.getArticleCount(),
+                board.getIsDeleted(),
+                board.getIsNotice(),
+                board.getParentId(),
+                board.getSeq(),
+                board.getChildren().isEmpty()
+                    ? null : board.getChildren().stream().map(InnerBoardResponse::from).toList(),
+                board.getCreatedAt(),
+                board.getUpdatedAt()
+            );
+        }
+    }
+
+    @JsonNaming(value = SnakeCaseStrategy.class)
+    private record InnerCommentResponse(
+        Long id,
+        Long articleId,
+        String content,
+        Long userId,
+        String nickname,
+        Boolean isDeleted,
+        @JsonProperty("grantEdit") Boolean grantEdit,
+        @JsonProperty("grantDelete") Boolean grantDelete,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt
+    ) {
+
+        public static InnerCommentResponse from(Comment comment) {
+            return new InnerCommentResponse(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getContent(),
+                comment.getUserId(),
+                comment.getNickname(),
+                comment.getIsDeleted(),
+                comment.getGrantEdit(),
+                comment.getGrantDelete(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt()
+            );
+        }
+    }
+}
---
+++ src/main/java/in/koreatech/koin/domain/community/model/Comment.java
@@ -0,0 +1,80 @@
+package in.koreatech.koin.domain.community.model;
+
+import org.hibernate.annotations.Where;
+
+import in.koreatech.koin.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "comments")
+@Where(clause = "is_deleted=0")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+
+    @NotNull
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @NotNull
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "nickname", nullable = false, length = 50)
+    private String nickname;
+
+    @NotNull
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+
+    @Transient
+    private Boolean grantEdit = false;
+
+    @Transient
+    private Boolean grantDelete = false;
+
+    public void updateAuthority(Long userId) {
+        if (this.userId.equals(userId)) {
+            this.grantEdit = true;
+            this.grantDelete = true;
+        }
+    }
+
+    @Builder
+    public Comment(Article article, String content, Long userId,
+        String nickname, Boolean isDeleted) {
+        this.article = article;
+        this.content = content;
+        this.userId = userId;
+        this.nickname = nickname;
+        this.isDeleted = isDeleted;
+    }
+}
