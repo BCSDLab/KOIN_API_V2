497bff1fcc391efe6dd6ab03deb3004028ce114b
songsunkook <songsunkook@gmail.com>
2024-02-09 22:52:11 +0900

    feat: 도메인 엔티티 생성

---
+++ src/main/java/in/koreatech/koin/domain/shop/model/Shop.java
@@ -0,0 +1,111 @@
+package in.koreatech.koin.domain.shop.model;
+
+import org.hibernate.annotations.Where;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "shops")
+@Where(clause = "is_deleted=0")
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "owner_id")
+    private Long ownerId;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "internal_name", nullable = false, length = 50)
+    private String internalName;
+
+    @Size(max = 3)
+    @Column(name = "chosung", length = 3)
+    private String chosung;
+
+    @Size(max = 50)
+    @Column(name = "phone", length = 50)
+    private String phone;
+
+    @Lob
+    @Column(name = "address")
+    private String address;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "delivery", nullable = false)
+    private Boolean delivery = false;
+
+    @NotNull
+    @Column(name = "delivery_price", nullable = false)
+    private Long deliveryPrice;
+
+    @NotNull
+    @Column(name = "pay_card", nullable = false)
+    private Boolean payCard = false;
+
+    @NotNull
+    @Column(name = "pay_bank", nullable = false)
+    private Boolean payBank = false;
+
+    @NotNull
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+
+    @NotNull
+    @Column(name = "is_event", nullable = false)
+    private Boolean isEvent = false;
+
+    @Lob
+    @Column(name = "remarks")
+    private String remarks;
+
+    @NotNull
+    @Column(name = "hit", nullable = false)
+    private Long hit;
+
+    @Builder
+    public Shop(Long ownerId, String name, String internalName, String chosung, String phone, String address,
+        String description, Boolean delivery, Long deliveryPrice, Boolean payCard, Boolean payBank,
+        Boolean isDeleted, Boolean isEvent, String remarks, Long hit) {
+        this.ownerId = ownerId;
+        this.name = name;
+        this.internalName = internalName;
+        this.chosung = chosung;
+        this.phone = phone;
+        this.address = address;
+        this.description = description;
+        this.delivery = delivery;
+        this.deliveryPrice = deliveryPrice;
+        this.payCard = payCard;
+        this.payBank = payBank;
+        this.isDeleted = isDeleted;
+        this.isEvent = isEvent;
+        this.remarks = remarks;
+        this.hit = hit;
+    }
+}
