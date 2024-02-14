46d07594c0562ca09642e9214b78d1e29b4701d6
songsunkook <songsunkook@gmail.com>
2024-02-14 20:26:45 +0900

    refactor: 읽기 전용 트랜잭션으로 수정

--- src/main/java/in/koreatech/koin/domain/ownershop/service/OwnerShopService.java
+++ src/main/java/in/koreatech/koin/domain/ownershop/service/OwnerShopService.java
@@ -3,6 +3,7 @@ package in.koreatech.koin.domain.ownershop.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import in.koreatech.koin.domain.ownershop.dto.OwnerShopsResponse;
 import in.koreatech.koin.domain.shop.model.Shop;
@@ -11,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class OwnerShopService {
 
     private final ShopRepository shopRepository;
