ab0fa003c0c7381541c84d9c59cc24a3e676f88e
songsunkook <songsunkook@gmail.com>
2024-02-15 19:49:57 +0900

    feat: 예외 추가

--- src/main/java/in/koreatech/koin/domain/bus/repository/BusRepository.java
+++ src/main/java/in/koreatech/koin/domain/bus/repository/BusRepository.java
@@ -1,12 +1,21 @@
 package in.koreatech.koin.domain.bus.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import in.koreatech.koin.domain.bus.exception.BusNotFoundException;
 import in.koreatech.koin.domain.bus.model.Bus;
 
 public interface BusRepository extends MongoRepository<Bus, String> {
 
     List<Bus> findAll();
+
+    Optional<Bus> findByBusTypeAndDirection(String busType, String direction);
+
+    default Bus getBusByBusTypeAndDirection(String busType, String direction) {
+        return findByBusTypeAndDirection(busType, direction)
+            .orElseThrow(() -> BusNotFoundException.withDetail("busType: " + busType + ", direction: " + direction));
+    }
 }
