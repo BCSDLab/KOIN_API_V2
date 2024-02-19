6f5581fa1a2500600b994a9261a40413685b635f
송선권 <songsunkook@gmail.com>
2024-02-19 15:51:21 +0900

    feat: 사장님 자신의 모든 상점 조회 (#183)
    * feat: 도메인 엔티티 생성
    
    * feat: 응답 객체 초안 작성
    
    * feat: 도메인 클래스 작성
    
    * feat: API 작성
    
    1. 경로 수정(다른 API로 착각하고 작업중이었어서 수정)
    2. 연관관계 매핑
    3. API 로직 작성
    
    * test: 테스트 작성
    
    * feat: API 작성
    
    * test: 테스트 작성
    
    * refactor: 트랜잭션 적용
    
    * docs: 응답 객체 문서 주석 추가
    
    * fix: 버그 수정
    
    * refactor: 읽기 전용 트랜잭션으로 수정
    
    * refactor: 개행 컨벤션 준수

--- src/main/java/in/koreatech/koin/domain/shop/repository/ShopRepository.java
+++ src/main/java/in/koreatech/koin/domain/shop/repository/ShopRepository.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import org.springframework.data.repository.Repository;
 
-import in.koreatech.koin.domain.owner.domain.Owner;
 import in.koreatech.koin.domain.shop.model.Shop;
 
 public interface ShopRepository extends Repository<Shop, Long> {
 
     Shop save(Shop shop);
 
-    List<Shop> findAllByOwner(Owner owner);
+    List<Shop> findAllByOwnerId(Long ownerId);
 }
