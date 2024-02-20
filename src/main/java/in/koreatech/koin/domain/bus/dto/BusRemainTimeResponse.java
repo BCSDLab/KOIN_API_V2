023840c236e3349345bbae1dac6be3bd8089637c
songsunkook <songsunkook@gmail.com>
2024-02-20 16:57:01 +0900

    feat: 가장 최근 버스까지 남은 시간 응답 구현

--- src/main/java/in/koreatech/koin/domain/bus/dto/BusRemainTimeResponse.java
+++ src/main/java/in/koreatech/koin/domain/bus/dto/BusRemainTimeResponse.java
@@ -2,28 +2,38 @@ package in.koreatech.koin.domain.bus.dto;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
+import java.util.List;
+
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-import in.koreatech.koin.domain.bus.model.BusCourse;
+import in.koreatech.koin.domain.bus.model.BusRemainTime;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public record BusRemainTimeResponse(
     String busType,
-    InnerBusResponse nextBus,
-    InnerBusResponse now_bus
+    InnerBusResponse nowBus,
+    InnerBusResponse nextBus
 ) {
-    public static BusRemainTimeResponse from(BusCourse busCourse) {
-        return null;
+
+    public static BusRemainTimeResponse of(String busType, List<BusRemainTime> remainTimes) {
+        return new BusRemainTimeResponse(
+            busType,
+            InnerBusResponse.from(remainTimes, 0),
+            InnerBusResponse.from(remainTimes, 1)
+        );
     }
 
     @JsonNaming(SnakeCaseStrategy.class)
     private record InnerBusResponse(
-        Long busNumber,
         Long remainTime
     ) {
 
-        public static InnerBusResponse from(BusCourse busCourse) {
-            return null;
+        public static InnerBusResponse from(List<BusRemainTime> remainTimes, int index) {
+            Long result = null;
+            if (index < remainTimes.size()) {
+                result = remainTimes.get(index).getRemainSeconds();
+            }
+            return new InnerBusResponse(result);
         }
     }
 }
