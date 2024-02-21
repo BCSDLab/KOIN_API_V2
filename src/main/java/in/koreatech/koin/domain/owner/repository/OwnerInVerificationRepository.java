b73b5385f4bc732950ff5c89b530a563928e09d4
daheeParkk <qkrekgml7414@naver.com>
2024-02-21 13:51:06 +0900

    feat: 레디스에 있는 사장님 정보 조회 구현

--- src/main/java/in/koreatech/koin/domain/owner/repository/OwnerInVerificationRepository.java
+++ src/main/java/in/koreatech/koin/domain/owner/repository/OwnerInVerificationRepository.java
@@ -1,10 +1,19 @@
 package in.koreatech.koin.domain.owner.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.Repository;
 
 import in.koreatech.koin.domain.owner.model.OwnerInVerification;
+import in.koreatech.koin.global.common.email.exception.EmailNotFoundException;
 
 public interface OwnerInVerificationRepository extends Repository<OwnerInVerification, String> {
 
     OwnerInVerification save(OwnerInVerification ownerInVerification);
+
+    Optional<OwnerInVerification> findByEmail(String email);
+
+    default OwnerInVerification getByEmail(String email) {
+        return findByEmail(email).orElseThrow(() -> EmailNotFoundException.withDetail("email: " + email));
+    }
 }
