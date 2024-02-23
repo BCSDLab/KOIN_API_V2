90a8714622df7a24d122cbb1ecd538adcc3124e2
daheeParkk <qkrekgml7414@naver.com>
2024-02-23 21:32:40 +0900

    refactor: Aws SES 서울 리전으로 수정

--- src/main/java/in/koreatech/koin/global/domain/email/config/AwsSesConfig.java
+++ src/main/java/in/koreatech/koin/global/domain/email/config/AwsSesConfig.java
@@ -25,7 +25,7 @@ public class AwsSesConfig {
 
         return AmazonSimpleEmailServiceAsyncClient.asyncBuilder()
             .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-            .withRegion(Regions.US_WEST_2)
+            .withRegion(Regions.AP_NORTHEAST_2)
             .build();
     }
 }
