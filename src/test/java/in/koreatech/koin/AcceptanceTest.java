5f6bb596be0e3b36f9e460c768c229f8b59a160d
songsunkook <songsunkook@gmail.com>
2024-02-23 19:08:58 +0900

    test: 테스트컨테이너 mongoDB 추가

--- src/test/java/in/koreatech/koin/AcceptanceTest.java
+++ src/test/java/in/koreatech/koin/AcceptanceTest.java
@@ -2,8 +2,6 @@ package in.koreatech.koin;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import in.koreatech.koin.support.DBInitializer;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +14,9 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
+
+import in.koreatech.koin.support.DBInitializer;
+import io.restassured.RestAssured;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @Import(DBInitializer.class)
@@ -37,6 +38,9 @@ public abstract class AcceptanceTest {
     @Container
     protected static GenericContainer<?> redisContainer;
 
+    @Container
+    protected static GenericContainer<?> mongoContainer;
+
     @DynamicPropertySource
     private static void configureProperties(final DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", mySqlContainer::getJdbcUrl);
@@ -44,6 +48,9 @@ public abstract class AcceptanceTest {
         registry.add("spring.datasource.password", () -> ROOT_PASSWORD);
         registry.add("spring.data.redis.host", redisContainer::getHost);
         registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379).toString());
+        registry.add("spring.data.mongodb.host", mongoContainer::getHost);
+        registry.add("spring.data.mongodb.port", () -> mongoContainer.getMappedPort(27017).toString());
+        registry.add("spring.data.mongodb.database", () -> "test");
     }
 
     static {
@@ -57,8 +64,13 @@ public abstract class AcceptanceTest {
             DockerImageName.parse("redis:4.0.10"))
             .withExposedPorts(6379);
 
+        mongoContainer = new GenericContainer<>(
+            DockerImageName.parse("mongo:7.0"))
+            .withExposedPorts(27017);
+
         mySqlContainer.start();
         redisContainer.start();
+        mongoContainer.start();
     }
 
     @BeforeEach
