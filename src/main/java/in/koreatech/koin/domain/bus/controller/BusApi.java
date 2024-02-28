ea771da467fe8770fc01daec36b9f1eb4173f8a5
songsunkook <songsunkook@gmail.com>
2024-02-28 10:54:53 +0900

    docs: API 문서에 파라미터 정보 명시

--- src/main/java/in/koreatech/koin/domain/bus/controller/BusApi.java
+++ src/main/java/in/koreatech/koin/domain/bus/controller/BusApi.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import in.koreatech.koin.domain.bus.dto.BusRemainTimeResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -24,8 +25,8 @@ public interface BusApi {
     @Operation(summary = "이번 / 다음 버스 남은 시간 조회")
     @GetMapping("/bus")
     ResponseEntity<BusRemainTimeResponse> getBusRemainTime(
-        @RequestParam(value = "bus_type") String busType,
-        @RequestParam String depart,
-        @RequestParam String arrival
+        @Parameter(description = "버스 종류(city, express, shuttle, commuting)") @RequestParam(value = "bus_type") String busType,
+        @Parameter(description = "koreatech, station, terminal") @RequestParam String depart,
+        @Parameter(description = "koreatech, station, terminal") @RequestParam String arrival
     );
 }
