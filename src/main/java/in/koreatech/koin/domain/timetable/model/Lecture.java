570c24c9c159ffe70150a6af96bfdbdf26c3b10e
dradnats1012 <herjebi1012@gmail.com>
2024-03-11 23:49:20 +0900

    feat : Lecture 클래스 생성

---
+++ src/main/java/in/koreatech/koin/domain/timetable/model/Lecture.java
@@ -0,0 +1,111 @@
+package in.koreatech.koin.domain.timetable.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "lectures")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Lecture {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "semester_date", nullable = false)
+    private String semester;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "code", nullable = false)
+    private String code;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "grades", nullable = false)
+    private String grades;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "class", nullable = false)
+    private String lectureClass;
+
+    @Size(max = 255)
+    @Column(name = "regular_number")
+    private String regularNumber;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "department", nullable = false)
+    private String department;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "target", nullable = false)
+    private String target;
+
+    @Size(max = 255)
+    @Column(name = "professor")
+    private String professor;
+
+    @Size(max = 255)
+    @Column(name = "is_english")
+    private String isEnglish;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "design_score", nullable = false)
+    private String designScore;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "is_elearning", nullable = false)
+    private String isElearning;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "class_time", nullable = false)
+    private String classTime;
+
+    @Builder
+    private Lecture(String code, String semester,
+        String name,
+        String grades, String lectureClass,
+        String regularNumber, String department,
+        String target, String professor,
+        String isEnglish, String designScore,
+        String isElearning, String classTime) {
+
+        this.code = code;
+        this.semester = semester;
+        this.name = name;
+        this.grades = grades;
+        this.lectureClass = lectureClass;
+        this.regularNumber = regularNumber;
+        this.department = department;
+        this.target = target;
+        this.professor = professor;
+        this.isEnglish = isEnglish;
+        this.designScore = designScore;
+        this.isElearning = isElearning;
+        this.classTime = classTime;
+    }
+}
