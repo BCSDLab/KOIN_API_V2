94d9b6e87ece10c6798a35fea39a17fd4e9837b9
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-03-13 02:57:48 +0900

    refactor: 전역 데이터 중복 예외 제거(이미 전에 누가 만들었음)

--- src/main/java/in/koreatech/koin/global/exception/AlreadyExistDataException.java
+++
@@ -1,7 +0,0 @@
-package in.koreatech.koin.global.exception;
-
-public class AlreadyExistDataException extends RuntimeException{
-    public AlreadyExistDataException(String message) {
-        super(message);
-    }
-}
