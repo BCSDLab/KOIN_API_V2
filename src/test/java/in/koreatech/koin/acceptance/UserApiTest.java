ce7ef0437e3e8a7c9b6cab1cdf7f40693edc2053
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-03-13 02:38:42 +0900

    feat: 닉네임 중복 중복아닐때, 제약조건위반시 테스트 추가

--- src/test/java/in/koreatech/koin/acceptance/UserApiTest.java
+++ src/test/java/in/koreatech/koin/acceptance/UserApiTest.java
@@ -260,8 +260,8 @@ class UserApiTest extends AcceptanceTest {
     }
 
     @Test
-    @DisplayName("닉네임 중복 체크")
-    void checkDuplicationOfNickname(){
+    @DisplayName("닉네임 중복이 아닐시 상태코드 200을 반환한다.")
+    void checkDuplicationOfNickname() {
         User user = User.builder()
             .password("1234")
             .nickname("주노")
@@ -279,15 +279,43 @@ class UserApiTest extends AcceptanceTest {
         ExtractableResponse<Response> response = RestAssured
             .given()
             .when()
-            .get("/user/check/nickname?nickname=주노")
+            .get("/user/check/nickname?nickname=" + "철수")
             .then()
-            .statusCode(HttpStatus.CONFLICT.value())
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+    }
+
+    @Test
+    @DisplayName("닉네임 제약조건 위반시 상태코드 422를 반환한다.")
+    void checkDuplicationOfNicknameBadRequest() {
+        User user = User.builder()
+            .password("1234")
+            .nickname("주노")
+            .name("최준호")
+            .phoneNumber("010-1234-5678")
+            .userType(STUDENT)
+            .gender(UserGender.MAN)
+            .email("test@koreatech.ac.kr")
+            .isAuthed(true)
+            .isDeleted(false)
+            .build();
+
+        userRepository.save(user);
+
+        RestAssured
+            .given()
+            .when()
+            .get("/user/check/nickname?nickname=" + "철".repeat(11))
+            .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
             .extract();
 
-        assertSoftly(
-            softly -> {
-                softly.assertThat(response.body().jsonPath().getString("message")).isEqualTo("이미 존재하는 데이터입니다.");
-            }
-        );
+        RestAssured
+            .given()
+            .when()
+            .get("/user/check/nickname?nickname=")
+            .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .extract();
     }
 }
