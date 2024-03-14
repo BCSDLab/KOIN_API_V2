ce3848964590a0d9734552412dd8d66761377037
duehee <149302959+duehee@users.noreply.github.com>
2024-03-14 11:10:50 +0900

    feat : 학기 조회(전학기) (#196)
    * feat : Semester get API 구현 완료
    
    * chore : 리뷰 반영 코드 위치 수정 / 패키지 이름 변경
    
    * style : 라인 포매팅 및 클래스 삭제
    
    * test : 테스트 코드 수정

---
+++ src/main/java/in/koreatech/koin/domain/timetable/service/SemesterService.java
@@ -0,0 +1,24 @@
+package in.koreatech.koin.domain.timetable.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import in.koreatech.koin.domain.timetable.dto.SemesterResponse;
+import in.koreatech.koin.domain.timetable.repository.SemesterRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SemesterService {
+
+    private final SemesterRepository semesterRepository;
+
+    public List<SemesterResponse> getSemesters() {
+        return semesterRepository.findAll().stream()
+            .map(SemesterResponse::from)
+            .toList();
+    }
+}
