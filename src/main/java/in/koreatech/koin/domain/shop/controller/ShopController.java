357d7403214e2e1304fc10a419689d7919c80db6
HyeonsuLee <leehyeonsu4888@naver.com>
2024-03-14 21:05:05 +0900

    feat: 스웨거 설명 추가

--- src/main/java/in/koreatech/koin/domain/shop/controller/ShopController.java
+++ src/main/java/in/koreatech/koin/domain/shop/controller/ShopController.java
@@ -1,7 +1,5 @@
 package in.koreatech.koin.domain.shop.controller;
 
-import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import in.koreatech.koin.domain.shop.dto.MenuCategoriesResponse;
 import in.koreatech.koin.domain.shop.dto.ShopMenuResponse;
 import in.koreatech.koin.domain.shop.dto.ShopResponse;
 import in.koreatech.koin.domain.shop.service.ShopService;
-import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -20,7 +17,6 @@ public class ShopController implements ShopApi {
 
     private final ShopService shopService;
 
-    @Override
     @GetMapping("/shops/{shopId}/menus/{menuId}")
     public ResponseEntity<ShopMenuResponse> findMenu(
         @PathVariable Long shopId,
@@ -30,7 +26,6 @@ public class ShopController implements ShopApi {
         return ResponseEntity.ok(shopMenu);
     }
 
-    @Override
     @GetMapping("/shops/{shopId}/menus/categories")
     public ResponseEntity<MenuCategoriesResponse> getMenuCategories(
         @PathVariable Long shopId
@@ -39,9 +34,10 @@ public class ShopController implements ShopApi {
         return ResponseEntity.ok(menuCategories);
     }
 
-    @Override
     @GetMapping("/shops/{id}")
-    public ResponseEntity<ShopResponse> getShopById(@PathVariable Long id) {
+    public ResponseEntity<ShopResponse> getShopById(
+        @PathVariable Long id
+    ) {
         ShopResponse shopResponse = shopService.getShop(id);
         return ResponseEntity.ok(shopResponse);
     }
