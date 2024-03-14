17b18eec26f73a888aa8500e7abc34c1be6f1df0
Hyeonsu Lee <127578418+20hyeonsulee@users.noreply.github.com>
2024-03-14 21:27:40 +0900

    feat : 사장님 식당 생성, 특정 상점 조회 API구현 (#199)
    * feat: 상점 생성 기능 추가
    
    * feat: 상점등록을 하기 위해 연관된 model정의
    
    * feat: shop과 관련된 repository, exception작성
    
    * feat: 1차 테스트 완료
    
    * feat: 사장님 상점 생성 기능 구현
    
    * feat: 특정상점조회 API구현
    
    * refactor: 특정상점조회 테스트코드 수정
    
    * chore: import경로 오류 수정
    
    * chore: pull충돌 해결
    
    * refactor: saveAll제거
    
    * feat: 스웨거 설명 추가
    
    * refactor: Builder생성자 접근제어 변경
    
    * refactor: ShopResponse 적팩메이름 of로 변경
    
    * refactor: ShopResponse 적팩메이름 of로 변경

---
+++ src/main/java/in/koreatech/koin/domain/shop/repository/ShopCategoryRepository.java
@@ -0,0 +1,23 @@
+package in.koreatech.koin.domain.shop.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
+
+import in.koreatech.koin.domain.shop.exception.ShopCategoryNotFoundException;
+import in.koreatech.koin.domain.shop.model.ShopCategory;
+
+public interface ShopCategoryRepository extends Repository<ShopCategory, Long> {
+
+    Optional<ShopCategory> findById(Long shopCategoryId);
+
+    ShopCategory save(ShopCategory shopCategory);
+
+    List<ShopCategory> findAllByIdIn(List<Long> ids);
+
+    default ShopCategory getById(Long shopCategoryId) {
+        return findById(shopCategoryId)
+            .orElseThrow(() -> ShopCategoryNotFoundException.withDetail("shopCategoryId: " + shopCategoryId));
+    }
+}
---
+++ src/main/java/in/koreatech/koin/domain/shop/repository/ShopOpenRepository.java
@@ -0,0 +1,14 @@
+package in.koreatech.koin.domain.shop.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.Repository;
+
+import in.koreatech.koin.domain.shop.model.ShopOpen;
+
+public interface ShopOpenRepository extends Repository<ShopOpen, Long> {
+
+    ShopOpen save(ShopOpen shopOpen);
+
+    List<ShopOpen> findAllByShopId(Long shopId);
+}
