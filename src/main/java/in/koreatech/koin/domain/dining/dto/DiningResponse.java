e2c03a896f95bcaf7c1a79585cccf34b0c55557c
황현식 <142300831+Choon0414@users.noreply.github.com>
2024-03-19 17:18:10 +0900

    feat : 식단 Api 구현 (#193)
    * feat : 충돌 해결
    
    * chore : VersionException 제거
    
    * chore : SnakeCaseStrategy import static 수정
    
    * refactor : 엔티티 id int -> Long 수정
    
    * feat : 사장님 식당 생성, 특정 상점 조회 API구현 (#199)
    
    * feat: 상점 생성 기능 추가
    
    * feat: 상점등록을 하기 위해 연관된 model정의
    
    * feat: shop과 관련된 repository, exception작성
    
    * feat: 1차 테스트 완료
    
    * feat: 사장님 상점 생성 기능 구현
    
    * feat: 특정상점조회 API구현
    
    * refactor: 특정상점조회 테스트코드 수정
    
    * chore: import경로 오류 수정
    
    * chore: pull충돌 해결
    
    * refactor: saveAll제거
    
    * feat: 스웨거 설명 추가
    
    * refactor: Builder생성자 접근제어 변경
    
    * refactor: ShopResponse 적팩메이름 of로 변경
    
    * refactor: ShopResponse 적팩메이름 of로 변경
    
    * feat : 커스텀 예외처리 추가
    
    * chore : 충돌 해결
    
    * chore : 테스트 정보 수정
    
    * chore : 파라미터 설명 추가
    
    * refactor : menu String -> List 수정
    
    * fix : 테이블명 수정
    
    * refactor : 현재시간에 대한 테스트를 위해 clock 모킹 추가
    
    * remove : 사용하지 않는 예외 삭제
    
    * chore : 테스트코드 public 제외, 불필요한 공백 삭제
    
    ---------
    
    Co-authored-by: Hyeonsu Lee <127578418+20HyeonsuLee@users.noreply.github.com>

---
+++ src/main/java/in/koreatech/koin/domain/dining/dto/DiningResponse.java
@@ -0,0 +1,71 @@
+package in.koreatech.koin.domain.dining.dto;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import in.koreatech.koin.domain.dining.model.Dining;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record DiningResponse(
+
+    @Schema(description = "메뉴 고유 ID", example = "1")
+    Long id,
+
+    @Schema(description = "식단 제공 날짜", example = "2024-03-11")
+    String date,
+
+    @Schema(description = "식사 시간", example = "LUNCH")
+    String type,
+
+    @Schema(description = "식단 제공 장소", example = "A코스")
+    String place,
+
+    @Schema(description = "카드 가격", example = "5000")
+    Integer priceCard,
+
+    @Schema(description = "현금 가격", example = "5000")
+    Integer priceCash,
+
+    @Schema(description = "칼로리", example = "790")
+    Integer kcal,
+
+    @Schema(description = "식단", example = """
+        ["병아리콩밥", "(탕)소고기육개장", "땡초부추전", "고구마순들깨볶음", "총각김치", "생야채샐러드&D", "누룽지탕"]""")
+    List<String> menu,
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Schema(description = "생성 일자", example = "2024-03-15 14:02:48") LocalDateTime createdAt,
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Schema(description = "최신화 일자", example = "2024-03-15 14:02:48") LocalDateTime updatedAt
+) {
+    public static DiningResponse from(Dining dining) {
+        return new DiningResponse(
+            dining.getId(),
+            dining.getDate(),
+            dining.getType(),
+            dining.getPlace(),
+            dining.getPriceCard(),
+            dining.getPriceCash(),
+            dining.getKcal(),
+            toListMenus(dining.getMenu()),
+            dining.getCreatedAt(),
+            dining.getUpdatedAt()
+        );
+    }
+
+    public static List<String> toListMenus(String menu) {
+        menu = menu.substring(1, menu.length() - 1);
+        return Stream.of(menu.split(","))
+            .map(str -> str.trim().replaceAll("\"", ""))
+            .collect(Collectors.toList());
+    }
+}
