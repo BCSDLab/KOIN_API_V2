3d5ca71a3c8670e10f920c4f81c17e0a6ad81a18
황현식 <142300831+Choon0414@users.noreply.github.com>
2024-03-19 17:18:10 +0900

    feat : 식단 Api 구현 (#193)
    * feat : 충돌 해결
    
    * chore : VersionException 제거
    
    * chore : SnakeCaseStrategy import static 수정
    
    * refactor : 엔티티 id int -> Long 수정
    
    * feat : 사장님 식당 생성, 특정 상점 조회 API구현 (#199)
    
    * feat: 상점 생성 기능 추가
    
    * feat: 상점등록을 하기 위해 연관된 model정의
    
    * feat: shop과 관련된 repository, exception작성
    
    * feat: 1차 테스트 완료
    
    * feat: 사장님 상점 생성 기능 구현
    
    * feat: 특정상점조회 API구현
    
    * refactor: 특정상점조회 테스트코드 수정
    
    * chore: import경로 오류 수정
    
    * chore: pull충돌 해결
    
    * refactor: saveAll제거
    
    * feat: 스웨거 설명 추가
    
    * refactor: Builder생성자 접근제어 변경
    
    * refactor: ShopResponse 적팩메이름 of로 변경
    
    * refactor: ShopResponse 적팩메이름 of로 변경
    
    * feat : 커스텀 예외처리 추가
    
    * chore : 충돌 해결
    
    * chore : 테스트 정보 수정
    
    * chore : 파라미터 설명 추가
    
    * refactor : menu String -> List 수정
    
    * fix : 테이블명 수정
    
    * refactor : 현재시간에 대한 테스트를 위해 clock 모킹 추가
    
    * remove : 사용하지 않는 예외 삭제
    
    * chore : 테스트코드 public 제외, 불필요한 공백 삭제
    
    ---------
    
    Co-authored-by: Hyeonsu Lee <127578418+20HyeonsuLee@users.noreply.github.com>

---
+++ src/test/java/in/koreatech/koin/acceptance/DiningApiTest.java
@@ -0,0 +1,212 @@
+package in.koreatech.koin.acceptance;
+
+import static io.restassured.RestAssured.given;
+import static java.time.format.DateTimeFormatter.ofPattern;
+import static org.mockito.Mockito.when;
+
+import java.time.Clock;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import in.koreatech.koin.AcceptanceTest;
+import in.koreatech.koin.domain.dining.model.Dining;
+import in.koreatech.koin.domain.dining.repository.DiningRepository;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+
+class DiningApiTest extends AcceptanceTest {
+
+    @Autowired
+    private DiningRepository diningRepository;
+
+    @Test
+    @DisplayName("특정 날짜의 모든 식단들을 조회한다.")
+    void findDinings() {
+        Dining request1 = Dining.builder()
+            .id(1L)
+            .date("2024-03-11")
+            .type("LUNCH")
+            .place("A코스")
+            .priceCard(6000)
+            .priceCash(6000)
+            .kcal(881)
+            .menu("""
+                ["병아리콩밥", "(탕)소고기육개장", "땡초부추전", "누룽지탕"]""")
+            .build();
+
+        Dining request2 = Dining.builder()
+            .id(2L)
+            .date("2024-03-01")
+            .type("LUNCH")
+            .place("2캠퍼스")
+            .priceCard(6000)
+            .priceCash(6000)
+            .kcal(881)
+            .menu("""
+                ["혼합잡곡밥", "가쓰오장국", "땡초부추전", "누룽지탕"]""")
+            .build();
+
+        Dining request3 = Dining.builder()
+            .id(3L)
+            .date("2024-03-11")
+            .type("LUNCH")
+            .place("능수관")
+            .priceCard(6000)
+            .priceCash(6000)
+            .kcal(300)
+            .menu("""
+                ["참치김치볶음밥", "유부된장국", "땡초부추전", "누룽지탕"]""")
+            .build();
+
+        Dining dining1 = diningRepository.save(request1);
+        Dining dining2 = diningRepository.save(request2);
+        Dining dining3 = diningRepository.save(request3);
+
+        ExtractableResponse<Response> response = given()
+            .when()
+            .get("/dinings?date=240311")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+
+        List<String> menus1 = List.of("병아리콩밥", "(탕)소고기육개장", "땡초부추전", "누룽지탕");
+        List<String> menus2 = List.of("참치김치볶음밥", "유부된장국", "땡초부추전", "누룽지탕");
+
+        SoftAssertions.assertSoftly(
+            softly -> {
+                softly.assertThat(response.body().jsonPath().getList(".").size()).isEqualTo(2);
+
+                softly.assertThat(response.body().jsonPath().getLong("[0].id")).isEqualTo(dining1.getId());
+                softly.assertThat(response.body().jsonPath().getString("[0].date")).isEqualTo(dining1.getDate());
+                softly.assertThat(response.body().jsonPath().getString("[0].type")).isEqualTo(dining1.getType());
+                softly.assertThat(response.body().jsonPath().getString("[0].place")).isEqualTo(dining1.getPlace());
+                softly.assertThat(response.body().jsonPath().getInt("[0].price_card"))
+                    .isEqualTo(dining1.getPriceCard());
+                softly.assertThat(response.body().jsonPath().getInt("[0].price_cash"))
+                    .isEqualTo(dining1.getPriceCash());
+                softly.assertThat(response.body().jsonPath().getInt("[0].kcal")).isEqualTo(dining1.getKcal());
+                softly.assertThat(response.body().jsonPath().getString("[0].created_at"))
+                    .isEqualTo(dining1.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                softly.assertThat(response.body().jsonPath().getString("[0].updated_at"))
+                    .isEqualTo(dining1.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                softly.assertThat(response.body().jsonPath().getList("[0].menu", String.class))
+                    .containsExactlyInAnyOrderElementsOf(menus1);
+
+                softly.assertThat(response.body().jsonPath().getLong("[1].id")).isEqualTo(dining3.getId());
+                softly.assertThat(response.body().jsonPath().getString("[1].date")).isEqualTo(dining3.getDate());
+                softly.assertThat(response.body().jsonPath().getString("[1].type")).isEqualTo(dining3.getType());
+                softly.assertThat(response.body().jsonPath().getString("[1].place")).isEqualTo(dining3.getPlace());
+                softly.assertThat(response.body().jsonPath().getInt("[1].price_card"))
+                    .isEqualTo(dining3.getPriceCard());
+                softly.assertThat(response.body().jsonPath().getInt("[1].price_cash"))
+                    .isEqualTo(dining3.getPriceCash());
+                softly.assertThat(response.body().jsonPath().getInt("[1].kcal")).isEqualTo(dining3.getKcal());
+                softly.assertThat(response.body().jsonPath().getString("[1].created_at"))
+                    .isEqualTo(dining3.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                softly.assertThat(response.body().jsonPath().getString("[1].updated_at"))
+                    .isEqualTo(dining3.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                softly.assertThat(response.body().jsonPath().getList("[1].menu", String.class))
+                    .containsExactlyInAnyOrderElementsOf(menus2);
+
+            }
+        );
+    }
+
+    @Test
+    @DisplayName("잘못된 형식의 날짜로 조회한다. - 날짜의 형식이 잘못되었다면 400")
+    void invalidFormatDate() {
+        Dining request = Dining.builder()
+            .id(1L)
+            .date("2024-03-11")
+            .type("LUNCH")
+            .place("A코스")
+            .priceCard(6000)
+            .priceCash(6000)
+            .kcal(881)
+            .menu("""
+                ["병아리콩밥", "(탕)소고기육개장", "땡초부추전", "누룽지탕"]""")
+            .build();
+
+        Dining dining = diningRepository.save(request);
+
+        given()
+            .when()
+            .get("/dinings?date=20240311")
+            .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value())
+            .extract();
+    }
+
+    @Test
+    @DisplayName("날짜가 비어있다. - 오늘 날짜를 받아 조회한다.")
+    void nullDate() {
+        when(clock.instant()).thenReturn(
+            ZonedDateTime.parse("2024-03-16 18:00:00 KST", ofPattern("yyyy-MM-dd " + "HH:mm:ss z")).toInstant());
+        when(clock.getZone()).thenReturn(Clock.systemDefaultZone().getZone());
+
+        Dining request1 = Dining.builder()
+            .id(1L)
+            .date("2024-03-16")
+            .type("LUNCH")
+            .place("A코스")
+            .priceCard(6000)
+            .priceCash(6000)
+            .kcal(881)
+            .menu("""
+                ["병아리콩밥", "(탕)소고기육개장", "땡초부추전", "고구마순들깨볶음", "총각김치"]""")
+            .build();
+
+        Dining request2 = Dining.builder()
+            .id(2L)
+            .date("2024-03-13")
+            .type("LUNCH")
+            .place("2캠퍼스")
+            .priceCard(6000)
+            .priceCash(6000)
+            .kcal(881)
+            .menu("""
+                ["혼합잡곡밥", "가쓰오장국", "땡초부추전", "누룽지탕"]""")
+            .build();
+
+        Dining dining1 = diningRepository.save(request1);
+        Dining dining2 = diningRepository.save(request2);
+
+        List<String> menus = List.of("병아리콩밥", "(탕)소고기육개장", "땡초부추전", "고구마순들깨볶음", "총각김치");
+
+        ExtractableResponse<Response> response = given()
+            .when()
+            .get("/dinings")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+
+        SoftAssertions.assertSoftly(
+            softly -> {
+                softly.assertThat(response.body().jsonPath().getList(".").size()).isEqualTo(1);
+
+                softly.assertThat(response.body().jsonPath().getLong("[0].id")).isEqualTo(dining1.getId());
+                softly.assertThat(response.body().jsonPath().getString("[0].date")).isEqualTo(dining1.getDate());
+                softly.assertThat(response.body().jsonPath().getString("[0].type")).isEqualTo(dining1.getType());
+                softly.assertThat(response.body().jsonPath().getString("[0].place")).isEqualTo(dining1.getPlace());
+                softly.assertThat(response.body().jsonPath().getInt("[0].price_card"))
+                    .isEqualTo(dining1.getPriceCard());
+                softly.assertThat(response.body().jsonPath().getInt("[0].price_cash"))
+                    .isEqualTo(dining1.getPriceCash());
+                softly.assertThat(response.body().jsonPath().getInt("[0].kcal")).isEqualTo(dining1.getKcal());
+                softly.assertThat(response.body().jsonPath().getString("[0].created_at"))
+                    .isEqualTo(dining1.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                softly.assertThat(response.body().jsonPath().getString("[0].updated_at"))
+                    .isEqualTo(dining1.getUpdatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                softly.assertThat(response.body().jsonPath().getList("[0].menu", String.class))
+                    .containsExactlyInAnyOrderElementsOf(menus);
+            }
+        );
+    }
+}
