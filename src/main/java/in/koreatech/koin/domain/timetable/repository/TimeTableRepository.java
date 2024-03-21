5de12ea9f82b100f9f554ad93d19fc02a34bda70
김원경 <148550522+kwoo28@users.noreply.github.com>
2024-03-21 09:31:00 +0900

    feat: 시간표 기능 구현 (#212)
    * feat: 시간표 조회 기능 추가
    
    * feat: 시간표 생성 기능 추가
    
    * feat: 시간표 수정/삭제 기능 추가
    
    * fix: 시간표 수정 변경
    
    * refactor: 리뷰 반영 1차
    
    * refactor: 리뷰 반영 2차
    
    * refactor: 리뷰 반영 3차
    
    * refactor: UpdateTimeTableRequest 이름 수정
    
    UpdateTimeTableRequest -> TimeTableUpdateRequest
    
    * refactor: 리뷰 반영 5차
    
    * refactor: 리뷰 반영 6차

---
+++ src/main/java/in/koreatech/koin/domain/timetable/repository/TimeTableRepository.java
@@ -0,0 +1,25 @@
+package in.koreatech.koin.domain.timetable.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
+
+import in.koreatech.koin.domain.timetable.exception.TimeTableNotFoundException;
+import in.koreatech.koin.domain.timetable.model.TimeTable;
+
+public interface TimeTableRepository extends Repository<TimeTable, Long> {
+
+    TimeTable save(TimeTable timeTable);
+
+    List<TimeTable> findAllByUserIdAndSemesterId(Long userId, Long semesterId);
+
+    Optional<TimeTable> findById(Long id);
+
+    void deleteByUserIdAndSemesterId(Long userId, Long semesterId);
+
+    default TimeTable getById(Long id) {
+        return findById(id)
+            .orElseThrow(() -> TimeTableNotFoundException.withDetail("id: " + id));
+    }
+}
