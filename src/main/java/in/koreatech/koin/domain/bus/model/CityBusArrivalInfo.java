0c16d2d7d605bef36a6de27b24173364a2274da6
박성빈 <psb106305@gmail.com>
2024-03-22 18:45:40 +0900

    refactor: record로 수정
    Co-authored-by: dradnats1012 <herjebi1012@gmail.com>
    Co-authored-by: Choon0414 <hyunn815@naver.com>

--- src/main/java/in/koreatech/koin/domain/bus/model/CityBusArrivalInfo.java
+++ src/main/java/in/koreatech/koin/domain/bus/model/CityBusArrivalInfo.java
@@ -1,30 +1,28 @@
 package in.koreatech.koin.domain.bus.model;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
-public class CityBusArrivalInfo/* implements Comparable<CityBusArrivalInfo> */ {
-    /**
-     * "arrprevstationcnt": 5,
-     * "arrtime": 222,
-     * "nodeid": "CAB285000405",
-     * "nodenm": "코리아텍",
-     * "routeid": "CAB285000147",
-     * "routeno": 402,
-     * "routetp": "일반버스",
-     * "vehicletp": "일반차량"
-     */
-    private Long arrprevstationcnt; // 남은 정거장 개수
-    private Long arrtime; // 도착까지 남은 시간 [초]
-    private String nodeid; // 정류소 id
-    private String nodenm; // 정류소명
-    private String routeid; // 노선 id
-    private Long routeno; // 버스 번호
-    private String routetp; // 노선 유형
-    private String vehicletp; // 차량 유형 (저상버스)
+/**
+ * "arrprevstationcnt": 5,
+ * "arrtime": 222,
+ * "nodeid": "CAB285000405",
+ * "nodenm": "코리아텍",
+ * "routeid": "CAB285000147",
+ * "routeno": 402,
+ * "routetp": "일반버스",
+ * "vehicletp": "일반차량"
+ */
 
-    //TODO: record로 변환 가능한지 확인
+public record CityBusArrivalInfo(
+    Long arrprevstationcnt, // 남은 정거장 개수
+    Long arrtime, // 도착까지 남은 시간 [초]
+    String nodeid, // 정류소 id
+    String nodenm, // 정류소명
+    String routeid, // 노선 id
+    Long routeno, // 버스 번호
+    String routetp, // 노선 유형
+    String vehicletp // 차량 유형 (저상버스)
+) {
     public static CityBusArrivalInfo getEmpty(String nodeid) {
         return builder()
             .nodeid(nodeid)
@@ -33,16 +31,7 @@ public class CityBusArrivalInfo/* implements Comparable<CityBusArrivalInfo> */ {
     }
 
     @Builder
-    private CityBusArrivalInfo(Long arrprevstationcnt, Long arrtime, String nodeid, String nodenm, String routeid,
-        Long routeno, String routetp, String vehicletp) {
-        this.arrprevstationcnt = arrprevstationcnt;
-        this.arrtime = arrtime;
-        this.nodeid = nodeid;
-        this.nodenm = nodenm;
-        this.routeid = routeid;
-        this.routeno = routeno;
-        this.routetp = routetp;
-        this.vehicletp = vehicletp;
+    public CityBusArrivalInfo {
     }
     /*
     @Override
