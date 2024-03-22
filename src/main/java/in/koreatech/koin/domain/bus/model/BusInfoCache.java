d181251b5c9813836dc82267ad2c02b1b52085e7
박성빈 <psb106305@gmail.com>
2024-03-23 01:26:11 +0900

    refactor: 주석 추가
    Co-authored-by: dradnats1012 <herjebi1012@gmail.com>
    Co-authored-by: Choon0414 <hyunn815@naver.com>

--- src/main/java/in/koreatech/koin/domain/bus/model/BusInfoCache.java
+++ src/main/java/in/koreatech/koin/domain/bus/model/BusInfoCache.java
@@ -8,6 +8,13 @@ public record BusInfoCache(
     LocalTime remainTime
 ) {
 
+    /**
+     * <pre>
+     * 남은 시간 = (캐시 저장 시각 + 저장된 남은시간) - 현재시각
+     * {@link BusRemainTime#getRemainSeconds}에서는 남은 시간을 (저장된 남은시간 - 현재시각)으로 계산중
+     * 학교 버스는 도착 시간을 저장하고, 시내버스는 남은 시간만을 저장하므로
+     * Redis에 저장할때 (캐시 저장 시각 + 남은시간)으로 저장하여 통일시켜줌</pre>
+     * */
     public static BusInfoCache from(CityBusArrivalInfo busArrivalInfo, LocalDateTime updatedAt) {
         return new BusInfoCache(
             busArrivalInfo.routeno(),
