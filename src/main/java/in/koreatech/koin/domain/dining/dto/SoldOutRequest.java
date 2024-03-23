01596d89c59b33b088eea38d34d59a09fba7bdb6
dradnats1012 <herjebi1012@gmail.com>
2024-03-24 00:50:48 +0900

    feat : SoldOutRequest DTO 클래스 추가(메뉴아이디, 품절여부)

---
+++ src/main/java/in/koreatech/koin/domain/dining/dto/SoldOutRequest.java
@@ -0,0 +1,11 @@
+package in.koreatech.koin.domain.dining.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record SoldOutRequest(
+    @Schema(description = "메뉴 고유 ID", example = "1") Long menuId,
+
+    @Schema(description = "품절 여부", example = "true") Boolean soldOut
+) {
+
+}
