dada19f9c9ddcb0a21de6f60727c1c95a6d05b9b
dradnats1012 <herjebi1012@gmail.com>
2024-03-24 01:01:38 +0900

    feat : CoopDiningService 클래스 추가

---
+++ src/main/java/in/koreatech/koin/domain/dining/service/CoopDiningService.java
@@ -0,0 +1,20 @@
+package in.koreatech.koin.domain.dining.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import in.koreatech.koin.domain.dining.dto.SoldOutRequest;
+import in.koreatech.koin.domain.dining.repository.DiningRepository;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CoopDiningService {
+
+    private final DiningRepository diningRepository;
+
+    public void changeSoldOut(SoldOutRequest soldOutRequest) {
+        diningRepository.updateSoldOut(soldOutRequest.menuId(), soldOutRequest.soldOut());
+    }
+}
