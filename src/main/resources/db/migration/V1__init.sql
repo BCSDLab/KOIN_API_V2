022b261931135f6c7fd9facd0807cfa9ece8b8fc
최준호 <junho5336@gmail.com>
2024-03-23 10:44:37 +0900

    refactor: flyway schema 제거

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -327,24 +327,6 @@ create table if not exists faqs
 )
     collate = utf8_unicode_ci;
 
-create table if not exists flyway_schema_history
-(
-    installed_rank int                                 not null
-        primary key,
-    version        varchar(50)                         null,
-    description    varchar(200)                        not null,
-    type           varchar(20)                         not null,
-    script         varchar(1000)                       not null,
-    checksum       int                                 null,
-    installed_by   varchar(100)                        not null,
-    installed_on   timestamp default CURRENT_TIMESTAMP not null,
-    execution_time int                                 not null,
-    success        tinyint(1)                          not null
-);
-
-create index flyway_schema_history_s_idx
-    on flyway_schema_history (success);
-
 create table if not exists holidays
 (
     id   int unsigned auto_increment
