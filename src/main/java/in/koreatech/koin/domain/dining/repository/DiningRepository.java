5cff1ff3d0ff7197be0f7ae3fdff49438617daa6
dradnats1012 <herjebi1012@gmail.com>
2024-03-24 01:02:14 +0900

    feat : DiningRepository 품절여부 변경 메서드 추가, 테스트용 메서드 추

--- src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
+++ src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
@@ -2,13 +2,27 @@ package in.koreatech.koin.domain.dining.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import in.koreatech.koin.domain.dining.model.Dining;
+import io.lettuce.core.dynamic.annotation.Param;
+import jakarta.transaction.Transactional;
 
 public interface DiningRepository extends Repository<Dining, Long> {
 
     Dining save(Dining dining);
 
     List<Dining> findAllByDate(String date);
+
+    Dining findById(Long id);
+
+    @Transactional
+    @Modifying
+    @Query(value = "UPDATE dining_menus dm SET dm.sold_out = :soldOut WHERE dm.id = :menuId", nativeQuery = true)
+    void updateSoldOut(@Param("menuId") Long menuId, @Param("soldOut") Boolean soldOut);
+
+    @Query(value = "SELECT id FROM dining_menus where sold_out = true", nativeQuery = true)
+    Long findSoldOut();
 }
