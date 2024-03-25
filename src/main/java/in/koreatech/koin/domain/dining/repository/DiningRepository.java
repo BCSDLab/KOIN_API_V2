7358269dfbe1b204d0c68abc59d7b6e143b8ea75
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-03-25 17:38:59 +0900

    feat : 특정 식단의 이미지 업데이트 쿼리 추가

--- src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
+++ src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
@@ -2,13 +2,24 @@ package in.koreatech.koin.domain.dining.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import in.koreatech.koin.domain.dining.model.Dining;
+import jakarta.transaction.Transactional;
 
 public interface DiningRepository extends Repository<Dining, Long> {
 
     Dining save(Dining dining);
+
+    Dining findById(Long id);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Dining d SET d.image = :image WHERE d.id = :id", nativeQuery = true)
+    void updateDiningImage(@Param("id") Long id, @Param("image") String imageUrl);
 
     List<Dining> findAllByDate(String date);
 }
