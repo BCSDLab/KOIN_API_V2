fe5de8f4b53acbd3cb1aeaf2d5d95bb84ed3c54a
dradnats1012 <herjebi1012@gmail.com>
2024-03-26 20:22:15 +0900

    feat : 메뉴가 없는 경우 예외 추가

---
+++ src/main/java/in/koreatech/koin/domain/coop/exception/MenuNotFoundException.java
@@ -0,0 +1,17 @@
+package in.koreatech.koin.domain.coop.exception;
+
+import in.koreatech.koin.domain.shop.exception.ShopCategoryNotFoundException;
+import in.koreatech.koin.global.exception.DataNotFoundException;
+
+public class MenuNotFoundException extends DataNotFoundException {
+    private static final String DEFAULT_MESSAGE = "존재하지 않는 메뉴입니다.";
+
+    public MenuNotFoundException(String message) {
+        super(message);
+    }
+
+    public static ShopCategoryNotFoundException withDetail(String detail) {
+        String message = String.format("%s %s", DEFAULT_MESSAGE, detail);
+        return new ShopCategoryNotFoundException(message);
+    }
+}
