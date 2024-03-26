99de701349b0d758d41c6fb61b3badfbfa4c386e
허준기 <112807640+dradnats1012@users.noreply.github.com>
2024-03-26 20:30:37 +0900

    feat : 영양사 품절 정보 입력 (#238)
    * feat : Dining 클래스에 soldOut 컬럼 추가
    
    * feat : DiningResponse DTO에 soldOut 컬럼 추가
    
    * feat : flyway DB에 sold_out 컬럼 추가
    
    * feat : SoldOutRequest DTO 클래스 추가(메뉴아이디, 품절여부)
    
    * feat : CoopDiningController, CoopDiningApi 클래스 추
    
    * feat : CoopDiningService 클래스 추가
    
    * feat : DiningRepository 품절여부 변경 메서드 추가, 테스트용 메서드 추
    
    * feat : 영양사 품절 요청 테스트, 권한 확인 테스트 추
    
    * feat : 기존 테스트 동작하도록 수정
    
    * feat : 리뷰 반
    
    * feat : 리뷰 반영, 테스트 방식 변
    
    * feat : coop 패키지 분
    
    * feat : setSoldOut() 메서드 추
    
    * feat : update 방식을 조회 후 set해주는 방식으로 변
    
    * feat : 사용하지 않는 메서드 삭
    
    * feat : 패키지 변경으로 인한 테스트 변경
    
    * feat : 인가 테스트로 변경
    
    * feat : getById()로 수정
    
    * feat : 개행 추가
    
    * feat : 메뉴가 없는 경우 예외 추가
    
    * refactor: 컨벤션 반영
    
    * refactor: snakecase로 변경

--- src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
+++ src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
@@ -1,9 +1,11 @@
 package in.koreatech.koin.domain.dining.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.Repository;
 
+import in.koreatech.koin.domain.coop.exception.MenuNotFoundException;
 import in.koreatech.koin.domain.dining.model.Dining;
 
 public interface DiningRepository extends Repository<Dining, Long> {
@@ -11,4 +13,11 @@ public interface DiningRepository extends Repository<Dining, Long> {
     Dining save(Dining dining);
 
     List<Dining> findAllByDate(String date);
+
+    Optional<Dining> findById(Long id);
+
+    default Dining getById(Long id){
+        return findById(id)
+            .orElseThrow(() -> MenuNotFoundException.withDetail("menuId: " + id));
+    }
 }
