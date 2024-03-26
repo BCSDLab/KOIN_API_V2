ee5459e7a8b8a232cca8378ce706803a5d6b2601
dradnats1012 <herjebi1012@gmail.com>
2024-03-26 20:24:09 +0900

    refactor: snakecase로 변경

--- src/main/java/in/koreatech/koin/domain/coop/dto/SoldOutRequest.java
+++ src/main/java/in/koreatech/koin/domain/coop/dto/SoldOutRequest.java
@@ -1,7 +1,11 @@
 package in.koreatech.koin.domain.coop.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record SoldOutRequest(
     @Schema(description = "메뉴 고유 ID", example = "1")
     Long menuId,
