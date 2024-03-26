9606015d350273c35a0f2d516467dddcc6f32a46
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-03-26 20:56:49 +0900

    chore : 충돌 해결

--- src/main/java/in/koreatech/koin/domain/coop/controller/CoopApi.java
+++ src/main/java/in/koreatech/koin/domain/coop/controller/CoopApi.java
@@ -5,6 +5,7 @@ import static in.koreatech.koin.domain.user.model.UserType.COOP;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.koreatech.koin.domain.coop.dto.SoldOutRequest;
 import in.koreatech.koin.domain.coop.dto.DiningImageRequest;
@@ -16,6 +17,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@RequestMapping("/coop")
 @Tag(name = "(OWNER) Coop Dining : 영양사 식단", description = "영양사 식단 페이지")
 public interface CoopApi {
 
@@ -29,12 +31,23 @@ public interface CoopApi {
         }
     )
     @Operation(summary = "특정 코너 품절 요청")
-    @PatchMapping("/coop/dining/soldout")
+    @PatchMapping("/dining/soldout")
     ResponseEntity<Void> changeSoldOut(
         @Auth(permit = {COOP}) Long userId,
         @RequestBody SoldOutRequest soldOutRequest
+    );
+
+    @ApiResponses(
+
+        value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true)))
+        }
+    )
     @Operation(summary = "이미지 사진 업로드")
-    @PatchMapping("/coop/dining/image")
+    @PatchMapping("/dining/image")
     ResponseEntity<Void> saveDiningImage(
         @Auth(permit = {COOP}) Long userId,
         @RequestBody DiningImageRequest imageRequest
