3ecd9c19353c498ffc195d15ecc4a1ffc921eb34
dradnats1012 <herjebi1012@gmail.com>
2024-03-26 13:00:31 +0900

    feat : 사용하지 않는 메서드 삭

--- src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
+++ src/main/java/in/koreatech/koin/domain/dining/repository/DiningRepository.java
@@ -2,13 +2,9 @@ package in.koreatech.koin.domain.dining.repository;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import in.koreatech.koin.domain.dining.model.Dining;
-import io.lettuce.core.dynamic.annotation.Param;
-import jakarta.transaction.Transactional;
 
 public interface DiningRepository extends Repository<Dining, Long> {
 
@@ -17,9 +13,4 @@ public interface DiningRepository extends Repository<Dining, Long> {
     List<Dining> findAllByDate(String date);
 
     Dining findById(Long id);
-
-    @Transactional
-    @Modifying
-    @Query(value = "UPDATE dining_menus dm SET dm.sold_out = :soldOut WHERE dm.id = :menuId", nativeQuery = true)
-    void updateSoldOut(@Param("menuId") Long menuId, @Param("soldOut") Boolean soldOut);
 }
