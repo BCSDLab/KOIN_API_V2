bd2227465bd5484a4f4adbfca2cc277819433ebe
박성빈 <psb106305@gmail.com>
2024-03-27 21:51:51 +0900

    refactor: 주석 수정
    Co-authored-by: dradnats1012 <herjebi1012@gmail.com>
    Co-authored-by: Choon0414 <hyunn815@naver.com>

--- src/main/java/in/koreatech/koin/domain/bus/model/CityBusArrivalInfo.java
+++ src/main/java/in/koreatech/koin/domain/bus/model/CityBusArrivalInfo.java
@@ -2,26 +2,15 @@ package in.koreatech.koin.domain.bus.model;
 
 import lombok.Builder;
 
-/**
- * "arrprevstationcnt": 5,
- * "arrtime": 222,
- * "nodeid": "CAB285000405",
- * "nodenm": "코리아텍",
- * "routeid": "CAB285000147",
- * "routeno": 402,
- * "routetp": "일반버스",
- * "vehicletp": "일반차량"
- */
-
 public record CityBusArrivalInfo(
-    Long arrprevstationcnt, // 남은 정거장 개수
-    Long arrtime, // 도착까지 남은 시간 [초]
-    String nodeid, // 정류소 id
-    String nodenm, // 정류소명
-    String routeid, // 노선 id
-    Long routeno, // 버스 번호
-    String routetp, // 노선 유형
-    String vehicletp // 차량 유형 (저상버스)
+    Long arrprevstationcnt, // 남은 정거장 개수, 5
+    Long arrtime, // 도착까지 남은 시간 [초], 222
+    String nodeid, // 정류소 id, "CAB285000405"
+    String nodenm, // 정류소명, "코리아텍"
+    String routeid, // 노선 id, "CAB285000147"
+    Long routeno, // 버스 번호, 402
+    String routetp, // 노선 유형, "일반 버스"
+    String vehicletp // 차량 유형 (저상버스), "일반차량"
 ) {
     public static CityBusArrivalInfo getEmpty(String nodeid) {
         return builder()
