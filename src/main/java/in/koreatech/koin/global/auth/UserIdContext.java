a31fc3f082ba34f767d4f0a0e53a23cbff504521
최준호 <junho5336@gmail.com>
2024-03-28 18:06:38 +0900

    feat: 사장님 인증코드 검증 (#248)
    * feat: 사장님 인증번호 인증 구현
    
    * refactor: 로직 수정 및 인증이 완료된 값에 대해 레디스 제거
    
    * test: 테스트 추가
    
    * build: 테이블 복원
    
    * refactor: 인증방식 수정, 사용자 인증없이 사용자 id만 필요한 경우 `@UserId`를 사용하도록 수정
    
    * test: MySQL 테스트 컨테이너 버전 8.0.29로 수정
    
    * refactor: ArgumentResolver Webconfig 등록
    
    * refactor: 파일업로드 임시 토큰에 대한 허용
    
    * test: 테스트 수정
    
    * refactor: 변수 사용

--- src/main/java/in/koreatech/koin/global/auth/UserIdContext.java
+++ src/main/java/in/koreatech/koin/global/auth/UserIdContext.java
@@ -6,23 +6,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import in.koreatech.koin.domain.user.model.UserType;
-import in.koreatech.koin.global.auth.exception.AuthenticationException;
 
 @Component
 @RequestScope
-public class AuthContext {
+public class UserIdContext {
 
     private Long userId;
 
     public Long getUserId() {
-        if (userId == null) {
-            throw AuthenticationException.withDetail("userId is null");
+        if (Objects.equals(userId, UserType.ANONYMOUS_ID)) {
+            return null;
         }
         return userId;
-    }
-
-    public boolean isAnonymous() {
-        return Objects.equals(userId, UserType.ANONYMOUS_ID);
     }
 
     public void setUserId(Long userId) {
--- src/main/resources/db/migration/V5__update_owners_table.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE `owners` DROP `company_registration_certificate_image_url`;
