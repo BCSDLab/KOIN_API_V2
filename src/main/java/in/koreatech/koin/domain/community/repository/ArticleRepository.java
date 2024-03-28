2bf53159f20ca38b432d2a41eb606e5791098a21
송선권 <songsunkook@gmail.com>
2024-03-28 14:38:29 +0900

    fix: 게시판 공지사항 판단 로직 수정 (#246)
    * fix: 공지사항 판단 로직 수정
    
    * fix: 공지사항 판단 로직 재수정
    
    * refactor: 게시판 태그 enum으로 분리

--- src/main/java/in/koreatech/koin/domain/community/repository/ArticleRepository.java
+++ src/main/java/in/koreatech/koin/domain/community/repository/ArticleRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 
@@ -14,15 +15,13 @@ public interface ArticleRepository extends Repository<Article, Long> {
 
     Article save(Article article);
 
-    Page<Article> findByBoardId(Long boardId, Pageable pageable);
-
-    default Page<Article> getByBoardId(Long boardId, Pageable pageable) {
-        return findByBoardId(boardId, pageable);
-    }
+    Page<Article> findByIsNotice(Boolean isNotice, Pageable pageable);
 
     Optional<Article> findById(Long articleId);
 
     List<Article> findAll(Pageable pageable);
+
+    Page<Article> findByBoardId(Long boardId, PageRequest pageRequest);
 
     default Article getById(Long articleId) {
         return findById(articleId).orElseThrow(
