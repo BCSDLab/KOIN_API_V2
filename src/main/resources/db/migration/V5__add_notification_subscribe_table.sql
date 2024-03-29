f01308f3e841efa47a30f203a06db86c9b2ddcb8
songsunkook <songsunkook@gmail.com>
2024-03-29 14:09:10 +0900

    feat: flyway 테이블 추가

---
+++ src/main/resources/db/migration/V5__add_notification_subscribe_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE if not exists notification_subscribe
+(
+    id                          BIGINT AUTO_INCREMENT NOT NULL,
+    created_at                  datetime              NOT NULL,
+    updated_at                  datetime              NOT NULL,
+    notification_subscribe_type VARCHAR(255)          NOT NULL,
+    user_id                     BIGINT                NOT NULL,
+    CONSTRAINT pk_notification_subscribe PRIMARY KEY (id)
+);
+
+ALTER TABLE notification_subscribe
+    ADD CONSTRAINT FK_NOTIFICATION_SUBSCRIBE_ON_USER FOREIGN KEY (user_id) REFERENCES users (id);
