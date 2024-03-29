3494822ef828c79a2ec2bde900d04c0795127971
최준호 <junho5336@gmail.com>
2024-03-29 13:47:25 +0900

    feat: 사장님 비밀번호 재발급 구현 (#253)
    * feat: 사장님 인증번호 인증 구현
    
    * refactor: 로직 수정 및 인증이 완료된 값에 대해 레디스 제거
    
    * test: 테스트 추가
    
    * build: 테이블 복원
    
    * refactor: 인증방식 수정, 사용자 인증없이 사용자 id만 필요한 경우 `@UserId`를 사용하도록 수정
    
    * test: MySQL 테스트 컨테이너 버전 8.0.29로 수정
    
    * refactor: ArgumentResolver Webconfig 등록
    
    * refactor: 파일업로드 임시 토큰에 대한 허용
    
    * test: 테스트 수정
    
    * feat: 사장님 비밀번호 찾기 API 구현
    
    * refactor: 이메일 형식 수정
    
    * refactor: 인증 제거

---
+++ src/main/java/in/koreatech/koin/domain/owner/dto/OwnerVerifyRequest.java
@@ -0,0 +1,23 @@
+package in.koreatech.koin.domain.owner.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record OwnerVerifyRequest(
+    @JsonProperty(value = "address")
+    @Schema(description = "사장님 이메일", example = "junho5336@gmail.com")
+    String email,
+
+    @NotBlank(message = "인증 코드는 필수입니다.")
+    @Digits(integer = 6, fraction = 0, message = "인증 코드는 6자리 정수여야 합니다.")
+    @Schema(description = "인증 코드", example = "123456")
+    String certificationCode
+) {
+
+}
