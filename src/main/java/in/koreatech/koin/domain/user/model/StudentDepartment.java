fcea148f6c803e4e6754003b606c1642ad4a3246
duehee <149302959+duehee@users.noreply.github.com>
2024-03-30 15:52:51 +0900

    chore : 검증 ENUM으로 이동

--- src/main/java/in/koreatech/koin/domain/user/model/StudentDepartment.java
+++ src/main/java/in/koreatech/koin/domain/user/model/StudentDepartment.java
@@ -1,34 +1,27 @@
 package in.koreatech.koin.domain.user.model;
 
-import java.util.Arrays;
-
-import in.koreatech.koin.domain.user.exception.StudentDepartmentNotValidException;
 import lombok.Getter;
 
 @Getter
 public enum StudentDepartment {
-    COMPUTER("컴퓨터공학부"),
-    MECHANICAL("기계공학부"),
-    MECHATRONICS("메카트로닉스공학부"),
-    ELECTRONIC("전기전자통신공학부"),
-    DESIGN("디자인공학부"),
-    ARCHITECTURAL("건축공학부"),
-    CHEMICAL("화학생명공학부"),
-    ENERGY("에너지신소재공학부"),
-    INDUSTRIAL("산업경영학부"),
-    EMPLOYMENT("고용서비스정책학과"),
+    컴퓨터공학부,
+    기계공학부,
+    메카트로닉스공학부,
+    전기전자통신공학부,
+    디자인공학부,
+    건축공학부,
+    화학생명공학부,
+    에너지신소재공학부,
+    산업경영학부,
+    고용서비스정책학과,
     ;
 
-    private final String value;
-
-    StudentDepartment(String value) {
-        this.value = value;
-    }
-
-    public static StudentDepartment from(String value) {
-        return Arrays.stream(values())
-            .filter(it -> it.value.equals(value))
-            .findAny()
-            .orElseThrow(() -> StudentDepartmentNotValidException.withDetail("학부(학과) : " + value));
+    public static boolean isValid(String department) {
+        for (StudentDepartment value : StudentDepartment.values()) {
+            if (value.name().equals(department)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/in/koreatech/koin/domain/user/service/StudentService.java
+++ src/main/java/in/koreatech/koin/domain/user/service/StudentService.java
@@ -1,8 +1,5 @@
 package in.koreatech.koin.domain.user.service;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +9,7 @@ import in.koreatech.koin.domain.user.dto.StudentUpdateResponse;
 import in.koreatech.koin.domain.user.exception.DuplicationNicknameException;
 import in.koreatech.koin.domain.user.exception.StudentDepartmentNotValidException;
 import in.koreatech.koin.domain.user.model.Student;
+import in.koreatech.koin.domain.user.model.StudentDepartment;
 import in.koreatech.koin.domain.user.model.User;
 import in.koreatech.koin.domain.user.model.UserGender;
 import in.koreatech.koin.domain.user.repository.StudentRepository;
@@ -52,20 +50,7 @@ public class StudentService {
     }
 
     public void CheckDepartmentValid(String department) {
-        List<String> departments = Arrays.asList(
-            "컴퓨터공학부",
-            "기계공학부",
-            "메카트로닉스공학부",
-            "전기전자통신공학부",
-            "디자인공학부",
-            "건축공학부",
-            "화학생명공학부",
-            "에너지신소재공학부",
-            "산업경영학부",
-            "고용서비스정책학과"
-        );
-
-        if (department != null & !departments.contains(department)) {
+        if (department != null & !StudentDepartment.isValid(department)) {
             throw StudentDepartmentNotValidException.withDetail("학부(학과) : " + department);
         }
     }
