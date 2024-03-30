8e17f5e58451c01a17d92105341a90f61ff09618
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-03-30 16:55:47 +0900

    feat: 서비스 로직 구현

--- src/main/java/in/koreatech/koin/domain/user/service/StudentService.java
+++ src/main/java/in/koreatech/koin/domain/user/service/StudentService.java
@@ -5,8 +5,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import in.koreatech.koin.domain.user.dto.StudentRegisterRequest;
 import in.koreatech.koin.domain.user.dto.StudentResponse;
+import in.koreatech.koin.domain.user.exception.DuplicationNicknameException;
 import in.koreatech.koin.domain.user.model.Student;
 import in.koreatech.koin.domain.user.repository.StudentRepository;
+import in.koreatech.koin.domain.user.repository.UserRepository;
+import in.koreatech.koin.global.domain.email.exception.DuplicationEmailException;
+import in.koreatech.koin.global.domain.email.model.EmailAddress;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -15,14 +19,49 @@ import lombok.RequiredArgsConstructor;
 public class StudentService {
 
     private final StudentRepository studentRepository;
+    private final UserRepository userRepository;
 
     public StudentResponse getStudent(Long userId) {
         Student student = studentRepository.getById(userId);
         return StudentResponse.from(student);
     }
 
-    public void StudentRegister(StudentRegisterRequest request){
+    public void StudentRegister(StudentRegisterRequest request) {
         Student student = StudentRegisterRequest.toStudent(request);
+        validateInRegister(student);
+    }
+
+    private void validateInRegister(Student student) {
+        EmailAddress emailAddress = EmailAddress.from(student.getUser().getEmail());
+        emailAddress.validatePortalEmail();
+
+        validateUniqueness(student);
+        validateStudentNumber(student);
+    }
+
+    private void validateUniqueness(Student student) {
+        validateEmailUniqueness(student.getUser().getEmail());
+        validateNicknameUniqueness(student.getUser().getNickname());
+    }
+
+    private void validateEmailUniqueness(String email) {
+        userRepository.findByEmail(email)
+            .ifPresent(user -> {
+                throw DuplicationEmailException.withDetail("email: " + email);
+            });
+    }
+
+    private void validateNicknameUniqueness(String nickname) {
+        userRepository.findByEmail(nickname)
+            .ifPresent(user -> {
+                throw DuplicationNicknameException.withDetail("nickname: " + nickname);
+            });
+    }
+
+    private void validateStudentNumber(Student student) {
+        if (student.getStudentNumber() == null) {
+            return;
+        }
 
     }
 }
--- src/main/java/in/koreatech/koin/global/domain/email/model/EmailAddress.java
+++ src/main/java/in/koreatech/koin/global/domain/email/model/EmailAddress.java
@@ -1,5 +1,13 @@
 package in.koreatech.koin.global.domain.email.model;
 
+import java.util.Hashtable;
+
+import javax.naming.NamingException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
+import in.koreatech.koin.global.domain.email.exception.EmailDomainInvalidException;
 import jakarta.validation.constraints.Email;
 
 public record EmailAddress(
@@ -10,7 +18,50 @@ public record EmailAddress(
     private static final String DOMAIN_PATTERN = "[^-][A-Za-z0-9\\+-]+(\\.[A-Za-z0-9\\+-]+)*(\\.[A-Za-z]{2,})$";
     private static final String EMAIL_PATTERN = LOCAL_PARTS_PATTERN + DOMAIN_PATTERN;
 
+    private static final int BEGIN_INDEX = 0;
+    private static final String domainSeparator = "@";
+    private static final String PORTAL_DOMAIN = "koreatech.ac.kr";
+
     public static EmailAddress from(String email) {
         return new EmailAddress(email);
+    }
+
+    public void validatePortalEmail() {
+        if(email.equals(PORTAL_DOMAIN)) {
+            throw EmailDomainInvalidException.withDetail("domain: " + domainForm());
+        }
+    }
+
+    public void validateSendable(){
+        if (!canSend()) {
+            throw EmailDomainInvalidException.withDetail("domain: " + domainForm());
+        }
+    }
+
+    private String domainForm() {
+        return email.substring(getSeparateIndex() + domainSeparator.length());
+    }
+
+    private String localPartsFor() {
+        return email.substring(BEGIN_INDEX, getSeparateIndex());
+    }
+
+    private int getSeparateIndex() {
+        return email.lastIndexOf(domainSeparator);
+    }
+
+    private boolean canSend(){
+        try {
+            Hashtable<String, String> environmentSetting = new Hashtable<>();
+
+            environmentSetting.put("java.naming.factory.initial", "com.sun.jndi.dns.DnsContextFactory");
+            Attribute attribute = ((DirContext) new InitialDirContext(environmentSetting)).getAttributes(domainForm(),
+                new String[]{"MX"}).get("MX");
+
+            return attribute != null;
+
+        } catch (NamingException exception) {
+            throw EmailDomainInvalidException.withDetail("email: " + domainForm());
+        }
     }
 }
