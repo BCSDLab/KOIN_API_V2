18eb20f1a96d2468dbf6d16cdd6c1580c0453056
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-03-31 14:38:41 +0900

    fix: 상속 받는 예외 클래스 수정

--- src/main/java/in/koreatech/koin/global/domain/notification/exception/SubscribeNotFoundException.java
+++ src/main/java/in/koreatech/koin/global/domain/notification/exception/SubscribeNotFoundException.java
@@ -1,8 +1,8 @@
 package in.koreatech.koin.global.domain.notification.exception;
 
-import in.koreatech.koin.global.exception.DuplicationException;
+import in.koreatech.koin.global.exception.DataNotFoundException;
 
-public class SubscribeNotFoundException extends DuplicationException {
+public class SubscribeNotFoundException extends DataNotFoundException {
 
     private static final String DEFAULT_MESSAGE = "존재하지 않는 알림타입입니다.";
 
