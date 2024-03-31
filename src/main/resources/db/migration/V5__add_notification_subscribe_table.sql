d6bf1a749a6a3d6da51af8c4220f1eadc29fec80
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-03-31 14:40:44 +0900

    fix: BIGINT -> UNSIGNED INT 수정

--- src/main/resources/db/migration/V5__add_notification_subscribe_table.sql
+++ src/main/resources/db/migration/V5__add_notification_subscribe_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE `notification_subscribe` (
     created_at     datetime              NOT NULL,
     updated_at     datetime              NOT NULL,
     subscribe_type VARCHAR(255)          NOT NULL,
-    user_id        BIGINT                NOT NULL,
+    user_id        INT UNSIGNED                NOT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `FK_NOTIFICATION_SUBSCRIBE_ON_USER` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
