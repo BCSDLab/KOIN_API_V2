0a97ae8c85bd6694f9958793b2e3453c985edf20
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-04-03 00:35:31 +0900

    refactor: 비밀번호 example 중복 제거

--- src/main/java/in/koreatech/koin/domain/user/dto/StudentRegisterRequest.java
+++ src/main/java/in/koreatech/koin/domain/user/dto/StudentRegisterRequest.java
@@ -32,10 +32,7 @@ public record StudentRegisterRequest(
     @Size(max = 50, message = "이름은 50자 이내여야 합니다.")
     String name,
 
-    @Schema(description = """
-        SHA 256 해시 알고리즘으로 암호화된 비밀번호
-        example: asdf1234!
-        """, example = "cd06f8c2b0dd065faf6ef910c7f15934363df71c33740fd245590665286ed268")
+    @Schema(description = " SHA 256 해시 알고리즘으로 암호화된 비밀번호", example = "cd06f8c2b0dd065faf6ef910c7f15934363df71c33740fd245590665286ed268")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     String password,
 
