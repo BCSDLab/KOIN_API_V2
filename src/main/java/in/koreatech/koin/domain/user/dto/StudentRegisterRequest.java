58994c55b6bd4beb577d96ce5468256fcba06a64
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-04-03 02:31:33 +0900

    refactor: 최신 전공 형식에 맞게 수정

--- src/main/java/in/koreatech/koin/domain/user/dto/StudentRegisterRequest.java
+++ src/main/java/in/koreatech/koin/domain/user/dto/StudentRegisterRequest.java
@@ -46,7 +46,7 @@ public record StudentRegisterRequest(
     @Schema(description = "졸업 여부", example = "false")
     Boolean isGraduated,
 
-    @Schema(description = "전공{기계공학부, 컴퓨터공학부, 메카트로닉스공학부, 전기전자통신공학부, 디자인건축공학부, 에너지신소재화학공학부, 산업경영학부}", example = "컴퓨터공학부")
+    @Schema(description = "전공{기계공학부, 컴퓨터공학부, 메카트로닉스공학부, 전기전자통신공학부, 디자인공학부, 건축공학부, 화학생명공학부, 에너지신소재공학부, 산업경영학부, 고용서비스정책학과}", example = "컴퓨터공학부")
     @JsonProperty("major")
     String department,
 
