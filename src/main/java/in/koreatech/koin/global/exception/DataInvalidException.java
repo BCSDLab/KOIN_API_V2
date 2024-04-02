169a15ad1cbf2db03662a499e0902fa2fb4da2e9
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-04-03 00:30:06 +0900

    refactor: 라인포맷팅

--- src/main/java/in/koreatech/koin/global/exception/DataInvalidException.java
+++ src/main/java/in/koreatech/koin/global/exception/DataInvalidException.java
@@ -1,6 +1,7 @@
 package in.koreatech.koin.global.exception;
 
 public class DataInvalidException extends RuntimeException {
+
     public DataInvalidException(String message) {
         super(message);
     }
