3d78909a1135785579faee246adce3bc19fd9923
최준호 <junho5336@gmail.com>
2024-04-03 14:42:19 +0900

    feat: 로깅 추가

--- src/main/java/in/koreatech/koin/global/exception/GlobalExceptionHandler.java
+++ src/main/java/in/koreatech/koin/global/exception/GlobalExceptionHandler.java
@@ -1,73 +1,182 @@
 package in.koreatech.koin.global.exception;
 
 import java.time.format.DateTimeParseException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.WebUtils;
 
 import in.koreatech.koin.global.auth.exception.AuthenticationException;
 import in.koreatech.koin.global.auth.exception.AuthorizationException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
+        HttpServletRequest request,
+        IllegalArgumentException e
+    ) {
         log.warn(e.getMessage());
-        return ResponseEntity.badRequest().body(ErrorResponse.from(e.getMessage()));
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        log.warn(e.getMessage());
+        requestLogging(request);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(ErrorResponse.from("요청 파라미터가 잘못되었습니다."));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException e) {
+    public ResponseEntity<ErrorResponse> handleAuthorizationException(
+        HttpServletRequest request,
+        AuthorizationException e
+    ) {
         log.warn(e.getMessage());
+        requestLogging(request);
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
             .body(ErrorResponse.from("잘못된 권한입니다."));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+    public ResponseEntity<ErrorResponse> handleAuthenticationException(
+        HttpServletRequest request,
+        AuthenticationException e
+    ) {
         log.warn(e.getMessage());
+        requestLogging(request);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(ErrorResponse.from("잘못된 인증정보입니다."));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleDataNotFoundException(DataNotFoundException e) {
+    public ResponseEntity<ErrorResponse> handleDataNotFoundException(
+        HttpServletRequest request,
+        DataNotFoundException e
+    ) {
         log.warn(e.getMessage());
+        requestLogging(request);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
             .body(ErrorResponse.from("데이터를 찾을 수 없습니다."));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleDataNotFoundException(DuplicationException e) {
+    public ResponseEntity<ErrorResponse> handleDataNotFoundException(
+        HttpServletRequest request,
+        DuplicationException e
+    ) {
         log.warn(e.getMessage());
+        requestLogging(request);
         return ResponseEntity.status(HttpStatus.CONFLICT)
             .body(ErrorResponse.from("이미 존재하는 데이터입니다."));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleExternalServiceException(ExternalServiceException e) {
+    public ResponseEntity<ErrorResponse> handleExternalServiceException(
+        HttpServletRequest request,
+        ExternalServiceException e
+    ) {
         log.warn(e.getMessage());
+        requestLogging(request);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.from("외부 API 호출 과정에서 문제가 발생했습니다."));
+            .body(ErrorResponse.from("외부 API 호출 과정에서 문제가 발생했습니다."));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleDateTimeParseException(DateTimeParseException e) {
+    public ResponseEntity<ErrorResponse> handleDateTimeParseException(
+        HttpServletRequest request,
+        DateTimeParseException e
+    ) {
         log.warn(e.getMessage());
+        requestLogging(request);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(ErrorResponse.from("잘못된 날짜 형식입니다."));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+        MethodArgumentNotValidException ex,
+        HttpHeaders headers,
+        HttpStatusCode status,
+        WebRequest webRequest
+    ) {
+        HttpServletRequest request = ((ServletWebRequest) webRequest).getRequest();
+        log.warn("검증과정에서 문제가 발생했습니다. uri: {} {}, ", request.getMethod(), request.getRequestURI(), ex);
+        requestLogging(request);
+        String errorMessages = ex.getBindingResult().getAllErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(ErrorResponse.from(errorMessages));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+        Exception ex,
+        Object body,
+        HttpHeaders headers,
+        HttpStatusCode statusCode,
+        WebRequest webRequest
+    ) {
+        HttpServletRequest request = ((ServletWebRequest) webRequest).getRequest();
+        log.error("예외가 발생했습니다. uri: {} {}, ", request.getMethod(), request.getRequestURI(), ex);
+        requestLogging(request);
+        return ResponseEntity.status(statusCode)
+            .body(ErrorResponse.from(ex.getMessage()));
+    }
+
+    private void requestLogging(HttpServletRequest request) {
+        log.info("request header: {}", getHeaders(request));
+        log.info("request query string: {}", getQueryString(request));
+        log.info("request body: {}", getRequestBody(request));
+    }
+
+    private Map<String, Object> getHeaders(HttpServletRequest request) {
+        Map<String, Object> headerMap = new HashMap<>();
+        Enumeration<String> headerArray = request.getHeaderNames();
+        while (headerArray.hasMoreElements()) {
+            String headerName = headerArray.nextElement();
+            headerMap.put(headerName, request.getHeader(headerName));
+        }
+        return headerMap;
+    }
+
+    private String getQueryString(HttpServletRequest httpRequest) {
+        String queryString = httpRequest.getQueryString();
+        if (queryString == null) {
+            return " - ";
+        }
+        return queryString;
+    }
+
+    private String getRequestBody(HttpServletRequest request) {
+        var wrapper = WebUtils.getNativeRequest(request, ContentCachingRequestWrapper.class);
+        if (wrapper == null) {
+            return " - ";
+        }
+        try {
+            // body 가 읽히지 않고 예외처리 되는 경우에 캐시하기 위함
+            wrapper.getInputStream().readAllBytes();
+            byte[] buf = wrapper.getContentAsByteArray();
+            if (buf.length == 0) {
+                return " - ";
+            }
+            return new String(buf, wrapper.getCharacterEncoding());
+        } catch (Exception e
+        ) {
+            return " - ";
+        }
     }
 }
