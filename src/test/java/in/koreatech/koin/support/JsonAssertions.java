32685f02f9b4e1d47319a5b1100b716ed54b48ba
최준호 <junho5336@gmail.com>
2024-04-04 01:27:07 +0900

    test: add JsonAssertions

---
+++ src/test/java/in/koreatech/koin/support/JsonAssertions.java
@@ -0,0 +1,39 @@
+package in.koreatech.koin.support;
+
+import java.util.Map;
+
+import org.assertj.core.api.Assertions;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonAssertions {
+
+
+    public static JsonStringAssert assertThat(String expect) {
+        return new JsonStringAssert(expect);
+    }
+
+    public static class JsonStringAssert {
+
+        private final ObjectMapper objectMapper = new ObjectMapper();
+        private final String expect;
+
+        JsonStringAssert(String expect) {
+            this.expect = expect;
+        }
+
+        public void isEqualTo(String actual) {
+            try {
+                Map<String, Object> responseMap = objectMapper.readValue(expect, new TypeReference<>() {
+                });
+                Map<String, Object> expectedMap = objectMapper.readValue(actual, new TypeReference<>() {
+                });
+                Assertions.assertThat(responseMap).isEqualTo(expectedMap);
+            } catch (Exception ignored) {
+                
+            }
+        }
+
+    }
+}
