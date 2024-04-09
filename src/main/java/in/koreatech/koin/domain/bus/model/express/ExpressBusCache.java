4196333a8ae46ca0b80a7a212a0f49f4f3470ab0
dradnats1012 <herjebi1012@gmail.com>
2024-04-09 23:04:36 +0900

    feat : ExpressBusCache busInfos 생성자 다시 추가

--- src/main/java/in/koreatech/koin/domain/bus/model/express/ExpressBusCache.java
+++ src/main/java/in/koreatech/koin/domain/bus/model/express/ExpressBusCache.java
@@ -1,5 +1,6 @@
 package in.koreatech.koin.domain.bus.model.express;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -11,7 +12,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@RedisHash(value = "Tago@busArrivalInfo:intercityBus:")
+@RedisHash(value = "expressBus")
 public class ExpressBusCache {
 
     private static final long CACHE_EXPIRE_HOUR = 1L;
@@ -27,6 +28,7 @@ public class ExpressBusCache {
     @Builder
     private ExpressBusCache(String id, List<ExpressBusCacheInfo> busInfos, Long expiration) {
         this.id = id;
+        this.busInfos = (busInfos == null) ? new ArrayList<>() : busInfos;
         this.expiration = expiration == null ? CACHE_EXPIRE_HOUR : expiration;
     }
 
