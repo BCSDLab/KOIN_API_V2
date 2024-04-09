ea5dfe83877f400434178c12e7971f6712f991ae
최준호 <junho5336@gmail.com>
2024-04-09 17:30:20 +0900

    refactor: Dining id, date 수정

--- src/main/java/in/koreatech/koin/domain/dining/model/Dining.java
+++ src/main/java/in/koreatech/koin/domain/dining/model/Dining.java
@@ -1,34 +1,35 @@
 package in.koreatech.koin.domain.dining.model;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import in.koreatech.koin.global.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 @Table(name = "dining_menus")
 public class Dining extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
-    private Long id;
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
 
     @NotNull
     @Column(name = "date", nullable = false)
-    private String date;
+    private LocalDate date;
 
     @NotNull
     @Column(name = "type", nullable = false)
@@ -61,9 +62,8 @@ public class Dining extends BaseEntity {
     private LocalDateTime isChanged;
 
     @Builder
-    private Dining(Long id, String date, String type, String place, Integer priceCard, Integer priceCash,
+    private Dining(LocalDate date, String type, String place, Integer priceCard, Integer priceCash,
                    Integer kcal, String menu, String imageUrl, LocalDateTime soldOut, LocalDateTime isChanged) {
-        this.id = id;
         this.date = date;
         this.type = type;
         this.place = place;
