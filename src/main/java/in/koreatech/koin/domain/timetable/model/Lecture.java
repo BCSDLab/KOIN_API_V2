9448c728c9a030c11546f03e18d7d4f0fd8ef7b4
최준호 <junho5336@gmail.com>
2024-04-09 17:55:43 +0900

    refactor: Lecture id Long -> Integer 수정

--- src/main/java/in/koreatech/koin/domain/timetable/model/Lecture.java
+++ src/main/java/in/koreatech/koin/domain/timetable/model/Lecture.java
@@ -1,14 +1,15 @@
 package in.koreatech.koin.domain.timetable.model;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,13 +17,12 @@ import lombok.NoArgsConstructor;
 @Getter
 @Entity
 @Table(name = "lectures")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = PROTECTED)
 public class Lecture {
 
     @Id
-    @Column(name = "id", nullable = false)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
 
     @Size(max = 255)
     @NotNull
