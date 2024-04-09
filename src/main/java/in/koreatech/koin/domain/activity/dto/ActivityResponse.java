c0af412c756850fa5c46773d4125f1d9aede2788
최준호 <junho5336@gmail.com>
2024-04-09 16:56:32 +0900

    refactor: Activity id Long -> Integer 수정

--- src/main/java/in/koreatech/koin/domain/activity/dto/ActivityResponse.java
+++ src/main/java/in/koreatech/koin/domain/activity/dto/ActivityResponse.java
@@ -38,11 +38,12 @@ public record ActivityResponse(
     List<String> imageUrls,
 
     @Schema(description = "고유 식별자", example = "1")
-    Long id,
+    Integer id,
 
     @Schema(description = "제목", example = "코인 시간표 기능 추가")
     String title
 ) {
+
     public static ActivityResponse of(Activity activity, List<String> imageUrls) {
         return new ActivityResponse(
             activity.getDate(),
