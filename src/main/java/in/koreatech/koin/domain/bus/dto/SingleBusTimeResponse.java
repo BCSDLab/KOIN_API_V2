e61912f5f4d1b5d2172e3f63f83db206abb5266e
박성빈 <psb106305@gmail.com>
2024-04-09 09:48:32 +0900

    feat: 포맷 추가

--- src/main/java/in/koreatech/koin/domain/bus/dto/SingleBusTimeResponse.java
+++ src/main/java/in/koreatech/koin/domain/bus/dto/SingleBusTimeResponse.java
@@ -2,12 +2,19 @@ package in.koreatech.koin.domain.bus.dto;
 
 import java.time.LocalTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @JsonNaming(SnakeCaseStrategy.class)
 public record SingleBusTimeResponse(
+    @Schema(description = "버스 타입", example = "shuttle")
     String busName,
+
+    @Schema(description = "도착 시간", example = "12:00")
+    @JsonFormat(pattern = "HH:mm")
     LocalTime busTime
 ) {
 }
