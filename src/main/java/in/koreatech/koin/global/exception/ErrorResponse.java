1cc3c40853546fe322214cc5a49096acd33830ae
최준호 <junho5336@gmail.com>
2024-04-10 13:49:22 +0900

    refactor: time 제거

--- src/main/java/in/koreatech/koin/global/exception/ErrorResponse.java
+++ src/main/java/in/koreatech/koin/global/exception/ErrorResponse.java
@@ -1,8 +1,5 @@
 package in.koreatech.koin.global.exception;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
@@ -15,13 +12,9 @@ public class ErrorResponse {
     private final String message;
     private final String code;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private final LocalDateTime time;
-
     public ErrorResponse(int status, String message) {
         this.status = status;
         this.message = message;
         this.code = "";
-        this.time = LocalDateTime.now();
     }
 }
