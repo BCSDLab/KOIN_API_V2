ce1865ff8613be7c59859f42e865e305708c5155
최준호 <junho5336@gmail.com>
2024-04-15 21:05:18 +0900

    docs: land dto 보완

--- src/main/java/in/koreatech/koin/domain/land/dto/LandsResponse.java
+++ src/main/java/in/koreatech/koin/domain/land/dto/LandsResponse.java
@@ -1,6 +1,8 @@
 package in.koreatech.koin.domain.land.dto;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -9,28 +11,28 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonNaming(value = SnakeCaseStrategy.class)
 public record LandsResponse(
-    @Schema(description = "상세 이름", example = "럭키빌(투베이)")
+    @Schema(description = "상세 이름", example = "럭키빌(투베이)", requiredMode = NOT_REQUIRED)
     String internalName,
 
-    @Schema(description = "월세", example = "220~250만원(6개월)")
+    @Schema(description = "월세", example = "220~250만원(6개월)", requiredMode = NOT_REQUIRED)
     String monthlyFee,
 
-    @Schema(description = "위도", example = "36.769204")
+    @Schema(description = "위도", example = "36.769204", requiredMode = NOT_REQUIRED)
     String latitude,
 
-    @Schema(description = "보증금", example = "4500")
+    @Schema(description = "보증금", example = "4500", requiredMode = NOT_REQUIRED)
     String charterFee,
 
-    @Schema(description = "이름", example = "럭키빌(투베이)")
+    @Schema(description = "이름", example = "럭키빌(투베이)", requiredMode = NOT_REQUIRED)
     String name,
 
-    @Schema(description = "복덕방 고유 ID", example = "1")
+    @Schema(description = "복덕방 고유 ID", example = "1", requiredMode = REQUIRED)
     Integer id,
 
-    @Schema(description = "경도", example = "127.282554")
+    @Schema(description = "경도", example = "127.282554", requiredMode = NOT_REQUIRED)
     String longitude,
 
-    @Schema(description = "방 종류", example = "투베이")
+    @Schema(description = "방 종류", example = "투베이", requiredMode = NOT_REQUIRED)
     String roomType
 ) {
 
