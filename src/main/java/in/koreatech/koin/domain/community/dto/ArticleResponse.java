5fd8618b0430b0b26789ac92fc1ef2b7c426c731
최준호 <junho5336@gmail.com>
2024-04-15 18:37:18 +0900

    dosc: article 응답 추가

--- src/main/java/in/koreatech/koin/domain/community/dto/ArticleResponse.java
+++ src/main/java/in/koreatech/koin/domain/community/dto/ArticleResponse.java
@@ -1,5 +1,8 @@
 package in.koreatech.koin.domain.community.dto;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,40 +18,40 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public record ArticleResponse(
-    @Schema(description = "게시글 고유 ID", example = "1")
+    @Schema(description = "게시글 고유 ID", example = "1", requiredMode = REQUIRED)
     Integer id,
 
-    @Schema(description = "게시판 고유 ID", example = "1")
+    @Schema(description = "게시판 고유 ID", example = "1", requiredMode = REQUIRED)
     Integer boardId,
 
-    @Schema(description = "제목", example = "제목")
+    @Schema(description = "제목", example = "제목", requiredMode = REQUIRED)
     String title,
 
-    @Schema(description = "내용", example = "내용")
+    @Schema(description = "내용", example = "내용", requiredMode = REQUIRED)
     String content,
 
-    @Schema(description = "작성자 닉네임", example = "닉네임")
+    @Schema(description = "작성자 닉네임", example = "닉네임", requiredMode = REQUIRED)
     String nickname,
 
-    @Schema(description = "해결 여부", example = "false")
-    Boolean isSolved,
+    @Schema(description = "해결 여부", example = "false", requiredMode = REQUIRED)
+    boolean isSolved,
 
-    @Schema(description = "공지 여부", example = "false")
-    Boolean isNotice,
+    @Schema(description = "공지 여부", example = "false", requiredMode = REQUIRED)
+    boolean isNotice,
 
-    @Schema(description = "내용 요약", example = "내용 요약")
+    @Schema(description = "내용 요약", example = "내용 요약", requiredMode = NOT_REQUIRED)
     @JsonProperty("contentSummary") String contentSummary,
 
-    @Schema(description = "조회수", example = "1")
-    Integer hit,
+    @Schema(description = "조회수", example = "1", requiredMode = REQUIRED)
+    int hit,
 
-    @Schema(description = "댓글 수", example = "1")
-    Byte commentCount,
+    @Schema(description = "댓글 수", example = "1", requiredMode = REQUIRED)
+    int commentCount,
 
-    @Schema(description = "게시판 정보")
+    @Schema(description = "게시판 정보", requiredMode = REQUIRED)
     InnerBoardResponse board,
 
-    @Schema(description = "댓글 목록")
+    @Schema(description = "댓글 목록", requiredMode = NOT_REQUIRED)
     List<InnerCommentResponse> comments,
 
     @Schema(description = "생성 일자", example = "2023-01-04 12:00:01")
@@ -79,41 +82,43 @@ public record ArticleResponse(
 
     @JsonNaming(value = SnakeCaseStrategy.class)
     private record InnerBoardResponse(
-        @Schema(description = "게시판 고유 ID", example = "1")
+        @Schema(description = "게시판 고유 ID", example = "1", requiredMode = REQUIRED)
         Integer id,
 
-        @Schema(description = "게시판 태그", example = "tag")
+        @Schema(description = "게시판 태그", example = "tag", requiredMode = REQUIRED)
         String tag,
 
-        @Schema(description = "게시판 이름", example = "게시판 이름")
+        @Schema(description = "게시판 이름", example = "게시판 이름", requiredMode = REQUIRED)
         String name,
 
-        @Schema(description = "익명 여부", example = "false")
+        @Schema(description = "익명 여부", example = "false", requiredMode = REQUIRED)
         boolean isAnonymous,
 
-        @Schema(description = "게시글 수", example = "1")
-        Integer articleCount,
+        @Schema(description = "게시글 수", example = "1", requiredMode = REQUIRED)
+        int articleCount,
 
-        @Schema(description = "삭제 여부", example = "false")
+        @Schema(description = "삭제 여부", example = "false", requiredMode = REQUIRED)
         boolean isDeleted,
 
-        @Schema(description = "공지 여부", example = "false")
+        @Schema(description = "공지 여부", example = "false", requiredMode = REQUIRED)
         boolean isNotice,
 
-        @Schema(description = "부모 게시판 고유 ID", example = "1")
+        @Schema(description = "부모 게시판 고유 ID", example = "1", requiredMode = NOT_REQUIRED)
         Integer parentId,
 
-        @Schema(description = "순서", example = "1")
-        Integer seq,
+        @Schema(description = "순서", example = "1", requiredMode = REQUIRED)
+        int seq,
 
-        @Schema(description = "하위 게시판 목록")
+        @Schema(description = "하위 게시판 목록", requiredMode = NOT_REQUIRED)
         List<InnerBoardResponse> children,
 
-        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
+        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt,
 
-        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt
+        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime updatedAt
     ) {
 
         public static InnerBoardResponse from(Board board) {
@@ -137,34 +142,36 @@ public record ArticleResponse(
 
     @JsonNaming(value = SnakeCaseStrategy.class)
     private record InnerCommentResponse(
-        @Schema(description = "댓글 고유 ID", example = "1")
+        @Schema(description = "댓글 고유 ID", example = "1", requiredMode = NOT_REQUIRED)
         Integer id,
 
-        @Schema(description = "게시글 고유 ID", example = "1")
+        @Schema(description = "게시글 고유 ID", example = "1", requiredMode = REQUIRED)
         Integer articleId,
 
-        @Schema(description = "내용", example = "내용")
+        @Schema(description = "내용", example = "내용", requiredMode = REQUIRED)
         String content,
 
-        @Schema(description = "작성자 고유 ID", example = "1")
+        @Schema(description = "작성자 고유 ID", example = "1", requiredMode = REQUIRED)
         Integer userId,
 
-        @Schema(description = "작성자 닉네임", example = "닉네임")
+        @Schema(description = "작성자 닉네임", example = "닉네임", requiredMode = REQUIRED)
         String nickname,
 
-        @Schema(description = "삭제 여부", example = "false")
-        Boolean isDeleted,
+        @Schema(description = "삭제 여부", example = "false", requiredMode = REQUIRED)
+        boolean isDeleted,
 
-        @Schema(description = "수정 권한", example = "false")
-        @JsonProperty("grantEdit") Boolean grantEdit,
+        @Schema(description = "수정 권한", example = "false", requiredMode = REQUIRED)
+        @JsonProperty("grantEdit")
+        boolean grantEdit,
 
-        @Schema(description = "삭제 권한", example = "false")
-        @JsonProperty("grantDelete") Boolean grantDelete,
+        @Schema(description = "삭제 권한", example = "false", requiredMode = REQUIRED)
+        @JsonProperty("grantDelete")
+        boolean grantDelete,
 
-        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01")
+        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
 
-        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01")
+        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt
     ) {
 
@@ -176,8 +183,8 @@ public record ArticleResponse(
                 comment.getUserId(),
                 comment.getNickname(),
                 comment.getIsDeleted(),
-                comment.getGrantEdit(),
-                comment.getGrantDelete(),
+                comment.isGrantEdit(),
+                comment.isGrantDelete(),
                 comment.getCreatedAt(),
                 comment.getUpdatedAt()
             );
--- src/main/java/in/koreatech/koin/domain/community/dto/ArticlesResponse.java
+++ src/main/java/in/koreatech/koin/domain/community/dto/ArticlesResponse.java
@@ -1,5 +1,8 @@
 package in.koreatech.koin.domain.community.dto;
 
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,14 +16,14 @@ import in.koreatech.koin.domain.community.model.Board;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ArticlesResponse(
-    @Schema(description = "게시글 목록")
+    @Schema(description = "게시글 목록", requiredMode = NOT_REQUIRED)
     List<InnerArticleResponse> articles,
 
-    @Schema(description = "게시판 정보")
+    @Schema(description = "게시판 정보", requiredMode = REQUIRED)
     InnerBoardResponse board,
 
-    @Schema(description = "총 페이지 수", example = "1")
-    Long totalPage
+    @Schema(description = "총 페이지 수", example = "1", requiredMode = REQUIRED)
+    long totalPage
 ) {
 
     public static ArticlesResponse of(List<Article> articles, Board board, Long totalPage) {
@@ -36,58 +39,58 @@ public record ArticlesResponse(
     @JsonNaming(value = SnakeCaseStrategy.class)
     private record InnerArticleResponse(
 
-        @Schema(description = "게시글 고유 ID", example = "1")
+        @Schema(description = "게시글 고유 ID", example = "1", requiredMode = REQUIRED)
         Integer id,
 
-        @Schema(description = "게시판 고유 ID", example = "1")
+        @Schema(description = "게시판 고유 ID", example = "1", requiredMode = REQUIRED)
         Integer boardId,
 
-        @Schema(description = "제목", example = "제목")
+        @Schema(description = "제목", example = "제목", requiredMode = REQUIRED)
         String title,
 
-        @Schema(description = "내용", example = "내용")
+        @Schema(description = "내용", example = "내용", requiredMode = REQUIRED)
         String content,
 
-        @Schema(description = "작성자 고유 ID", example = "1")
-        Integer userId,
+        @Schema(description = "작성자 고유 ID", example = "1", requiredMode = REQUIRED)
+        int userId,
 
-        @Schema(description = "작성자 닉네임", example = "닉네임")
+        @Schema(description = "작성자 닉네임", example = "닉네임", requiredMode = REQUIRED)
         String nickname,
 
-        @Schema(description = "조회수", example = "1")
-        Integer hit,
+        @Schema(description = "조회수", example = "1", requiredMode = REQUIRED)
+        int hit,
 
         @Schema(description = "IP 주소", example = "123.12.1.3")
         String ip,
 
         @Schema(description = "해결 여부", example = "false")
-        Boolean isSolved,
+        boolean isSolved,
 
         @Schema(description = "삭제 여부", example = "false")
-        Boolean isDeleted,
+        boolean isDeleted,
 
         @Schema(description = "댓글 수", example = "1")
-        Byte commentCount,
+        int commentCount,
 
-        @Schema(description = "메타 정보", example = "메타 정보")
+        @Schema(description = "메타 정보", example = "메타 정보", requiredMode = NOT_REQUIRED)
         String meta,
 
-        @Schema(description = "공지 여부", example = "false")
-        Boolean isNotice,
+        @Schema(description = "공지 여부", example = "false", requiredMode = REQUIRED)
+        boolean isNotice,
 
-        @Schema(description = "공지 게시글 고유 ID", example = "1")
-        Integer noticeArticleId,
+        @Schema(description = "공지 게시글 고유 ID", example = "1", requiredMode = REQUIRED)
+        int noticeArticleId,
 
-        @Schema(description = "요약", example = "요약")
+        @Schema(description = "요약", example = "요약", requiredMode = NOT_REQUIRED)
         String summary,
 
-        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01")
+        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
 
-        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01")
+        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt,
 
-        @Schema(description = "내용 요약", example = "내용 요약")
+        @Schema(description = "내용 요약", example = "내용 요약", requiredMode = NOT_REQUIRED)
         @JsonProperty("contentSummary") String contentSummary
     ) {
 
@@ -117,41 +120,43 @@ public record ArticlesResponse(
 
     @JsonNaming(value = SnakeCaseStrategy.class)
     public record InnerBoardResponse(
-        @Schema(description = "게시판 고유 ID", example = "1")
+        @Schema(description = "게시판 고유 ID", example = "1", requiredMode = REQUIRED)
         Integer id,
 
-        @Schema(description = "게시판 태그", example = "notice")
+        @Schema(description = "게시판 태그", example = "notice", requiredMode = NOT_REQUIRED)
         String tag,
 
-        @Schema(description = "게시판 명", example = "공지사항")
+        @Schema(description = "게시판 명", example = "공지사항", requiredMode = REQUIRED)
         String name,
 
-        @Schema(description = "익명 여부", example = "false")
+        @Schema(description = "익명 여부", example = "false", requiredMode = REQUIRED)
         boolean isAnonymous,
 
-        @Schema(description = "게시글 수", example = "1")
-        Integer articleCount,
+        @Schema(description = "게시글 수", example = "1", requiredMode = REQUIRED)
+        int articleCount,
 
-        @Schema(description = "삭제 여부", example = "false")
+        @Schema(description = "삭제 여부", example = "false", requiredMode = REQUIRED)
         boolean isDeleted,
 
-        @Schema(description = "공지 여부", example = "false")
+        @Schema(description = "공지 여부", example = "false", requiredMode = REQUIRED)
         boolean isNotice,
 
-        @Schema(description = "부모 게시판 고유 ID", example = "1")
+        @Schema(description = "부모 게시판 고유 ID", example = "1", requiredMode = NOT_REQUIRED)
         Integer parentId,
 
-        @Schema(description = "순서", example = "1")
-        Integer seq,
+        @Schema(description = "순서", example = "1", requiredMode = REQUIRED)
+        int seq,
 
-        @Schema(description = "하위 게시판 목록")
+        @Schema(description = "하위 게시판 목록", requiredMode = NOT_REQUIRED)
         List<InnerBoardResponse> children,
 
-        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
+        @Schema(description = "생성 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime createdAt,
 
-        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01")
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt
+        @Schema(description = "수정 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+        LocalDateTime updatedAt
     ) {
 
         public static InnerBoardResponse from(Board board) {
@@ -166,7 +171,9 @@ public record ArticlesResponse(
                 board.getParentId(),
                 board.getSeq(),
                 board.getChildren().isEmpty()
-                    ? null : board.getChildren().stream().map(InnerBoardResponse::from).toList(),
+                    ? null : board.getChildren().stream()
+                    .map(InnerBoardResponse::from)
+                    .toList(),
                 board.getCreatedAt(),
                 board.getUpdatedAt()
             );
