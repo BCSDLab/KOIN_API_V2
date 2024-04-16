95e3fd123c9f37455619486e6c880661cabee875
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-04-16 21:24:53 +0900

    feature: 리프레시 토큰 만료 기한을 14일에서 20초로 변경 (#431)

--- src/main/java/in/koreatech/koin/domain/user/model/UserToken.java
+++ src/main/java/in/koreatech/koin/domain/user/model/UserToken.java
@@ -1,7 +1,5 @@
 package in.koreatech.koin.domain.user.model;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
@@ -12,14 +10,14 @@ import lombok.Getter;
 @RedisHash("refreshToken")
 public class UserToken {
 
-    private static final long REFRESH_TOKEN_EXPIRE_DAY = 14L;
+    private static final long REFRESH_TOKEN_EXPIRE_SECONDS = 20L;
 
     @Id
     private Integer id;
 
     private final String refreshToken;
 
-    @TimeToLive(unit = TimeUnit.DAYS)
+    @TimeToLive
     private final Long expiration;
 
     private UserToken(Integer id, String refreshToken, Long expiration) {
@@ -29,6 +27,6 @@ public class UserToken {
     }
 
     public static UserToken create(Integer userId, String refreshToken) {
-        return new UserToken(userId, refreshToken, REFRESH_TOKEN_EXPIRE_DAY);
+        return new UserToken(userId, refreshToken, REFRESH_TOKEN_EXPIRE_SECONDS);
     }
 }
