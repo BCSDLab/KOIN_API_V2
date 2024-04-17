9ef23fa544ee60d4e4a854141a98a2fc0a939891
최준호 <junho5336@gmail.com>
2024-04-17 14:35:36 +0900

    fix: 사장님 이메일 중복요청시 재요청 시간제한 (#434)
    * refactor: 사장님 이메일 인증번호 만료시간 2시간
    
    * feat: 사장님 이메일 발송 시 제한시간 추가
    
    * test: stage 환경과 DB 버전 맞춤
    
    * test: redis, mongo 초기화 로직 추가
    
    * refactor: 만료시간 final 제거 및 로직 수정
    
    * test: 이메일 중복가입요청 테스트
    
    * refactor: 이름 변경
    
    * test: 버스 테스트 수정

--- src/main/java/in/koreatech/koin/domain/owner/model/OwnerInVerification.java
+++ src/main/java/in/koreatech/koin/domain/owner/model/OwnerInVerification.java
@@ -1,22 +1,31 @@
 package in.koreatech.koin.domain.owner.model;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
 
 import lombok.Getter;
 
 @Getter
-@RedisHash(value = "owner@", timeToLive = 60 * 60 * 2)
+@RedisHash(value = "owner@")
 public class OwnerInVerification {
+
+    private static final long CACHE_EXPIRE_HOUR = 2L;
 
     @Id
     private String email;
     private String certificationCode;
     private boolean isAuthed = false;
 
+    @TimeToLive(unit = TimeUnit.HOURS)
+    private Long expiration;
+
     public OwnerInVerification(String email, String certificationCode) {
         this.email = email;
         this.certificationCode = certificationCode;
+        this.expiration = CACHE_EXPIRE_HOUR;
     }
 
     public void verify() {
--- src/main/java/in/koreatech/koin/domain/owner/service/OwnerService.java
+++ src/main/java/in/koreatech/koin/domain/owner/service/OwnerService.java
@@ -18,11 +18,13 @@ import in.koreatech.koin.domain.owner.dto.OwnerVerifyRequest;
 import in.koreatech.koin.domain.owner.dto.OwnerVerifyResponse;
 import in.koreatech.koin.domain.owner.dto.VerifyEmailRequest;
 import in.koreatech.koin.domain.owner.exception.DuplicationCompanyNumberException;
+import in.koreatech.koin.domain.owner.model.EmailVerifyRequest;
 import in.koreatech.koin.domain.owner.model.Owner;
 import in.koreatech.koin.domain.owner.model.OwnerEmailRequestEvent;
 import in.koreatech.koin.domain.owner.model.OwnerInVerification;
 import in.koreatech.koin.domain.owner.model.OwnerRegisterEvent;
 import in.koreatech.koin.domain.owner.model.OwnerShop;
+import in.koreatech.koin.domain.owner.repository.EmailVerifyRequestRedisRepository;
 import in.koreatech.koin.domain.owner.repository.OwnerInVerificationRedisRepository;
 import in.koreatech.koin.domain.owner.repository.OwnerRepository;
 import in.koreatech.koin.domain.owner.repository.OwnerShopRedisRepository;
@@ -37,6 +39,7 @@ import in.koreatech.koin.global.domain.email.form.OwnerRegistrationData;
 import in.koreatech.koin.global.domain.email.service.MailService;
 import in.koreatech.koin.global.domain.random.model.CertificateNumberGenerator;
 import in.koreatech.koin.global.exception.DuplicationException;
+import in.koreatech.koin.global.exception.RequestTooFastException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -54,9 +57,13 @@ public class OwnerService {
     private final ApplicationEventPublisher eventPublisher;
     private final OwnerShopRedisRepository ownerShopRedisRepository;
     private final OwnerInVerificationRedisRepository ownerInVerificationRedisRepository;
+    private final EmailVerifyRequestRedisRepository emailVerifyRequestRedisRepository;
 
     @Transactional
     public void requestSignUpEmailVerification(VerifyEmailRequest request) {
+        emailVerifyRequestRedisRepository.findById(request.email()).ifPresent(it -> {
+            throw new RequestTooFastException("요청이 너무 빠릅니다. %d초 뒤에 다시 시도해주세요".formatted(it.getExpiration()));
+        });
         userRepository.findByEmail(request.email()).ifPresent(user -> {
             throw DuplicationEmailException.withDetail("email: " + request.email());
         });
@@ -67,6 +74,7 @@ public class OwnerService {
             certificationCode
         );
         ownerInVerificationRedisRepository.save(ownerInVerification);
+        emailVerifyRequestRedisRepository.save(new EmailVerifyRequest(request.email()));
         eventPublisher.publishEvent(new OwnerEmailRequestEvent(ownerInVerification.getEmail()));
     }
 
