86b7ef0d1fbc8de770e1def371af2b066553f831
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-04-18 15:58:09 +0900

    test : 버스 시간표 조회 v2 테스트 추가

--- src/test/java/in/koreatech/koin/acceptance/BusApiTest.java
+++ src/test/java/in/koreatech/koin/acceptance/BusApiTest.java
@@ -24,6 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import in.koreatech.koin.AcceptanceTest;
+import in.koreatech.koin.domain.bus.dto.BusTimetableResponse;
 import in.koreatech.koin.domain.bus.dto.SingleBusTimeResponse;
 import in.koreatech.koin.domain.bus.model.BusRemainTime;
 import in.koreatech.koin.domain.bus.model.city.CityBusArrival;
@@ -40,8 +41,10 @@ import in.koreatech.koin.domain.bus.model.mongo.Route;
 import in.koreatech.koin.domain.bus.repository.BusRepository;
 import in.koreatech.koin.domain.bus.repository.CityBusCacheRepository;
 import in.koreatech.koin.domain.bus.repository.ExpressBusCacheRepository;
+import in.koreatech.koin.domain.version.dto.VersionResponse;
 import in.koreatech.koin.domain.version.model.Version;
 import in.koreatech.koin.domain.version.repository.VersionRepository;
+import in.koreatech.koin.domain.version.service.VersionService;
 import in.koreatech.koin.support.JsonAssertions;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
@@ -61,6 +64,9 @@ class BusApiTest extends AcceptanceTest {
 
     @Autowired
     private ExpressBusCacheRepository expressBusCacheRepository;
+
+    @Autowired
+    private VersionService versionService;
 
     private final Instant UPDATED_AT = ZonedDateTime.parse(
             "2024-02-21 18:00:00 KST",
@@ -157,7 +163,6 @@ class BusApiTest extends AcceptanceTest {
             }
         );
     }
-
 
     @Test
     @DisplayName("다음 시내버스까지 남은 시간을 조회한다. - Redis")
@@ -519,6 +524,7 @@ class BusApiTest extends AcceptanceTest {
             .param("region", region)
             .get("/bus/timetable")
             .then()
+            .log().all()
             .statusCode(HttpStatus.OK.value())
             .extract();
 
@@ -546,6 +552,64 @@ class BusApiTest extends AcceptanceTest {
                         ]
                     }
                 ]
+                """);
+    }
+
+    @Test
+    @DisplayName("셔틀버스 시간표를 조회한다(업데이트 시각 포함).")
+    void getShuttleBusTimetableWithUpdatedAt() {
+        when(dateTimeProvider.getNow()).thenReturn(Optional.of(UPDATED_AT));
+
+        versionRepository.save(
+            Version.builder()
+                .version("20240_1712920946")
+                .type("shuttle_bus_timetable")
+                .build()
+        );
+
+        BusType busType = BusType.from("shuttle");
+        String direction = "from";
+        String region = "천안";
+
+        ExtractableResponse<Response> response = RestAssured
+            .given()
+            .when()
+            .param("bus_type", busType.name().toLowerCase())
+            .param("direction", direction)
+            .param("region", region)
+            .get("/bus/timetable/v2")
+            .then()
+            .log().all()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+
+        JsonAssertions.assertThat(response.asPrettyString())
+            .isEqualTo("""
+                {
+                    "bus_timetable": [
+                        {
+                            "route_name": "주중",
+                            "arrival_info": [
+                                {
+                                    "nodeName": "한기대",
+                                    "arrivalTime": "18:10"
+                                },
+                                {
+                                    "nodeName": "신계초,운전리,연춘리",
+                                    "arrivalTime": "정차"
+                                },
+                                {
+                                    "nodeName": "천안역(학화호두과자)",
+                                    "arrivalTime": "18:50"
+                                },{
+                                    "nodeName": "터미널(신세계 앞 횡단보도)",
+                                    "arrivalTime": "18:55"
+                                }
+                            ]
+                        }
+                    ],
+                    "updated_at": "2024-02-21T18:00:00"
+                }
                 """);
     }
 }
