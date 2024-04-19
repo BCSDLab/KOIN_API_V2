6811402685fc4a5e2f2d686498152b5047540897
HyeonsuLee <leehyeonsu4888@naver.com>
2024-04-19 17:20:57 +0900

    feat: 중복 데이터 검즘 stream사용

--- src/main/java/in/koreatech/koin/global/validation/UniqueIdValidator.java
+++ src/main/java/in/koreatech/koin/global/validation/UniqueIdValidator.java
@@ -1,8 +1,6 @@
 package in.koreatech.koin.global.validation;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -16,12 +14,6 @@ public class UniqueIdValidator implements ConstraintValidator<UniqueId, List<Int
 
     @Override
     public boolean isValid(List<Integer> elements, ConstraintValidatorContext context) {
-        Set<Integer> set = new HashSet<>(elements.size());
-        for (Integer element : elements) {
-            if (!set.add(element)) {
-                return false;
-            }
-        }
-        return true;
+        return elements.stream().distinct().count() == elements.size();
     }
 }
--- src/main/java/in/koreatech/koin/global/validation/UniqueUrlsValidator.java
+++ src/main/java/in/koreatech/koin/global/validation/UniqueUrlsValidator.java
@@ -1,8 +1,6 @@
 package in.koreatech.koin.global.validation;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
@@ -16,12 +14,6 @@ public class UniqueUrlsValidator implements ConstraintValidator<UniqueUrl, List<
 
     @Override
     public boolean isValid(List<String> elements, ConstraintValidatorContext context) {
-        Set<String> set = new HashSet<>(elements.size());
-        for (String element : elements) {
-            if (!set.add(element)) {
-                return false;
-            }
-        }
-        return true;
+        return elements.stream().distinct().count() == elements.size();
     }
 }
