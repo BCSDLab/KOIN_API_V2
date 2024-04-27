d7c946b36b280ad9b183afb7f82b17bdb17c40e0
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-04-28 01:30:19 +0900

    refactor : JsonCreator 추가

--- src/main/java/in/koreatech/koin/domain/dining/model/DiningType.java
+++ src/main/java/in/koreatech/koin/domain/dining/model/DiningType.java
@@ -3,21 +3,23 @@ package in.koreatech.koin.domain.dining.model;
 import java.time.LocalTime;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import lombok.Getter;
 
 @Getter
 public enum DiningType {
     BREAKFAST(Map.of("startTime", LocalTime.of(8, 30), "endTime", LocalTime.of(9, 30))),
     LUNCH(Map.of("startTime", LocalTime.of(11, 30), "endTime", LocalTime.of(13, 30))),
-    DINNER(Map.of("startTime", LocalTime.of(18, 30), "endTime", LocalTime.of(19, 30)));
-    ;
+    DINNER(Map.of("startTime", LocalTime.of(18, 30), "endTime", LocalTime.of(19, 30)));;
 
     private final Map<String, LocalTime> diningTime;
 
-    DiningType(Map<String, LocalTime> diningTime){
+    DiningType(Map<String, LocalTime> diningTime) {
         this.diningTime = diningTime;
     }
 
+    @JsonCreator
     public static Map<String, LocalTime> getDiningTime(String diningType) {
         return DiningType.valueOf(diningType.toUpperCase()).getDiningTime();
     }
