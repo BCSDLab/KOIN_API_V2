7b51f20d5dda061132df8cb6dac84cfdf8ae8a9d
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-04-30 11:09:45 +0900

    feat: 비밀번호 검증 api 작성 (#478)
    * feature: 패스워드 체크 controller, service dto 추가
    
    * chore: 컨트롤러 반환값 수정
    
    * chore: 반환값 정확하게 수정
    
    * fix: 영양사님도 허용 되게 swagger 변경
    
    * test: 테스트 코드 작성
    
    * chore: swagger에 표시되는 문구 변경
    
    * style: boolean이 아닌 상태를 반환하는 것으로 변경
    
    * test: 테스트 코드 변경 및 추가 작성
    
    * chore: 오타 수정
    
    * chore: 피드백 반영
    
    * fix: 조건문 수정
    
    * chore: 함수에 넣은 값 수정

--- src/test/java/in/koreatech/koin/acceptance/UserApiTest.java
+++ src/test/java/in/koreatech/koin/acceptance/UserApiTest.java
@@ -698,4 +698,46 @@ class UserApiTest extends AcceptanceTest {
             .then()
             .statusCode(HttpStatus.BAD_REQUEST.value());
     }
+
+    @Test
+    @DisplayName("사용자가 비밀번호를 통해 자신이 맞는지 인증한다.")
+    void userCheckPassword() {
+        Student student = userFixture.준호_학생();
+        String token = userFixture.getToken(student.getUser());
+
+        RestAssured
+            .given()
+            .header("Authorization", "Bearer " + token)
+            .contentType(ContentType.JSON)
+            .body("""
+                  {
+                    "password": "1234"
+                  }
+                """)
+            .when()
+            .post("/user/check/password")
+            .then()
+            .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("사용자가 비밀번호를 통해 자신이 맞는지 인증한다. - 비밀번호가 다르면 401 반환")
+    void userCheckPasswordInvalid() {
+        Student student = userFixture.준호_학생();
+        String token = userFixture.getToken(student.getUser());
+
+        RestAssured
+            .given()
+            .header("Authorization", "Bearer " + token)
+            .contentType(ContentType.JSON)
+            .body("""
+                  {
+                    "password": "1233"
+                  }
+                """)
+            .when()
+            .post("/user/check/password")
+            .then()
+            .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
 }
