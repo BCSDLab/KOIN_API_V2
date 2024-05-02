e09c7bf0dd9c55de68c3aca2722555a125878e6c
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-05-03 00:55:43 +0900

    chore : 저녁 식사시간 수정

--- src/main/java/in/koreatech/koin/domain/dining/model/DiningType.java
+++ src/main/java/in/koreatech/koin/domain/dining/model/DiningType.java
@@ -11,7 +11,7 @@ import lombok.Getter;
 public enum DiningType {
     BREAKFAST("아침", Map.of("startTime", LocalTime.of(8, 30), "endTime", LocalTime.of(9, 30))),
     LUNCH("점심", Map.of("startTime", LocalTime.of(11, 30), "endTime", LocalTime.of(13, 30))),
-    DINNER("저녁", Map.of("startTime", LocalTime.of(18, 30), "endTime", LocalTime.of(19, 30)));;
+    DINNER("저녁", Map.of("startTime", LocalTime.of(17, 30), "endTime", LocalTime.of(18, 30)));
 
     private final String label;
     private final Map<String, LocalTime> diningTime;
