4d130e97162081a724e05795cb1de02ba21f7d5d
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-05-06 19:55:27 +0900

    refactor : detail_type을 notification_subscribe에 병합

--- src/main/resources/db/migration/V13__add_notification_detail_subscribe_table.sql
+++ src/main/resources/db/migration/V13__add_notification_detail_subscribe_table.sql
@@ -1,11 +1,2 @@
-CREATE TABLE `notification_detail_subscribe`
-(
-    id             BIGINT AUTO_INCREMENT NOT NULL,
-    created_at     datetime              NOT NULL,
-    updated_at     datetime              NOT NULL,
-    subscribe_type VARCHAR(255)          NOT NULL,
-    detail_subscribe_type VARCHAR(255)          NOT NULL,
-    user_id        INT UNSIGNED          NOT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `FK_NOTIFICATION_DETAIL_SUBSCRIBE_ON_USER` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-);
+ALTER TABLE `notification_subscribe`
+    ADD COLUMN detail_type VARCHAR(255);
