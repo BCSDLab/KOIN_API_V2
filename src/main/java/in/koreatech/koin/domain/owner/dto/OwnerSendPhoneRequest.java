f057be4681efbbc8e73b954c66b5eea4a4840818
HyeonsuLee <leehyeonsu4888@naver.com>
2024-05-07 21:29:52 +0900

    feat: dto수정

--- src/main/java/in/koreatech/koin/domain/owner/dto/OwnerSendPhoneRequest.java
+++ src/main/java/in/koreatech/koin/domain/owner/dto/OwnerSendPhoneRequest.java
@@ -13,7 +13,7 @@ import jakarta.validation.constraints.NotBlank;
 public record OwnerSendPhoneRequest(
     @JsonProperty(value = "address")
     @NotBlank(message = "검증값은 필수입니다.")
-    @Schema(description = "검증값 (전화번호, 이메일)", example = "01012341234", requiredMode = REQUIRED)
+    @Schema(description = "검증값 (전화번호)", example = "01012341234", requiredMode = REQUIRED)
     String phoneNumber
 ) {
 
