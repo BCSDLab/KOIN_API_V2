c25f561fd2fa6d7b5608eb9869ad19018e756870
HyeonsuLee <leehyeonsu4888@naver.com>
2024-05-12 14:09:51 +0900

    refactor: dto전화번호 검증 자리수 삭제

--- src/main/java/in/koreatech/koin/domain/owner/dto/OwnerPhoneVerifyRequest.java
+++ src/main/java/in/koreatech/koin/domain/owner/dto/OwnerPhoneVerifyRequest.java
@@ -2,19 +2,19 @@ package in.koreatech.koin.domain.owner.dto;
 
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public record OwnerPhoneVerifyRequest(
-    @JsonProperty(value = "address")
-    @NotBlank(message = "검증값은 필수입니다.")
-    @Schema(description = "검증값 (이메일)", example = "01012341234", requiredMode = REQUIRED)
+    @NotBlank(message = "휴대폰번호는 필수입니다.")
+    @Schema(description = "휴대폰 번호", example = "010-0000-0000", requiredMode = REQUIRED)
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}", message = "전화번호 형식이 올바르지 않습니다.")
     String phoneNumber,
 
     @NotBlank(message = "인증 코드는 필수입니다.")
--- src/main/java/in/koreatech/koin/domain/owner/dto/OwnerSendPhoneRequest.java
+++ src/main/java/in/koreatech/koin/domain/owner/dto/OwnerSendPhoneRequest.java
@@ -8,12 +8,10 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public record OwnerSendPhoneRequest(
     @NotBlank(message = "휴대폰번호는 필수입니다.")
-    @Size(min = 10, max = 11, message = "휴대폰번호는 10자리에서 11자리 사이어야 합니다.")
     @Schema(description = "휴대폰 번호", example = "010-0000-0000", requiredMode = REQUIRED)
     @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}", message = "전화번호 형식이 올바르지 않습니다.")
     String phoneNumber
