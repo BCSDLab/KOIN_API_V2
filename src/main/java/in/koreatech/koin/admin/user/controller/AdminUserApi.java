fbae002a2176e18a86670e9c2347f85092361025
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-05-15 16:41:17 +0900

    refactor: 충돌 해결

--- src/main/java/in/koreatech/koin/admin/user/controller/AdminUserApi.java
+++ src/main/java/in/koreatech/koin/admin/user/controller/AdminUserApi.java
@@ -4,7 +4,6 @@ import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
--- src/main/java/in/koreatech/koin/admin/user/controller/AdminUserController.java
+++ src/main/java/in/koreatech/koin/admin/user/controller/AdminUserController.java
@@ -5,7 +5,6 @@ import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
