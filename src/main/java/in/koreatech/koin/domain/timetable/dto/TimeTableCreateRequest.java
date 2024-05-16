c5196d5f46a90c293792a4372c572a935367509e
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-05-16 15:48:10 +0900

    fix: 빌더 코드 삭제

--- src/main/java/in/koreatech/koin/domain/timetable/dto/TimeTableCreateRequest.java
+++ src/main/java/in/koreatech/koin/domain/timetable/dto/TimeTableCreateRequest.java
@@ -18,7 +18,6 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
 
 @JsonNaming(value = SnakeCaseStrategy.class)
 public record TimeTableCreateRequest(
