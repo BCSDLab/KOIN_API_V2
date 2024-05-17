b23f18597461f69c8a3e64b964c2dfe92ecf9823
김원경 <148550522+kwoo28@users.noreply.github.com>
2024-05-17 20:17:34 +0900

    feat: 가입 신청한 사장님 페이지네이션 조회(어드민계정) (#539)
    * feat: 응답객체 생성
    
    * feat: controller 생성
    
    * refactor: 접근제어 변경
    
    * feat: 요청 모델앤뷰 dto
    
    * feat: 응답 객체
    
    * feat: repository 생성
    
    * feat: shop_id와 shop_name이 있는 owner객체 생성
    
    * feat: service
    
    * feat: 테스트 추가
    
    * fix: 필요없는 코드 제거
    
    * refactor: required 관련 수정
    
    * refactor: 컨벤션 적용
    
    * refactor: 로직 수정
    
    * refactor: 로직 수정
    
    * refactor: AdminShopRepository 제거
    
    * refactor: controller api 수정
    
    * refactor: 충돌 해결
    
    * refactor: 충돌 해결
    
    * refactor: 주석 추가
    
    * refactor: 빌더 삭제 및 코드 개선
    
    * refactor: 요청dto requiredMode 전부 적용
    
    * refactor: 빌더 생성
    
    * refactor: Enum위치 변경 및 빌더 삭제
    
    * refactor: controller api implement 추가
    
    * refactor: dto 메서드 스웨거에서 숨기기

--- src/main/java/in/koreatech/koin/admin/user/controller/AdminUserController.java
+++ src/main/java/in/koreatech/koin/admin/user/controller/AdminUserController.java
@@ -4,15 +4,18 @@ import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import in.koreatech.koin.admin.user.dto.AdminNewOwnersResponse;
 import in.koreatech.koin.admin.user.dto.AdminOwnerResponse;
 import in.koreatech.koin.admin.user.dto.AdminStudentResponse;
 import in.koreatech.koin.admin.user.dto.AdminStudentUpdateRequest;
 import in.koreatech.koin.admin.user.dto.AdminStudentUpdateResponse;
+import in.koreatech.koin.admin.user.dto.NewOwnersCondition;
 import in.koreatech.koin.admin.user.service.AdminUserService;
 import in.koreatech.koin.global.auth.Auth;
 import jakarta.validation.Valid;
@@ -20,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-public class AdminUserController {
+public class AdminUserController implements AdminUserApi{
 
     private final AdminUserService adminUserService;
 
@@ -31,7 +34,7 @@ public class AdminUserController {
     ) {
         AdminStudentResponse adminStudentResponse = adminUserService.getStudent(id);
         return ResponseEntity.ok(adminStudentResponse);
-    };
+    }
 
     @PutMapping("/admin/users/student/{id}")
     public ResponseEntity<AdminStudentUpdateResponse> updateStudent(
@@ -44,11 +47,19 @@ public class AdminUserController {
     }
 
     @GetMapping("/admin/users/owner/{id}")
-    ResponseEntity<AdminOwnerResponse> getOwner(
+    public ResponseEntity<AdminOwnerResponse> getOwner(
         @PathVariable Integer id,
         @Auth(permit = {ADMIN}) Integer adminId
     ) {
         AdminOwnerResponse adminOwnerResponse = adminUserService.getOwner(id);
         return ResponseEntity.ok(adminOwnerResponse);
+    }
+
+    @GetMapping("/admin/users/new-owners")
+    public ResponseEntity<AdminNewOwnersResponse> getNewOwners(
+        @ModelAttribute NewOwnersCondition newOwnersCondition,
+        @Auth(permit = {ADMIN}) Integer adminId
+    ) {
+        return ResponseEntity.ok().body(adminUserService.getNewOwners(newOwnersCondition));
     }
 }
