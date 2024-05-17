938781ed54a1755f8ea52840f2a4c6bef8ac5e19
김원경 <148550522+kwoo28@users.noreply.github.com>
2024-05-17 20:17:34 +0900

    feat: 가입 신청한 사장님 페이지네이션 조회(어드민계정) (#539)
    * feat: 응답객체 생성
    
    * feat: controller 생성
    
    * refactor: 접근제어 변경
    
    * feat: 요청 모델앤뷰 dto
    
    * feat: 응답 객체
    
    * feat: repository 생성
    
    * feat: shop_id와 shop_name이 있는 owner객체 생성
    
    * feat: service
    
    * feat: 테스트 추가
    
    * fix: 필요없는 코드 제거
    
    * refactor: required 관련 수정
    
    * refactor: 컨벤션 적용
    
    * refactor: 로직 수정
    
    * refactor: 로직 수정
    
    * refactor: AdminShopRepository 제거
    
    * refactor: controller api 수정
    
    * refactor: 충돌 해결
    
    * refactor: 충돌 해결
    
    * refactor: 주석 추가
    
    * refactor: 빌더 삭제 및 코드 개선
    
    * refactor: 요청dto requiredMode 전부 적용
    
    * refactor: 빌더 생성
    
    * refactor: Enum위치 변경 및 빌더 삭제
    
    * refactor: controller api implement 추가
    
    * refactor: dto 메서드 스웨거에서 숨기기

---
+++ src/main/java/in/koreatech/koin/admin/user/dto/NewOwnersCondition.java
@@ -0,0 +1,87 @@
+package in.koreatech.koin.admin.user.dto;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+
+import java.util.Objects;
+
+import org.springframework.data.domain.Sort.Direction;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import in.koreatech.koin.global.exception.KoinIllegalArgumentException;
+import in.koreatech.koin.global.model.Criteria;
+import in.koreatech.koin.global.model.Criteria.Sort;
+import io.micrometer.common.util.StringUtils;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@JsonNaming(value = SnakeCaseStrategy.class)
+public record NewOwnersCondition(
+    @Schema(description = "페이지", example = "1", defaultValue = "1", requiredMode = NOT_REQUIRED)
+    Integer page,
+
+    @Schema(description = "페이지당 조회할 최대 개수", example = "10", defaultValue = "10", requiredMode = NOT_REQUIRED)
+    Integer limit,
+
+    @Schema(description = "검색 대상[`EMAIL` (이메일 검색), NAME` (이름 검색)]", example = "EMAIL", requiredMode = NOT_REQUIRED)
+    SearchType searchType,
+
+    @Schema(description = "검색 문자열", requiredMode = NOT_REQUIRED)
+    String query,
+
+    @Schema(description = "정렬 기준['CREATED_AT_ASC` (오래된순), 'CREATED_AT_DESC` (최신순)]", example = "CREATED_AT_ASC", defaultValue = "CREATED_AT_ASC", requiredMode = NOT_REQUIRED)
+    Sort sort
+) {
+    public NewOwnersCondition {
+        if (Objects.isNull(page)) {
+            page = Criteria.DEFAULT_PAGE;
+        }
+        if (Objects.isNull(limit)) {
+            limit = Criteria.DEFAULT_LIMIT;
+        }
+        if (Objects.isNull(sort)) {
+            sort = Sort.CREATED_AT_ASC;
+        }
+    }
+
+    public enum SearchType {
+        EMAIL,
+        NAME
+    }
+
+    public void checkDataConstraintViolation() {
+        if (this.query != null) {
+            checkSearchTypeNotNull();
+            checkQueryIsEmpty();
+            checkQueryIsBlank();
+        }
+    }
+
+    @Hidden
+    public Direction getDirection() {
+        if (this.sort == Sort.CREATED_AT_ASC) {
+            return Direction.ASC;
+        } else {
+            return Direction.DESC;
+        }
+    }
+
+    private void checkSearchTypeNotNull() {
+        if (this.searchType == null) {
+            throw new KoinIllegalArgumentException("검색 내용이 존재할 경우 검색 대상은 필수입니다.");
+        }
+    }
+
+    private void checkQueryIsEmpty() {
+        if (this.query.length() == 0) {
+            throw new KoinIllegalArgumentException("검색 내용의 최소 길이는 1입니다.");
+        }
+    }
+
+    private void checkQueryIsBlank() {
+        if (StringUtils.isBlank(this.query)) {
+            throw new KoinIllegalArgumentException("검색 내용은 공백 문자로만 이루어져 있으면 안됩니다.");
+        }
+    }
+}
--- src/test/java/in/koreatech/koin/admin/acceptance/AdminUserApiTest.java
+++ src/test/java/in/koreatech/koin/admin/acceptance/AdminUserApiTest.java
@@ -1,15 +1,24 @@
 package in.koreatech.koin.admin.acceptance;
 
+import static in.koreatech.koin.domain.user.model.UserGender.MAN;
+import static in.koreatech.koin.domain.user.model.UserType.OWNER;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import java.util.List;
+
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import in.koreatech.koin.AcceptanceTest;
+import in.koreatech.koin.admin.user.repository.AdminOwnerRepository;
 import in.koreatech.koin.admin.user.repository.AdminStudentRepository;
 import in.koreatech.koin.domain.owner.model.Owner;
+import in.koreatech.koin.domain.owner.model.OwnerAttachment;
 import in.koreatech.koin.domain.shop.model.Shop;
 import in.koreatech.koin.domain.user.model.Student;
 import in.koreatech.koin.domain.user.model.User;
@@ -27,6 +36,9 @@ public class AdminUserApiTest extends AcceptanceTest {
     private AdminStudentRepository adminStudentRepository;
 
     @Autowired
+    private AdminOwnerRepository adminOwnerRepository;
+
+    @Autowired
     private TransactionTemplate transactionTemplate;
 
     @Autowired
@@ -34,6 +46,9 @@ public class AdminUserApiTest extends AcceptanceTest {
 
     @Autowired
     private ShopFixture shopFixture;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Test
     @DisplayName("관리자가 특정 학생 정보를 조회한다. - 관리자가 아니면 403 반환")
@@ -195,5 +210,121 @@ public class AdminUserApiTest extends AcceptanceTest {
                 }
                 """, shop.getId()
             ));
+    }
+
+    @Test
+    @DisplayName("관리자가 가입 신청한 사장님 리스트 조회한다.")
+    void getNewOwnersAdmin() {
+        Owner unauthenticatedOwner = userFixture.철수_사장님();
+        Owner authenticatedOwner = userFixture.준영_사장님();
+
+        Shop shopA = shopFixture.마슬랜(unauthenticatedOwner);
+        Shop shopB = shopFixture.신전_떡볶이(unauthenticatedOwner);
+
+        User adminUser = userFixture.코인_운영자();
+        String token = userFixture.getToken(adminUser);
+
+        var response = RestAssured
+            .given()
+            .header("Authorization", "Bearer " + token)
+            .when()
+            .param("searchType", "NAME")
+            .param("query", "철수")
+            .param("sort", "CREATED_AT_DESC")
+            .get("/admin/users/new-owners")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+
+        JsonAssertions.assertThat(response.asPrettyString())
+            .isEqualTo(String.format("""
+                {
+                    "total_count": 2,
+                    "current_count": 2,
+                    "total_page": 1,
+                    "current_page": 1,
+                    "owners": [
+                        {
+                            "id": 1,
+                            "email": "testchulsu@gmail.com",
+                            "name": "테스트용_철수(인증X)",
+                            "phone_number": "010-9776-5112",
+                            "shop_id": 1,
+                            "shop_name": "마슬랜 치킨",
+                            "created_at" : "2024-01-15 12:00:00"
+                        },
+                        {
+                            "id": 1,
+                            "email": "testchulsu@gmail.com",
+                            "name": "테스트용_철수(인증X)",
+                            "phone_number": "010-9776-5112",
+                            "shop_id": 2,
+                            "shop_name": "신전 떡볶이",
+                            "created_at" : "2024-01-15 12:00:00"
+                        }
+                    ]
+                }
+                """
+            ));
+    }
+
+    @Test
+    @DisplayName("관리자가 가입 신청한 사장님 리스트 조회한다 - V2")
+    void getNewOwnersAdminV2() {
+        for (int i = 0; i < 11; i++) {
+            Owner request = Owner.builder()
+                .companyRegistrationNumber("118-80-567" + i)
+                .attachments(List.of(
+                        OwnerAttachment.builder()
+                            .url("https://test.com/사장님_인증사진_1" + i + ".jpg")
+                            .isDeleted(false)
+                            .build(),
+                        OwnerAttachment.builder()
+                            .url("https://test.com/사장님_인증사진_2" + i + ".jpg")
+                            .isDeleted(false)
+                            .build()
+                    )
+                )
+                .grantShop(true)
+                .grantEvent(true)
+                .user(
+                    User.builder()
+                        .password(passwordEncoder.encode("1234"))
+                        .nickname("사장님" + i)
+                        .name("테스트용(인증X)" + i)
+                        .phoneNumber("010-9776-511" + i)
+                        .userType(OWNER)
+                        .gender(MAN)
+                        .email("testchulsu@gmail.com" + i)
+                        .isAuthed(false)
+                        .isDeleted(false)
+                        .build()
+                )
+                .build();
+
+            adminOwnerRepository.save(request);
+        }
+
+        User adminUser = userFixture.코인_운영자();
+        String token = userFixture.getToken(adminUser);
+
+        var response = RestAssured
+            .given()
+            .header("Authorization", "Bearer " + token)
+            .when()
+            .get("/admin/users/new-owners")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .extract();
+
+        assertSoftly(
+            softly -> {
+                softly.assertThat(response.body().jsonPath().getInt("total_count")).isEqualTo(11);
+                softly.assertThat(response.body().jsonPath().getInt("current_count")).isEqualTo(10);
+                softly.assertThat(response.body().jsonPath().getInt("total_page")).isEqualTo(2);
+                softly.assertThat(response.body().jsonPath().getInt("current_page")).isEqualTo(1);
+                softly.assertThat(response.body().jsonPath().getList("owners").size()).isEqualTo(10);
+            }
+        );
     }
 }
