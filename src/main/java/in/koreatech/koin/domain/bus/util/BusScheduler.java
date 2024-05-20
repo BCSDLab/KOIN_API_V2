a6203aae13c12af951ac1c7acaaee88dd8851244
dradnats1012 <herjebi1012@gmail.com>
2024-05-20 23:54:24 +0900

    fix: openApi scheduled에서 try catch

--- src/main/java/in/koreatech/koin/domain/bus/util/BusScheduler.java
+++ src/main/java/in/koreatech/koin/domain/bus/util/BusScheduler.java
@@ -3,8 +3,11 @@ package in.koreatech.koin.domain.bus.util;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import in.koreatech.koin.domain.bus.exception.BusOpenApiException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BusScheduler {
@@ -14,12 +17,20 @@ public class BusScheduler {
 
     @Scheduled(cron = "0 */1 * * * *")
     public void cacheCityBusByOpenApi() {
-        cityBusOpenApiClient.storeRemainTimeByOpenApi();
+        try {
+            cityBusOpenApiClient.storeRemainTimeByOpenApi();
+        } catch (BusOpenApiException e) {
+            log.warn("버스 Open API 응답이 정상적이지 않습니다.");
+        }
     }
 
     // TODO: 시외버스 Open API 복구되면 주석 해제
 /*    @Scheduled(cron = "0 0 * * * *")
     public void cacheExpressBusByOpenApi() {
-        expressBusOpenApiClient.storeRemainTimeByOpenApi();
+        try {
+            expressBusOpenApiClient.storeRemainTimeByOpenApi();
+        } catch (BusOpenApiException e) {
+            log.warn("버스 Open API 응답이 정상적이지 않습니다.");
+        }
     }*/
 }
