1ca7bc898dd1b02f4010f47e6866eb3d61aa0a1e
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-05-24 20:42:31 +0900

    chore : 충돌해결

--- src/main/java/in/koreatech/koin/domain/bus/util/BusScheduler.java
+++ src/main/java/in/koreatech/koin/domain/bus/util/BusScheduler.java
@@ -4,7 +4,9 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BusScheduler {
