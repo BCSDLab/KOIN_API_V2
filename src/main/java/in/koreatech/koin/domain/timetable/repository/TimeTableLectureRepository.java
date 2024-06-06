d0aa1fa767b861649fc2e25ce3e11edd24f3dcac
seongjae6751 <seongjae6751@naver.com>
2024-06-07 00:07:37 +0900

    chore: 개행 추가

--- src/main/java/in/koreatech/koin/domain/timetable/repository/TimeTableLectureRepository.java
+++ src/main/java/in/koreatech/koin/domain/timetable/repository/TimeTableLectureRepository.java
@@ -8,6 +8,7 @@ import in.koreatech.koin.domain.timetable.model.TimeTableFrame;
 import in.koreatech.koin.domain.timetable.model.TimeTableLecture;
 
 public interface TimeTableLectureRepository extends Repository<TimeTableLecture, Integer> {
+
     TimeTableLecture save(TimeTableLecture lecture);
 
     List<TimeTableLecture> findAllByTimetableFrame(TimeTableFrame frame);
