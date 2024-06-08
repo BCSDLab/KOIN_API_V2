1bc1c62d28df38bbde712cd3c98aeb91c0568742
Jang Jun Young <police0022@naver.com>
2024-06-08 20:05:26 +0900

    feat: AdminLandApi 구현

--- src/main/java/in/koreatech/koin/admin/land/controller/AdminLandApi.java
+++ src/main/java/in/koreatech/koin/admin/land/controller/AdminLandApi.java
@@ -3,7 +3,9 @@ package in.koreatech.koin.admin.land.controller;
 import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -55,6 +57,22 @@ public interface AdminLandApi {
     ResponseEntity<AdminLandsResponse> postLands(
         @RequestBody @Valid AdminLandsRequest adminLandsRequest,
         @Auth(permit = {ADMIN}) Integer adminId
+    );
+
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "201"),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(hidden = true))),
+        }
+    )
+    @Operation(summary = "복덕방 삭제")
+    @SecurityRequirement(name = "Jwt Authentication")
+    @DeleteMapping("/admin/lands/{id}")
+    ResponseEntity<Void> deleteLand(
+        @PathVariable("id") Integer id
+        //@Auth(permit = {ADMIN}) Integer adminId
     );
 
 }
