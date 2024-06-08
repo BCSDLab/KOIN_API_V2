ad5949f743c56218c28dd6a182c48d4bd69011d8
Jang Jun Young <police0022@naver.com>
2024-06-08 20:05:51 +0900

    feat: AdminLandController

--- src/main/java/in/koreatech/koin/admin/land/controller/AdminLandController.java
+++ src/main/java/in/koreatech/koin/admin/land/controller/AdminLandController.java
@@ -4,7 +4,9 @@ import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,6 +42,15 @@ public class AdminLandController implements AdminLandApi {
     ) {
         adminLandService.createLands(adminLandsRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/admin/lands/{id}")
+    public ResponseEntity<Void> deleteLand(
+        @PathVariable("id") Integer id
+        //@Auth(permit = {ADMIN}) Integer adminId
+    ) {
+        adminLandService.deleteLand(id);
+        return null;
     }
 
 }
