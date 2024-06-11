8e93c5d72b3103b1e41d7d332090a8e949d81aa2
YunYongWoon <46861704+YunYongWoon@users.noreply.github.com>
2024-06-11 23:28:03 +0900

    feat : put /admin/members/{id} api 추가 (#595)
    * feat : put /admin/members/{id} api 추가
    
    * fix: trackName 체크 메서드 생성
    
    * fix: track 변경 체크 로직 및 track 수정 로직 수정

--- src/main/java/in/koreatech/koin/admin/member/controller/AdminMemberController.java
+++ src/main/java/in/koreatech/koin/admin/member/controller/AdminMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,7 +48,6 @@ public class AdminMemberController implements AdminMemberApi {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-
     @GetMapping("/admin/members/{id}")
     public ResponseEntity<AdminMemberResponse> getMember(
         @PathVariable("id") Integer memberId,
@@ -62,6 +62,16 @@ public class AdminMemberController implements AdminMemberApi {
         @Auth(permit = {ADMIN}) Integer adminId
     ) {
         adminMemberService.deleteMember(memberId);
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @PutMapping("/admin/members/{id}")
+    public ResponseEntity<Void> updateMember(
+        @PathVariable("id") Integer memberId,
+        @RequestBody @Valid AdminMemberRequest request,
+        @Auth(permit = {ADMIN}) Integer adminId
+    ) {
+        adminMemberService.updateMember(memberId, request);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
