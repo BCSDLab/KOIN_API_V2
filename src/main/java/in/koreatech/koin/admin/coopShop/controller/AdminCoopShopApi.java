0fd714debbd6a0bd062a5229748c76dfdd17de77
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-06-26 16:24:40 +0900

    feature : AdminCoopShop 생협 매장 조회 API 구현

---
+++ src/main/java/in/koreatech/koin/admin/coopShop/controller/AdminCoopShopApi.java
@@ -0,0 +1,56 @@
+package in.koreatech.koin.admin.coopShop.controller;
+
+import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+import static io.swagger.v3.oas.annotations.enums.ParameterIn.PATH;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import in.koreatech.koin.admin.coopShop.dto.AdminCoopShopsResponse;
+import in.koreatech.koin.domain.coopshop.dto.CoopShopResponse;
+import in.koreatech.koin.global.auth.Auth;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "(ADMIN) AdminCoopShop : 생협 매장 정보", description = "생협 매장 정보 조회 페이지")
+public interface AdminCoopShopApi {
+
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true)))
+        }
+    )
+    @Operation(summary = "모든 생협 매장 정보 조회")
+    @GetMapping("/admin/coop")
+    ResponseEntity<AdminCoopShopsResponse> getCoopsShops(
+        @RequestParam(name = "page", defaultValue = "1") Integer page,
+        @RequestParam(name = "limit", defaultValue = "10", required = false) Integer limit,
+        @RequestParam(name = "is_deleted", defaultValue = "false") Boolean isDeleted,
+        @Auth(permit = {ADMIN}) Integer adminId
+    );
+
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true)))
+        }
+    )
+    @Operation(summary = "특정 생협 매장 정보 조회")
+    @GetMapping("/admin/coop/{coopId}")
+    ResponseEntity<CoopShopResponse> getCoopShop(
+        @Auth(permit = {ADMIN}) Integer adminId,
+        @Parameter(in = PATH) @PathVariable Integer coopId
+    );
+}
---
+++ src/main/java/in/koreatech/koin/admin/coopShop/controller/AdminCoopShopController.java
@@ -0,0 +1,43 @@
+package in.koreatech.koin.admin.coopShop.controller;
+
+import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.koreatech.koin.admin.coopShop.dto.AdminCoopShopsResponse;
+import in.koreatech.koin.admin.coopShop.service.AdminCoopShopService;
+import in.koreatech.koin.domain.coopshop.dto.CoopShopResponse;
+import in.koreatech.koin.global.auth.Auth;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class AdminCoopShopController implements AdminCoopShopApi {
+
+    private final AdminCoopShopService adminCoopShopService;
+
+    @GetMapping("/admin/coop")
+    public ResponseEntity<AdminCoopShopsResponse> getCoopsShops(
+        @RequestParam(name = "page", defaultValue = "1") Integer page,
+        @RequestParam(name = "limit", defaultValue = "10", required = false) Integer limit,
+        @RequestParam(name = "is_deleted", defaultValue = "false") Boolean isDeleted,
+        @Auth(permit = {ADMIN}) Integer adminId
+    ) {
+        AdminCoopShopsResponse coopShops = adminCoopShopService.getCoopsShops(page, limit, isDeleted);
+        return ResponseEntity.ok(coopShops);
+    }
+
+    @GetMapping("/admin/coop/{coopId}")
+    public ResponseEntity<CoopShopResponse> getCoopShop(
+        @Auth(permit = {ADMIN}) Integer adminId,
+        @PathVariable Integer coopId
+    ) {
+        CoopShopResponse coopShop = adminCoopShopService.getCoopShop(coopId);
+        return ResponseEntity.ok(coopShop);
+    }
+
+}
