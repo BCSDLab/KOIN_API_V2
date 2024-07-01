aec0227debbac0d9ac6a4508ddfecb1c117f278d
kwoo28 <148550522+kwoo28@users.noreply.github.com>
2024-07-01 20:46:39 +0900

    fix: 이모지 검증 수정

--- src/main/java/in/koreatech/koin/global/validation/EmojiValidator.java
+++ src/main/java/in/koreatech/koin/global/validation/EmojiValidator.java
@@ -1,5 +1,6 @@
 package in.koreatech.koin.global.validation;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Component;
@@ -10,20 +11,7 @@ import jakarta.validation.ConstraintValidatorContext;
 @Component
 public class EmojiValidator implements ConstraintValidator<NotEmoji, String> {
 
-    private static final Pattern EMOJI_PATTERN = Pattern.compile(
-        "[\\uD83C-\\uDBFF\\uDC00-\\uDFFF]|" +     // 서플리먼터리
-            "[\\u2600-\\u26FF]|" +                // Miscellaneous Symbols
-            "[\\u2700-\\u27BF]|" +                // Dingbats
-            "[\\u1F300-\\u1F5FF]|" +              // Miscellaneous Symbols and Pictographs
-            "[\\u1F600-\\u1F64F]|" +              // Emoticons
-            "[\\u1F680-\\u1F6FF]|" +              // Transport and Map Symbols
-            "[\\u1F700-\\u1F77F]|" +              // Alchemical Symbols
-            "[\\u1F780-\\u1F7FF]|" +              // Geometric Shapes Extended
-            "[\\u1F800-\\u1F8FF]|" +              // Supplemental Arrows-C
-            "[\\u1F900-\\u1F9FF]|" +              // Supplemental Symbols and Pictographs
-            "[\\u1FA00-\\u1FA6F]|" +              // Chess Symbols
-            "[\\u1FA70-\\u1FAFF]"                 // Symbols and Pictographs Extended-A
-    );
+    private static final Pattern EMOJI_PATTERN = Pattern.compile("[\\uD83C-\\uDBFF\\uDC00-\\uDFFF]+");
 
     @Override
     public void initialize(NotEmoji constraintAnnotation) {
@@ -32,14 +20,9 @@ public class EmojiValidator implements ConstraintValidator<NotEmoji, String> {
 
     @Override
     public boolean isValid(String field, ConstraintValidatorContext constraintValidatorContext) {
-        for (int i = 0; i < field.length(); i++) {
-            int codePoint = field.codePointAt(i);
-            if (Character.isSupplementaryCodePoint(codePoint)) {
-                i++;
-            }
-            if (isEmoji(codePoint)) {
-                return false;
-            }
+        Matcher emojiMatcher = EMOJI_PATTERN.matcher(field);
+        if (emojiMatcher.find()) {
+            return false;
         }
         return true;
     }
