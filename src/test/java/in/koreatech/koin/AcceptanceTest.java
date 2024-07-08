586247d282caef4741da68514872d918f1be6b77
김원경 <kyg279044@gmail.com>
2024-07-09 02:21:11 +0900

    feat: 설정 초기화

--- src/test/java/in/koreatech/koin/AcceptanceTest.java
+++ src/test/java/in/koreatech/koin/AcceptanceTest.java
@@ -12,6 +12,12 @@ import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
 
 import in.koreatech.koin.config.TestJpaConfiguration;
 import in.koreatech.koin.config.TestTimeConfig;
@@ -30,6 +36,9 @@ import jakarta.persistence.EntityManager;
 @Import({DBInitializer.class, TestJpaConfiguration.class, TestTimeConfig.class})
 @ActiveProfiles("test")
 public abstract class AcceptanceTest {
+
+    private static final String ROOT = "test";
+    private static final String ROOT_PASSWORD = "1234";
 
     @LocalServerPort
     protected int port;
@@ -63,6 +72,47 @@ public abstract class AcceptanceTest {
 
     @Autowired
     protected Clock clock;
+
+    @Container
+    protected static MySQLContainer mySqlContainer;
+
+    @Container
+    protected static GenericContainer<?> redisContainer;
+
+    @Container
+    protected static GenericContainer<?> mongoContainer;
+
+    @DynamicPropertySource
+    private static void configureProperties(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mySqlContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", () -> ROOT);
+        registry.add("spring.datasource.password", () -> ROOT_PASSWORD);
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379).toString());
+        registry.add("spring.data.mongodb.host", mongoContainer::getHost);
+        registry.add("spring.data.mongodb.port", () -> mongoContainer.getMappedPort(27017).toString());
+        registry.add("spring.data.mongodb.database", () -> "test");
+    }
+
+    static {
+        mySqlContainer = (MySQLContainer)new MySQLContainer("mysql:8.0.29")
+                .withDatabaseName("test")
+                .withUsername(ROOT)
+                .withPassword(ROOT_PASSWORD)
+                .withCommand("--character-set-server=utf8mb4", "--collation-server=utf8mb4_unicode_ci");
+
+        redisContainer = new GenericContainer<>(
+                DockerImageName.parse("redis:7.0.9"))
+                .withExposedPorts(6379);
+
+        mongoContainer = new GenericContainer<>(
+                DockerImageName.parse("mongo:6.0.14"))
+                .withExposedPorts(27017);
+
+        mySqlContainer.start();
+        redisContainer.start();
+        mongoContainer.start();
+    }
 
     @BeforeEach
     void delete() {
