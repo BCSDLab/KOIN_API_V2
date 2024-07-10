a5d8bf665fcc6a289f63c15d84847fe31ba18e76
김원경 <kyg279044@gmail.com>
2024-07-11 04:17:22 +0900

    feat: 테스트용 RedisConfig 설정

--- src/test/java/in/koreatech/koin/KoinApplicationTest.java
+++ src/test/java/in/koreatech/koin/KoinApplicationTest.java
@@ -3,6 +3,7 @@ package in.koreatech.koin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -11,7 +12,7 @@ import in.koreatech.koin.config.TestTimeConfig;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@EnableRedisRepositories
+@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @Import({TestJpaConfiguration.class, TestTimeConfig.class})
 class KoinApplicationTest {
 
