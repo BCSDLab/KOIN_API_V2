43500d78224d6a4642b3236a3e52d6dd9d09bf45
김원경 <kyg279044@gmail.com>
2024-07-11 03:01:41 +0900

    feat: 테스트용 RedisConfig 설정

--- src/main/java/in/koreatech/koin/global/config/RedisConfig.java
+++ src/main/java/in/koreatech/koin/global/config/RedisConfig.java
@@ -7,7 +7,6 @@ import java.time.Duration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -22,7 +21,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
-@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
+@EnableRedisRepositories(basePackages = {"in.koreatech.koin.domain.user.repository"}, enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
     @Bean
