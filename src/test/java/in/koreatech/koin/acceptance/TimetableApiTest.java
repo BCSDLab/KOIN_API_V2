444d533f3d799b88e59e3742d55bccdcbd560451
seongjae6751 <seongjae6751@naver.com>
2024-07-13 16:22:27 +0900

    feat: 시간표 생성시 전체 반환

--- src/test/java/in/koreatech/koin/acceptance/TimetableApiTest.java
+++ src/test/java/in/koreatech/koin/acceptance/TimetableApiTest.java
@@ -412,6 +412,124 @@ class TimetableApiTest extends AcceptanceTest {
     }
 
     @Test
+    @DisplayName("시간표를 단일 생성한다. - 전체 반환")
+    void createTimeTablesReturnAll() {
+        User user = userFixture.준호_학생().getUser();
+        String token = userFixture.getToken(user);
+        Semester semester = semesterFixture.semester("20192");
+
+        lectureFixture.건축구조의_이해_및_실습(semester.getSemester());
+        lectureFixture.HRD_개론(semester.getSemester());
+
+        timetableV2Fixture.시간표1(user, semester);
+
+        var response1 = RestAssured
+            .given()
+            .header("Authorization", "Bearer " + token)
+            .contentType(ContentType.JSON)
+            .body("""
+            {
+              "timetable": [
+               {
+                    "regular_number": "25",
+                    "code": "ARB244",
+                    "design_score": "0",
+                    "class_time": [200, 201, 202, 203, 204, 205, 206, 207],
+                    "class_place": null,
+                    "memo": null,
+                    "grades": "3",
+                    "class_title": "건축구조의 이해 및 실습",
+                    "lecture_class": "01",
+                    "target": "디자 1 건축",
+                    "professor": "황현식",
+                    "department": "디자인ㆍ건축공학부"
+               }
+             ],
+              "semester": "20192"
+            }
+         """)
+            .when()
+            .post("/timetables")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .extract()
+            .response();
+
+        var response2 = RestAssured
+            .given()
+            .header("Authorization", "Bearer " + token)
+            .contentType(ContentType.JSON)
+            .body("""
+            {
+              "timetable": [
+               {
+                    "regular_number": "22",
+                    "code": "BSM590",
+                    "design_score": "0",
+                    "class_time": [12, 13, 14, 15, 210, 211, 212, 213],
+                    "class_place": null,
+                    "memo": null,
+                    "grades": "3",
+                    "class_title": "컴퓨팅사고",
+                    "lecture_class": "06",
+                    "target": "기공1",
+                    "professor": "박한수,최준호",
+                    "department": "기계공학부"
+               }
+             ],
+              "semester": "20192"
+            }
+         """)
+            .when()
+            .post("/timetables")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .extract()
+            .response();
+
+        JsonAssertions.assertThat(response2.asPrettyString())
+            .isEqualTo("""
+        {
+            "semester": "20192",
+            "timetable": [
+                {
+                    "id": 1,
+                    "regular_number": "25",
+                    "code": "ARB244",
+                    "design_score": "0",
+                    "class_time": [200, 201, 202, 203, 204, 205, 206, 207],
+                    "class_place": null,
+                    "memo": null,
+                    "grades": "3",
+                    "class_title": "건축구조의 이해 및 실습",
+                    "lecture_class": "01",
+                    "target": "디자 1 건축",
+                    "professor": "황현식",
+                    "department": "디자인ㆍ건축공학부"
+                },
+                {
+                    "id": 2,
+                    "regular_number": "22",
+                    "code": "BSM590",
+                    "design_score": "0",
+                    "class_time": [12, 13, 14, 15, 210, 211, 212, 213],
+                    "class_place": null,
+                    "memo": null,
+                    "grades": "3",
+                    "class_title": "컴퓨팅사고",
+                    "lecture_class": "06",
+                    "target": "기공1",
+                    "professor": "박한수,최준호",
+                    "department": "기계공학부"
+                }
+            ],
+            "grades": 6,
+            "total_grades": 6
+        }
+        """);
+    }
+
+    @Test
     @DisplayName("시간표를 삭제한다.")
     void deleteTimetable() {
         User user = userFixture.준호_학생().getUser();
