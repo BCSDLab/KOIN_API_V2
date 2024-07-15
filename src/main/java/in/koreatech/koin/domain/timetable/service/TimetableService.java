cd3fcd8979de06be0b4c17d585603376171a6e01
seongjae6751 <seongjae6751@naver.com>
2024-07-15 16:20:16 +0900

    chore: 충돌중 잘못 합쳐진 것 수정

--- src/main/java/in/koreatech/koin/domain/timetable/service/TimetableService.java
+++ src/main/java/in/koreatech/koin/domain/timetable/service/TimetableService.java
@@ -15,10 +15,10 @@ import in.koreatech.koin.domain.timetable.dto.TimetableUpdateRequest;
 import in.koreatech.koin.domain.timetable.exception.SemesterNotFoundException;
 import in.koreatech.koin.domain.timetable.model.Lecture;
 import in.koreatech.koin.domain.timetable.model.Semester;
-import in.koreatech.koin.domain.timetable.repository.LectureRepository;
-import in.koreatech.koin.domain.timetable.repository.SemesterRepository;
 import in.koreatech.koin.domain.timetableV2.model.TimetableFrame;
 import in.koreatech.koin.domain.timetableV2.model.TimetableLecture;
+import in.koreatech.koin.domain.timetableV2.repository.LectureRepositoryV2;
+import in.koreatech.koin.domain.timetableV2.repository.SemesterRepositoryV2;
 import in.koreatech.koin.domain.timetableV2.repository.TimetableFrameRepositoryV2;
 import in.koreatech.koin.domain.timetableV2.repository.TimetableLectureRepositoryV2;
 import in.koreatech.koin.domain.user.model.User;
@@ -31,14 +31,14 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class TimetableService {
 
-    private final LectureRepository lectureRepository;
+    private final LectureRepositoryV2 lectureRepositoryV2;
     private final TimetableLectureRepositoryV2 timetableLectureRepositoryV2;
     private final TimetableFrameRepositoryV2 timetableFrameRepositoryV2;
-    private final SemesterRepository semesterRepository;
+    private final SemesterRepositoryV2 semesterRepositoryV2;
     private final UserRepository userRepository;
 
     public List<LectureResponse> getLecturesBySemester(String semester) {
-        List<Lecture> lectures = lectureRepository.findBySemester(semester);
+        List<Lecture> lectures = lectureRepositoryV2.findBySemester(semester);
         if (lectures.isEmpty()) {
             throw SemesterNotFoundException.withDetail(semester);
         }
@@ -49,13 +49,13 @@ public class TimetableService {
 
     @Transactional
     public TimetableResponse createTimetables(Integer userId, TimetableCreateRequest request) {
-        Semester semester = semesterRepository.getBySemester(request.semester());
+        Semester semester = semesterRepositoryV2.getBySemester(request.semester());
         List<TimetableLecture> timetableLectures = new ArrayList<>();
         TimetableFrame timetableFrame = timetableFrameRepositoryV2.getMainTimetableByUserIdAndSemesterId(userId,
                 semester.getId());
 
         for (TimetableCreateRequest.InnerTimetableRequest timeTable : request.timetable()) {
-            Lecture lecture = lectureRepository.getBySemesterAndCodeAndLectureClass(request.semester(),
+            Lecture lecture = lectureRepositoryV2.getBySemesterAndCodeAndLectureClass(request.semester(),
                     timeTable.code(), timeTable.lectureClass());
             TimetableLecture timetableLecture = TimetableLecture.builder()
                     .classPlace(timeTable.classPlace())
@@ -73,7 +73,7 @@ public class TimetableService {
 
     @Transactional
     public TimetableResponse updateTimetables(Integer userId, TimetableUpdateRequest request) {
-        Semester semester = semesterRepository.getBySemester(request.semester());
+        Semester semester = semesterRepositoryV2.getBySemester(request.semester());
         TimetableFrame timetableFrame = timetableFrameRepositoryV2.getMainTimetableByUserIdAndSemesterId(userId,
                 semester.getId());
         for (TimetableUpdateRequest.InnerTimetableRequest timetableRequest : request.timetable()) {
@@ -86,7 +86,7 @@ public class TimetableService {
 
     @Transactional
     public TimetableResponse getTimetables(Integer userId, String semesterRequest) {
-        Semester semester = semesterRepository.getBySemester(semesterRequest);
+        Semester semester = semesterRepositoryV2.getBySemester(semesterRequest);
         User user = userRepository.getById(userId);
 
         Optional<TimetableFrame> timetableFrame = timetableFrameRepositoryV2.findByUserIdAndSemesterIdAndIsMainTrue(userId,
