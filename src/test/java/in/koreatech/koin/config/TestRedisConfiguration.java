d436385d09fa74bf37a4bf737c8c8de063a2adf7
김원경 <kyg279044@gmail.com>
2024-07-15 13:40:40 +0900

    feat: RedisConfig 설정

--- src/test/java/in/koreatech/koin/config/TestRedisConfiguration.java
+++ src/test/java/in/koreatech/koin/config/TestRedisConfiguration.java
@@ -2,19 +2,12 @@ package in.koreatech.koin.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisKeyValueAdapter;
-import org.springframework.data.redis.core.RedisKeyValueTemplate;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.mapping.RedisMappingContext;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.StringHttpMessageConverter;
@@ -33,7 +26,6 @@ public class TestRedisConfiguration {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(mapper);
-        template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(serializer);
         template.setConnectionFactory(connectionFactory);
         return template;
@@ -46,12 +38,5 @@ public class TestRedisConfiguration {
                 setConnectTimeout(Duration.ofMillis(5000))
                 .setReadTimeout(Duration.ofMillis(5000))
                 .additionalMessageConverters(new StringHttpMessageConverter(UTF_8)).build();
-    }
-
-    @Bean
-    public RedisKeyValueTemplate redisKeyValueTemplate(RedisTemplate redisTemplate) {
-        RedisMappingContext mappingContext = new RedisMappingContext();
-        RedisKeyValueAdapter adapter = new RedisKeyValueAdapter(redisTemplate);
-        return new RedisKeyValueTemplate(adapter, mappingContext);
     }
 }
