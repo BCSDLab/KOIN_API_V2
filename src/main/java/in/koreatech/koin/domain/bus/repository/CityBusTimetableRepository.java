5f07e9ddb55bf05f35e80ba2436022fa815c458c
박성빈 <psb106305@gmail.com>
2024-07-19 19:15:46 +0900

    refactor: repository 코드 정리

--- src/main/java/in/koreatech/koin/domain/bus/repository/CityBusTimetableRepository.java
+++ src/main/java/in/koreatech/koin/domain/bus/repository/CityBusTimetableRepository.java
@@ -1,6 +1,5 @@
 package in.koreatech.koin.domain.bus.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.Repository;
@@ -9,15 +8,9 @@ import in.koreatech.koin.domain.bus.exception.BusCacheNotFoundException;
 import in.koreatech.koin.domain.bus.model.mongo.CityBusTimetable;
 
 public interface CityBusTimetableRepository extends Repository<CityBusTimetable, String> {
-    List<CityBusTimetable> findAll();
-
-    Optional<CityBusTimetable> findByRouteId(String routeId);
+    CityBusTimetable save(CityBusTimetable cityBusTimetable);
 
     Optional<CityBusTimetable> findByBusInfoNumberAndBusInfoArrival(Long number, String arrivalNode);
-
-    default CityBusTimetable getByRouteId(String routeId) {
-        return findByRouteId(routeId).orElseThrow(() -> BusCacheNotFoundException.withDetail("routeId: " + routeId));
-    }
 
     default CityBusTimetable getByBusInfoNumberAndBusInfoArrival(Long number, String arrivalNode) {
         return findByBusInfoNumberAndBusInfoArrival(number, arrivalNode)
