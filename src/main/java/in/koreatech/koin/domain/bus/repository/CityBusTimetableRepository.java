ad532f4e96bff7cb9510d005a9aca4c3c0a7b2e2
박성빈 <psb106305@gmail.com>
2024-07-19 20:45:40 +0900

    style: 개행 추가

--- src/main/java/in/koreatech/koin/domain/bus/repository/CityBusTimetableRepository.java
+++ src/main/java/in/koreatech/koin/domain/bus/repository/CityBusTimetableRepository.java
@@ -8,6 +8,7 @@ import in.koreatech.koin.domain.bus.exception.BusCacheNotFoundException;
 import in.koreatech.koin.domain.bus.model.mongo.CityBusTimetable;
 
 public interface CityBusTimetableRepository extends Repository<CityBusTimetable, String> {
+
     CityBusTimetable save(CityBusTimetable cityBusTimetable);
 
     Optional<CityBusTimetable> findByBusInfoNumberAndBusInfoArrival(Long number, String arrivalNode);
