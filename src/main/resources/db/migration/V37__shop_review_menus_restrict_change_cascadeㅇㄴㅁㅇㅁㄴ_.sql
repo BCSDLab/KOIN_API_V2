b15860c36e278cdcdebe8c21b84fbfc9e3f4315e
HyeonsuLee <leehyeonsu4888@naver.com>
2024-07-23 15:36:51 +0900

    feat: 충돌 확인을 위한 sql파일 제거

--- src/main/resources/db/migration/V37__shop_review_menus_restrict_change_cascadeㅇㄴㅁㅇㅁㄴ_.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `koin`.`shop_review_menus`
-    DROP FOREIGN KEY `shop_review_menus_ibfk_1`;
-
-ALTER TABLE `koin`.`shop_review_menus`
-    ADD CONSTRAINT `shop_review_menus_ibfk_1`
-        FOREIGN KEY (`review_id`)
-        REFERENCES `koin`.`shop_reviews` (`id`)
-        ON DELETE CASCADE;
