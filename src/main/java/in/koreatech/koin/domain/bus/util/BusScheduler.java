312596e42ef34087e8f076a70c7b96f02c6df87b
춘식 <142300831+Choon0414@users.noreply.github.com>
2024-08-05 10:16:47 +0900

    refactor : CallController -> CallControllable 인터페이스로 수정

--- src/main/java/in/koreatech/koin/domain/bus/util/BusScheduler.java
+++ src/main/java/in/koreatech/koin/domain/bus/util/BusScheduler.java
@@ -1,11 +1,8 @@
 package in.koreatech.koin.domain.bus.util;
 
-import java.util.List;
-
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import in.koreatech.koin.global.domain.callcontoller.CallController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,8 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class BusScheduler {
 
-    private final List<ExpressBusClient<?, ?>> expressBusClients;
-    private final CallController callController;
+    private final ExpressBusFactory expressBusFactory;
     private final CityBusClient cityBusClient;
     private final CityBusRouteClient cityBusRouteClient;
 
@@ -32,7 +28,7 @@ public class BusScheduler {
     @Scheduled(cron = "0 30 0 * * *")
     public void cacheExpressBusByOpenApi() {
         try {
-            callController.selectCallApi(expressBusClients).storeRemainTimeByOpenApi();
+            expressBusFactory.getInstanceByRatio().storeRemainTimeByOpenApi();
         } catch (Exception e) {
             log.warn("시외버스 스케줄링 과정에서 오류가 발생했습니다.");
         }
