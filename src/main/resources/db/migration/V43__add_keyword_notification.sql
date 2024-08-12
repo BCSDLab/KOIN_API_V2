a2b642111ae02fa9f970d322c1976ab994b5eab7
seongjae6751 <seongjae6751@naver.com>
2024-08-12 23:58:32 +0900

    feat: flyway 파일 작성

---
+++ src/main/resources/db/migration/V43__add_keyword_notification.sql
@@ -0,0 +1,20 @@
+CREATE TABLE `keywords`
+(
+    `id`           INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `keyword`      VARCHAR(50) NOT NULL UNIQUE,
+    `last_used_at` TIMESTAMP NULL,
+    `created_at`   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at`   TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `keyword_user_map`
+(
+    `keyword_id` INT UNSIGNED NOT NULL,
+    `user_id`    INT UNSIGNED NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`keyword_id`, `user_id`),
+    FOREIGN KEY (`keyword_id`) REFERENCES `keywords` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+);
