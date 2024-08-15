b55ec520cbbdab1057dd4f80e6bd105532813837
HyeonsuLee <leehyeonsu4888@naver.com>
2024-08-15 23:41:00 +0900

    Revert "Revert "Merge pull request #782 from BCSDLab/develop""
    This reverts commit e3b664c15943793d36d6882b6a25f237cf6c3c7f.

--- src/main/java/in/koreatech/koin/domain/community/dto/HotArticleItemResponse.java
+++ src/main/java/in/koreatech/koin/domain/community/dto/HotArticleItemResponse.java
@@ -1,13 +1,11 @@
 package in.koreatech.koin.domain.community.dto;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
-import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import in.koreatech.koin.domain.community.model.Article;
@@ -15,6 +13,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public record HotArticleItemResponse(
+
     @Schema(description = "게시글 고유 ID", example = "1", requiredMode = REQUIRED)
     Integer id,
 
@@ -24,19 +23,17 @@ public record HotArticleItemResponse(
     @Schema(description = "제목", example = "제목", requiredMode = REQUIRED)
     String title,
 
-    @Schema(description = "내용 요약", example = "내용 요약", requiredMode = NOT_REQUIRED)
-    @JsonProperty("contentSummary")
-    String contentSummary,
-
-    @Schema(description = "댓글 수", example = "1", requiredMode = REQUIRED)
-    Byte commentCount,
+    @Schema(description = "작성자 닉네임", example = "닉네임", requiredMode = REQUIRED)
+    String nickname,
 
     @Schema(description = "조회수", example = "1", requiredMode = REQUIRED)
     Integer hit,
 
     @Schema(description = "생성 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    LocalDateTime createdAt
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime createdAt,
+
+    @Schema(description = "수정 일자", example = "2023-01-04 12:00:01", requiredMode = REQUIRED)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime updatedAt
 ) {
 
     public static HotArticleItemResponse from(Article article) {
@@ -44,10 +41,10 @@ public record HotArticleItemResponse(
             article.getId(),
             article.getBoard().getId(),
             article.getTitle(),
-            article.getContentSummary(),
-            article.getCommentCount(),
+            article.getNickname(),
             article.getHit(),
-            article.getCreatedAt()
+            article.getCreatedAt(),
+            article.getUpdatedAt()
         );
     }
 }
