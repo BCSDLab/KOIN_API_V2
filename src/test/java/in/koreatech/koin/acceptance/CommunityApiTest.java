c1b362236b9af1c066b185e7235908421feda656
songsunkook <songsunkook@gmail.com>
2024-08-17 14:22:26 +0900

    test: 테스트 수정

--- src/test/java/in/koreatech/koin/acceptance/CommunityApiTest.java
+++ src/test/java/in/koreatech/koin/acceptance/CommunityApiTest.java
@@ -355,17 +355,16 @@ class CommunityApiTest extends AcceptanceTest {
                             "id": 1,
                             "board_id": 1,
                             "title": "자유 글의 제목입니다",
-                            "content": "<p>내용</p>",
                             "nickname": "준호",
                             "hit": 1,
                             "created_at": "2024-01-15 12:00:00",
                             "updated_at": "2024-01-15 12:00:00"
                         }
                     ],
-                    "totalCount": 2,
-                    "currentCount": 1,
-                    "totalPage": 2,
-                    "currentPage": 2
+                   "total_count": 2,
+                   "current_count": 1,
+                   "total_page": 2,
+                   "current_page": 2
                 }
                 """);
     }
