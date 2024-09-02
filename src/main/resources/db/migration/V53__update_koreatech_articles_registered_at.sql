0ea17a90db237986801c82af21296f1ecf8fc631
박성빈 <psb106305@gmail.com>
2024-09-02 16:08:27 +0900

    fix: 시간 정보 저장으로 수정

--- src/main/resources/db/migration/V53__update_koreatech_articles_registered_at.sql
+++ src/main/resources/db/migration/V53__update_koreatech_articles_registered_at.sql
@@ -4,9 +4,11 @@ UPDATE koin.koreatech_articles
 SET registered_at =
         CASE
             WHEN registered_at REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
-                THEN DATE(STR_TO_DATE(registered_at, '%Y-%m-%d %H:%i:%s'))
+                THEN STR_TO_DATE(registered_at, '%Y-%m-%d %H:%i:%s')
             WHEN registered_at REGEXP '^[0-9]{2}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
-                THEN DATE(STR_TO_DATE(registered_at, '%y.%m.%d %H:%i:%s'))
+                THEN STR_TO_DATE(registered_at, '%y.%m.%d %H:%i:%s')
+            WHEN registered_at REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
+                THEN STR_TO_DATE(CONCAT(registered_at, ' 00:00:00'), '%Y-%m-%d %H:%i:%s')
             ELSE registered_at
             END;
 
