31afe7c36d7e4a50ccff36d5500d4bea6aa4b7f3
박성빈 <psb106305@gmail.com>
2024-09-02 16:31:47 +0900

    chore: 인덴트 수정

--- src/main/resources/db/migration/V53__update_koreatech_articles_registered_at.sql
+++ src/main/resources/db/migration/V53__update_koreatech_articles_registered_at.sql
@@ -2,15 +2,15 @@ SET SQL_SAFE_UPDATES = 0;
 
 UPDATE koin.koreatech_articles
 SET registered_at =
-        CASE
-            WHEN registered_at REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
-                THEN STR_TO_DATE(registered_at, '%Y-%m-%d %H:%i:%s')
-            WHEN registered_at REGEXP '^[0-9]{2}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
-                THEN STR_TO_DATE(registered_at, '%y.%m.%d %H:%i:%s')
-            WHEN registered_at REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
-                THEN STR_TO_DATE(CONCAT(registered_at, ' 00:00:00'), '%Y-%m-%d %H:%i:%s')
-            ELSE registered_at
-            END;
+    CASE
+        WHEN registered_at REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
+            THEN STR_TO_DATE(registered_at, '%Y-%m-%d %H:%i:%s')
+        WHEN registered_at REGEXP '^[0-9]{2}\.[0-9]{2}\.[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$'
+            THEN STR_TO_DATE(registered_at, '%y.%m.%d %H:%i:%s')
+        WHEN registered_at REGEXP '^[0-9]{4}-[0-9]{2}-[0-9]{2}$'
+            THEN STR_TO_DATE(CONCAT(registered_at, ' 00:00:00'), '%Y-%m-%d %H:%i:%s')
+        ELSE registered_at
+        END;
 
 SET SQL_SAFE_UPDATES = 1;
 
