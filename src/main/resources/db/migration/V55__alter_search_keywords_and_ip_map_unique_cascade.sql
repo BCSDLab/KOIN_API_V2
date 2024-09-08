acb54017aab7780ee909eb9cc39081649a1a69f9
seongjae6751 <seongjae6751@naver.com>
2024-09-08 21:50:48 +0900

    chore: 주석 제거

--- src/main/resources/db/migration/V55__alter_search_keywords_and_ip_map_unique_cascade.sql
+++ src/main/resources/db/migration/V55__alter_search_keywords_and_ip_map_unique_cascade.sql
@@ -2,7 +2,7 @@ ALTER TABLE `article_search_keywords`
     MODIFY COLUMN `keyword` VARCHAR(255) NOT NULL UNIQUE;
 
 ALTER TABLE article_search_keyword_ip_map
-    ADD CONSTRAINT unique_keyword_ip UNIQUE (keyword_id, ip_address),  -- 유니크 제약 조건 추가
+    ADD CONSTRAINT unique_keyword_ip UNIQUE (keyword_id, ip_address),
     ADD CONSTRAINT fk_keyword_id
     FOREIGN KEY (keyword_id)
     REFERENCES article_search_keywords (id)
