1b506a101321ad6b1f8bcec3b85ee4a5cf692a40
seongjae6751 <seongjae6751@naver.com>
2024-09-17 12:28:11 +0900

    chore: TestRedissonConfig 설정 변경

--- src/test/java/in/koreatech/koin/config/TestRedissonConfig.java
+++ src/test/java/in/koreatech/koin/config/TestRedissonConfig.java
@@ -3,19 +3,26 @@ package in.koreatech.koin.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @TestConfiguration
 public class TestRedissonConfig {
 
-    @Bean
-    public RedissonClient redissonClient(
-        @Value("${spring.data.redis.host}") String redisHost,
+    @Autowired
+    private Environment environment;
 
-        @Value("${spring.data.redis.port}") int redisPort
-    ) {
+    @Bean
+    public RedissonClient redissonClient() {
+        String redisHost = environment.getProperty("spring.data.redis.host");
+        String redisPort = environment.getProperty("spring.data.redis.port");
+
+        System.out.println("Redis Host: " + redisHost);
+        System.out.println("Redis Port: " + redisPort);
+
         Config config = new Config();
         config.useSingleServer()
             .setAddress("redis://" + redisHost + ":" + redisPort);
