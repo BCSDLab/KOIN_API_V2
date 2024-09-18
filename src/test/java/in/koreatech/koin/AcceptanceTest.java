3206f1ec6a9d942ffd06a889d9c672f160b640ad
seongjae6751 <seongjae6751@naver.com>
2024-09-18 18:54:49 +0900

    chore: 테스트 환경에서 redisson 연결 차단

--- src/test/java/in/koreatech/koin/AcceptanceTest.java
+++ src/test/java/in/koreatech/koin/AcceptanceTest.java
@@ -4,6 +4,7 @@ import java.time.Clock;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -67,6 +68,9 @@ public abstract class AcceptanceTest {
 
     @MockBean
     protected ArticleKeywordEventListener articleKeywordEventListener;
+
+    @MockBean
+    private RedissonClient redissonClient;
 
     @SpyBean
     protected TestCircuitBreakerClient testCircuitBreakerClient;
