63ae620fc4dba51b884933a5bd07453d7835a2bf
김원경 <kyg279044@gmail.com>
2024-09-24 01:55:12 +0900

    feat: 로그인 여부 체크  API

---
+++ src/main/java/in/koreatech/koin/domain/user/dto/UserAccessTokenRequest.java
@@ -0,0 +1,16 @@
+package in.koreatech.koin.domain.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+public record UserAccessTokenRequest(
+        @Schema(description = "access_token", example = "eyJ0eXAiOiJKV1QiLCJhbGcIUzUxMic9.eyJpZCI6NTM5NhwIjoxkzI3MTA5ODE5fQ.rLEYGQfKI5_24ZlwLVwlgwnriqySPKwXNOeTRrbmxoCtlOzCVvM8FFcO9BA2vkqsmhf-w", requiredMode = REQUIRED)
+        @NotNull(message = "access_token을 입력해주세요.")
+        @JsonProperty("access_token")
+        String accessToken
+) {
+
+}
