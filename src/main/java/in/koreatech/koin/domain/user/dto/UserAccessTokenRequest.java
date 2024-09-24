8f2ee36f552ead55b3124506d9f0b4410e4a82c7
김원경 <kyg279044@gmail.com>
2024-09-24 11:58:22 +0900

    fix: notnull 삭제

--- src/main/java/in/koreatech/koin/domain/user/dto/UserAccessTokenRequest.java
+++ src/main/java/in/koreatech/koin/domain/user/dto/UserAccessTokenRequest.java
@@ -10,7 +10,6 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 public record UserAccessTokenRequest(
         @Schema(description = "access_token", example = "eyJ0eXAiOiJKV1QiLCJhbGcIUzUxMic9.eyJpZCI6NTM5NhwIjoxkzI3MTA5ODE5fQ.rLEYGQfKI5_24ZlwLVwlgwnriqySPKwXNOeTRrbmxoCtlOzCVvM8FFcO9BA2vkqsmhf-w", requiredMode = REQUIRED)
         @NotBlank(message = "access_token을 입력해주세요.")
-        @NotNull(message = "access_token을 입력해주세요.")
         @JsonProperty("access_token")
         String accessToken
 ) {
