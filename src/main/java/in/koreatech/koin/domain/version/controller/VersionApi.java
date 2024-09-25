973271d0ab5c4b95d4b7855335a8756c64b6c292
Hwang HyeonSik <142300831+Choon0414@users.noreply.github.com>
2024-09-25 20:48:06 +0900

    refactor: version의 content를 배열에서 문자열로 변경 (#920)

--- src/main/java/in/koreatech/koin/domain/version/controller/VersionApi.java
+++ src/main/java/in/koreatech/koin/domain/version/controller/VersionApi.java
@@ -3,9 +3,9 @@ package in.koreatech.koin.domain.version.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.koreatech.koin.domain.version.dto.VersionMessageResponse;
+import in.koreatech.koin.domain.version.dto.VersionResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "(Normal) Version: 버전", description = "버전 정보를 관리한다")
-@RequestMapping("/version")
 public interface VersionApi {
 
     @ApiResponses(
@@ -25,7 +24,20 @@ public interface VersionApi {
         }
     )
     @Operation(summary = "특정 타입의 버전 조회")
-    @GetMapping("/{type}")
+    @GetMapping("/versions/{type}")
+    ResponseEntity<VersionResponse> getVersion(
+        @Parameter(description = "android, ios, timetable, express_bus_timetable, shuttle_bus_timetable, city_bus_timetable")
+        @PathVariable(value = "type") String type
+    );
+
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
+        }
+    )
+    @Operation(summary = "업데이트를 위한 특정 타입의 버전 조회")
+    @GetMapping("/version/{type}")
     ResponseEntity<VersionMessageResponse> getVersionWithMessage(
         @Parameter(description = "android, ios, timetable, express_bus_timetable, shuttle_bus_timetable, city_bus_timetable")
         @PathVariable(value = "type") String type
--- src/test/java/in/koreatech/koin/fixture/VersionFixture.java
+++ src/test/java/in/koreatech/koin/fixture/VersionFixture.java
@@ -1,11 +1,8 @@
 package in.koreatech.koin.fixture;
 
-import java.util.List;
-
 import org.springframework.stereotype.Component;
 
 import in.koreatech.koin.domain.version.model.Version;
-import in.koreatech.koin.domain.version.model.VersionContent;
 import in.koreatech.koin.domain.version.model.VersionType;
 import in.koreatech.koin.domain.version.repository.VersionRepository;
 
@@ -25,21 +22,8 @@ public final class VersionFixture {
                 .type(VersionType.ANDROID.getValue())
                 .version("3.5.0")
                 .title("코인 신기능 업데이트")
+                .content("더 빠른 알림을 위해 업데이트 해주세요!")
                 .build()
-        );
-        updateVersion.getContents().addAll(
-            List.of(
-                VersionContent.builder()
-                    .version(updateVersion)
-                    .title("Android 백그라운드 푸시 알림")
-                    .content("더 빠른 알림을 위해 업데이트 해주세요!")
-                    .build(),
-                VersionContent.builder()
-                    .version(updateVersion)
-                    .title("Android 키워드 알림")
-                    .content("더 빠른 알림을 위해 업데이트 해주세요!")
-                    .build()
-            )
         );
         return versionRepository.save(updateVersion);
     }
@@ -50,16 +34,8 @@ public final class VersionFixture {
                 .type(VersionType.IOS.getValue())
                 .version("3.5.0")
                 .title("코인 신기능 업데이트")
+                .content("더 빠른 알림을 위해 업데이트 해주세요!")
                 .build()
-        );
-        updateVersion.getContents().addAll(
-            List.of(
-                VersionContent.builder()
-                    .version(updateVersion)
-                    .title("iOS 백그라운드 푸시 알림")
-                    .content("더 빠른 알림을 위해 업데이트 해주세요!")
-                    .build()
-            )
         );
         return versionRepository.save(updateVersion);
     }
