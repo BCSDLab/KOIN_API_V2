898936b7fd04af50ffd0298667175cce3f93d3be
박성빈 <psb106305@gmail.com>
2024-10-11 22:58:16 +0900

    fix: 게시글 첨부파일 유니크 키 추가

--- src/main/resources/db/migration/V81__update_article_attachments_uk.sql
+++ src/main/resources/db/migration/V81__update_article_attachments_uk.sql
@@ -1,5 +1,2 @@
-DELETE a1 FROM article_attachments a1
-INNER JOIN article_attachments a2
-WHERE a1.id > a2.id
-  AND a1.article_id = a2.article_id
-  AND a1.hash = a2.hash;
+ALTER TABLE `koin`.`article_attachments`
+    ADD UNIQUE KEY ux_article_attachment (`article_id`, `hash`);
