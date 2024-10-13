88b3ec886392a64b69cde631855c4ebc59a76708
김원경 <kyg279044@gmail.com>
2024-10-13 17:35:48 +0900

    fix: 포매팅 수정

--- src/main/java/in/koreatech/koin/domain/timetableV2/dto/TimetableLectureResponse.java
+++ src/main/java/in/koreatech/koin/domain/timetableV2/dto/TimetableLectureResponse.java
@@ -16,109 +16,111 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonNaming(value = SnakeCaseStrategy.class)
 public record TimetableLectureResponse(
-        @Schema(description = "시간표 프레임 id", example = "1")
-        Integer timetableFrameId,
+    @Schema(description = "시간표 프레임 id", example = "1")
+    Integer timetableFrameId,
 
-        @Schema(description = "시간표 상세정보")
-        List<InnerTimetableLectureResponse> timetable,
+    @Schema(description = "시간표 상세정보")
+    List<InnerTimetableLectureResponse> timetable,
 
-        @Schema(description = "해당 학기 학점", example = "21")
-        Integer grades,
+    @Schema(description = "해당 학기 학점", example = "21")
+    Integer grades,
 
-        @Schema(description = "전체 학기 학점", example = "121")
-        Integer totalGrades
+    @Schema(description = "전체 학기 학점", example = "121")
+    Integer totalGrades
 ) {
     @JsonNaming(value = SnakeCaseStrategy.class)
     public record InnerTimetableLectureResponse(
-            @Schema(description = "시간표 id", example = "1", requiredMode = REQUIRED)
-            Integer id,
+        @Schema(description = "시간표 id", example = "1", requiredMode = REQUIRED)
+        Integer id,
 
-            @Schema(description = "강의 id", example = "1", requiredMode = NOT_REQUIRED)
-            Integer lectureId,
+        @Schema(description = "강의 id", example = "1", requiredMode = NOT_REQUIRED)
+        Integer lectureId,
 
-            @Schema(description = "수강 정원", example = "38", requiredMode = NOT_REQUIRED)
-            String regularNumber,
+        @Schema(description = "수강 정원", example = "38", requiredMode = NOT_REQUIRED)
+        String regularNumber,
 
-            @Schema(description = "과목 코드", example = "ARB244", requiredMode = NOT_REQUIRED)
-            String code,
+        @Schema(description = "과목 코드", example = "ARB244", requiredMode = NOT_REQUIRED)
+        String code,
 
-            @Schema(description = "설계 학점", example = "0", requiredMode = NOT_REQUIRED)
-            String designScore,
+        @Schema(description = "설계 학점", example = "0", requiredMode = NOT_REQUIRED)
+        String designScore,
 
-            @Schema(description = "강의(커스텀) 시간", example = "[204, 205, 206, 207, 302, 303]", requiredMode = REQUIRED)
-            List<Integer> classTime,
+        @Schema(description = "강의(커스텀) 시간", example = "[204, 205, 206, 207, 302, 303]", requiredMode = REQUIRED)
+        List<Integer> classTime,
 
-            @Schema(description = "강의 장소", example = "2공학관", requiredMode = NOT_REQUIRED)
-            String classPlace,
+        @Schema(description = "강의 장소", example = "2공학관", requiredMode = NOT_REQUIRED)
+        String classPlace,
 
-            @Schema(description = "메모", example = "null", requiredMode = NOT_REQUIRED)
-            String memo,
+        @Schema(description = "메모", example = "null", requiredMode = NOT_REQUIRED)
+        String memo,
 
-            @Schema(description = "학점", example = "3", requiredMode = REQUIRED)
-            String grades,
+        @Schema(description = "학점", example = "3", requiredMode = REQUIRED)
+        String grades,
 
-            @Schema(description = "강의(커스텀) 이름", example = "한국사", requiredMode = REQUIRED)
-            String classTitle,
+        @Schema(description = "강의(커스텀) 이름", example = "한국사", requiredMode = REQUIRED)
+        String classTitle,
 
-            @Schema(description = "분반", example = "01", requiredMode = NOT_REQUIRED)
-            String lectureClass,
+        @Schema(description = "분반", example = "01", requiredMode = NOT_REQUIRED)
+        String lectureClass,
 
-            @Schema(description = "대상", example = "디자 1 건축", requiredMode = NOT_REQUIRED)
-            String target,
+        @Schema(description = "대상", example = "디자 1 건축", requiredMode = NOT_REQUIRED)
+        String target,
 
-            @Schema(description = "강의 교수", example = "이돈우", requiredMode = NOT_REQUIRED)
-            String professor,
+        @Schema(description = "강의 교수", example = "이돈우", requiredMode = NOT_REQUIRED)
+        String professor,
 
-            @Schema(description = "학부", example = "디자인ㆍ건축공학부", requiredMode = NOT_REQUIRED)
-            String department
+        @Schema(description = "학부", example = "디자인ㆍ건축공학부", requiredMode = NOT_REQUIRED)
+        String department
     ) {
 
-        public static List<InnerTimetableLectureResponse> from(List<TimetableLecture> timetableLectures) {
+        public static List<InnerTimetableLectureResponse> from(
+            List<TimetableLecture> timetableLectures) {
             List<InnerTimetableLectureResponse> timetableLectureList = new ArrayList<>();
 
             for (TimetableLecture timetableLecture : timetableLectures) {
                 InnerTimetableLectureResponse response;
                 if (timetableLecture.getLecture() == null) {
                     response = new InnerTimetableLectureResponse(
-                            timetableLecture.getId(),
-                            null,
-                            null,
-                            null,
-                            null,
-                            parseIntegerClassTimesFromString(timetableLecture.getClassTime()),
-                            timetableLecture.getClassPlace(),
-                            timetableLecture.getMemo(),
-                            timetableLecture.getGrades(),
-                            timetableLecture.getClassTitle(),
-                            null,
-                            null,
-                            timetableLecture.getProfessor(),
-                            null
+                        timetableLecture.getId(),
+                        null,
+                        null,
+                        null,
+                        null,
+                        parseIntegerClassTimesFromString(timetableLecture.getClassTime()),
+                        timetableLecture.getClassPlace(),
+                        timetableLecture.getMemo(),
+                        timetableLecture.getGrades(),
+                        timetableLecture.getClassTitle(),
+                        null,
+                        null,
+                        timetableLecture.getProfessor(),
+                        null
                     );
                 } else {
                     response = new InnerTimetableLectureResponse(
-                            timetableLecture.getId(),
-                            timetableLecture.getLecture().getId(),
-                            timetableLecture.getLecture().getRegularNumber(),
-                            timetableLecture.getLecture().getCode(),
-                            timetableLecture.getLecture().getDesignScore(),
-                            timetableLecture.getClassTime() == null
-                                    ? parseIntegerClassTimesFromString(timetableLecture.getLecture().getClassTime())
-                                    : parseIntegerClassTimesFromString(timetableLecture.getClassTime()),
-                            timetableLecture.getClassPlace(),
-                            timetableLecture.getMemo(),
-                            timetableLecture.getGrades() == null
-                                    ? timetableLecture.getLecture().getGrades()
-                                    : timetableLecture.getGrades(),
-                            timetableLecture.getClassTitle() == null
-                                    ? timetableLecture.getLecture().getName()
-                                    : timetableLecture.getClassTitle(),
-                            timetableLecture.getLecture().getLectureClass(),
-                            timetableLecture.getLecture().getTarget(),
-                            timetableLecture.getProfessor() == null
-                                    ? timetableLecture.getLecture().getProfessor()
-                                    : timetableLecture.getProfessor(),
-                            timetableLecture.getLecture().getDepartment()
+                        timetableLecture.getId(),
+                        timetableLecture.getLecture().getId(),
+                        timetableLecture.getLecture().getRegularNumber(),
+                        timetableLecture.getLecture().getCode(),
+                        timetableLecture.getLecture().getDesignScore(),
+                        timetableLecture.getClassTime() == null
+                            ? parseIntegerClassTimesFromString(
+                            timetableLecture.getLecture().getClassTime())
+                            : parseIntegerClassTimesFromString(timetableLecture.getClassTime()),
+                        timetableLecture.getClassPlace(),
+                        timetableLecture.getMemo(),
+                        timetableLecture.getGrades() == null
+                            ? timetableLecture.getLecture().getGrades()
+                            : timetableLecture.getGrades(),
+                        timetableLecture.getClassTitle() == null
+                            ? timetableLecture.getLecture().getName()
+                            : timetableLecture.getClassTitle(),
+                        timetableLecture.getLecture().getLectureClass(),
+                        timetableLecture.getLecture().getTarget(),
+                        timetableLecture.getProfessor() == null
+                            ? timetableLecture.getLecture().getProfessor()
+                            : timetableLecture.getProfessor(),
+                        timetableLecture.getLecture().getDepartment()
                     );
                 }
                 timetableLectureList.add(response);
@@ -127,22 +129,25 @@ public record TimetableLectureResponse(
         }
     }
 
-    public static TimetableLectureResponse of(Integer timetableFrameId, List<TimetableLecture> timetableLectures,
-                                              Integer grades, Integer totalGrades) {
-        return new TimetableLectureResponse(timetableFrameId, InnerTimetableLectureResponse.from(timetableLectures),
-                grades, totalGrades);
+    public static TimetableLectureResponse of(Integer timetableFrameId,
+        List<TimetableLecture> timetableLectures,
+        Integer grades, Integer totalGrades) {
+        return new TimetableLectureResponse(timetableFrameId,
+            InnerTimetableLectureResponse.from(timetableLectures),
+            grades, totalGrades);
     }
 
     private static final int INITIAL_BRACE_INDEX = 1;
 
     private static List<Integer> parseIntegerClassTimesFromString(String classTime) {
-        String classTimeWithoutBrackets = classTime.substring(INITIAL_BRACE_INDEX, classTime.length() - 1);
+        String classTimeWithoutBrackets = classTime.substring(INITIAL_BRACE_INDEX,
+            classTime.length() - 1);
 
         if (!classTimeWithoutBrackets.isEmpty()) {
             return Arrays.stream(classTimeWithoutBrackets.split(","))
-                    .map(String::strip)
-                    .map(Integer::parseInt)
-                    .toList();
+                .map(String::strip)
+                .map(Integer::parseInt)
+                .toList();
         } else {
             return Collections.emptyList();
         }
