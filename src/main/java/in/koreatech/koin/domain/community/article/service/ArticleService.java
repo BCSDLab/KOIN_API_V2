c18c5f9ce78da34c926f22fbc8aace8f0f2652a0
seongjae6751 <seongjae6751@naver.com>
2024-10-18 00:16:57 +0900

    chore: 게시글 조회 api 레디슨 락 시간 증가

--- src/main/java/in/koreatech/koin/domain/community/article/service/ArticleService.java
+++ src/main/java/in/koreatech/koin/domain/community/article/service/ArticleService.java
@@ -113,7 +113,7 @@ public class ArticleService {
         return cacheList.stream().map(HotArticleItemResponse::from).toList();
     }
 
-    @ConcurrencyGuard(lockName = "searchLog")
+    @ConcurrencyGuard(lockName = "searchLog", waitTime = 6, leaseTime = 4)
     public ArticlesResponse searchArticles(String query, Integer boardId, Integer page, Integer limit,
         String ipAddress) {
         if (query.length() >= MAXIMUM_SEARCH_LENGTH) {
