138acb0d9c15e03287013dd41f446e6eaf78c836
신관규 <soundbar91@gmail.com>
2024-10-29 10:08:30 +0900

    chore: 어드민 계정 업데이트 API ApiResponse 추가

--- src/main/java/in/koreatech/koin/admin/user/controller/AdminUserApi.java
+++ src/main/java/in/koreatech/koin/admin/user/controller/AdminUserApi.java
@@ -193,6 +193,7 @@ public interface AdminUserApi {
     @ApiResponses(
         value = {
             @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
             @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
             @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true))),
             @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(hidden = true))),
