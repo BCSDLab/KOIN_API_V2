b17a046cdfd08c769b5ef54f14be250ed9ddd198
krSeonghyeon <149303551+krSeonghyeon@users.noreply.github.com>
2024-10-31 23:06:08 +0900

    fix: ShopCategory isDeleted 필드 삭제 및 관련 코드 수정 (#984)
    * fix: ShopCategory isDeleted 필드 삭제 및 관련 코드 수정
    
    * refactor: 개행 추가 및 미사용 인자 삭제
    
    * refactor: 인자 변경으로 인한 빌더변경
    
    * refactor: 인자 변경으로 인한 테스트 코드 변경
    
    * feat: 상점 카테고리 삭제 예외처리 추가
    
    * feat: 응답코드 400 swagger 명세 추가

--- src/main/java/in/koreatech/koin/domain/shop/model/shop/ShopCategory.java
+++ src/main/java/in/koreatech/koin/domain/shop/model/shop/ShopCategory.java
@@ -7,8 +7,6 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.annotations.Where;
-
 import in.koreatech.koin.global.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,7 +14,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -27,7 +24,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "shop_categories")
-@Where(clause = "is_deleted=0")
 public class ShopCategory extends BaseEntity {
 
     @Id
@@ -42,26 +38,17 @@ public class ShopCategory extends BaseEntity {
     @Column(name = "image_url")
     private String imageUrl;
 
-    @NotNull
-    @Column(name = "is_deleted", nullable = false)
-    private boolean isDeleted = false;
-
     @OneToMany(mappedBy = "shopCategory", orphanRemoval = true, cascade = {PERSIST, REMOVE})
     private List<ShopCategoryMap> shopCategoryMaps = new ArrayList<>();
 
     @Builder
-    private ShopCategory(String name, String imageUrl, Boolean isDeleted) {
+    private ShopCategory(String name, String imageUrl) {
         this.name = name;
         this.imageUrl = imageUrl;
-        this.isDeleted = isDeleted;
     }
 
     public void modifyShopCategory(String name, String imageUrl) {
         this.name = name;
         this.imageUrl = imageUrl;
-    }
-
-    public void delete() {
-        this.isDeleted = true;
     }
 }
