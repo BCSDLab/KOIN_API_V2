63406843d828902fd92d92b1d4ed9d0b5ceeef92
seongjae6751 <seongjae6751@naver.com>
2024-11-02 02:34:29 +0900

    refactor: 메서드 분리

--- src/main/java/in/koreatech/koin/domain/community/keyword/model/ArticleKeywordEventListener.java
+++ src/main/java/in/koreatech/koin/domain/community/keyword/model/ArticleKeywordEventListener.java
@@ -6,6 +6,8 @@ import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMI
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,7 @@ import in.koreatech.koin.domain.community.keyword.repository.UserNotificationSta
 import in.koreatech.koin.domain.community.keyword.service.KeywordService;
 import in.koreatech.koin.global.domain.notification.model.Notification;
 import in.koreatech.koin.global.domain.notification.model.NotificationFactory;
+import in.koreatech.koin.global.domain.notification.model.NotificationSubscribe;
 import in.koreatech.koin.global.domain.notification.repository.NotificationSubscribeRepository;
 import in.koreatech.koin.global.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
@@ -30,35 +33,46 @@ public class ArticleKeywordEventListener {
     private final UserNotificationStatusRepository userNotificationStatusRepository;
     private final KeywordService keywordService;
 
-
     @TransactionalEventListener(phase = AFTER_COMMIT)
     public void onKeywordRequest(ArticleKeywordEvent event) {
         List<Notification> notifications = notificationSubscribeRepository
             .findAllBySubscribeTypeAndDetailType(ARTICLE_KEYWORD, null)
             .stream()
-            .filter(subscribe -> subscribe.getUser().getDeviceToken() != null)
-            .filter(subscribe -> event.keyword().getArticleKeywordUserMaps().stream()
-                .anyMatch(map -> map.getUser().getId().equals(subscribe.getUser().getId())))
-            .filter(subscribe -> {
-                Integer userId = subscribe.getUser().getId();
-                Integer lastNotifiedId = userNotificationStatusRepository.findLastNotifiedArticleIdByUserId(userId)
-                    .orElse(0);
-                return lastNotifiedId == event.articleId();
-            })
-            .map(subscribe -> {
-                Integer userId = subscribe.getUser().getId();
-                Notification notification = notificationFactory.generateKeywordNotification(
-                    KEYWORD,
-                    event.articleId(),
-                    event.keyword().getKeyword(),
-                    subscribe.getUser()
-                );
-
-                keywordService.updateLastNotifiedArticle(userId, event.articleId());
-                return notification;
-            })
+            .filter(this::hasDeviceToken)
+            .filter(subscribe -> isKeywordRegistered(event, subscribe))
+            .filter(subscribe -> isNewArticle(event, subscribe))
+            .map(subscribe -> createAndRecordNotification(event, subscribe))
             .toList();
 
         notificationService.push(notifications);
+    }
+
+    private boolean hasDeviceToken(NotificationSubscribe subscribe) {
+        return subscribe.getUser().getDeviceToken() != null;
+    }
+
+    private boolean isKeywordRegistered(ArticleKeywordEvent event, NotificationSubscribe subscribe) {
+        return event.keyword().getArticleKeywordUserMaps().stream()
+            .anyMatch(map -> map.getUser().getId().equals(subscribe.getUser().getId()));
+    }
+
+    private boolean isNewArticle(ArticleKeywordEvent event, NotificationSubscribe subscribe) {
+        Integer userId = subscribe.getUser().getId();
+        Integer lastNotifiedId = userNotificationStatusRepository
+            .findLastNotifiedArticleIdByUserId(userId)
+            .orElse(0);
+        return !lastNotifiedId.equals(event.articleId());
+    }
+
+    private Notification createAndRecordNotification(ArticleKeywordEvent event, NotificationSubscribe subscribe) {
+        Integer userId = subscribe.getUser().getId();
+        Notification notification = notificationFactory.generateKeywordNotification(
+            KEYWORD,
+            event.articleId(),
+            event.keyword().getKeyword(),
+            subscribe.getUser()
+        );
+        keywordService.updateLastNotifiedArticle(userId, event.articleId());
+        return notification;
     }
 }
