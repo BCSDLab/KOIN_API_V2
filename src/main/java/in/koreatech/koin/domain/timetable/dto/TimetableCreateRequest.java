fc0cbee8aaa5206a59a81632cba45f0455e4eeef
duehee <149302959+duehee@users.noreply.github.com>
2024-11-04 12:20:06 +0900

    chore : Swagger Schema 일관성을 위해 name -> description으로 변경

--- src/main/java/in/koreatech/koin/domain/timetable/dto/TimetableCreateRequest.java
+++ src/main/java/in/koreatech/koin/domain/timetable/dto/TimetableCreateRequest.java
@@ -43,33 +43,33 @@ public record TimetableCreateRequest(
         @Schema(description = "강의 장소", example = "2공학관", requiredMode = NOT_REQUIRED)
         String classPlace,
 
-        @Schema(name = "강의 교수", example = "이돈우", requiredMode = NOT_REQUIRED)
+        @Schema(description = "강의 교수", example = "이돈우", requiredMode = NOT_REQUIRED)
         String professor,
 
         @Schema(description = "학점", example = "3", requiredMode = NOT_REQUIRED)
         String grades,
 
-        @Schema(name = "분반", example = "01", requiredMode = NOT_REQUIRED)
+        @Schema(description = "분반", example = "01", requiredMode = NOT_REQUIRED)
         @Size(max = 3, message = "분반은 3자 이하로 입력해주세요.")
         String lectureClass,
 
-        @Schema(name = "대상", example = "디자 1 건축", requiredMode = NOT_REQUIRED)
+        @Schema(description = "대상", example = "디자 1 건축", requiredMode = NOT_REQUIRED)
         @Size(max = 200, message = "대상은 200자 이하로 입력해주세요.")
         String target,
 
-        @Schema(name = "수강 인원", example = "25", requiredMode = NOT_REQUIRED)
+        @Schema(description = "수강 인원", example = "25", requiredMode = NOT_REQUIRED)
         @Size(max = 4, message = "수강 인원은 4자 이하로 입력해주세요.")
         String regularNumber,
 
-        @Schema(name = "설계 학점", example = "0", requiredMode = NOT_REQUIRED)
+        @Schema(description = "설계 학점", example = "0", requiredMode = NOT_REQUIRED)
         @Size(max = 4, message = "설계 학점은 4자 이하로 입력해주세요.")
         String designScore,
 
-        @Schema(name = "학부", example = "디자인ㆍ건축공학부", requiredMode = NOT_REQUIRED)
+        @Schema(description = "학부", example = "디자인ㆍ건축공학부", requiredMode = NOT_REQUIRED)
         @Size(max = 30, message = "학부는 30자 이하로 입력해주세요.")
         String department,
 
-        @Schema(name = "memo", example = "메모메모", requiredMode = NOT_REQUIRED)
+        @Schema(description = "memo", example = "메모메모", requiredMode = NOT_REQUIRED)
         @Size(max = 200, message = "메모는 200자 이하로 입력해주세요.")
         String memo
     ) {
