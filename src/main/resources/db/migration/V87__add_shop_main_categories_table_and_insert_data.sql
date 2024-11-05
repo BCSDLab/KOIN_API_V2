aecf3555734e4e47805d887d067812498fdda86a
krSeonghyeon <ya41921174@gmail.com>
2024-11-05 14:35:34 +0900

    chore: 라인 포맷팅 수정

--- src/main/resources/db/migration/V87__add_shop_main_categories_table_and_insert_data.sql
+++ src/main/resources/db/migration/V87__add_shop_main_categories_table_and_insert_data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `shop_main_categories`
 (
     id                      INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'shop_main_categories 고유 id',
-    name                    VARCHAR(255)                        NOT NULL COMMENT '메인 카테고리 이름',
+    name                    VARCHAR(255) NOT NULL COMMENT '메인 카테고리 이름',
     notification_message_id INT UNSIGNED NOT NULL COMMENT '알림 메시지 id',
     created_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '생성 일자',
     updated_at              TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일자',
