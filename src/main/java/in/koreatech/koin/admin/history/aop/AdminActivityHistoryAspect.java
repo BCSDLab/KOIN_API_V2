e09eb7e5fc5220a4bdc0724303384b3058d65d55
신관규 <soundbar91@gmail.com>
2024-11-06 22:03:02 +0900

    feat: 어드민 히스토리 기능 구현 (#985)
    * feat: AdminActivityHistory 테이블 flyway 추가
    
    * feat: AdminActivityHistory 모델 추가
    
    * feat: AdminActivityHistoryAspect 추가
    
    * feat: DomainType 추가
    
    * chore: BaseEntity 상속
    
    * refactor: AdminActivityHistoryAspect 로직 리펙토링
    
    * feat: HistoryApi 추가
    
    * feat: 히스토리 조회 API 추가
    
    * chore: save() 메소드 추가
    
    * chore: response 형식 변경
    
    * chore: JsonNaming 어노테이션 적용
    
    * chore: 내부 클래스 레코드으로 변경
    
    * chore: 카테고리 enum 추가
    
    * refactor: aspect 리펙터링
    
    * chore: 리뷰 반영
    
    * chore: user 코드 admin으로 변경
    
    * feat: 어드민 계정 히스토리 추가

---
+++ src/main/java/in/koreatech/koin/admin/history/aop/AdminActivityHistoryAspect.java
@@ -0,0 +1,136 @@
+package in.koreatech.koin.admin.history.aop;
+
+import org.apache.commons.lang3.EnumUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+
+import in.koreatech.koin.admin.history.enums.DomainType;
+import in.koreatech.koin.admin.history.model.AdminActivityHistory;
+import in.koreatech.koin.admin.history.repository.AdminActivityHistoryRepository;
+import in.koreatech.koin.admin.user.model.Admin;
+import in.koreatech.koin.admin.user.repository.AdminRepository;
+import in.koreatech.koin.global.auth.AuthContext;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@Aspect
+@Component
+@Profile("!test")
+@RequiredArgsConstructor
+public class AdminActivityHistoryAspect {
+    private static final String REGEX_NUMERIC = "^[0-9]*$";
+    private static final String SEGMENT_SHOPS = "SHOPS";
+    private static final String SEGMENT_BENEFIT = "benefit";
+    private static final String SEGMENT_CATEGORIES = "CATEGORIES";
+    private static final String SEGMENT_CLOSE = "close";
+    private static final String SEGMENT_ABTEST = "abtest";
+
+    private final AuthContext authContext;
+    private final AdminRepository adminRepository;
+    private final AdminActivityHistoryRepository adminActivityHistoryRepository;
+
+    @Pointcut("execution(* in.koreatech.koin.admin..controller.*.*(..))")
+    private void allAdminControllers() {
+    }
+
+    @Pointcut("!@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void excludeGetMapping() {
+    }
+
+    @Pointcut("!execution(* in.koreatech.koin.admin.user.controller.AdminUserController.adminLogin(..)) && "
+        + "!execution(* in.koreatech.koin.admin.user.controller.AdminUserController.logout(..)) && "
+        + "!execution(* in.koreatech.koin.admin.user.controller.AdminUserController.refresh(..)) && "
+        + "!execution(* in.koreatech.koin.admin.user.controller.AdminUserController.createAdmin(..)) && "
+        + "!execution(* in.koreatech.koin.admin.user.controller.AdminUserController.adminPasswordChange(..)) && "
+        + "!execution(* in.koreatech.koin.admin.abtest.controller.AbtestController.assignOrGetAbtestVariable(..))")
+    private void excludeSpecificMethods() {
+    }
+
+    @Around("allAdminControllers() && excludeGetMapping() && excludeSpecificMethods()")
+    public Object logAdminActivity(ProceedingJoinPoint joinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest();
+        String requestURI = request.getRequestURI();
+        String requestMethod = request.getMethod();
+
+        ContentCachingRequestWrapper cachingRequest = (ContentCachingRequestWrapper)request;
+        String requestMessage = new String(cachingRequest.getContentAsByteArray());
+
+        Object result = joinPoint.proceed();
+
+        Admin admin = adminRepository.getById(authContext.getUserId());
+        DomainInfo domainInfo = getDomainInfo(requestURI);
+
+        adminActivityHistoryRepository.save(AdminActivityHistory.builder()
+            .domainId(domainInfo.domainId())
+            .admin(admin)
+            .requestMethod(requestMethod)
+            .domainName(domainInfo.domainName())
+            .requestMessage(requestMessage)
+            .build());
+
+        return result;
+    }
+
+    private DomainInfo getDomainInfo(String requestURI) {
+        String[] segments = requestURI.split("/");
+        Integer domainId = null;
+        String domainName = null;
+
+        for (int i = segments.length - 1; i >= 0; i--) {
+            String segment = segments[i];
+
+            if (isDomainType(segment)) {
+                domainName = getDomainName(segment, segments, i);
+                domainId = getDomainId(segments, i);
+                break;
+            }
+
+            if (isCloseAbtest(segment, segments, i)) {
+                domainName = segments[i - 1].toUpperCase();
+                domainId = Integer.valueOf(segments[i + 1]);
+                break;
+            }
+        }
+
+        return new DomainInfo(domainId, domainName);
+    }
+
+    private boolean isDomainType(String segment) {
+        return EnumUtils.isValidEnumIgnoreCase(DomainType.class, segment);
+    }
+
+    private String getDomainName(String segment, String[] segments, int index) {
+        String domainName = segment.toUpperCase();
+
+        if (SEGMENT_SHOPS.equals(domainName) && SEGMENT_BENEFIT.equals(segments[index - 2])) {
+            return segments[index - 2].toUpperCase();
+        }
+
+        if (SEGMENT_CATEGORIES.equals(domainName)) {
+            return (segments[index - 1] + domainName).toUpperCase();
+        }
+
+        return domainName;
+    }
+
+    private Integer getDomainId(String[] segments, int index) {
+        if (index != segments.length - 1 && segments[index + 1].matches(REGEX_NUMERIC)) {
+            return Integer.valueOf(segments[index + 1]);
+        }
+        return null;
+    }
+
+    private boolean isCloseAbtest(String segment, String[] segments, int index) {
+        return SEGMENT_CLOSE.equals(segment) && SEGMENT_ABTEST.equals(segments[index - 1]);
+    }
+
+    private record DomainInfo(Integer domainId, String domainName) {
+    }
+}
---
+++ src/main/java/in/koreatech/koin/admin/history/dto/AdminHistoryResponse.java
@@ -0,0 +1,58 @@
+package in.koreatech.koin.admin.history.dto;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import in.koreatech.koin.admin.history.enums.DomainType;
+import in.koreatech.koin.admin.history.enums.HttpMethodType;
+import in.koreatech.koin.admin.history.model.AdminActivityHistory;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@JsonNaming(value = SnakeCaseStrategy.class)
+public record AdminHistoryResponse(
+    @Schema(description = "고유 id", example = "1", requiredMode = REQUIRED)
+    Integer id,
+
+    @Schema(description = "도메인 엔티티 id", example = "null", requiredMode = REQUIRED)
+    Integer domainId,
+
+    @Schema(description = "이름", example = "신관규", requiredMode = REQUIRED)
+    String name,
+
+    @Schema(description = "도메인 이름", example = "코인 공지", requiredMode = REQUIRED)
+    String domainName,
+
+    @Schema(description = "HTTP 요청 메소드 종류", example = "생성", requiredMode = REQUIRED)
+    String requestMethod,
+
+    @Schema(description = "HTTP 요청 메시지 바디", example = """
+        {
+            "title": "제목 예시",
+            "content": "본문 내용 예시"
+        }
+        """,
+        requiredMode = REQUIRED
+    )
+    String requestMessage,
+
+    @Schema(description = "요청 시간", example = "2019-08-16-23-01-52", requiredMode = REQUIRED)
+    @JsonFormat(pattern = "yyyy-MM-dd-HH-mm-ss")
+    LocalDateTime createdAt
+) {
+    public static AdminHistoryResponse from(AdminActivityHistory adminActivityHistory) {
+        return new AdminHistoryResponse(
+            adminActivityHistory.getId(),
+            adminActivityHistory.getDomainId(),
+            adminActivityHistory.getAdmin().getUser().getName(),
+            DomainType.valueOf(adminActivityHistory.getDomainName()).getDescription(),
+            HttpMethodType.valueOf(adminActivityHistory.getRequestMethod()).getValue(),
+            adminActivityHistory.getRequestMessage(),
+            adminActivityHistory.getCreatedAt()
+        );
+    }
+}
