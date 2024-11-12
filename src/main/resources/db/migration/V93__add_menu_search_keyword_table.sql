3171bb9e53fbea5ca16a70dd2da6eb371b2479a9
HyeonsuLee <leehyeonsu4888@naver.com>
2024-11-12 15:01:30 +0900

    chore: flyway 버전 충돌 해결

--- src/main/resources/db/migration/V93__add_menu_search_keyword_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE if not exists `shop_menu_search_keywords`
-(
-    id              INT UNSIGNED AUTO_INCREMENT NOT NULL,
-    keyword         VARCHAR(255)                NOT NULL,
-    created_at      timestamp                   default CURRENT_TIMESTAMP NOT NULL,
-    updated_at      timestamp                   default CURRENT_TIMESTAMP NOT NULL on update CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
-    );
