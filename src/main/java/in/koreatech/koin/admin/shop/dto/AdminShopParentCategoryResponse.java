0112ad1dc58597112e26e65606d4600aa06934ef
krSeonghyeon <ya41921174@gmail.com>
2024-11-12 14:26:56 +0900

    chore: static import 적용

--- src/main/java/in/koreatech/koin/admin/shop/dto/AdminShopParentCategoryResponse.java
+++ src/main/java/in/koreatech/koin/admin/shop/dto/AdminShopParentCategoryResponse.java
@@ -1,12 +1,13 @@
 package in.koreatech.koin.admin.shop.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.*;
+
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import in.koreatech.koin.domain.shop.model.shop.ShopParentCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+@JsonNaming(value = SnakeCaseStrategy.class)
 public record AdminShopParentCategoryResponse(
     @Schema(description = "상위 카테고리 고유 ID", example = "1")
     int id,
