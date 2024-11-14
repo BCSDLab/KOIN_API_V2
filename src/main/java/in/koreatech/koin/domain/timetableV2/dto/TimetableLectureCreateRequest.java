6cb491cd95e318f3c4d637ed9f217a756c2de14f
신관규 <soundbar91@gmail.com>
2024-11-14 20:15:36 +0900

    fix: 강의 시간 null 체크 추가 (#1021)
    * fix: 강의 시간 null 체크
    
    * chore: 미사용 import 삭제

--- src/main/java/in/koreatech/koin/domain/timetableV2/dto/TimetableLectureCreateRequest.java
+++ src/main/java/in/koreatech/koin/domain/timetableV2/dto/TimetableLectureCreateRequest.java
@@ -3,7 +3,6 @@ package in.koreatech.koin.domain.timetableV2.dto;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
-import java.util.Arrays;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
@@ -54,16 +53,17 @@ public record TimetableLectureCreateRequest(
 
         @Schema(description = "강의 고유 번호", example = "1", requiredMode = NOT_REQUIRED)
         Integer lectureId
-    ){
+    ) {
         public InnerTimeTableLectureRequest {
             if (grades == null) {
                 grades = "0";
             }
         }
+
         public TimetableLecture toTimetableLecture(TimetableFrame timetableFrame) {
             return new TimetableLecture(
                 classTitle,
-                Arrays.toString(classTime().stream().toArray()),
+                getClassTimeToString(),
                 classPlace,
                 professor,
                 grades,
@@ -77,7 +77,7 @@ public record TimetableLectureCreateRequest(
         public TimetableLecture toTimetableLecture(TimetableFrame timetableFrame, Lecture lecture) {
             return new TimetableLecture(
                 classTitle,
-                Arrays.toString(classTime().stream().toArray()),
+                getClassTimeToString(),
                 classPlace,
                 professor,
                 grades,
@@ -86,6 +86,13 @@ public record TimetableLectureCreateRequest(
                 lecture,
                 timetableFrame
             );
+        }
+
+        private String getClassTimeToString() {
+            if (classTime != null) {
+                return classTime.toString();
+            }
+            return null;
         }
     }
 }
