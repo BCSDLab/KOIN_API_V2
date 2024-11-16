9e1f09b730ebb7ca23463e7b33df7ed0250e8345
신관규 <soundbar91@gmail.com>
2024-11-16 13:43:31 +0900

    chore: 미사용 import 삭제

--- src/main/java/in/koreatech/koin/domain/timetableV2/dto/request/TimetableLectureCreateRequest.java
+++ src/main/java/in/koreatech/koin/domain/timetableV2/dto/request/TimetableLectureCreateRequest.java
@@ -3,7 +3,6 @@ package in.koreatech.koin.domain.timetableV2.dto.request;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
-import java.util.Arrays;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
@@ -54,7 +53,7 @@ public record TimetableLectureCreateRequest(
 
         @Schema(description = "강의 고유 번호", example = "1", requiredMode = NOT_REQUIRED)
         Integer lectureId
-    ){
+    ) {
         public InnerTimeTableLectureRequest {
             if (grades == null) {
                 grades = "0";
