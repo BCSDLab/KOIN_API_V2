e7b1457b3f0ccb947ccb6ee1d7adef8334a6a0fc
신관규 <soundbar91@gmail.com>
2024-11-20 18:57:51 +0900

    chore: classPlace 크기 변경

--- src/main/java/in/koreatech/koin/domain/timetableV2/model/TimetableLecture.java
+++ src/main/java/in/koreatech/koin/domain/timetableV2/model/TimetableLecture.java
@@ -42,8 +42,8 @@ public class TimetableLecture extends BaseEntity {
     @Column(name = "class_time", columnDefinition = "TEXT")
     private String classTime;
 
-    @Size(max = 30)
-    @Column(name = "class_place", length = 30)
+    @Size(max = 255)
+    @Column(name = "class_place", length = 255)
     private String classPlace;
 
     @Size(max = 30)
