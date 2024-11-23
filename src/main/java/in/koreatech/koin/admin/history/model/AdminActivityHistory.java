53090e8e2d4c29b575aeb3c18ef409ca039df994
신관규 <soundbar91@gmail.com>
2024-11-23 20:07:29 +0900

    chore: 미사용 import 삭제

--- src/main/java/in/koreatech/koin/admin/history/model/AdminActivityHistory.java
+++ src/main/java/in/koreatech/koin/admin/history/model/AdminActivityHistory.java
@@ -17,7 +17,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
