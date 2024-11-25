177e5fd557aa533a61902870c25cb37dfbc7a37b
duehee <149302959+duehee@users.noreply.github.com>
2024-11-25 21:20:37 +0900

    chore : 미사용 메소드 삭제

--- src/main/java/in/koreatech/koin/domain/student/model/Student.java
+++ src/main/java/in/koreatech/koin/domain/student/model/Student.java
@@ -70,8 +70,4 @@ public class Student {
         this.studentNumber = studentNumber;
         this.department = department;
     }
-
-    public static Integer parseStudentNumberYear(String studentNumber) {
-        return Integer.parseInt(studentNumber.substring(0, 4));
-    }
 }
