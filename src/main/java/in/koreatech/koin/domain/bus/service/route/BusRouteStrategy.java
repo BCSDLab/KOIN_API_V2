180f899d37c559d641b4a5a9d04bcf5155802050
DHkimgit <dukekim001@naver.com>
2024-11-26 14:47:58 +0900

    feat : 전략패턴 인터페이스 작성

---
+++ src/main/java/in/koreatech/koin/domain/bus/service/route/BusRouteStrategy.java
@@ -0,0 +1,12 @@
+package in.koreatech.koin.domain.bus.service.route;
+
+import java.util.List;
+
+import in.koreatech.koin.domain.bus.dto.BusRouteCommand;
+import in.koreatech.koin.domain.bus.dto.BusScheduleResponse.ScheduleInfo;
+import in.koreatech.koin.domain.bus.model.enums.BusRouteType;
+
+public interface BusRouteStrategy {
+    List<ScheduleInfo> findSchedule(BusRouteCommand command);
+    boolean support(BusRouteType type);
+}
