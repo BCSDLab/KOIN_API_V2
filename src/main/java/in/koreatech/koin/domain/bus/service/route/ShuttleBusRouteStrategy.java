65a66b94722e367dc505c2c20037cc9b420eac21
DHkimgit <dukekim001@naver.com>
2024-11-26 20:14:36 +0900

    refactor : 셔틀버스 구현체 통합

--- src/main/java/in/koreatech/koin/domain/bus/service/route/ShuttleBusRouteStrategy.java
+++ src/main/java/in/koreatech/koin/domain/bus/service/route/ShuttleBusRouteStrategy.java
@@ -5,27 +5,26 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import in.koreatech.koin.domain.bus.dto.BusRouteCommand;
-import in.koreatech.koin.domain.bus.dto.BusScheduleResponse.ScheduleInfo;
+import in.koreatech.koin.domain.bus.dto.BusScheduleResponse;
 import in.koreatech.koin.domain.bus.model.enums.BusRouteType;
 import in.koreatech.koin.domain.bus.model.mongo.BusCourse;
-import in.koreatech.koin.domain.bus.model.mongo.Route;
 import in.koreatech.koin.domain.bus.repository.BusRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class CircularShuttleBusStrategy implements BusRouteStrategy{
-
+public class ShuttleBusRouteStrategy implements BusRouteStrategy{
     private final BusRepository busRepository;
+    private static final String BUS_TYPE = "shuttle";
+    private static final String REGION = "천안";
 
     @Override
-    public List<ScheduleInfo> findSchedule(BusRouteCommand command) {
-        return busRepository.findByBusTypeAndRegion("shuttle", "천안").stream()
+    public List<BusScheduleResponse.ScheduleInfo> findSchedule(BusRouteCommand command) {
+        return busRepository.findByBusTypeAndRegion(BUS_TYPE, REGION).stream()
             .map(BusCourse::getRoutes)
             .flatMap(routes ->
                 routes.stream()
                     .filter(route -> route.filterRoutesByDayOfWeek(command.date()))
-                    .filter(Route::filterCircularRoutes)
                     .filter(route -> route.filterDepartAndArriveNode(command.depart(), command.arrive()))
                     .map(route -> route.getShuttleBusScheduleInfo(command.depart()))
             )
