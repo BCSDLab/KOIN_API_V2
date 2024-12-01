d1d8ba9b3c45a3a914585ddc7f74441f5f5f8ab0
신관규 <soundbar91@gmail.com>
2024-12-01 14:54:33 +0900

    fix: flyway 수정

--- src/main/resources/db/migration/V104__add_semester_id_column.sql
+++ src/main/resources/db/migration/V104__add_semester_id_column.sql
@@ -1,11 +1,6 @@
 ALTER TABLE `koin`.`lectures`
     ADD COLUMN `semester_id` INT UNSIGNED NOT NULL;
 
-UPDATE `koin`.`lectures` l
-SET `semester_id` = (SELECT `id`
-                     FROM `koin`.`semester` s
-                     WHERE l.`semester_date` = s.`semester`);
-
 ALTER TABLE `koin`.`lectures`
     ADD CONSTRAINT `FK_LECTURES_ON_SEMESTER`
         FOREIGN KEY (`semester_id`)
--- src/main/resources/db/migration/V105__alter_lecture_drop_semester_date_column.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `koin`.`lectures`
-DROP COLUMN `semester_date`;
