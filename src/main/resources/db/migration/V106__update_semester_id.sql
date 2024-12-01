c36e7747c0cc5a32c856a98f176a027458a853d6
신관규 <soundbar91@gmail.com>
2024-12-01 15:21:59 +0900

    fix: flyway 수정

--- src/main/resources/db/migration/V106__update_semester_id.sql
+++ src/main/resources/db/migration/V106__update_semester_id.sql
@@ -2,9 +2,3 @@ UPDATE `koin`.`lectures` l
 SET `semester_id` = (SELECT `id`
                      FROM `koin`.`semester` s
                      WHERE l.`semester_date` = s.`semester`);
-
-ALTER TABLE `koin`.`lectures`
-    ADD CONSTRAINT `FK_LECTURES_ON_SEMESTER`
-        FOREIGN KEY (`semester_id`)
-            REFERENCES `koin`.`semester` (`id`)
-            ON UPDATE CASCADE;
