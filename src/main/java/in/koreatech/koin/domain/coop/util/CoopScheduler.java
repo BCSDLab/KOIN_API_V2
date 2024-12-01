9f353bc788a5fb72bdbd1f861189a09e6c72a795
dradnats1012 <herjebi1012@gmail.com>
2024-12-02 01:13:05 +0900

    feat: CoopScheduler 클래스 구현

---
+++ src/main/java/in/koreatech/koin/domain/coop/util/CoopScheduler.java
@@ -0,0 +1,30 @@
+package in.koreatech.koin.domain.coop.util;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import in.koreatech.koin.domain.coop.service.CoopService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CoopScheduler {
+
+    private final CoopService coopService;
+
+    @Scheduled(cron = "0 0/6 7-8 * * *")
+    @Scheduled(cron = "0 30-59/6 10 * * *")
+    @Scheduled(cron = "0 0/6 11-12 * * *")
+    @Scheduled(cron = "0 0-30/6 13 * * *")
+    @Scheduled(cron = "0 0/6 16-17 * * *")
+    @Scheduled(cron = "0 0-30/6 18 * * *")
+    public void notifyDiningImageUpload() {
+        try {
+            coopService.sendDiningNotify();
+        } catch (Exception e) {
+            log.warn("식단 이미지 알림 과정에서 오류가 발생했습니다.");
+        }
+    }
+}
