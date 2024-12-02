86a81418863db752f2e32fcf4f875d9c376aa5bc
seongjae6751 <seongjae6751@naver.com>
2024-12-02 22:27:43 +0900

    chore: 불필요한 쿼리 제거

--- src/main/resources/db/migration/V108__add_is_deleted_column_to_article_keywords.sql
+++ src/main/resources/db/migration/V108__add_is_deleted_column_to_article_keywords.sql
@@ -1,5 +1,5 @@
 ALTER TABLE `article_keywords`
-    ADD COLUMN `is_deleted` TINYINT(1) NOT NULL DEFAULT 0 AFTER `updated_at`;
+    ADD COLUMN `is_deleted` TINYINT(1) NOT NULL DEFAULT 0;
 
 ALTER TABLE `article_keyword_user_map`
-    ADD COLUMN `is_deleted` TINYINT(1) NOT NULL DEFAULT 0 AFTER `updated_at`;
+    ADD COLUMN `is_deleted` TINYINT(1) NOT NULL DEFAULT 0;
