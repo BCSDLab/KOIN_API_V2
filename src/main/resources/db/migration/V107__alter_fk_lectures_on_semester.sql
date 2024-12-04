d0aa2442b7a865d3db797aaff20af9574db70f4c
신관규 <soundbar91@gmail.com>
2024-12-04 15:47:11 +0900

    fix: flyway 삭제

--- src/main/resources/db/migration/V107__alter_fk_lectures_on_semester.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `koin`.`lectures`
-    ADD CONSTRAINT `FK_LECTURES_ON_SEMESTER`
-        FOREIGN KEY (`semester_id`)
-            REFERENCES `koin`.`semester` (`id`)
-            ON UPDATE CASCADE;
