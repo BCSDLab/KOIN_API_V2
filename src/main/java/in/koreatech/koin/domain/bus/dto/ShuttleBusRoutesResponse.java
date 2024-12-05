9303f98b1957bf6198a2ef33445e6838973241f5
강인화 <rkddlsghk1998@gmail.com>
2024-12-05 16:15:28 +0900

    feat: 학교버스 노선 조회 response 스웨거 명세

--- src/main/java/in/koreatech/koin/domain/bus/dto/ShuttleBusRoutesResponse.java
+++ src/main/java/in/koreatech/koin/domain/bus/dto/ShuttleBusRoutesResponse.java
@@ -11,25 +11,44 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import in.koreatech.koin.domain.bus.model.mongo.ShuttleBusRoute;
 import in.koreatech.koin.domain.version.dto.VersionMessageResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @JsonNaming(SnakeCaseStrategy.class)
+@Schema(description = "셔틀버스 경로 응답")
 public record ShuttleBusRoutesResponse(
+    @Schema(description = "순환 노선 경로 목록")
     List<RouteRegion> shuttleRoutes,
+    @Schema(description = "주중 노선 경로 목록")
     List<RouteRegion> commutingRoutes,
+    @Schema(description = "주말 노선 경로 목록")
     List<RouteRegion> weekendRoutes,
+    @Schema(description = "학기 정보")
     RouteSemester semesterInfo
 ) {
 
     @JsonNaming(SnakeCaseStrategy.class)
-    private record RouteRegion(String region, List<RouteName> routes) {
+    @Schema(description = "경로 지역 정보")
+    private record RouteRegion(
+        @Schema(description = "지역 이름", example = "천안") String region,
+        @Schema(description = "해당 지역의 경로 목록") List<RouteName> routes
+    ) {
     }
 
     @JsonNaming(SnakeCaseStrategy.class)
-    private record RouteName(String id, String routeName, String subName) {
+    @Schema(description = "경로 이름 정보")
+    private record RouteName(
+        @Schema(description = "경로 ID", example = "675013f9465776d6265ddfdb") String id,
+        @Schema(description = "경로 이름", example = "대학원") String routeName,
+        @Schema(description = "경로 부가 이름", example = "토요일") String subName
+    ) {
     }
 
     @JsonNaming(SnakeCaseStrategy.class)
-    private record RouteSemester(String name, String term) {
+    @Schema(description = "학기 정보")
+    private record RouteSemester(
+        @Schema(description = "학기 이름", example = "정규학기") String name,
+        @Schema(description = "학기 기간", example = "2024-09-02 ~ 2024-12-20") String term
+    ) {
     }
 
     public static ShuttleBusRoutesResponse from(List<ShuttleBusRoute> shuttleBusRoutes,
