f3fecd8ff224aa2be2037ee9b6f3c401ce17f79f
Hyeonsu Lee <127578418+20HyeonsuLee@users.noreply.github.com>
2024-12-06 15:21:27 +0900

    feature: 모든 상점 조회 API에 혜택 상세 필드 추가 (#1121)
    * feat: 모든 상점 조회시 혜택 필드 추가
    
    * refactor: 리뷰 반영
    
    ---------
    
    Co-authored-by: 송선권 <songsunkook@gmail.com>
    Co-authored-by: Hwang HyeonSik <142300831+Choon0414@users.noreply.github.com>
    Co-authored-by: 배진호 <onionring4028@gmail.com>
    Co-authored-by: 김성재 <103095432+seongjae6751@users.noreply.github.com>
    Co-authored-by: 신관규 <soundbar91@gmail.com>
    Co-authored-by: HyeonsuLee <leehyeonsu4888@naver.com>

--- src/main/java/in/koreatech/koin/domain/benefit/repository/BenefitCategoryMapRepository.java
+++ src/main/java/in/koreatech/koin/domain/benefit/repository/BenefitCategoryMapRepository.java
@@ -2,6 +2,7 @@ package in.koreatech.koin.domain.benefit.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
 import in.koreatech.koin.domain.benefit.model.BenefitCategoryMap;
@@ -9,6 +10,13 @@ import in.koreatech.koin.domain.benefit.model.BenefitCategoryMap;
 public interface BenefitCategoryMapRepository extends Repository<BenefitCategoryMap, Integer> {
 
     List<BenefitCategoryMap> findAllByBenefitCategoryId(Integer benefitCategoryId);
+
+    @Query("""
+            SELECT bcm FROM BenefitCategoryMap bcm
+            JOIN FETCH bcm.shop s
+            JOIN FETCH bcm.benefitCategory bc
+            """)
+    List<BenefitCategoryMap> findAllWithFetchJoin();
 
     BenefitCategoryMap save(BenefitCategoryMap benefitCategoryMap);
 }
