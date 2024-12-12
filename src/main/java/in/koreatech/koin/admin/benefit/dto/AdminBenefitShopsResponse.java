610fee0716feb378cf53d09a4b74bd10a2a1e3a0
김성현 <149303551+krSeonghyeon@users.noreply.github.com>
2024-12-12 19:55:54 +0900

    feat: 어드민 혜택 상세 페이지 혜택문구 미리보기 추가 (#1132)
    * feat: 특정 혜택 카테고리 상점 전체 조회에 혜택 문구 미리보기 추가 반환
    
    * feat: 특정 혜택 카테고리 상점 전체 추가에 혜택 문구 미리보기 추가
    
    * feat: 특정 혜택 카테고리 상점 수정 API 추가
    
    * test: 특정 혜택 카테고리의 상점 전체 조회 테스트 추가
    
    * test: 특정 혜택 카테고리의 상점 추가 테스트 작성
    
    * test: 특정 혜택 카테고리의 상점들 수정 테스트 작성
    
    * chore: 개행추가
    
    * chore: 멀티라인 문자열로 변경
    
    * chore: 멀티라인 문자열로 변경
    
    * feat: 수정에서 shopId가 아닌 mapId를 이용하도록 변경
    
    * feat: 불필요한 schema 삭제

--- src/main/java/in/koreatech/koin/admin/benefit/dto/AdminBenefitShopsResponse.java
+++ src/main/java/in/koreatech/koin/admin/benefit/dto/AdminBenefitShopsResponse.java
@@ -5,9 +5,11 @@ import java.util.List;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import in.koreatech.koin.domain.benefit.model.BenefitCategoryMap;
 import in.koreatech.koin.domain.shop.model.shop.Shop;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@JsonNaming(SnakeCaseStrategy.class)
 public record AdminBenefitShopsResponse(
     @Schema(example = "3", description = "상점 개수")
     Integer count,
@@ -16,27 +18,36 @@ public record AdminBenefitShopsResponse(
     List<InnerShopResponse> shops
 ) {
 
-    public static AdminBenefitShopsResponse from(List<Shop> shops) {
+    public static AdminBenefitShopsResponse from(List<BenefitCategoryMap> benefitCategoryMaps) {
         return new AdminBenefitShopsResponse(
-            shops.size(),
-            shops.stream()
+            benefitCategoryMaps.size(),
+            benefitCategoryMaps.stream()
                 .map(InnerShopResponse::from)
                 .toList()
         );
     }
 
+    @JsonNaming(SnakeCaseStrategy.class)
     private record InnerShopResponse(
+        @Schema(example = "1", description = "상점혜택 매핑id")
+        Integer shopBenefitMapId,
+
         @Schema(example = "1", description = "고유 id")
         Integer id,
 
         @Schema(example = "수신반점", description = "이름")
-        String name
+        String name,
+
+        @Schema(example = "4인 이상 픽업서비스", description = "혜택 미리보기 문구")
+        String detail
     ) {
 
-        public static InnerShopResponse from(Shop shop) {
+        public static InnerShopResponse from(BenefitCategoryMap benefitCategoryMap) {
             return new InnerShopResponse(
-                shop.getId(),
-                shop.getName()
+                benefitCategoryMap.getId(),
+                benefitCategoryMap.getShop().getId(),
+                benefitCategoryMap.getShop().getName(),
+                benefitCategoryMap.getDetail()
             );
         }
     }
--- src/main/java/in/koreatech/koin/admin/benefit/dto/AdminModifyBenefitShopsRequest.java
+++ src/main/java/in/koreatech/koin/admin/benefit/dto/AdminModifyBenefitShopsRequest.java
@@ -1,10 +1,10 @@
 package in.koreatech.koin.admin.benefit.dto;
 
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.*;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import java.util.List;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import in.koreatech.koin.global.validation.NotBlankElement;
@@ -14,17 +14,17 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @JsonNaming(SnakeCaseStrategy.class)
-public record AdminCreateBenefitShopsRequest(
-    @NotNull(message = "상점정보 리스트는 필수입니다.")
-    @NotBlankElement(message = "상점정보 리스트는 빈 요소가 존재할 수 없습니다.")
-    List<InnerBenefitShopsRequest> shopDetails
+public record AdminModifyBenefitShopsRequest(
+    @NotNull(message = "혜택문구 변경정보 리스트는 필수입니다.")
+    @NotBlankElement(message = "혜택문구 변경정보 리스트는 빈 요소가 존재할 수 없습니다.")
+    List<InnerBenefitShopsRequest> modifyDetails
 ) {
 
     @JsonNaming(SnakeCaseStrategy.class)
     public record InnerBenefitShopsRequest(
-        @Schema(description = "상점 고유 id", example = "2", requiredMode = REQUIRED)
-        @NotNull(message = "상점은 필수입니다.")
-        Integer shopId,
+        @Schema(description = "상점혜택 매핑id", example = "2", requiredMode = REQUIRED)
+        @NotNull(message = "상점혜택 매핑id는 필수입니다.")
+        Integer shopBenefitMapId,
 
         @Schema(description = "혜택 미리보기 문구", example = "4인 이상 픽업서비스", requiredMode = REQUIRED)
         @NotBlank(message = "혜택 미리보기 문구는 필수입니다.")
