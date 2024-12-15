b4a0e4c6677fcd6199111ef89248fb6a49771b74
허준기 <112807640+dradnats1012@users.noreply.github.com>
2024-12-15 12:43:42 +0900

    refactor: 이미지 업로드 알림 로직 분리  (#1124)
    * feat: DiningTypeNotFoundException 클래스 구현
    
    * feat: CoopShopService 클래스 diningType 찾는 메서드 구현
    
    * feat: DiningNotifyCache Redis 클래스 생성
    
    * feat: DiningNotifyCacheRepository Redis용 클래스 구현
    
    * feat: DiningRepository 날짜, 타입, 장소 기준으로 이미지 업로드 여부 확인 메서드 구현
    
    * feat: 식단 이미지 저장시 알림 발송 삭제, 스케줄러용 알림 발송 로직 구현
    
    * feat: CoopScheduler 클래스 구현
    
    * feat: LocalTime clock 추가
    
    * feat: test 완성
    
    * fix: 10분전 알림 로직 수정
    
    * fix: 테스트용 코드 삭제
    
    * fix: 테스트 수정
    
    * fix: conflict 해결
    
    * fix: 리뷰 반영

--- src/main/java/in/koreatech/koin/domain/coop/service/CoopService.java
+++ src/main/java/in/koreatech/koin/domain/coop/service/CoopService.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.time.Clock;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -38,12 +39,15 @@ import in.koreatech.koin.domain.coop.exception.DiningNowDateException;
 import in.koreatech.koin.domain.coop.exception.StartDateAfterEndDateException;
 import in.koreatech.koin.domain.coop.model.Coop;
 import in.koreatech.koin.domain.coop.model.DiningImageUploadEvent;
+import in.koreatech.koin.domain.coop.model.DiningNotifyCache;
 import in.koreatech.koin.domain.coop.model.DiningSoldOutEvent;
 import in.koreatech.koin.domain.coop.repository.CoopRepository;
+import in.koreatech.koin.domain.coop.repository.DiningNotifyCacheRepository;
 import in.koreatech.koin.domain.coop.repository.DiningSoldOutCacheRepository;
 import in.koreatech.koin.domain.coopshop.model.CoopShopType;
 import in.koreatech.koin.domain.coopshop.service.CoopShopService;
 import in.koreatech.koin.domain.dining.model.Dining;
+import in.koreatech.koin.domain.dining.model.DiningType;
 import in.koreatech.koin.domain.dining.repository.DiningRepository;
 import in.koreatech.koin.domain.user.model.User;
 import in.koreatech.koin.domain.user.model.UserToken;
@@ -61,11 +65,13 @@ public class CoopService {
     private final ApplicationEventPublisher eventPublisher;
     private final DiningRepository diningRepository;
     private final DiningSoldOutCacheRepository diningSoldOutCacheRepository;
+    private final DiningNotifyCacheRepository diningNotifyCacheRepository;
     private final CoopRepository coopRepository;
     private final UserTokenRepository userTokenRepository;
     private final CoopShopService coopShopService;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
+    private final List<String> placeFilters = Arrays.asList("A코너", "B코너", "C코너");
 
     public static final LocalDate LIMIT_DATE = LocalDate.of(2022, 11, 29);
     private final int EXCEL_COLUMN_COUNT = 8;
@@ -90,15 +96,56 @@ public class CoopService {
     @Transactional
     public void saveDiningImage(DiningImageRequest imageRequest) {
         Dining dining = diningRepository.getById(imageRequest.menuId());
-        boolean isImageExist = diningRepository.existsByDateAndTypeAndImageUrlIsNotNull(dining.getDate(),
-            dining.getType());
-
-        LocalDateTime now = LocalDateTime.now(clock);
-        boolean isOpened = coopShopService.getIsOpened(now, CoopShopType.CAFETERIA, dining.getType(), true);
-        if (isOpened && !isImageExist) {
-            eventPublisher.publishEvent(new DiningImageUploadEvent(dining.getId(), dining.getImageUrl()));
-        }
         dining.setImageUrl(imageRequest.imageUrl());
+    }
+
+    public void sendDiningNotify() {
+        DiningType diningType = coopShopService.getDiningType();
+        LocalDate nowDate = LocalDate.now(clock);
+        List<Dining> dinings = diningRepository.findAllByDateAndType(nowDate, diningType);
+
+        if (dinings.isEmpty()) {
+            return;
+        }
+
+        boolean allImageExist = diningRepository.allExistsByDateAndTypeAndPlacesAndImageUrlIsNotNull(
+            nowDate, diningType, placeFilters
+        );
+
+        boolean isOpened = coopShopService.getIsOpened(
+            LocalDateTime.now(clock), CoopShopType.CAFETERIA, diningType, true
+        );
+
+        String diningNotifyId = nowDate.toString() + diningType;
+
+        if (isOpened && allImageExist) {
+            if (alreadyNotify(diningNotifyId))
+                return;
+
+            if (!diningNotifyCacheRepository.existsById(diningNotifyId)) {
+                sendNotify(diningNotifyId, dinings);
+            }
+        }
+
+        if (LocalTime.now().isAfter(diningType.getStartTime().minusMinutes(10))
+            && LocalTime.now().isBefore(diningType.getStartTime())
+            && !diningNotifyCacheRepository.existsById(diningNotifyId)
+            && diningRepository.existsByDateAndTypeAndImageUrlIsNotNull(nowDate, diningType)
+        ) {
+            sendNotify(diningNotifyId, dinings);
+        }
+    }
+
+    private boolean alreadyNotify(String diningNotifyId) {
+        if (diningNotifyCacheRepository.existsById(diningNotifyId)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void sendNotify(String diningNotifyId, List<Dining> dinings) {
+        diningNotifyCacheRepository.save(DiningNotifyCache.from(diningNotifyId));
+        eventPublisher.publishEvent(new DiningImageUploadEvent(dinings.get(0).getId(), dinings.get(0).getImageUrl()));
     }
 
     @Transactional
@@ -145,6 +192,7 @@ public class CoopService {
         if (startDate.isAfter(today) || endDate.isAfter(today)) {
             throw new DiningNowDateException("오늘 날짜 이후 기간은 설정할 수 없어요.");
         }
+
         if (startDate.isAfter(endDate)) {
             throw new StartDateAfterEndDateException("시작일은 종료일 이전으로 설정해주세요.");
         }
@@ -217,10 +265,10 @@ public class CoopService {
         row.createCell(6).setCellValue(Optional.ofNullable(dining.getSoldOut()).map(Object::toString).orElse(""));
         row.createCell(7).setCellValue(Optional.ofNullable(dining.getIsChanged()).map(Object::toString).orElse(""));
 
-        for (int i = 0; i < 8; i++) {
-            row.getCell(i).setCellStyle(commonStyle);
+            for (int i = 0; i < EXCEL_COLUMN_COUNT; i++) {
+                row.getCell(i).setCellStyle(commonStyle);
+            }
         }
-    }
 
     private String formatMenu(List<String> menu) {
         return String.join("\n", menu);
