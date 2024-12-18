904e6a608ca07554eb3a070300d572d3f094f712
dradnats1012 <herjebi1012@gmail.com>
2024-12-18 16:21:33 +0900

    fix: 스케줄러 cron식 수정

--- src/main/java/in/koreatech/koin/domain/coop/util/CoopScheduler.java
+++ src/main/java/in/koreatech/koin/domain/coop/util/CoopScheduler.java
@@ -15,8 +15,10 @@ public class CoopScheduler {
     private final CoopService coopService;
 
     @Scheduled(cron = "0 0/6 7 * * *")
-    @Scheduled(cron = "0 30/6 10-11 * * *")
-    @Scheduled(cron = "0 30/6 16-17 * * *")
+    @Scheduled(cron = "0 30/6 10 * * *")
+    @Scheduled(cron = "0 0/6 11 * * *")
+    @Scheduled(cron = "0 30/6 16 * * *")
+    @Scheduled(cron = "0 0/6 17 * * *")
     public void notifyDiningImageUpload() {
         try {
             coopService.sendDiningNotify();
