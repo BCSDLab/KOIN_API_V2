da0b03750ad8edff5f84ac7856f443897c369ed9
김원경 <kyg279044@gmail.com>
2024-12-21 16:19:10 +0900

    fix : 테스트코드 작성

--- src/test/java/in/koreatech/koin/acceptance/SemesterApiTest.java
+++ src/test/java/in/koreatech/koin/acceptance/SemesterApiTest.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import in.koreatech.koin.AcceptanceTest;
 import in.koreatech.koin.domain.timetable.model.Lecture;
 import in.koreatech.koin.domain.timetable.model.Semester;
-import in.koreatech.koin.domain.timetableV3.model.SemesterV3;
 import in.koreatech.koin.domain.user.model.User;
 import in.koreatech.koin.fixture.LectureFixture;
 import in.koreatech.koin.fixture.SemesterFixture;
@@ -139,54 +138,72 @@ public class SemesterApiTest extends AcceptanceTest {
             .andExpect(content().json("""
                 [
                     {
-                        "id": 8,
-                        "semester": "2020-겨울",
                         "year": 2020,
                         "term": "겨울학기"
                     },
                     {
-                        "id": 6,
-                        "semester": "20202",
                         "year": 2020,
                         "term": "2학기"
                     },
                     {
-                        "id": 7,
-                        "semester": "2020-여름",
                         "year": 2020,
                         "term": "여름학기"
                     },
                     {
-                        "id": 5,
-                        "semester": "20201",
                         "year": 2020,
                         "term": "1학기"
                     },
                     {
-                        "id": 4,
-                        "semester": "2019-겨울",
                         "year": 2019,
                         "term": "겨울학기"
                     },
                     {
-                        "id": 2,
-                        "semester": "20192",
                         "year": 2019,
                         "term": "2학기"
                     },
                     {
-                        "id": 3,
-                        "semester": "2019-여름",
                         "year": 2019,
                         "term": "여름학기"
                     },
                     {
-                        "id": 1,
-                        "semester": "20191",
                         "year": 2019,
                         "term": "1학기"
                     }
                 ]
+                """));
+    }
+
+    @Test
+    void 학생이_가진_시간표의_학기를_조회한다_V3() throws Exception {
+        User user = userFixture.준호_학생().getUser();
+        String token = userFixture.getToken(user);
+        Semester semester1 = semesterFixture.semesterV3_2019년도_2학기();
+        Semester semester2 = semesterFixture.semesterV3_2020년도_1학기();
+        Lecture HRD_개론 = lectureFixture.HRD_개론(semester1.getSemester());
+        Lecture 건축구조의_이해_및_실습 = lectureFixture.건축구조의_이해_및_실습(semester2.getSemester());
+        timetableV2Fixture.시간표6(user, semester1, HRD_개론, null);
+        timetableV2Fixture.시간표6(user, semester2, 건축구조의_이해_및_실습, null);
+
+        mockMvc.perform(
+                get("/v3/semesters/check")
+                    .header("Authorization", "Bearer " + token)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().json("""
+                {
+                    "user_id": 1,
+                    "semesters": [
+                    {
+                        "year": 2019,
+                        "term": "2학기"
+                    },
+                    {
+                        "year": 2020,
+                        "term": "1학기"
+                    }
+                    ]
+                }
                 """));
     }
 }
--- src/test/java/in/koreatech/koin/fixture/SemesterFixture.java
+++ src/test/java/in/koreatech/koin/fixture/SemesterFixture.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 import in.koreatech.koin.domain.timetable.model.Semester;
 import in.koreatech.koin.domain.timetable.repository.SemesterRepository;
-import in.koreatech.koin.domain.timetableV3.model.SemesterV3;
 import in.koreatech.koin.domain.timetableV3.model.Term;
 import in.koreatech.koin.domain.timetableV3.repository.SemesterRepositoryV3;
 
@@ -28,9 +27,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2019년도_1학기() {
+    public Semester semesterV3_2019년도_1학기() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("20191")
             .year(2019)
             .term(Term.FIRST)
@@ -38,9 +37,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2019년도_2학기() {
+    public Semester semesterV3_2019년도_2학기() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("20192")
             .year(2019)
             .term(Term.SECOND)
@@ -48,9 +47,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2019년도_여름() {
+    public Semester semesterV3_2019년도_여름() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("2019-여름")
             .year(2019)
             .term(Term.SUMMER)
@@ -58,9 +57,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2019년도_겨울() {
+    public Semester semesterV3_2019년도_겨울() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("2019-겨울")
             .year(2019)
             .term(Term.WINTER)
@@ -68,9 +67,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2020년도_1학기() {
+    public Semester semesterV3_2020년도_1학기() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("20201")
             .year(2020)
             .term(Term.FIRST)
@@ -78,9 +77,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2020년도_2학기() {
+    public Semester semesterV3_2020년도_2학기() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("20202")
             .year(2020)
             .term(Term.SECOND)
@@ -88,9 +87,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2020년도_여름() {
+    public Semester semesterV3_2020년도_여름() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("2020-여름")
             .year(2020)
             .term(Term.SUMMER)
@@ -98,9 +97,9 @@ public class SemesterFixture {
         );
     }
 
-    public SemesterV3 semesterV3_2020년도_겨울() {
+    public Semester semesterV3_2020년도_겨울() {
         return semesterRepositoryV3.save(
-            SemesterV3.builder()
+            Semester.builder()
             .semester("2020-겨울")
             .year(2020)
             .term(Term.WINTER)
