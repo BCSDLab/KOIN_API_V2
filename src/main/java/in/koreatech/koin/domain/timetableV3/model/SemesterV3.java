f11caf13b4786c9b4bcd26d04f7cfebb50566dbb
김원경 <kyg279044@gmail.com>
2024-12-21 14:53:41 +0900

    feat: Semester V3생성

--- src/main/java/in/koreatech/koin/domain/timetableV3/model/SemesterV3.java
+++ src/main/java/in/koreatech/koin/domain/timetableV3/model/SemesterV3.java
@@ -1,21 +1,15 @@
-package in.koreatech.koin.domain.timetable.model;
+package in.koreatech.koin.domain.timetableV3.model;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import in.koreatech.koin.domain.timetableV3.model.Term;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -25,18 +19,16 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "semester")
 @NoArgsConstructor(access = PROTECTED)
-public class Semester {
+public class SemesterV3 {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Integer id;
 
     @Size(max = 10)
-    @NotNull
     @Column(name = "semester", nullable = false, unique = true)
     private String semester;
 
-    @NotNull
     @Column(name = "year", nullable = false)
     private Integer year;
 
@@ -46,7 +38,9 @@ public class Semester {
     private Term term;
 
     @Builder
-    public Semester(String semester) {
+    public SemesterV3(String semester, Integer year, Term term) {
         this.semester = semester;
+        this.year = year;
+        this.term = term;
     }
 }
