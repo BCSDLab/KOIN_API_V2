e43732778229697f25cb0384feb25e4d623185b5
김원경 <kyg279044@gmail.com>
2024-12-29 21:51:17 +0900

    fix: 리뷰 반영

--- src/main/java/in/koreatech/koin/domain/graduation/repository/StudentCourseCalculationRepository.java
+++ src/main/java/in/koreatech/koin/domain/graduation/repository/StudentCourseCalculationRepository.java
@@ -2,15 +2,15 @@ package in.koreatech.koin.domain.graduation.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.Repository;
 
 import in.koreatech.koin.domain.graduation.model.StudentCourseCalculation;
 
-@Repository
-public interface StudentCourseCalculationRepository extends JpaRepository<StudentCourseCalculation, Integer> {
+public interface StudentCourseCalculationRepository extends Repository<StudentCourseCalculation, Integer> {
 
     Optional<StudentCourseCalculation> findByUserId(Integer userId);
 
     void deleteAllByUserId(Integer userId);
+
+    void save(StudentCourseCalculation studentCourseCalculation);
 }
