77b6feffb174db9875c1d8adb5be04d5bc06adb2
신관규 <soundbar91@gmail.com>
2024-12-31 17:08:51 +0900

    feat: 시간표 조회 API 구현

--- src/main/java/in/koreatech/koin/domain/timetableV3/service/TimetableLectureServiceV3.java
+++ src/main/java/in/koreatech/koin/domain/timetableV3/service/TimetableLectureServiceV3.java
@@ -1,15 +1,32 @@
 package in.koreatech.koin.domain.timetableV3.service;
 
+import static in.koreatech.koin.domain.timetableV2.util.GradeCalculator.calculateGradesMainFrame;
+import static in.koreatech.koin.domain.timetableV2.util.GradeCalculator.calculateTotalGrades;
+import static in.koreatech.koin.domain.timetableV2.validation.TimetableFrameValidate.validateUserAuthorization;
+
 import org.springframework.stereotype.Service;
 
+import in.koreatech.koin.domain.timetableV2.dto.response.TimetableLectureResponse;
+import in.koreatech.koin.domain.timetableV2.model.TimetableFrame;
 import in.koreatech.koin.domain.timetableV3.dto.response.TimetableLectureResponseV3;
+import in.koreatech.koin.domain.timetableV3.repository.TimetableFrameRepositoryV3;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class TimetableLectureServiceV3 {
 
+    private final TimetableFrameRepositoryV3 timetableFrameRepositoryV3;
+
     public TimetableLectureResponseV3 getTimetableLecture(Integer timetableFrameId, Integer userId) {
-        return null;
+        TimetableFrame frame = timetableFrameRepositoryV3.getById(timetableFrameId);
+        validateUserAuthorization(frame.getUser().getId(), userId);
+        return getTimetableLectureResponse(userId, frame);
+    }
+
+    private TimetableLectureResponseV3 getTimetableLectureResponse(Integer userId, TimetableFrame timetableFrame) {
+        int grades = calculateGradesMainFrame(timetableFrame);
+        int totalGrades = calculateTotalGrades(timetableFrameRepositoryV3.findByUserIdAndIsMainTrue(userId));
+        return TimetableLectureResponseV3.of(timetableFrame, grades, totalGrades);
     }
 }
