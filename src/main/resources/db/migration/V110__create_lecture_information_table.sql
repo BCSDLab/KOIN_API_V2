cdd6f649a230542f6561fd35eda5cd1d667e54c1
신관규 <soundbar91@gmail.com>
2025-01-04 00:48:52 +0900

    chore: flyway 삭제

--- src/main/resources/db/migration/V110__create_lecture_information_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `koin`.`lecture_information`(
-    `id`         int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '고유 id',
-    `lecture_id` int UNSIGNED NOT NULL COMMENT 'lecture 고유 id',
-    `start_time` int NULL COMMENT '정규 강의 시작 시간',
-    `end_time`   int NULL COMMENT '정규 강의 마감 시간',
-    PRIMARY KEY (`id`),
-    index (`lecture_id`)
-);
