8c9cba90d747a3497832d492a234480c0a06bae5
신관규 <soundbar91@gmail.com>
2025-01-06 00:12:19 +0900

    feat: lecture, timetableLecture API v3 (#1156)
    * feat: 정규 강의 생성 API
    
    * chore: 컨트롤러 네이밍 변경
    
    * chore: 스웨거 설명 변경
    
    * chore: 스웨거 에러 코드 추가
    
    * chore: 서비스 네이밍 변경
    
    * chore: 서비스 네이밍 변경
    
    * feat: 커스텀 강의 생성 API
    
    * chore: 미사용 import 삭제
    
    * fix: 조회 버그 수정
    
    * chore: api implements
    
    * docs: TODO 작성
    
    * feat: 커스텀 강의 수정 API 기본 틀 추가
    
    * feat: 정규 강의 수정 API 기본 틀 추가
    
    * feat: 커스텀 강의 수정 API dto 수정
    
    * feat: timetableLectureV3 조회 API 기본 틀
    
    * chore: 미사용 import 삭제
    
    * chore: 미사용 import 삭제
    
    * feat: Lecture V3
    
    * feat: lecture 정규화
    
    * chore: lecture - LectureInformation 연간관계 설정
    
    * chore: 메소드 명 변경 및 미사용 import 삭제
    
    * feat: 커스텀 강의 업데이트 로직 구현
    
    * feat: 정규 강의 수정 임시 커밋
    
    * feat: 정규 강의 수정 기능 구현
    
    * fix: 응답 버그 수정
    
    * docs: TODO 작성
    
    * feat: 시간표 조회 API 구현
    
    * feat: timetableV3 스웨거 그룹화
    
    * feat: TimetableLectureInformation 추가 및 flyway 수정
    
    * fix: TimetableLEctureInformation으로 코드 변경
    
    * chore: 이전 코드 삭제
    
    * chore: 이전 코드 삭제 및 flyway 수정
    
    * feat: 강의 중복 체크 추가
    
    * feat: 프로시저 추가
    
    * chore: 정규 강의 API 정규화 롤백
    
    * chore: 커스텀 강의 API 정규화 롤백
    
    * chore: 응답값 설정
    
    * chore: 패키지 이동
    
    * chore: 코드 위치 변경
    
    * feat: 롤백 API
    
    * chore: 미사용 import 삭제
    
    * test: 테스트 코드 작성
    
    * test: 테스트 코드 작성
    
    * chore: 주석 작성
    
    * chore: flyway 삭제
    
    * chore: 주석 수정
    
    * chore: 주석 수정
    
    * fix: dto 의존성 끊기
    
    * test: 테스트 실패 수정
    
    * fix: 정규 강의 수정기능 버그 수정
    
    * docs: 스웨거 응답 코드 변경
    
    * chore: lecture API 파라미터 변경
    
    * test: 테스트 수정
    
    * chore: 리뷰 반영
    
    * chore: 커스텀 정규화 메소드 수정
    
    * chore: 리뷰 반영
    
    * chore: lecture v3 파라미터 변경
    
    * chore: frame 복구 api 삭제
    
    * Revert "chore: frame 복구 api 삭제"
    
    This reverts commit 7f293c74ebb7e7db0b85654ff36327bd90784700.
    
    * chore: 리뷰 반영
    
    * chore: 리뷰 반영

--- src/main/java/in/koreatech/koin/domain/timetableV3/repository/TimetableFrameRepositoryV3.java
+++ src/main/java/in/koreatech/koin/domain/timetableV3/repository/TimetableFrameRepositoryV3.java
@@ -1,12 +1,36 @@
 package in.koreatech.koin.domain.timetableV3.repository;
 
-import org.springframework.data.repository.Repository;
-
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import in.koreatech.koin.domain.timetable.exception.TimetableNotFoundException;
+import in.koreatech.koin.domain.timetable.model.Semester;
+import in.koreatech.koin.domain.timetableV2.exception.TimetableFrameNotFoundException;
 import in.koreatech.koin.domain.timetableV2.model.TimetableFrame;
+import in.koreatech.koin.domain.user.model.User;
 
 public interface TimetableFrameRepositoryV3 extends Repository<TimetableFrame, Integer> {
 
+    Optional<TimetableFrame> findById(Integer id);
+
+    default TimetableFrame getById(Integer id) {
+        return findById(id)
+            .orElseThrow(() -> TimetableNotFoundException.withDetail("id: " + id));
+    }
+
     List<TimetableFrame> findByUserIdAndIsMainTrue(Integer userId);
+
+    @Query(value = "SELECT * FROM timetable_frame WHERE id = :id", nativeQuery = true)
+    Optional<TimetableFrame> findByIdWithDeleted(@Param("id") Integer id);
+
+    default TimetableFrame getByIdWithDeleted(Integer id) {
+        return findByIdWithDeleted(id)
+            .orElseThrow(() -> TimetableFrameNotFoundException.withDetail("id: " + id));
+    }
+
+    boolean existsByUserAndSemester(User user, Semester semester);
 }
