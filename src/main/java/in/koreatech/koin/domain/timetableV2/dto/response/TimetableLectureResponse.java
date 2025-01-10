4784c3dd4f2b73853a90adea56be4ebefa59699f
신관규 <soundbar91@gmail.com>
2025-01-10 16:52:49 +0900

    fix: 온라인 정규 강의 classInfo 처리 추가

--- src/main/java/in/koreatech/koin/domain/timetableV2/dto/response/TimetableLectureResponse.java
+++ src/main/java/in/koreatech/koin/domain/timetableV2/dto/response/TimetableLectureResponse.java
@@ -5,6 +5,7 @@ import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -82,6 +83,10 @@ public record TimetableLectureResponse(
             public static List<ClassInfo> of(String classTime, String classPlace) {
                 // 강의 장소가 없는 경우 강의 시간과 매핑을 못하기 때문에 바로 반환
                 if (classPlace == null) {
+                    // 온라인 강의인 경우
+                    if (classTime.equals("[]")) {
+                        return Collections.emptyList();
+                    }
                     return List.of(new ClassInfo(parseClassTimes(classTime), null));
                 }
 
