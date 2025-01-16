2c88f26e805a9a5e0f8cfad26e807f9336af7371
강인화 <164142264+kih1015@users.noreply.github.com>
2025-01-16 21:36:43 +0900

    fix: 시내버스 시간표 한기대 출발 시간 보정 (#1152)
    * fix: 코리아텍 승차 시간 오프셋 설정
    
    코리아텍 승차 시간의 기점과의 오차를 보정하도록 로직 수정
    
    * feat: 리뷰 반영

--- src/main/java/in/koreatech/koin/domain/bus/model/enums/CityBusDirection.java
+++ src/main/java/in/koreatech/koin/domain/bus/model/enums/CityBusDirection.java
@@ -5,13 +5,19 @@ import java.util.Arrays;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 import in.koreatech.koin.domain.bus.exception.BusTypeNotFoundException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Getter
 public enum CityBusDirection {
-    종합터미널,
-    병천3리,
-    황사동,
-    유관순열사사적지
-    ;
+    종합터미널("코리아텍", "천안 터미널"),
+    병천3리("천안 터미널", "코리아텍"),
+    황사동("천안 터미널", "코리아텍"),
+    유관순열사사적지("천안 터미널", "코리아텍");
+
+    private final String departNode;
+    private final String apartNode;
 
     @JsonCreator
     public static CityBusDirection from(String direction) {
