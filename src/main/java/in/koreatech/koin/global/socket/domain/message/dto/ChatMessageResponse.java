90226fa331402ea79fb8144ff1c384a4f57e6e97
DHkimgit <dukekim001@naver.com>
2025-01-22 11:22:50 +0900

    fix : snake case 적용

--- src/main/java/in/koreatech/koin/global/socket/domain/message/dto/ChatMessageResponse.java
+++ src/main/java/in/koreatech/koin/global/socket/domain/message/dto/ChatMessageResponse.java
@@ -2,9 +2,14 @@ package in.koreatech.koin.global.socket.domain.message.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import in.koreatech.koin.global.socket.domain.message.model.ChatMessageCommand;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@JsonNaming(value = SnakeCaseStrategy.class)
 public record ChatMessageResponse(
     @Schema(description = "메시지 전송 사용자 ID", example = "5665")
     Integer userId,
