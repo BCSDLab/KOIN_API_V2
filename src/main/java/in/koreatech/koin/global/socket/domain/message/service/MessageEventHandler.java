6698f2107474dbe3749c92de023291064f6614e4
DHkimgit <dukekim001@naver.com>
2025-01-27 01:21:48 +0900

    chore : 주석 제거

--- src/main/java/in/koreatech/koin/global/socket/domain/message/service/MessageEventHandler.java
+++ src/main/java/in/koreatech/koin/global/socket/domain/message/service/MessageEventHandler.java
@@ -14,9 +14,7 @@ public class MessageEventHandler {
     private final ApplicationEventPublisher eventPublisher;
 
     public void onMessageReceived(Integer articleId, Integer chatRoomId, Integer userId, ChatMessageCommand messageCommand) {
-        System.out.println("이벤트 발행 시작");
         MessageReceivedEvent event = new MessageReceivedEvent(articleId, chatRoomId, userId, messageCommand);
         eventPublisher.publishEvent(event);
-        System.out.println("이벤트 발행 완료");
     }
 }
