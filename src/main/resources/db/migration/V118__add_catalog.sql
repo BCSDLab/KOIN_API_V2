34ef6de9e4506d71d0d22b9d8c3f6fc9376a1234
신관규 <soundbar91@gmail.com>
2025-01-28 12:14:54 +0900

    feat: 교양 영역 테이블 추가 (#1205)
    * feat: 이수 구분 수정 기능 추가
    
    * feat: 교양 영역 엔티티 추가
    
    * feat: Catalog - GeneralEducationArea 연관관계 설정
    
    * feat: Catalog - Department 연관관계 설정
    
    * chore: 리뷰 반영

--- src/main/resources/db/migration/V118__add_catalog.sql
+++ src/main/resources/db/migration/V118__add_catalog.sql
@@ -5,13 +5,15 @@ CREATE TABLE if not exists `koin`.`catalog`
     `code`           VARCHAR(20)  NOT NULL comment '강의 코드',
     `lecture_name`   VARCHAR(255) NOT NULL comment '강의 이름',
     `department_id`  INT UNSIGNED NOT NULL comment '학과 id',
-    `major_id`  INT UNSIGNED NOT NULL comment '전공 id',
+    `major_id`       INT UNSIGNED NULL comment '전공 id',
     `credit`         INT UNSIGNED NOT NULL DEFAULT 0 comment '학점',
     `course_type_id` INT UNSIGNED NOT NULL comment '이수 구분 id',
+    `general_education_area_id` INT UNSIGNED NULL comment '교양 영역 id',
     PRIMARY KEY (`id`),
     FOREIGN KEY (`major_id`) REFERENCES `major` (`id`),
     FOREIGN KEY (`course_type_id`) REFERENCES `course_type` (`id`),
     FOREIGN KEY (`department_id`) REFERENCES `department` (`id`),
+    FOREIGN KEY (`general_education_area_id`) REFERENCES `general_education_area` (`id`),
     created_at       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP comment '생성 일자',
     updated_at       timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '수정 일자'
 );
