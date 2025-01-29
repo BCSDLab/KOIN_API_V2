0c6c1fa6dbe3ee1b288faeba6c2a7a23ed424cc9
dradnats1012 <herjebi1012@gmail.com>
2025-01-29 19:52:45 +0900

    refactor: 코드 리팩토링

--- src/main/java/in/koreatech/koin/domain/coop/service/CoopService.java
+++ src/main/java/in/koreatech/koin/domain/coop/service/CoopService.java
@@ -26,7 +26,6 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFColor;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -79,10 +78,13 @@ public class CoopService {
     private final CoopShopService coopShopService;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
-    private final List<String> placeFilters = Arrays.asList("A코너", "B코너", "C코너");
+    private final List<String> cafeteriaPlaceFilters = Arrays.asList("A코너", "B코너", "C코너");
+    private final List<String> allPlaceFilters = Arrays.asList("A코너", "B코너", "C코너", "능수관", "2캠퍼스");
 
     public static final LocalDate LIMIT_DATE = LocalDate.of(2022, 11, 29);
-    private final int EXCEL_COLUMN_COUNT = 8;
+    private static final int mealColumIndex = 0;
+    private static final int cornerColumnIndex = 1;
+    private static final int mealAndColumnWidth = 4000;
 
     @Transactional
     public void changeSoldOut(SoldOutRequest soldOutRequest) {
@@ -184,125 +186,36 @@ public class CoopService {
     }
 
     public ByteArrayInputStream generateDiningExcel(LocalDate startDate, LocalDate endDate, Boolean isCafeteria) {
-        checkDuplicateExcelRequest(startDate, endDate);     // 중복 요청 검사
-        validateDates(startDate, endDate);                  // 날짜 유효성 검사
-        List<Dining> dinings = fetchDiningData(startDate, endDate, isCafeteria);        // 식단 뽑기
+        checkDuplicateExcelRequest(startDate, endDate);
+        validateDates(startDate, endDate);
+        List<Dining> dinings = fetchDiningData(startDate, endDate, isCafeteria);
 
         Map<LocalDate, List<Dining>> dateDinings = dinings.stream()
             .collect(Collectors.groupingBy(
-                Dining::getDate, // 그룹화 기준
-                TreeMap::new,    // 정렬된 Map 생성
-                Collectors.toList() // 그룹화된 값 리스트화
+                Dining::getDate,
+                TreeMap::new,
+                Collectors.toList()
             ));
 
-        try (SXSSFWorkbook workbook = new SXSSFWorkbook()) {
-            //SXSSFSheet sheet = createSheet(workbook, "식단 메뉴");
-            ByteArrayInputStream excelDesign = createDiningExcelWithDetailedFormat(dateDinings, isCafeteria);
+        ByteArrayInputStream excelDesign = createDiningExcelWithDetailedFormat(dateDinings, isCafeteria);
 
-            return excelDesign;
-        } catch (IOException e) {
-            throw new RuntimeException("엑셀 파일 생성 중 오류가 발생했습니다.", e);
-        }
+        return excelDesign;
     }
 
     private ByteArrayInputStream createDiningExcelWithDetailedFormat(
         Map<LocalDate, List<Dining>> dateDinings, Boolean isCafeteria
     ) {
         Workbook workbook = new XSSFWorkbook();
+        Map<String, CellStyle> cellStyles = initializeWorkbookAndStyles(workbook);
         Sheet sheet = workbook.createSheet("식단");
-        CellStyle headerStyle = createHeaderStyle(workbook);
-        CellStyle commonStyle = createCommonStyle(workbook);
-        CellStyle cornerStyle = createCornerStyle(workbook);
 
+        Map<Integer, Row> rowCache = initializeRowCache(sheet, 100);
         List<String> meals = List.of("조식", "중식", "석식");
-        List<String> corners;
+        List<String> corners = isCafeteria ? cafeteriaPlaceFilters : allPlaceFilters;
 
-        if (isCafeteria) {
-            corners = List.of("A코너", "B코너", "C코너");
-        } else {
-            corners = List.of("A코너", "B코너", "C코너", "능수관", "2캠퍼스");
-        }
+        addMealAndCornerData(sheet, rowCache, meals, corners, cellStyles.get("cornerStyle"));
+        addDateAndDiningData(sheet, rowCache, dateDinings, cellStyles, isCafeteria);
 
-        // **1. 초기 행 생성 및 캐싱**
-        Map<Integer, Row> rowCache = new HashMap<>();
-        for (int i = 0; i < 1000; i++) { // 필요할 만큼의 행을 미리 생성
-            rowCache.put(i, sheet.createRow(i));
-        }
-
-        int colIndex = 2; // 데이터를 작성할 첫 번째 열 (예: 2열)
-
-        // **2. 날짜별 데이터 작성**
-        for (Map.Entry<LocalDate, List<Dining>> entry : dateDinings.entrySet()) {
-            LocalDate date = entry.getKey();
-            List<Dining> dinings = entry.getValue();
-            //int rowIndex1 = 1; // 데이터를 작성할 첫 번째 행 (1행부터 시작)
-
-            // 날짜 헤더 추가 (0행 고정)
-            Row dateRow = rowCache.get(0); // 0행을 캐시에서 가져옴
-            Cell dateCell = dateRow.createCell(colIndex);
-            dateCell.setCellValue(date.toString());
-            dateCell.setCellStyle(headerStyle);
-
-            // Dining 데이터를 작성
-            for (Dining dining : dinings) {
-                ExcelDiningPosition position = ExcelDiningPosition.from(dining.getType(), dining.getPlace());
-                int startPosition = isCafeteria
-                    ? position.getStartPositionOnlyCafeteria()
-                    : position.getStartPositionAllPlace();
-
-                if (placeFilters.contains(dining.getPlace())) {
-                    drawExcelCafeteria(dining, rowCache, startPosition, colIndex, commonStyle);
-                } else {
-                    drawExcelNoneCafeteria(dining, rowCache, startPosition, colIndex, commonStyle);
-                }
-            }
-
-            // 열 너비 설정
-            sheet.setColumnWidth(colIndex, 6000); // 고정된 열 너비
-            colIndex++; // 다음 날짜로 이동
-        }
-
-        int rowIndex = 1; // 코너 정보 추가를 위한 초기 행 인덱스
-        for (String meal : meals) {
-            int mealStartRow = rowIndex;
-
-            for (String corner : corners) {
-                int cornerStartRow = rowIndex;
-                if (placeFilters.contains(corner)) {
-                    rowIndex += 4;
-                } else
-                    rowIndex += 2;
-
-                sheet.addMergedRegion(new CellRangeAddress(cornerStartRow, rowIndex - 1, 1, 1));
-                int startRow = cornerStartRow;
-                int endRow = rowIndex - 1;
-                int columnIndex = 1;
-
-                for (int row = startRow; row <= endRow; row++) {
-                    Row currentRow = rowCache.get(row);
-                    if (currentRow == null) {
-                        currentRow = sheet.createRow(row);
-                        rowCache.put(row, currentRow);
-                    }
-                    Cell cell = currentRow.createCell(columnIndex);
-                    cell.setCellValue(corner); // 병합된 셀 값 설정
-                    cell.setCellStyle(cornerStyle);
-                }
-            }
-
-            Row mealRow = rowCache.get(mealStartRow); // 캐시된 행 가져오기
-            Cell mealCell = mealRow.createCell(0);
-            mealCell.setCellValue(meal);
-            mealCell.setCellStyle(cornerStyle);
-
-            sheet.addMergedRegion(new CellRangeAddress(mealStartRow, rowIndex - 1, 0, 0));
-        }
-
-        // 열 너비 설정
-        sheet.setColumnWidth(0, 4000); // 식사
-        sheet.setColumnWidth(1, 4000); // 코너
-
-        // **3. 엑셀 데이터를 메모리에 저장**
         try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
             workbook.write(outputStream);
             return new ByteArrayInputStream(outputStream.toByteArray());
@@ -317,14 +230,104 @@ public class CoopService {
         }
     }
 
+    private Map<String, CellStyle> initializeWorkbookAndStyles(Workbook workbook) {
+        Map<String, CellStyle> styles = new HashMap<>();
+        styles.put("headerStyle", getHeaderStyle(workbook));
+        styles.put("commonStyle", getCommonStyle(workbook));
+        styles.put("cornerStyle", getCornerStyle(workbook));
+
+        return styles;
+    }
+
+    private Map<Integer, Row> initializeRowCache(Sheet sheet, int numRows) {
+        Map<Integer, Row> rowCache = new HashMap<>();
+        for (int i = 0; i < numRows; i++) {
+            rowCache.put(i, sheet.createRow(i));
+        }
+
+        return rowCache;
+    }
+
+    private void addMealAndCornerData(
+        Sheet sheet, Map<Integer, Row> rowCache, List<String> meals, List<String> corners, CellStyle cornerStyle
+    ) {
+        int rowIndex = 1;
+
+        for (String meal : meals) {
+            int mealStartRow = rowIndex;
+
+            for (String corner : corners) {
+                int cornerStartRow = rowIndex;
+                rowIndex += cafeteriaPlaceFilters.contains(corner) ? 4 : 2;
+
+                sheet.addMergedRegion(
+                    new CellRangeAddress(cornerStartRow, rowIndex - 1, cornerColumnIndex, cornerColumnIndex));
+                fillMergedCellsWithStyle(rowCache, cornerStartRow, rowIndex - 1, cornerColumnIndex, corner,
+                    cornerStyle);
+            }
+
+            sheet.addMergedRegion(new CellRangeAddress(mealStartRow, rowIndex - 1, mealColumIndex, mealColumIndex));
+            setCellValueWithStyle(rowCache.get(mealStartRow), mealColumIndex, meal, cornerStyle);
+        }
+
+        sheet.setColumnWidth(mealColumIndex, mealAndColumnWidth);
+        sheet.setColumnWidth(cornerColumnIndex, mealAndColumnWidth);
+    }
+
+    private void fillMergedCellsWithStyle(
+        Map<Integer, Row> rowCache, int startRow, int endRow, int col, String value, CellStyle style
+    ) {
+        for (int row = startRow; row <= endRow; row++) {
+            Row currentRow = rowCache.get(row);
+            setCellValueWithStyle(currentRow, col, value, style);
+        }
+    }
+
+    private void setCellValueWithStyle(Row row, int col, String value, CellStyle style) {
+        Cell cell = row.createCell(col);
+        cell.setCellValue(value);
+        cell.setCellStyle(style);
+    }
+
+    private void addDateAndDiningData(
+        Sheet sheet, Map<Integer, Row> rowCache, Map<LocalDate, List<Dining>> dateDinings,
+        Map<String, CellStyle> styles, Boolean isCafeteria
+    ) {
+        int colIndex = 2;
+
+        for (Map.Entry<LocalDate, List<Dining>> entry : dateDinings.entrySet()) {
+            LocalDate date = entry.getKey();
+            List<Dining> dinings = entry.getValue();
+
+            setCellValueWithStyle(rowCache.get(0), colIndex, date.toString(), styles.get("headerStyle"));
+
+            for (Dining dining : dinings) {
+                ExcelDiningPosition position = ExcelDiningPosition.from(dining.getType(), dining.getPlace());
+                int startPosition = isCafeteria
+                    ? position.getStartPositionOnlyCafeteria()
+                    : position.getStartPositionAllPlace();
+
+                if (cafeteriaPlaceFilters.contains(dining.getPlace())) {
+                    drawExcelCafeteria(dining, rowCache, startPosition, colIndex, styles.get("commonStyle"));
+                } else {
+                    drawExcelNoneCafeteria(dining, rowCache, startPosition, colIndex, styles.get("commonStyle"));
+                }
+            }
+
+            sheet.setColumnWidth(colIndex, 6000);
+            colIndex++;
+        }
+    }
+
     private void drawExcelNoneCafeteria(Dining dining, Map<Integer, Row> rowCache, int startPosition, int colIndex,
-        CellStyle cellStyle) {
-        Row menuRow = rowCache.get(startPosition); // 캐시된 행 가져오기
+        CellStyle cellStyle
+    ) {
+        Row menuRow = rowCache.get(startPosition);
         Cell menuCell = menuRow.createCell(colIndex);
-        menuCell.setCellValue(formatMenu(dining.getMenu())); // 메뉴 추가
+        menuCell.setCellValue(formatMenu(dining.getMenu()));
         menuCell.setCellStyle(cellStyle);
 
-        Row kcalRow = rowCache.get(startPosition + 1); // 캐시된 행 가져오기
+        Row kcalRow = rowCache.get(startPosition + 1);
         Cell kcalCell = kcalRow.createCell(colIndex);
         kcalCell.setCellValue(dining.getKcal() == null ? "0 kcal" : dining.getKcal() + " kcal");
         kcalCell.setCellStyle(cellStyle);
@@ -332,22 +335,22 @@ public class CoopService {
 
     private void drawExcelCafeteria(Dining dining, Map<Integer, Row> rowCache, int startPosition, int colIndex,
         CellStyle cellStyle) {
-        Row menuRow = rowCache.get(startPosition); // 캐시된 행 가져오기
+        Row menuRow = rowCache.get(startPosition);
         Cell menuCell = menuRow.createCell(colIndex);
-        menuCell.setCellValue(formatMenu(dining.getMenu())); // 메뉴 추가
+        menuCell.setCellValue(formatMenu(dining.getMenu()));
         menuCell.setCellStyle(cellStyle);
 
-        Row imageRow = rowCache.get(startPosition + 1); // 캐시된 행 가져오기
+        Row imageRow = rowCache.get(startPosition + 1);
         Cell imageCell = imageRow.createCell(colIndex);
-        imageCell.setCellValue(dining.getImageUrl()); // URL 추가
+        imageCell.setCellValue(dining.getImageUrl());
         imageCell.setCellStyle(cellStyle);
 
-        Row kcalRow = rowCache.get(startPosition + 2); // 캐시된 행 가져오기
+        Row kcalRow = rowCache.get(startPosition + 2);
         Cell kcalCell = kcalRow.createCell(colIndex);
         kcalCell.setCellValue(dining.getKcal() == null ? "0 kcal" : dining.getKcal() + " kcal");
         kcalCell.setCellStyle(cellStyle);
 
-        Row soldOutRow = rowCache.get(startPosition + 3); // 캐시된 행 가져오기
+        Row soldOutRow = rowCache.get(startPosition + 3);
         Cell soldOutCell = soldOutRow.createCell(colIndex);
         soldOutCell.setCellValue(dining.getSoldOut() == null ? "미품절" : "품절 (" + dining.getSoldOut() + ")");
         soldOutCell.setCellStyle(cellStyle);
@@ -378,66 +381,57 @@ public class CoopService {
 
     private List<Dining> fetchDiningData(LocalDate startDate, LocalDate endDate, Boolean isCafeteria) {
         if (isCafeteria) {
-            List<String> cafeteriaPlaces = Arrays.asList("A코너", "B코너", "C코너");
-            return diningRepository.findByDateBetweenAndPlaceIn(startDate, endDate, cafeteriaPlaces);
+            return diningRepository.findByDateBetweenAndPlaceIn(startDate, endDate, cafeteriaPlaceFilters);
         }
+
         return diningRepository.findByDateBetween(startDate, endDate);
-    }
-
-    private CellStyle createHeaderStyle(Workbook workbook) {
-        CellStyle style = workbook.createCellStyle();
-        Font font = workbook.createFont();
-        font.setBold(true);
-        font.setColor(IndexedColors.BLACK.getIndex());
-        style.setFont(font);
-        if (workbook instanceof XSSFWorkbook) {
-            XSSFColor customColor = new XSSFColor(new byte[] {(byte)151, (byte)190, (byte)192}, null);
-            ((XSSFCellStyle)style).setFillForegroundColor(customColor);
-        }
-
-        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        style.setAlignment(HorizontalAlignment.CENTER);
-        return style;
     }
 
     private String formatMenu(List<String> menu) {
         return String.join("\n", menu);
     }
 
-    private CellStyle createCommonStyle(Workbook workbook) {
+    private CellStyle createCellStyle(Workbook workbook, byte[] backgroundColor) {
         CellStyle style = workbook.createCellStyle();
-        style.setAlignment(HorizontalAlignment.CENTER);
-        style.setVerticalAlignment(VerticalAlignment.CENTER);
-        style.setWrapText(true);
-        return style;
-    }
 
-    private CellStyle createCornerStyle(Workbook workbook) {
-        CellStyle style = workbook.createCellStyle();
         Font font = workbook.createFont();
-        font.setBold(true);
         font.setColor(IndexedColors.BLACK.getIndex());
         style.setFont(font);
 
-        if (workbook instanceof XSSFWorkbook) {
-            XSSFColor customColor = new XSSFColor(new byte[] {(byte)252, (byte)237, (byte)186}, null);
+        if (backgroundColor != null && workbook instanceof XSSFWorkbook) {
+            XSSFColor customColor = new XSSFColor(backgroundColor, null);
             ((XSSFCellStyle)style).setFillForegroundColor(customColor);
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
         }
 
-        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        style.setVerticalAlignment(VerticalAlignment.CENTER);
         style.setAlignment(HorizontalAlignment.CENTER);
+        style.setVerticalAlignment(VerticalAlignment.CENTER);
+
+        style.setWrapText(true);
 
         style.setBorderTop(BorderStyle.THIN);
         style.setBorderBottom(BorderStyle.THIN);
         style.setBorderLeft(BorderStyle.THIN);
         style.setBorderRight(BorderStyle.THIN);
 
-        style.setTopBorderColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        style.setBottomBorderColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        style.setLeftBorderColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        style.setRightBorderColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        short borderColor = IndexedColors.GREY_25_PERCENT.getIndex();
+        style.setTopBorderColor(borderColor);
+        style.setBottomBorderColor(borderColor);
+        style.setLeftBorderColor(borderColor);
+        style.setRightBorderColor(borderColor);
 
         return style;
+    }
+
+    private CellStyle getCommonStyle(Workbook workbook) {
+        return createCellStyle(workbook, null);
+    }
+
+    private CellStyle getHeaderStyle(Workbook workbook) {
+        return createCellStyle(workbook, new byte[] {(byte)151, (byte)190, (byte)192});
+    }
+
+    private CellStyle getCornerStyle(Workbook workbook) {
+        return createCellStyle(workbook, new byte[] {(byte)252, (byte)237, (byte)186});
     }
 }
