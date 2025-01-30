1cf6e22a98cd9fdc82698656a31a83bc2f295c58
강인화 <rkddlsghk1998@gmail.com>
2025-01-30 23:27:00 +0900

    feat: LostItemReportRepository 추가

---
+++ src/main/java/in/koreatech/koin/domain/community/article/repository/LostItemReportRepository.java
@@ -0,0 +1,14 @@
+package in.koreatech.koin.domain.community.article.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.Repository;
+
+import in.koreatech.koin.domain.community.article.model.LostItemReport;
+
+public interface LostItemReportRepository extends Repository<LostItemReport, Integer> {
+
+    LostItemReport save(LostItemReport shopReviewReport);
+
+    Optional<LostItemReport> findById(Integer id);
+}
