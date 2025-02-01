7db4b016cf1d1815a4e7440e22c991524ca78efd
강인화 <rkddlsghk1998@gmail.com>
2025-02-01 22:32:28 +0900

    chore: 매개변수 이름 수정

--- src/main/java/in/koreatech/koin/domain/community/article/repository/LostItemReportRepository.java
+++ src/main/java/in/koreatech/koin/domain/community/article/repository/LostItemReportRepository.java
@@ -1,14 +1,18 @@
 package in.koreatech.koin.domain.community.article.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.Repository;
 
+import in.koreatech.koin.domain.community.article.model.LostItemArticle;
 import in.koreatech.koin.domain.community.article.model.LostItemReport;
 
 public interface LostItemReportRepository extends Repository<LostItemReport, Integer> {
 
-    LostItemReport save(LostItemReport shopReviewReport);
+    LostItemReport save(LostItemReport lostItemReport);
 
     Optional<LostItemReport> findById(Integer id);
+
+    List<LostItemReport> findAllByLostItemArticle(LostItemArticle lostItemArticle);
 }
