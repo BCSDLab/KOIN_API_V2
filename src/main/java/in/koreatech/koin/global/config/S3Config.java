b663d71f0355f06d07fc234d767c567bc721bfb8
songsunkook <songsunkook@gmail.com>
2025-02-06 17:35:42 +0900

    refactor: aws key 주입 제거

--- src/main/java/in/koreatech/koin/global/config/S3Config.java
+++ src/main/java/in/koreatech/koin/global/config/S3Config.java
@@ -2,7 +2,6 @@ package in.koreatech.koin.global.config;
 
 import static software.amazon.awssdk.regions.Region.AP_NORTHEAST_2;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,19 +15,12 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
+/**
+ * Key 기반 인증 방식은 취약하므로 IAM Role 기반 인증 방식을 사용한다. (InstanceProfileCredentialsProvider)
+ * EC2 인스턴스에 부여된 "EC2_to_S3" IAM Role을 통해 인증을 수행한다.
+ */
 @Configuration
 public class S3Config {
-
-    private final String accessKey;
-    private final String secretKey;
-
-    public S3Config(
-        @Value("${s3.key}") String accessKey,
-        @Value("${s3.secret}") String secretKey
-    ) {
-        this.accessKey = accessKey;
-        this.secretKey = secretKey;
-    }
 
     /**
      * S3Presigner 사용 후 close()를 권장하므로, Builder 를 반환하여 필요 시 객체를 만들어 사용 후 close 되도록 구현.
@@ -48,7 +40,6 @@ public class S3Config {
         return AmazonS3ClientBuilder.standard()
             .withRegion(Regions.AP_NORTHEAST_2)
             .withClientConfiguration(clientConfig)
-            //.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey))) // IAM user 사용
             .withCredentials(new EC2ContainerCredentialsProviderWrapper()) // IAM Role 사용
             .build();
     }
