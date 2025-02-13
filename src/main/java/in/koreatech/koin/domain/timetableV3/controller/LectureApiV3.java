5dcd28caf7084e44c408171e96b92bd4c3544889
신관규 <soundbar91@gmail.com>
2025-02-13 18:41:56 +0900

    chore: 미사용 import문 삭제

--- src/main/java/in/koreatech/koin/domain/timetableV3/controller/LectureApiV3.java
+++ src/main/java/in/koreatech/koin/domain/timetableV3/controller/LectureApiV3.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import in.koreatech.koin.domain.timetableV3.dto.response.LectureResponseV3;
-import in.koreatech.koin.global.auth.UserId;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
