028614c8e927ecc403a0d70d8ba516b9de652896
신관규 <soundbar91@gmail.com>
2025-02-18 22:25:25 +0900

    feat: flyway 수정

--- src/main/resources/db/migration/V130__delete_major_and_department_data.sql
+++
@@ -1,3 +0,0 @@
-DELETE FROM koin.major WHERE id BETWEEN 1 AND 16;
-
-DELETE FROM koin.students WHERE department_id = 7 OR department_id = 8;
---
+++ src/main/resources/db/migration/V131__update_student_department.sql
@@ -0,0 +1,1 @@
+UPDATE `koin`.`students` SET department_id = NULL WHERE department_id = 7 OR department_id = 8;
