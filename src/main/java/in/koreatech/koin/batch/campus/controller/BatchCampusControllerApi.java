4b228c8f229b038eefa6f1abad74bb582cac534c
박성빈 <psb106305@gmail.com>
2025-02-22 18:58:18 +0900

    feat: 시내 버스 배치 api 작성

--- src/main/java/in/koreatech/koin/batch/campus/controller/BatchCampusControllerApi.java
+++ src/main/java/in/koreatech/koin/batch/campus/controller/BatchCampusControllerApi.java
@@ -35,4 +35,18 @@ public interface BatchCampusControllerApi {
         @Auth(permit = {ADMIN}) Integer adminId,
         @RequestBody @Valid BatchSchoolBusVersionUpdateRequest request
     );
+
+    @ApiResponses(
+        value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(hidden = true))),
+        }
+    )
+    @Operation(summary = "시내버스 시간표 업데이트")
+    @PostMapping("/bus/city")
+    ResponseEntity<Void> updateCityBus(
+        @Auth(permit = {ADMIN}) Integer adminId
+    );
 }
