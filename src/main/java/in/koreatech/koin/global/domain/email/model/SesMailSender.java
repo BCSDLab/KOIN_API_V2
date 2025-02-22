59a27a0e4822b5464ffedc7e07589a2ed3bc20d2
신관규 <soundbar91@gmail.com>
2025-02-23 02:06:51 +0900

    feat: 이메일 로그 설정

--- src/main/java/in/koreatech/koin/global/domain/email/model/SesMailSender.java
+++ src/main/java/in/koreatech/koin/global/domain/email/model/SesMailSender.java
@@ -2,15 +2,19 @@ package in.koreatech.koin.global.domain.email.model;
 
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.handlers.AsyncHandler;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceAsync;
 import com.amazonaws.services.simpleemail.model.Body;
 import com.amazonaws.services.simpleemail.model.Content;
 import com.amazonaws.services.simpleemail.model.Destination;
 import com.amazonaws.services.simpleemail.model.Message;
 import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+import com.amazonaws.services.simpleemail.model.SendEmailResult;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class SesMailSender {
@@ -25,6 +29,16 @@ public class SesMailSender {
                 .withBody(new Body().withHtml(new Content().withCharset("UTF-8").withData(htmlBody)))
                 .withSubject(new Content().withCharset("UTF-8").withData(subject)));
 
-        amazonSimpleEmailServiceAsync.sendEmailAsync(request);
+        amazonSimpleEmailServiceAsync.sendEmailAsync(request, new AsyncHandler<>() {
+            @Override
+            public void onError(Exception e) {
+                log.warn(e.getMessage());
+            }
+
+            @Override
+            public void onSuccess(SendEmailRequest request, SendEmailResult sendEmailResult) {
+                log.info("Email sent successfully");
+            }
+        });
     }
 }
