d30156bf589891703b978b753e61447d3784338a
박성빈 <psb106305@gmail.com>
2025-02-24 04:43:51 +0900

    feat: 식단 크롤링 scheduler 작성

--- src/main/java/in/koreatech/koin/batch/campus/controller/BatchCampusController.java
+++ src/main/java/in/koreatech/koin/batch/campus/controller/BatchCampusController.java
@@ -12,6 +12,7 @@ import in.koreatech.koin.batch.campus.bus.city.service.BatchCityBusService;
 import in.koreatech.koin.batch.campus.bus.school.dto.BatchSchoolBusVersionUpdateRequest;
 import in.koreatech.koin.batch.campus.bus.school.service.SchoolBusService;
 import in.koreatech.koin.batch.campus.koreatech.dining.service.BatchDiningService;
+import in.koreatech.koin.batch.campus.service.BatchPortalLoginService;
 import in.koreatech.koin.global.auth.Auth;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,7 @@ public class BatchCampusController implements BatchCampusControllerApi {
 
     private final SchoolBusService schoolBusService;
     private final BatchCityBusService batchCityBusService;
+    private final BatchPortalLoginService batchPortalLoginService;
     private final BatchDiningService batchDiningService;
 
     @Override
@@ -49,6 +51,7 @@ public class BatchCampusController implements BatchCampusControllerApi {
     public ResponseEntity<Void> updateDining(
         @Auth(permit = {ADMIN}) Integer adminId
     ) {
+        batchPortalLoginService.login();
         batchDiningService.update();
         return ResponseEntity.ok().build();
     }
--- src/main/java/in/koreatech/koin/batch/campus/scheduler/BatchCampusScheduler.java
+++ src/main/java/in/koreatech/koin/batch/campus/scheduler/BatchCampusScheduler.java
@@ -4,6 +4,8 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import in.koreatech.koin.batch.campus.bus.city.service.BatchCityBusService;
+import in.koreatech.koin.batch.campus.koreatech.dining.service.BatchDiningService;
+import in.koreatech.koin.batch.campus.service.BatchPortalLoginService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,9 +15,20 @@ import lombok.extern.slf4j.Slf4j;
 public class BatchCampusScheduler {
 
     private final BatchCityBusService batchCityBusService;
+    private final BatchPortalLoginService batchPortalLoginService;
+    private final BatchDiningService batchDiningService;
 
     @Scheduled(cron = "0 0 0 * * 0")
     public void updateCityBusTimetables() {
         batchCityBusService.update();
+    }
+
+    @Scheduled(cron = "0 0 5,15 * * *")
+    @Scheduled(cron = "0 15,30 17,18 * * *")
+    @Scheduled(cron = "0 */30 8-11 * * *")
+    @Scheduled(cron = "0 15 11 * * *")
+    public void updateDining() {
+        batchPortalLoginService.login();
+        batchDiningService.update();
     }
 }
