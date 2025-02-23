c21a993273242859523148b161c6b7afae7f9060
DHkimgit <dukekim001@naver.com>
2025-02-23 10:48:18 +0900

    chore : 디버깅 코드 제거

--- src/main/java/in/koreatech/koin/global/socket/domain/notification/service/MessageReceivedEventListener.java
+++ src/main/java/in/koreatech/koin/global/socket/domain/notification/service/MessageReceivedEventListener.java
@@ -21,7 +21,6 @@ import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class MessageReceivedEventListener {
 
     private final NotificationService notificationService;
