76d9dc22e8eb776b7c08aa57040864aea55cd44b
신관규 <soundbar91@gmail.com>
2025-02-23 18:05:49 +0900

    fix: timetable NPE 해결 (#1296)
    * feat: classTime NPE 해결
    
    * feat: NPE 해결

--- src/main/java/in/koreatech/koin/domain/timetableV3/utils/ClassTimeUtils.java
+++ src/main/java/in/koreatech/koin/domain/timetableV3/utils/ClassTimeUtils.java
@@ -2,6 +2,7 @@ package in.koreatech.koin.domain.timetableV3.utils;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -14,6 +15,11 @@ public class ClassTimeUtils {
 
     // 문자열 강의 시간 리스트로 변환
     public static List<Integer> parseToIntegerList(String classTime) {
+        // 온라인 강의 혹은 졸학계로 생성된 강의 중 classTime이 null인 경우
+        if (classTime == null || classTime.isEmpty()) {
+            return Collections.emptyList();
+        }
+
         return Stream.of(classTime.replaceAll("[\\[\\]]", "").split(","))
             .map(String::strip)
             .filter(time -> !time.isEmpty())
