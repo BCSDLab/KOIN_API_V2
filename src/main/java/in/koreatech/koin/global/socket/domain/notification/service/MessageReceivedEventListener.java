f3b9d4a614e365822100d1dec6ad12c3db91bcf8
DHkimgit <dukekim001@naver.com>
2025-02-23 10:48:55 +0900

    chore : 디버깅 코드 제거

--- src/main/java/in/koreatech/koin/global/socket/domain/notification/service/MessageReceivedEventListener.java
+++ src/main/java/in/koreatech/koin/global/socket/domain/notification/service/MessageReceivedEventListener.java
@@ -17,7 +17,6 @@ import in.koreatech.koin.global.socket.domain.session.model.UserSessionStatus;
 import in.koreatech.koin.global.socket.domain.session.service.UserSessionService;
 import in.koreatech.koin.global.socket.domain.session.service.implement.UserReader;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
