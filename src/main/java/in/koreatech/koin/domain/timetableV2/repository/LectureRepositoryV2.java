78786eb9e06a73283de25a4ac8edda159ae2d242
신관규 <soundbar91@gmail.com>
2025-03-02 23:46:19 +0900

    chore: 예외 원복

--- src/main/java/in/koreatech/koin/domain/timetableV2/repository/LectureRepositoryV2.java
+++ src/main/java/in/koreatech/koin/domain/timetableV2/repository/LectureRepositoryV2.java
@@ -7,6 +7,7 @@ import java.util.Set;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 
+import in.koreatech.koin.domain.timetable.exception.LectureNotFoundException;
 import in.koreatech.koin.domain.timetable.exception.SemesterNotFoundException;
 import in.koreatech.koin.domain.timetable.model.Lecture;
 import io.lettuce.core.dynamic.annotation.Param;
