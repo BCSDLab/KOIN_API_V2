b1b0d90262fc0d0445a4253486fd36d2a2d3852b
신관규 <soundbar91@gmail.com>
2025-03-04 09:08:32 +0900

    fix: 졸학계 로직 주석 처리 원복 (#1327)

--- src/main/java/in/koreatech/koin/domain/timetableV3/service/TimetableRegularLectureServiceV3.java
+++ src/main/java/in/koreatech/koin/domain/timetableV3/service/TimetableRegularLectureServiceV3.java
@@ -6,6 +6,7 @@ import static in.koreatech.koin.domain.timetableV2.validation.TimetableFrameVali
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -48,13 +49,10 @@ public class TimetableRegularLectureServiceV3 {
         TimetableFrame frame = timetableFrameRepositoryV3.getById(request.timetableFrameId());
         validateUserAuthorization(frame.getUser().getId(), userId);
         Lecture lecture = lectureRepositoryV3.getById(request.lectureId());
-        /*
         Catalog catalog = getCatalog(lecture, userId);
         CourseType courseType = getCourseType(catalog);
         GeneralEducationArea generalEducationArea = getGeneralEducationArea(catalog);
         TimetableLecture timetableLecture = request.toTimetableLecture(frame, lecture, courseType, generalEducationArea);
-         */
-        TimetableLecture timetableLecture = request.toTimetableLecture(frame, lecture);
         frame.addTimeTableLecture(timetableLecture);
         timetableLectureRepositoryV3.save(timetableLecture);
         return getTimetableLectureResponse(userId, frame);
@@ -112,16 +110,16 @@ public class TimetableRegularLectureServiceV3 {
         TimetableFrame frame = timetableFrameRepositoryV3.getById(request.timetableFrameId());
         validateUserAuthorization(frame.getUser().getId(), userId);
 
-        /*
         CourseType courseType = null;
         if (!Objects.isNull(request.timetableLecture().courseType())) {
             courseType = courseTypeRepository.getByName(request.timetableLecture().courseType());
-        }*/
+        }
 
         TimetableLecture timetableLecture = timetableLectureRepositoryV3.getById(request.timetableLecture().id());
         timetableLecture.updateRegularLecture(
             request.timetableLecture().classTitle(),
-            request.timetableLecture().classPlacesToString()
+            request.timetableLecture().classPlacesToString(),
+            courseType
         );
 
         timetableLectureRepositoryV3.save(timetableLecture);
