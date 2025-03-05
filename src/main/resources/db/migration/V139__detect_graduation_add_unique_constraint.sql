6bb0881e510edfe4b6a842c488928fea33629296
seongjae6751 <seongjae6751@naver.com>
2025-03-05 18:23:54 +0900

    chore: flyway 파일 삭제

--- src/main/resources/db/migration/V139__detect_graduation_add_unique_constraint.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE student_course_calculation
-    ADD CONSTRAINT unique_user_standard_graduation_requirements UNIQUE (user_id, standard_graduation_requirements_id);
-
-ALTER TABLE detect_graduation_calculation
-    ADD CONSTRAINT unique_user UNIQUE (user_id);
