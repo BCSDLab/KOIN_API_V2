db57cfd6df0f941e00616dfe7823ef8a303b5a29
박성빈 <psb106305@gmail.com>
2025-03-12 22:29:32 +0900

    feat: 학교 버스 배치 제거

--- src/main/java/in/koreatech/koin/batch/campus/controller/BatchCampusControllerApi.java
+++ src/main/java/in/koreatech/koin/batch/campus/controller/BatchCampusControllerApi.java
@@ -4,10 +4,8 @@ import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import in.koreatech.koin.batch.campus.bus.school.dto.BatchSchoolBusVersionUpdateRequest;
 import in.koreatech.koin.global.auth.Auth;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,26 +13,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 
 @Tag(name = "(Admin) Batch: 배치", description = "Campus 팀 배치 작업을 관리한다.")
 @RequestMapping("/batch/campus")
 public interface BatchCampusControllerApi {
-
-    @ApiResponses(
-        value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(hidden = true))),
-        }
-    )
-    @Operation(summary = "학교버스 시간표 업데이트")
-    @PostMapping("/bus/school")
-    ResponseEntity<Void> updateSchoolBus(
-        @Auth(permit = {ADMIN}) Integer adminId,
-        @RequestBody @Valid BatchSchoolBusVersionUpdateRequest request
-    );
 
     @ApiResponses(
         value = {
