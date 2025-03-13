c2d84e6dcc5b20e7e46b647d7f172b3369606ceb
신관규 <soundbar91@gmail.com>
2025-03-13 17:01:20 +0900

    chore: flyway 수정

--- src/main/resources/db/migration/V139__delete_legacy_table_and_column.sql
+++ src/main/resources/db/migration/V139__delete_legacy_table_and_column.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS users_owners;
 DROP TABLE IF EXISTS koreatech_articles;
 DROP TABLE IF EXISTS articles;
-DROP TABLE IF EXISTS bus_timetables;
 DROP TABLE IF EXISTS timetables;
 
 TRUNCATE TABLE comments;
