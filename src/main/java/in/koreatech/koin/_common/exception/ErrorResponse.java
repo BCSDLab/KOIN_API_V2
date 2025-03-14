c1ac55673cc16fdaf0ee13763db2847de29c17e8
배진호 <onionring4028@gmail.com>
2025-03-14 15:01:38 +0900

    hotfix: 에러 코드 int -> String (#1370)

--- src/main/java/in/koreatech/koin/_common/exception/ErrorResponse.java
+++ src/main/java/in/koreatech/koin/_common/exception/ErrorResponse.java
@@ -9,13 +9,13 @@ public class ErrorResponse {
 
     @JsonIgnore
     private final int status;
-    private final int code;
+    private final String code;
     private final String message;
     private final String errorTraceId;
 
     public ErrorResponse(int status, String message, String errorTraceId) {
         this.status = status;
-        this.code = 0;
+        this.code = "";
         this.message = message;
         this.errorTraceId = errorTraceId;
     }
