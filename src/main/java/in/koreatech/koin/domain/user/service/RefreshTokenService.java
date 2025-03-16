2309a2238faa4652356d05ee366c546e92f11d07
신관규 <soundbar91@gmail.com>
2025-03-16 15:45:27 +0900

    feat: 리프레쉬 토큰 서비스 메소드 추가

--- src/main/java/in/koreatech/koin/domain/user/service/RefreshTokenService.java
+++ src/main/java/in/koreatech/koin/domain/user/service/RefreshTokenService.java
@@ -2,7 +2,9 @@ package in.koreatech.koin.domain.user.service;
 
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import in.koreatech.koin._common.auth.exception.AuthorizationException;
@@ -17,8 +19,35 @@ import lombok.RequiredArgsConstructor;
 public class RefreshTokenService {
 
     private static final String REFRESH_TOKEN_FORMAT = "%s-%d";
+    private static final long REFRESH_TOKEN_EXPIRE_DAY = 90L;
 
     private final UserTokenRedisRepository userTokenRedisRepository;
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void saveRefreshToken(Integer userId, String platform) {
+        String key = getUserKey(userId, platform);
+        redisTemplate.opsForValue().set(key, String.valueOf(UUID.randomUUID()), REFRESH_TOKEN_EXPIRE_DAY, TimeUnit.DAYS);
+    }
+
+    public String getRefreshToken(Integer userId, String platform) {
+        String key = getUserKey(userId, platform);
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteRefreshToken(Integer userId, String platform) {
+        String key = getUserKey(userId, platform);
+        redisTemplate.delete(key);
+    }
+
+    public void deleteAllRefreshTokens(Integer userId) {
+        redisTemplate.delete(getUserKey(userId, "PC"));
+        redisTemplate.delete(getUserKey(userId, "MOBILE"));
+        redisTemplate.delete(getUserKey(userId, "TABLET"));
+    }
+
+    private String getUserKey(Integer userId, String platform) {
+        return "refreshToken:" + userId + ":" + platform.toUpperCase();
+    }
 
     public String createRefreshToken(User user) {
         return String.format(REFRESH_TOKEN_FORMAT, UUID.randomUUID(), user.getId());
