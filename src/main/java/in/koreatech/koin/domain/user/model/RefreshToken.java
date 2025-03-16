cdbdb899cbba1a47dfb5fc46e030546b509d8bc9
신관규 <soundbar91@gmail.com>
2025-03-16 21:28:14 +0900

    feat: NoargsConstructor 어노테이션 추가

--- src/main/java/in/koreatech/koin/domain/user/model/RefreshToken.java
+++ src/main/java/in/koreatech/koin/domain/user/model/RefreshToken.java
@@ -6,10 +6,13 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@RedisHash(value = "refreshToken@")
+@RedisHash(value = "refreshToken")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RefreshToken {
 
     private static final long REFRESH_TOKEN_EXPIRE_DAY = 90L;
@@ -21,11 +24,12 @@ public class RefreshToken {
     private String token;
 
     @TimeToLive(unit = TimeUnit.DAYS)
-    private final Long expiration = REFRESH_TOKEN_EXPIRE_DAY;
+    private Long expiration;
 
     private RefreshToken(Integer userId, String refreshToken, String platform) {
         this.id = generateKey(userId, platform);
         this.token = refreshToken + "-" + userId;
+        this.expiration = REFRESH_TOKEN_EXPIRE_DAY;
     }
 
     public static RefreshToken create(Integer userId, String refreshToken, String platform) {
