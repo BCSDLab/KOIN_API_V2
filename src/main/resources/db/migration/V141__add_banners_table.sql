1dc6b222289eb3e5dddd3bbc7714a8fb08f7fde1
신관규 <soundbar91@gmail.com>
2025-03-23 14:45:25 +0900

    chore: flyway 컬럼 이름 수정

--- src/main/resources/db/migration/V141__add_banners_table.sql
+++ src/main/resources/db/migration/V141__add_banners_table.sql
@@ -9,8 +9,8 @@ CREATE TABLE IF NOT EXISTS `koin`.`banners`
     `android_redirect_link` VARCHAR(255) NULL COMMENT '안드로이드 리다이렉션 URL',
     `ios_redirect_link`     VARCHAR(255) NULL COMMENT 'IOS 리다이렉션 URL',
     `is_active`             TINYINT(1) NOT NULL COMMENT '배너 활성화 여부',
-    created_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 일자',
-    updated_at              TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일자',
+    `created_at`            TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 일자',
+    `updated_at`            TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일자',
     PRIMARY KEY (`id`),
     FOREIGN KEY (`banner_category_id`) REFERENCES `koin`.`banner_categories` (`id`)
 );
