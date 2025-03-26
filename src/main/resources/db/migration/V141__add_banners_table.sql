6671271a8f411f103c8caf79041e4d3ffb57d721
신관규 <soundbar91@gmail.com>
2025-03-26 21:54:48 +0900

    docs: flyway 변경

--- src/main/resources/db/migration/V141__add_banners_table.sql
+++ src/main/resources/db/migration/V141__add_banners_table.sql
@@ -7,9 +7,9 @@ CREATE TABLE IF NOT EXISTS `koin`.`banners`
     `image_url`                 VARCHAR(255) NOT NULL COMMENT '배너 이미지 URL',
     `web_redirect_link`         VARCHAR(255) NULL COMMENT '웹 리다이렉션 URL',
     `android_redirect_link`     VARCHAR(255) NULL COMMENT '안드로이드 리다이렉션 URL',
-    `android_minimum_version`   VARCHAR(50) NULL COMMENT '안드로이드 최소 버전'
+    `android_minimum_version`   VARCHAR(50) NULL COMMENT '안드로이드 최소 버전',
     `ios_redirect_link`         VARCHAR(255) NULL COMMENT 'IOS 리다이렉션 URL',
-    `ios_minimum_version`       VARCHAR(50) NULL COMMENT 'IOS 최소 버전'
+    `ios_minimum_version`       VARCHAR(50) NULL COMMENT 'IOS 최소 버전',
     `is_active`                 TINYINT(1) NOT NULL COMMENT '배너 활성화 여부',
     `created_at`                TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 일자',
     `updated_at`                TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일자',
