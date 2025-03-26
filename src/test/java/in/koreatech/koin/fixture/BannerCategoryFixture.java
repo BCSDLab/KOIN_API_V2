f4caf3e12e4ba21632bd02ffa70687faf6783689
신관규 <soundbar91@gmail.com>
2025-03-26 12:29:41 +0900

    test: 배너 카테고리 응답값 추가

--- src/test/java/in/koreatech/koin/fixture/BannerCategoryFixture.java
+++ src/test/java/in/koreatech/koin/fixture/BannerCategoryFixture.java
@@ -18,6 +18,7 @@ public class BannerCategoryFixture {
     public BannerCategory 메인_모달() {
         return bannerCategoryRepository.save(BannerCategory.builder()
             .name("메인 모달")
+            .description("140*112 앱/웹 랜딩시 뜨는 모달입니다.")
             .build()
         );
     }
