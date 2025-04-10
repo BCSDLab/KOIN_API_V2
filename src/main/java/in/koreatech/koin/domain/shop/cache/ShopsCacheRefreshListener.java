2f414952d792b7c1ceb5097982e4c5cba8bca589
krSeonghyeon <ya41921174@gmail.com>
2025-04-10 16:33:43 +0900

    fix: profile 추가

--- src/main/java/in/koreatech/koin/domain/shop/cache/ShopsCacheRefreshListener.java
+++ src/main/java/in/koreatech/koin/domain/shop/cache/ShopsCacheRefreshListener.java
@@ -3,6 +3,7 @@ package in.koreatech.koin.domain.shop.cache;
 import static org.springframework.transaction.event.TransactionPhase.*;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -12,6 +13,7 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
+@Profile("!test")
 public class ShopsCacheRefreshListener {
 
     private final ShopsCacheService shopsCacheService;
