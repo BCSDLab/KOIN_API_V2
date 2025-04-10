c01a3f4d837c316c1793754aca25fc5bcd44558a
신관규 <soundbar91@gmail.com>
2025-04-10 23:43:19 +0900

    fix: flyway 수정

--- src/main/resources/db/migration/V146__delete_koin_notice_in_koin_article.sql
+++ src/main/resources/db/migration/V146__delete_koin_notice_in_koin_article.sql
@@ -1,7 +1,4 @@
-SET SQL_SAFE_UPDATES = 0;
-
 DELETE nka
 FROM new_koin_articles nka
-INNER JOIN admins a ON nka.user_id = a.user_id;
-
-SET SQL_SAFE_UPDATES = 1;
+INNER JOIN admins a ON nka.user_id = a.user_id
+WHERE nka.id = nka.id;
