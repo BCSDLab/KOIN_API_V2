50e0ee72c5c329414156953b785bd3bd07fcf10c
강인화 <rkddlsghk1998@gmail.com>
2025-04-25 01:06:42 +0900

    fix: 인증코드 요청 시 슬랙 알림 전송하도록 버그 수정

--- src/main/java/in/koreatech/koin/domain/user/model/UserEventListener.java
+++ src/main/java/in/koreatech/koin/domain/user/model/UserEventListener.java
@@ -2,6 +2,7 @@ package in.koreatech.koin.domain.user.model;
 
 import static org.springframework.transaction.event.TransactionPhase.AFTER_COMMIT;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,14 +29,14 @@ public class UserEventListener {
         slackClient.sendMessage(notification);
     }
 
-    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @EventListener
     public void onUserSmsVerificationSendEvent(UserSmsVerificationSendEvent userSmsVerificationSendEvent) {
         var notification = slackNotificationFactory.generateUserPhoneVerificationSendNotification(
             userSmsVerificationSendEvent.phoneNumber());
         slackClient.sendMessage(notification);
     }
 
-    @TransactionalEventListener(phase = AFTER_COMMIT)
+    @EventListener
     public void onUserEmailVerificationSendEvent(UserEmailVerificationSendEvent userEmailVerificationSendEvent) {
         var notification = slackNotificationFactory.generateUserEmailVerificationSendNotification(
             userEmailVerificationSendEvent.email());
