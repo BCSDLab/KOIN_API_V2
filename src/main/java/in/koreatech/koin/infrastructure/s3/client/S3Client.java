1e4d40292790ad1698d342acdf3998ffd6eb90a6
김성현 <ya41921174@gmail.com>
2025-05-11 14:27:00 +0900

    feat: 이미지 삭제나 수정 시 s3 정합성을 위한 이벤트 로직 추가  (#1502)
    * feat: s3 파일 단일 삭제, 배치 삭제 기능 구현
    
    * feat: shop 관련 이미지 삭제 이벤트, 이벤트리스너 추가
    
    * feat: url에서 Key를 분리하여 처리하도록 변경
    
    * feat: @Profile추가
    
    * chore: 리뷰반영 클래스명 변경

--- src/main/java/in/koreatech/koin/infrastructure/s3/client/S3Client.java
+++ src/main/java/in/koreatech/koin/infrastructure/s3/client/S3Client.java
@@ -1,5 +1,7 @@
 package in.koreatech.koin.infrastructure.s3.client;
 
+import static com.amazonaws.services.s3.model.DeleteObjectsRequest.*;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -9,12 +11,16 @@ import java.io.OutputStream;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.MultiObjectDeleteException;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
@@ -79,6 +85,31 @@ public class S3Client {
         return new UploadFileResponse(domainUrlPrefix + uploadFilePath);
     }
 
+    public void deleteFile(String s3Key) {
+        try {
+            amazonS3.deleteObject(bucketName, s3Key);
+        } catch (Exception e) {
+            throw new KoinIllegalStateException("S3 객체 삭제 중 문제가 발생했습니다. " + e.getMessage());
+        }
+    }
+
+    public void deleteFiles(List<String> s3Keys) {
+        try {
+            List<KeyVersion> keys = s3Keys.stream()
+                .map(KeyVersion::new)
+                .toList();
+            DeleteObjectsRequest request = new DeleteObjectsRequest(bucketName).withKeys(keys);
+            amazonS3.deleteObjects(request);
+        } catch (MultiObjectDeleteException e) {
+            List<String> failedKeys = e.getErrors().stream()
+                .map(MultiObjectDeleteException.DeleteError::getKey)
+                .toList();
+            throw new KoinIllegalStateException("일부 S3 객체의 삭제가 실패했습니다. : " + failedKeys);
+        } catch (Exception e) {
+            throw new KoinIllegalStateException("S3 객체 일괄 삭제 중 문제가 발생했습니다. : " + e.getMessage());
+        }
+    }
+
     public void downloadS3Object(String bucketName, String s3Key, File localFile) {
         try (S3Object s3Object = amazonS3.getObject(bucketName, s3Key);
              InputStream inputStream = s3Object.getObjectContent();
@@ -91,6 +122,19 @@ public class S3Client {
         } catch (IOException e) {
             throw new KoinIllegalStateException("S3 객체 다운로드 중 문제가 발생했습니다. " + e.getMessage());
         }
+    }
+
+    public String extractKeyFromUrl(String url) {
+        if (!url.startsWith(domainUrlPrefix)) {
+            throw new KoinIllegalStateException("잘못된 S3 URL입니다: " + url);
+        }
+        return url.substring(domainUrlPrefix.length());
+    }
+
+    public List<String> extractKeysFromUrls(List<String> urls) {
+        return urls.stream()
+            .map(this::extractKeyFromUrl)
+            .toList();
     }
 
     public String getBucketName() {
