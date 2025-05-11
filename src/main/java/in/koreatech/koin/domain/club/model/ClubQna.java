6627d80e8204c0083c33c95a286279b6a0f28844
krSeonghyeon <ya41921174@gmail.com>
2025-05-11 14:55:56 +0900

    feat: 엔티티 생성 및 is_deleted 필드 추가

---
+++ src/main/java/in/koreatech/koin/domain/club/model/ClubQna.java
@@ -0,0 +1,69 @@
+package in.koreatech.koin.domain.club.model;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static java.lang.Boolean.FALSE;
+import static lombok.AccessLevel.PROTECTED;
+
+import in.koreatech.koin._common.model.BaseEntity;
+import in.koreatech.koin.domain.user.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "club_qna")
+@NoArgsConstructor(access = PROTECTED)
+public class ClubQna extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @JoinColumn(name = "club_id", nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Club club;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = LAZY)
+    private User user;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "parent_id")
+    private ClubQna parent;
+
+    @NotNull
+    @Column(nullable = false)
+    private String content;
+
+    @NotNull
+    @Column(name = "is_deleted", nullable = false, columnDefinition = "TINYINT(1) DEFAULT 0")
+    private Boolean isDeleted = FALSE;
+
+    @Builder
+    private ClubQna(
+        Integer id,
+        Club club,
+        User user,
+        ClubQna parent,
+        String content,
+        Boolean isDeleted
+    ) {
+        this.id = id;
+        this.club = club;
+        this.user = user;
+        this.parent = parent;
+        this.content = content;
+        this.isDeleted = isDeleted;
+    }
+}
--- src/main/resources/db/migration/V154__add_club_qna_table.sql
+++ src/main/resources/db/migration/V154__add_club_qna_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `koin`.`club_qna`
     `user_id`            INT UNSIGNED NULL COMMENT '유저 ID',
     `parent_id`          INT UNSIGNED NULL COMMENT '부모 qna ID',
     `content`            VARCHAR(255) NOT NULL COMMENT '내용',
+    `is_deleted`         TINYINT(1) NOT NULL DEFAULT 0 COMMENT '삭제 여부',
     `created_at`         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성 일자',
     `updated_at`         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일자',
     PRIMARY KEY (`id`),
