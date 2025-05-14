db1b57d6f49bc79a32617228540f4ea33a135b8a
duehee <149302959+duehee@users.noreply.github.com>
2025-05-14 10:12:22 +0900

    feat : clubAdmin 내 isAccept 추가

--- src/main/java/in/koreatech/koin/domain/club/model/ClubAdmin.java
+++ src/main/java/in/koreatech/koin/domain/club/model/ClubAdmin.java
@@ -5,12 +5,14 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 import in.koreatech.koin.domain.user.model.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,14 +35,24 @@ public class ClubAdmin {
     @ManyToOne(fetch = LAZY)
     private User user;
 
+    @NotNull
+    @Column(name = "is_accept", nullable = false)
+    private Boolean isAccept = false;
+
     @Builder
     private ClubAdmin(
         Integer id,
         Club club,
-        User user
+        User user,
+        Boolean isAccept
     ) {
         this.id = id;
         this.club = club;
         this.user = user;
+        this.isAccept = isAccept;
+    }
+
+    public void acceptClubAdmin() {
+        this.isAccept = true;
     }
 }
