93cb6c11edb8cce9c24e6ec7a13f4dba44216aeb
krSeonghyeon <ya41921174@gmail.com>
2025-05-17 16:30:36 +0900

    feat: 로깅 추가

--- src/main/java/in/koreatech/koin/infrastructure/s3/client/CloudFrontClientWrapper.java
+++ src/main/java/in/koreatech/koin/infrastructure/s3/client/CloudFrontClientWrapper.java
@@ -3,11 +3,13 @@ package in.koreatech.koin.infrastructure.s3.client;
 import java.util.List;
 
 import in.koreatech.koin._common.exception.custom.KoinIllegalStateException;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.cloudfront.CloudFrontClient;
 import software.amazon.awssdk.services.cloudfront.model.CreateInvalidationRequest;
 import software.amazon.awssdk.services.cloudfront.model.InvalidationBatch;
 import software.amazon.awssdk.services.cloudfront.model.Paths;
 
+@Slf4j
 public class CloudFrontClientWrapper {
 
     private final CloudFrontClient cloudFrontClient;
@@ -30,6 +32,8 @@ public class CloudFrontClientWrapper {
                 .toList();
             if (normalizedPaths.isEmpty()) return;
 
+            log.info("CloudFront Invalidate 요청 경로: {}", normalizedPaths);
+
             Paths cloudFrontPaths = Paths.builder()
                 .items(normalizedPaths)
                 .quantity(normalizedPaths.size())
@@ -47,6 +51,7 @@ public class CloudFrontClientWrapper {
 
             cloudFrontClient.createInvalidation(request);
         } catch (Exception e) {
+            log.error("CloudFront invalidate 실패. 경로: {}", paths, e);
             throw new KoinIllegalStateException("CloudFront 캐시 무효화 중 문제가 발생했습니다. " + e.getMessage());
         }
     }
