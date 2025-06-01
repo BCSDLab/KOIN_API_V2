551f1f25632fcb5d3546a4d89e30217cdc47fa43
신관규 <soundbar91@gmail.com>
2025-06-01 11:54:02 +0900

    fix: 동아리 좋아요 숨김 여부 필드명 변경

--- src/main/java/in/koreatech/koin/domain/club/dto/request/ClubCreateRequest.java
+++ src/main/java/in/koreatech/koin/domain/club/dto/request/ClubCreateRequest.java
@@ -55,7 +55,11 @@ public record ClubCreateRequest(
     String phoneNumber,
 
     @Schema(description = "동아리 내 역할", example = "회장")
-    String role
+    String role,
+
+    @Schema(description = "동아리 좋아요 숨김 여부", example = "false", requiredMode = REQUIRED)
+    @NotNull(message = "좋아요 숨김 여부는 필수 입력 사항입니다.")
+    Boolean isLikeHidden
 ) {
     @JsonNaming(value = SnakeCaseStrategy.class)
     public record InnerClubManagerRequest(
