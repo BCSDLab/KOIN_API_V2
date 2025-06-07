5017652c42be0aa9810ccae60facad636ae00b77
신관규 <soundbar91@gmail.com>
2025-06-07 19:49:54 +0900

    feat: 동아리 이름 특수문자 유효성 검증 추가

--- src/main/java/in/koreatech/koin/admin/club/dto/request/AdminClubCreateRequest.java
+++ src/main/java/in/koreatech/koin/admin/club/dto/request/AdminClubCreateRequest.java
@@ -10,6 +10,7 @@ import java.util.Objects;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import in.koreatech.koin._common.validation.NotEmoji;
+import in.koreatech.koin._common.validation.SpecialCharNotAllowed;
 import in.koreatech.koin.domain.club.model.Club;
 import in.koreatech.koin.domain.club.model.ClubCategory;
 import in.koreatech.koin.domain.club.model.ClubManager;
@@ -26,6 +27,7 @@ public record AdminClubCreateRequest(
     @Size(max = 20, message = "동아리 이름은 최대 20자 입니다.")
     @NotBlank(message = "동아리 이름은 필수 입력 사항입니다.")
     @NotEmoji(message = "동아리 이름에는 이모지가 들어갈 수 없습니다.")
+    @SpecialCharNotAllowed(message = "동아리 이름에는 특수문자가 들어갈 수 없습니다.")
     String name,
 
     @Schema(description = "동아리 사진 링크", example = "https://bcsdlab.com/static/img/logo.d89d9cc.png", requiredMode = REQUIRED)
