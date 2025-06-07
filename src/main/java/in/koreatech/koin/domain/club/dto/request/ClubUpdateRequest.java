99a328f6142bb3cafe12449da4a39835a03aa3c2
신관규 <soundbar91@gmail.com>
2025-06-07 13:25:12 +0900

    fix: 코인 동아리 QA 2차 반영 (#1620)
    * fix: 코인 어드민 동아리 생성 시 기본값을 활성화로 변경
    
    * feat: NotEmoji 어노테이션 추가

--- src/main/java/in/koreatech/koin/domain/club/dto/request/ClubUpdateRequest.java
+++ src/main/java/in/koreatech/koin/domain/club/dto/request/ClubUpdateRequest.java
@@ -8,6 +8,7 @@ import java.util.Objects;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import in.koreatech.koin._common.validation.NotEmoji;
 import in.koreatech.koin.domain.club.model.Club;
 import in.koreatech.koin.domain.club.model.ClubCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -21,6 +22,7 @@ public record ClubUpdateRequest(
     @Schema(description = "동아리명", example = "BCSD", requiredMode = REQUIRED)
     @Size(max = 20, message = "동아리 이름은 최대 20자 입니다.")
     @NotEmpty(message = "동아리 이름은 필수 입력 사항입니다.")
+    @NotEmoji
     String name,
 
     @Schema(description = "동아리 사진 링크", example = "https://bcsdlab.com/static/img/logo.d89d9cc.png", requiredMode = REQUIRED)
@@ -35,6 +37,7 @@ public record ClubUpdateRequest(
     @Schema(description = "동아리 위치", example = "학생회관", requiredMode = REQUIRED)
     @Size(max = 20, message = "동아리 위치는 최대 20자 입니다.")
     @NotBlank(message = "동아리 위치는 필수 입력 사항입니다.")
+    @NotEmoji
     String location,
 
     @Schema(description = "동아리 소개", example = "즐겁게 일하고 열심히 노는 IT 특성화 동아리", requiredMode = REQUIRED)
@@ -43,18 +46,22 @@ public record ClubUpdateRequest(
 
     @Schema(description = "인스타그램 링크", example = "https://www.instagram.com/bcsdlab/", requiredMode = NOT_REQUIRED)
     @Size(max = 255, message = "인스타그램 링크는 최대 255자 입니다.")
+    @NotEmoji
     String instagram,
 
     @Schema(description = "구글 폼 링크", example = "https://forms.gle/example", requiredMode = NOT_REQUIRED)
     @Size(max = 255, message = "구글폼 링크는 최대 255자 입니다.")
+    @NotEmoji
     String googleForm,
 
     @Schema(description = "오픈 채팅 링크", example = "https://open.kakao.com/example", requiredMode = NOT_REQUIRED)
     @Size(max = 255, message = "오픈 채팅 링크는 최대 255자 입니다.")
+    @NotEmoji
     String openChat,
 
     @Schema(description = "전화번호", example = "01012345678", requiredMode = NOT_REQUIRED)
     @Size(max = 255, message = "전화번호는 최대 255자 입니다.")
+    @NotEmoji
     String phoneNumber,
 
     @Schema(description = "동아리 좋아요 숨김 여부", example = "false", requiredMode = REQUIRED)
