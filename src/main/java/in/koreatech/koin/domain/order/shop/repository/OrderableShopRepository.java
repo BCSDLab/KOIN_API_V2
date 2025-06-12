bec7453000a772cabb9b99ec53b4c4c352feeb42
DHkimgit <dukekim001@naver.com>
2025-06-12 20:29:18 +0900

    chore: 개행 수정

--- src/main/java/in/koreatech/koin/domain/order/shop/repository/OrderableShopRepository.java
+++ src/main/java/in/koreatech/koin/domain/order/shop/repository/OrderableShopRepository.java
@@ -13,48 +13,53 @@ import in.koreatech.koin.domain.order.shop.model.entity.OrderableShop;
 public interface OrderableShopRepository extends JpaRepository<OrderableShop, Integer> {
 
     @Query("""
-        SELECT new in.koreatech.koin.domain.order.shop.model.domain.OrderableShopInfoSummary(
-            s.id,
-            os.id,
-            s.name,
-            s.introduction,
-            os.delivery,
-            os.takeout,
-            os.minimumOrderAmount,
-            CAST(COALESCE(AVG(r.rating), 0.0) AS double),
-            CAST(COUNT(DISTINCT r.id) AS int),
-            CAST(COALESCE(MIN(bdt.fee), 0) AS int),
-            CAST(COALESCE(MAX(bdt.fee), 0) AS int)
-        )
-        FROM OrderableShop os
-        JOIN os.shop s
-        LEFT JOIN s.reviews r
-        LEFT JOIN ShopBaseDeliveryTip bdt ON bdt.shop.id = s.id
-        WHERE os.id = :orderableShopId
-        GROUP BY os.id, s.id
-    """)
-    Optional<OrderableShopInfoSummary> findOrderableShopInfoSummaryById(@Param("orderableShopId") Integer orderableShopId);
+            SELECT new in.koreatech.koin.domain.order.shop.model.domain.OrderableShopInfoSummary(
+                s.id,
+                os.id,
+                s.name,
+                s.introduction,
+                os.delivery,
+                os.takeout,
+                os.minimumOrderAmount,
+                CAST(COALESCE(AVG(r.rating), 0.0) AS double),
+                CAST(COUNT(DISTINCT r.id) AS int),
+                CAST(COALESCE(MIN(bdt.fee), 0) AS int),
+                CAST(COALESCE(MAX(bdt.fee), 0) AS int)
+            )
+            FROM OrderableShop os
+            JOIN os.shop s
+            LEFT JOIN s.reviews r
+            LEFT JOIN ShopBaseDeliveryTip bdt ON bdt.shop.id = s.id
+            WHERE os.id = :orderableShopId
+            GROUP BY os.id, s.id
+        """)
+    Optional<OrderableShopInfoSummary> findOrderableShopInfoSummaryById(
+        @Param("orderableShopId") Integer orderableShopId);
 
     default OrderableShopInfoSummary getOrderableShopInfoSummaryById(Integer shopId) {
         return findOrderableShopInfoSummaryById(shopId).orElseThrow(
-            () -> new OrderableShopNotFoundException("해당 상점이 존재하지 않습니다 : " + shopId)
+            () -> new OrderableShopNotFoundException(
+                "해당 상점이 존재하지 않습니다 : " + shopId
+            )
         );
     }
 
     @Query("""
-        SELECT os
-        FROM OrderableShop os
-        JOIN FETCH os.shop
-        LEFT JOIN FETCH os.shop.owner
-        LEFT JOIN FETCH os.shop.owner.user
-        LEFT JOIN FETCH os.shop.shopOpens
-        WHERE os.id = :orderableShopId
-    """)
+            SELECT os
+            FROM OrderableShop os
+            JOIN FETCH os.shop
+            LEFT JOIN FETCH os.shop.owner
+            LEFT JOIN FETCH os.shop.owner.user
+            LEFT JOIN FETCH os.shop.shopOpens
+            WHERE os.id = :orderableShopId
+        """)
     Optional<OrderableShop> findByIdJoinFetchShop(@Param("orderableShopId") Integer orderableShopId);
 
     default OrderableShop getOrderableShopInfoDetailById(Integer orderableShopId) {
         return findByIdJoinFetchShop(orderableShopId).orElseThrow(
-            () -> new OrderableShopNotFoundException("해당 상점이 존재하지 않습니다 : " + orderableShopId)
+            () -> new OrderableShopNotFoundException(
+                "해당 상점이 존재하지 않습니다 : " + orderableShopId
+            )
         );
     }
 }
