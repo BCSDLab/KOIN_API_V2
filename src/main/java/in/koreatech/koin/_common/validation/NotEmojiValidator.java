84268429f027882ec88bd6c9908b78424d6e7d1e
신관규 <soundbar91@gmail.com>
2025-06-15 13:01:15 +0900

    fix: 이모지 유효성 검증 로직 수정

--- src/main/java/in/koreatech/koin/_common/validation/NotEmojiValidator.java
+++ src/main/java/in/koreatech/koin/_common/validation/NotEmojiValidator.java
@@ -1,17 +1,14 @@
 package in.koreatech.koin._common.validation;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.springframework.stereotype.Component;
+
+import com.vdurmont.emoji.EmojiParser;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 @Component
 public class NotEmojiValidator implements ConstraintValidator<NotEmoji, String> {
-
-    private static final Pattern EMOJI_PATTERN = Pattern.compile("[\\uD83C-\\uDBFF\\uDC00-\\uDFFF]+");
 
     @Override
     public void initialize(NotEmoji constraintAnnotation) {
@@ -20,13 +17,7 @@ public class NotEmojiValidator implements ConstraintValidator<NotEmoji, String> 
 
     @Override
     public boolean isValid(String field, ConstraintValidatorContext constraintValidatorContext) {
-        if (field == null) {
-            return true;
-        }
-        Matcher emojiMatcher = EMOJI_PATTERN.matcher(field);
-        if (emojiMatcher.find()) {
-            return false;
-        }
-        return true;
+        if (field == null) return true;
+        return EmojiParser.extractEmojis(field).isEmpty();
     }
 }
