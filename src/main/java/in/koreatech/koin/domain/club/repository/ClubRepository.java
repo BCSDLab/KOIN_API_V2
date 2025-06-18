d8f1d62ca80fdc758b392ed6cddd012d76852d2b
krSeonghyeon <ya41921174@gmail.com>
2025-06-18 14:24:05 +0900

    chore: 잘못 삭제한부분 다시 추가

--- src/main/java/in/koreatech/koin/domain/club/repository/ClubRepository.java
+++ src/main/java/in/koreatech/koin/domain/club/repository/ClubRepository.java
@@ -3,13 +3,16 @@ package in.koreatech.koin.domain.club.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import in.koreatech.koin.domain.club.exception.ClubNotFoundException;
 import in.koreatech.koin.domain.club.model.Club;
 import in.koreatech.koin.domain.club.model.ClubCategory;
+import jakarta.persistence.LockModeType;
 
 public interface ClubRepository extends Repository<Club, Integer> {
 
@@ -19,6 +22,20 @@ public interface ClubRepository extends Repository<Club, Integer> {
 
     default Club getById(Integer id) {
         return findById(id).orElseThrow(() -> ClubNotFoundException.withDetail("id : " + id));
+    }
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM Club c WHERE c.id = :id")
+    Club findByIdWithPessimisticLock(@Param("id") Integer id);
+
+    default Club getByIdWithPessimisticLock(Integer id) {
+        Club club = findByIdWithPessimisticLock(id);
+
+        if (club == null) {
+            throw ClubNotFoundException.withDetail("id : " + id);
+        }
+
+        return club;
     }
 
     @Modifying
