782811fc0290b7938521be7f110b0c5430be7965
DHkimgit <dukekim001@naver.com>
2025-06-19 21:18:40 +0900

    refactor: 장바구니 조회시 fetch join 설정 불필요 조회 메서드 제거

--- src/main/java/in/koreatech/koin/domain/order/cart/repository/CartRepository.java
+++ src/main/java/in/koreatech/koin/domain/order/cart/repository/CartRepository.java
@@ -10,25 +10,11 @@ import in.koreatech.koin.domain.order.cart.model.Cart;
 
 public interface CartRepository extends JpaRepository<Cart, Integer> {
 
-    @Query(value = "SELECT c.* " +
-        "FROM cart c " +
-        "LEFT JOIN cart_menu_item cmi ON c.id = cmi.cart_id " +
-        "WHERE c.user_id = :userId " +
-        "AND c.orderable_shop_id = :orderableShopId",
-        nativeQuery = true)
-    Optional<Cart> findCartByUserAndShop(
-        @Param("userId") Integer userId,
-        @Param("orderableShopId") Integer orderableShopId
-    );
-
-    @Query(value = "SELECT c.* " +
-        "FROM cart c " +
-        "LEFT JOIN cart_menu_item cmi ON c.id = cmi.cart_id " +
-        "WHERE c.user_id = :userId ",
-        nativeQuery = true)
-    Optional<Cart> findCartByUserId(
-        @Param("userId") Integer userId
-    );
+    @Query("SELECT c FROM Cart c " +
+        "LEFT JOIN FETCH c.orderableShop os " +
+        "LEFT JOIN FETCH c.cartMenuItems cmi " +
+        "WHERE c.user.id = :userId")
+    Optional<Cart> findCartByUserId(@Param("userId") Integer userId);
 
     void deleteByUserId(Integer userId);
 }
