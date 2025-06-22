fc47e0eaa972de07c7f8ba3023e69817d7629717
신관규 <soundbar91@gmail.com>
2025-06-22 12:25:48 +0900

    feat: 주문 테이블 추가

---
+++ src/main/java/in/koreatech/koin/domain/order/model/Order.java
@@ -0,0 +1,67 @@
+package in.koreatech.koin.domain.order.model;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static java.lang.Boolean.FALSE;
+import static lombok.AccessLevel.PROTECTED;
+
+import org.hibernate.annotations.Where;
+
+import in.koreatech.koin._common.model.BaseEntity;
+import in.koreatech.koin.domain.order.shop.model.entity.shop.OrderableShop;
+import in.koreatech.koin.domain.user.model.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(schema = "koin", name = "order")
+@Where(clause = "is_deleted=0")
+@NoArgsConstructor(access = PROTECTED)
+public class Order extends BaseEntity {
+
+    @Id
+    @NotBlank
+    @Size(min = 6, max = 64)
+    @Column(name = "id", length = 64, nullable = false, updatable = false)
+    private String id;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "address", length = 100, nullable = false, updatable = false)
+    private String address;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "phone_number", length = 20, nullable = false, updatable = false)
+    private String phoneNumber;
+
+    @NotNull
+    @Size(max = 50)
+    @Column(name = "to_owner", length = 50, nullable = false, updatable = false)
+    private String toOwner;
+
+    @Size(max = 50)
+    @Column(name = "to_rider", length = 50, nullable = false, updatable = false)
+    private String toRider;
+
+    @NotNull
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = FALSE;
+
+    @JoinColumn(name = "orderable_shop_id")
+    @ManyToOne(fetch = LAZY)
+    private OrderableShop orderableShop;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = LAZY)
+    private User user;
+}
---
+++ src/main/java/in/koreatech/koin/domain/order/model/OrderMenuOption.java
@@ -0,0 +1,41 @@
+package in.koreatech.koin.domain.order.model;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(schema = "koin", name = "order_menu_option")
+@NoArgsConstructor(access = PROTECTED)
+public class OrderMenuOption {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Integer id;
+
+    @NotBlank
+    @Column(name = "option_name", nullable = false, updatable = false)
+    private String optionName;
+
+    @NotNull
+    @Column(name = "option_price", nullable = false, updatable = false)
+    private Integer optionPrice;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "order_menu_id", nullable = false)
+    private OrderMenu orderMenu;
+}
