6f782f6a91f4e2730464b3688411d10abed67fe5
krSeonghyeon <ya41921174@gmail.com>
2025-07-05 15:32:39 +0900

    feat: 알림관련 엔티티 추가

---
+++ src/main/java/in/koreatech/koin/domain/club/model/ClubRecruitmentSubscription.java
@@ -0,0 +1,50 @@
+package in.koreatech.koin.domain.club.model;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import in.koreatech.koin._common.model.BaseEntity;
+import in.koreatech.koin.domain.user.model.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(
+    name = "club_recruitment_subscription",
+    uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"club_id", "user_id"})
+    }
+)
+@NoArgsConstructor(access = PROTECTED)
+public class ClubRecruitmentSubscription extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "club_id", nullable = false)
+    private Club club;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    private ClubRecruitmentSubscription(
+        Club club,
+        User user
+    ) {
+        this.club = club;
+        this.user = user;
+    }
+}
