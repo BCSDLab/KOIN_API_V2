913338b36d9847166e4c10794cdd195984f38cce
신관규 <soundbar91@gmail.com>
2025-07-06 15:39:04 +0900

    feat: 동아리 모집 정렬 enum 수정
    - 동아리 생성 순, 동아리 모집 생성 순, 동아리 마감 순 추가

--- src/main/java/in/koreatech/koin/domain/club/enums/ClubSortType.java
+++ src/main/java/in/koreatech/koin/domain/club/enums/ClubSortType.java
@@ -1,27 +1,58 @@
 package in.koreatech.koin.domain.club.enums;
 
 import static in.koreatech.koin.domain.club.model.QClub.club;
+import static in.koreatech.koin.domain.club.model.QClubRecruitment.clubRecruitment;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberTemplate;
 
 import lombok.Getter;
 
 @Getter
 public enum ClubSortType {
     NONE {
+        // TODO. 향후 해당 필드를 사용하지 않는 경우 빈 리스트로 수정
         @Override
-        public OrderSpecifier<LocalDateTime> getOrderSpecifier() {
-            return club.createdAt.asc();
+        public List<OrderSpecifier<?>> getOrderSpecifiers() {
+            return List.of(club.createdAt.asc());
+        }
+    },
+    CREATED_AT_ASC {
+        @Override
+        public List<OrderSpecifier<?>> getOrderSpecifiers() {
+            return List.of(club.createdAt.asc());
         }
     },
     HITS_DESC {
         @Override
-        public OrderSpecifier<Integer> getOrderSpecifier() {
-            return club.hits.desc();
+        public List<OrderSpecifier<?>> getOrderSpecifiers() {
+            return List.of(club.hits.desc());
+        }
+    },
+    RECRUITMENT_UPDATED_DESC {
+        @Override
+        public List<OrderSpecifier<?>> getOrderSpecifiers() {
+            return List.of(clubRecruitment.updatedAt.desc());
+        }
+    },
+    RECRUITING_DEADLINE_ASC {
+        @Override
+        public List<OrderSpecifier<?>> getOrderSpecifiers() {
+            NumberTemplate<Integer> deadlineGap = Expressions.numberTemplate(
+                Integer.class,
+                "datediff({0}, current_date)",
+                clubRecruitment.endDate
+            );
+
+            return List.of(
+                deadlineGap.asc(),
+                clubRecruitment.isAlwaysRecruiting.desc()
+            );
         }
     };
 
-    public abstract OrderSpecifier<?> getOrderSpecifier();
+    public abstract List<OrderSpecifier<?>> getOrderSpecifiers();
 }
