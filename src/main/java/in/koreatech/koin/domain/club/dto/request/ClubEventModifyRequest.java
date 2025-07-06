6e494620e76d80d972355e99f51dcd4c6f7431f4
duehee <149302959+duehee@users.noreply.github.com>
2025-07-06 17:34:56 +0900

    feat : DTO 생성

---
+++ src/main/java/in/koreatech/koin/domain/club/dto/request/ClubEventModifyRequest.java
@@ -0,0 +1,52 @@
+package in.koreatech.koin.domain.club.dto.request;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import in.koreatech.koin.domain.club.model.Club;
+import in.koreatech.koin.domain.club.model.ClubEvent;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record ClubEventModifyRequest(
+    @Schema(description = "행사 이름", example = "B-CON", requiredMode = REQUIRED)
+    @NotBlank(message = "행사 이름은 필수입니다.")
+    @Size(max = 30, message = "행사 이름은 30자 이내여야 합니다.")
+    String name,
+
+    @Schema(description = "행사 이미지", example = "https://bcsdlab.com/static/img/logo.d89d9cc.png", requiredMode = NOT_REQUIRED)
+    String imageUrl,
+
+    @Schema(description = "행사 시작일", example = "2025-07-01T09:00:00", requiredMode = REQUIRED)
+    @NotNull(message = "행사 시작일은 필수입니다.")
+    LocalDateTime startDate,
+
+    @Schema(description = "행사 종료일", example = "2025-07-02T18:00:00", requiredMode = REQUIRED)
+    @NotNull(message = "행사 종료일은 필수입니다.")
+    LocalDateTime endDate,
+
+    @Schema(description = "행사 소개 (요약)", example = "BCSDLab의 멘토 혹은 레귤러들의 경험을 공유해요.", requiredMode = REQUIRED)
+    @NotBlank(message = "행사 소개는 필수입니다.")
+    @Size(max = 70, message = "소개는 70자 이내여야 합니다.")
+    String introduce,
+
+    @Schema(description = "행사 상세 내용", example = "여러 동아리원들과 자신의 생각, 경험에 대해 나눠요,", requiredMode = NOT_REQUIRED)
+    String content
+) {
+    public ClubEvent toEntity(Club club) {
+        return ClubEvent.builder()
+            .club(club)
+            .name(name)
+            .imageUrl(imageUrl)
+            .startDate(startDate)
+            .endDate(endDate)
+            .introduce(introduce)
+            .content(content)
+            .build();
+    }
+}
---
+++ src/main/java/in/koreatech/koin/domain/club/dto/response/ClubEventResponse.java
@@ -0,0 +1,70 @@
+package in.koreatech.koin.domain.club.dto.response;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.*;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import in.koreatech.koin.domain.club.enums.ClubEventStatus;
+import in.koreatech.koin.domain.club.model.ClubEvent;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record ClubEventResponse(
+    @Schema(description = "동아리 행사 ID", example = "12")
+    Integer id,
+
+    @Schema(description = "행사 이름", example = "B-CON")
+    String name,
+
+    @Schema(description = "행사 이미지", example = "https://bcsdlab.com/static/img/logo.d89d9cc.png", requiredMode = NOT_REQUIRED)
+    String imageUrl,
+
+    @Schema(description = "행사 시작일", example = "2025-07-01T09:00:00")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    LocalDateTime startDate,
+
+    @Schema(description = "행사 종료일", example = "2025-07-02T18:00:00")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    LocalDateTime endDate,
+
+    @Schema(description = "행사 소개 (요약)", example = "BCSDLab의 멘토 혹은 레귤러들의 경험을 공유해요.")
+    String introduce,
+
+    @Schema(description = "행사 상세 내용", example = "여러 동아리원들과 자신의 생각, 경험에 대해 나눠요,")
+    String content,
+
+    @Schema(description = "현재 행사 상태", example = "UPCOMING")
+    String status
+) {
+
+    public static ClubEventResponse from(ClubEvent event, LocalDateTime now) {
+        return new ClubEventResponse(
+            event.getId(),
+            event.getName(),
+            event.getImageUrl(),
+            event.getStartDate(),
+            event.getEndDate(),
+            event.getIntroduce(),
+            event.getContent(),
+            calculateStatus(event.getStartDate(), event.getEndDate(), now).getDisplayName()
+        );
+    }
+
+    public static ClubEventStatus calculateStatus(LocalDateTime start, LocalDateTime end, LocalDateTime now) {
+        if (now.isBefore(start.minusHours(1))) {
+            return ClubEventStatus.UPCOMING;
+        }
+        if (now.isBefore(start)) {
+            return ClubEventStatus.SOON;
+        }
+        if (now.isBefore(end.plusMinutes(1))) {
+            return ClubEventStatus.ONGOING;
+        }
+        return ClubEventStatus.ENDED;
+    }
+}
