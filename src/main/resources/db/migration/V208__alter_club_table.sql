ade0318cb207b118d3f97ff6a064d29cb716130b
krSeonghyeon <ya41921174@gmail.com>
2025-07-26 15:15:19 +0900

    chore: 줄바꿈수정

--- src/main/resources/db/migration/V208__alter_club_table.sql
+++ src/main/resources/db/migration/V208__alter_club_table.sql
@@ -1,4 +1,5 @@
 ALTER TABLE club ADD COLUMN normalized_name VARCHAR(50) COMMENT '검색용 정규화 이름';
 UPDATE club SET normalized_name = LOWER(REPLACE(name, ' ', ''));
 ALTER TABLE club MODIFY COLUMN normalized_name VARCHAR(50) NOT NULL COMMENT '검색용 정규화 이름';
-DROP INDEX idx_club_name ON club; CREATE INDEX idx_club_normalized_name ON club(normalized_name);
+DROP INDEX idx_club_name ON club;
+CREATE INDEX idx_club_normalized_name ON club(normalized_name);
