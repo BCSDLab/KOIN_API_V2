db527cebbb84aa67482903d05dd2d080c5c89769
DHkimgit <dukekim001@naver.com>
2025-07-27 00:53:26 +0900

    chore: 리뷰 반영

--- src/main/java/in/koreatech/koin/domain/order/shop/dto/shoplist/OrderableShopCategoryFilterCriteria.java
+++ src/main/java/in/koreatech/koin/domain/order/shop/dto/shoplist/OrderableShopCategoryFilterCriteria.java
@@ -10,15 +10,15 @@ public enum OrderableShopCategoryFilterCriteria {
     ALL("ALL", null),
     CHICKEN("CHICKEN", 2),
     PIZZA_BUGGER("PIZZA_BUGGER", 3),
-    DOSIRAK_BUNSIK ("DOSIRAK_BUNSIK", 4),
-    JOKBAL ("JOKBAL", 5),
-    CHINESE_RESTAURANT ("CHINESE_RESTAURANT", 6),
-    MEAT_RESTAURANT ("MEAT_RESTAURANT", 7),
-    KOREAN_RESTAURANT ("KOREAN_RESTAURANT", 8),
-    PUB ("PUB", 9),
-    CAFE ("CAFE", 10),
-    CALL_VAN ("CALL_VAN", 11),
-    ETC ("ETC", 12)
+    DOSIRAK_BUNSIK("DOSIRAK_BUNSIK", 4),
+    JOKBAL("JOKBAL", 5),
+    CHINESE_RESTAURANT("CHINESE_RESTAURANT", 6),
+    MEAT_RESTAURANT("MEAT_RESTAURANT", 7),
+    KOREAN_RESTAURANT("KOREAN_RESTAURANT", 8),
+    PUB("PUB", 9),
+    CAFE("CAFE", 10),
+    CALL_VAN("CALL_VAN", 11),
+    ETC("ETC", 12)
     ;
 
     private final String name;
@@ -27,9 +27,5 @@ public enum OrderableShopCategoryFilterCriteria {
     OrderableShopCategoryFilterCriteria(String name, Integer value) {
         this.name = name;
         this.value = value;
-    }
-
-    public boolean matches(List<Integer> categoryIds) {
-        return this.value == null || categoryIds.contains(value);
     }
 }
