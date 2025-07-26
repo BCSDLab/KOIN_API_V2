f0a948d66f89fa75c4b225a3a60ba1614a44ad64
DHkimgit <dukekim001@naver.com>
2025-07-26 19:33:46 +0900

    fix: flyway 대응

--- src/main/resources/db/migration/V209__alter_table_shop_operation_add_index.sql
+++ src/main/resources/db/migration/V209__alter_table_shop_operation_add_index.sql
@@ -1,3 +1,1 @@
-DROP INDEX idx_shop_operation_shop_id_is_open ON shop_operation;
-DROP INDEX idx_shop_operation_shop_id ON shop_operation;
 CREATE INDEX idx_shop_operation_shop_id_open_deleted ON shop_operation(shop_id, is_open, is_deleted);
