73eca147d3e6e17c07d8d7a8e320fc7f3a6ad3d8
Baejinho4028 <onionring4028@naver.com>
2025-07-30 10:50:25 +0900

    refactor: 상위 클래스 MockBean 선언 제거

--- src/test/java/in/koreatech/koin/acceptance/AcceptanceTest.java
+++ src/test/java/in/koreatech/koin/acceptance/AcceptanceTest.java
@@ -3,36 +3,27 @@ package in.koreatech.koin.acceptance;
 import java.time.Clock;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.transaction.TestTransaction;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
+import in.koreatech.koin.acceptance.support.DBInitializer;
 import in.koreatech.koin.config.TestJpaConfiguration;
 import in.koreatech.koin.config.TestRedisConfiguration;
+import in.koreatech.koin.config.TestResilience4jConfig;
 import in.koreatech.koin.config.TestTimeConfig;
-import in.koreatech.koin.domain.bus.service.express.client.PublicExpressBusClient;
-import in.koreatech.koin.domain.bus.service.express.client.StaticExpressBusClient;
-import in.koreatech.koin.domain.bus.service.express.client.TmoneyExpressBusClient;
-import in.koreatech.koin.domain.notification.eventlistener.ArticleKeywordEventListener;
-import in.koreatech.koin.domain.notification.eventlistener.CoopEventListener;
-import in.koreatech.koin.infrastructure.slack.eventlistener.OwnerEventListener;
-import in.koreatech.koin.infrastructure.slack.eventlistener.ReviewEventListener;
-import in.koreatech.koin.domain.notification.eventlistener.ShopEventListener;
-import in.koreatech.koin.infrastructure.slack.eventlistener.StudentEventListener;
-import in.koreatech.koin.acceptance.support.DBInitializer;
-import in.koreatech.koin.acceptance.util.TestCircuitBreakerClient;
 import jakarta.persistence.EntityManager;
 
 @SpringBootTest
@@ -40,54 +31,27 @@ import jakarta.persistence.EntityManager;
 @Import({DBInitializer.class,
     TestJpaConfiguration.class,
     TestTimeConfig.class,
-    TestRedisConfiguration.class})
+    TestRedisConfiguration.class,
+    TestResilience4jConfig.class})
 @ActiveProfiles("test")
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AcceptanceTest {
 
     private static final String ROOT = "test";
     private static final String ROOT_PASSWORD = "1234";
 
     @Autowired
-    public MockMvc mockMvc;
-
-    @MockBean
-    protected OwnerEventListener ownerEventListener;
-
-    @MockBean
-    protected ReviewEventListener reviewEventListener;
-
-    @MockBean
-    protected StudentEventListener studentEventListener;
-
-    @MockBean
-    protected ShopEventListener shopEventListener;
-
-    @MockBean
-    protected CoopEventListener coopEventListener;
-
-    @MockBean
-    protected ArticleKeywordEventListener articleKeywordEventListener;
-
-    @MockBean
-    protected PublicExpressBusClient publicExpressBusClient;
-
-    @MockBean
-    protected TmoneyExpressBusClient tmoneyExpressBusClient;
-
-    @MockBean
-    protected StaticExpressBusClient staticExpressBusClient;
-
-    @SpyBean
-    protected TestCircuitBreakerClient testCircuitBreakerClient;
-
-    @Autowired
-    private DBInitializer dataInitializer;
+    protected MockMvc mockMvc;
 
     @Autowired
     protected EntityManager entityManager;
 
     @Autowired
     protected Clock clock;
+
+    @Autowired
+    private DBInitializer dataInitializer;
 
     @Container
     protected static MySQLContainer mySqlContainer;
