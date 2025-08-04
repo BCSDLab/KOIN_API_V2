4818cf00663694e7ab6dc128175b0a2ac7d46f44
김두현 <96455522+DHkimgit@users.noreply.github.com>
2025-08-04 20:52:49 +0900

    feat: 분실물 채팅방 생성 및 조회 서비스 테스트 코드 작성 (#1863)
    * test: 분실물 채팅방 생성 및 조회 서비스 테스트 코드
    
    * chore: 값 수정
    
    * chore: 값 수정
    
    * chore: 리뷰 반영
    
    * chore: 리뷰 반영

---
+++ src/test/java/in/koreatech/koin/unit/fixutre/LostItemChatFixture.java
@@ -0,0 +1,19 @@
+package in.koreatech.koin.unit.fixutre;
+
+import in.koreatech.koin.domain.community.lostitem.chatroom.model.LostItemChatRoomInfoEntity;
+
+public class LostItemChatFixture {
+
+    private LostItemChatFixture() {}
+
+    public static LostItemChatRoomInfoEntity 분실물_게시글_채팅방(
+        Integer articleId, Integer chatRoomId, Integer authorId, Integer messageSenderId) {
+
+        return LostItemChatRoomInfoEntity.builder()
+            .articleId(articleId)
+            .chatRoomId(chatRoomId)
+            .authorId(authorId)
+            .ownerId(messageSenderId)
+            .build();
+    }
+}
