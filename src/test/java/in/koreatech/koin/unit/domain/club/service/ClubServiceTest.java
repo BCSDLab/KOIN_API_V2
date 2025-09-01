22d97f2910ac42848a7b66582562d142f39d593d
Donghun <2dh2@naver.com>
2025-09-01 18:52:16 +0900

    test: 동아리 정보 수정 단위 테스트 작성

--- src/test/java/in/koreatech/koin/unit/domain/club/service/ClubServiceTest.java
+++ src/test/java/in/koreatech/koin/unit/domain/club/service/ClubServiceTest.java
@@ -1,6 +1,9 @@
 package in.koreatech.koin.unit.domain.club.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.List;
 
@@ -14,6 +17,11 @@ import org.springframework.context.ApplicationEventPublisher;
 
 import in.koreatech.koin.common.event.ClubCreateEvent;
 import in.koreatech.koin.domain.club.dto.request.ClubCreateRequest;
+import in.koreatech.koin.domain.club.dto.request.ClubIntroductionUpdateRequest;
+import in.koreatech.koin.domain.club.dto.request.ClubUpdateRequest;
+import in.koreatech.koin.domain.club.dto.response.ClubResponse;
+import in.koreatech.koin.domain.club.model.Club;
+import in.koreatech.koin.domain.club.model.ClubCategory;
 import in.koreatech.koin.domain.club.model.redis.ClubCreateRedis;
 import in.koreatech.koin.domain.club.repository.ClubCategoryRepository;
 import in.koreatech.koin.domain.club.repository.ClubEventImageRepository;
@@ -34,6 +42,8 @@ import in.koreatech.koin.domain.club.repository.redis.ClubHotRedisRepository;
 import in.koreatech.koin.domain.club.service.ClubService;
 import in.koreatech.koin.domain.student.repository.StudentRepository;
 import in.koreatech.koin.domain.user.repository.UserRepository;
+import in.koreatech.koin.global.auth.exception.AuthorizationException;
+import in.koreatech.koin.unit.fixture.ClubFixture;
 
 @ExtendWith(MockitoExtension.class)
 public class ClubServiceTest {
@@ -89,6 +99,132 @@ public class ClubServiceTest {
 
         ArgumentCaptor<ClubCreateEvent> eventCaptor = ArgumentCaptor.forClass(ClubCreateEvent.class);
         verify(eventPublisher).publishEvent(eventCaptor.capture());
+    }
+
+    @Test
+    void 동아리_관리자가_정보_수정_요청을_보낸_경우_정상적으로_처리한다() {
+        // given
+        ClubUpdateRequest request = new ClubUpdateRequest(
+            "Updated BCSD",
+            "https://bcsdlab.com/static/img/new_logo.png",
+            2,
+            "2공학관",
+            "즐겁게 일하고 열심히 노는 IT 특성화 동아리",
+            "https://www.instagram.com/bcsdlab/",
+            "https://forms.gle/example",
+            "https://open.kakao.com/example",
+            "01012345678",
+            false
+        );
+
+        ClubCategory newCategory = ClubCategory.builder()
+            .id(2)
+            .name("문화")
+            .build();
+
+        Integer clubId = 1;
+        Integer studentId = 1;
+        Club club = ClubFixture.활성화_BCSD_동아리();
+
+        when(clubRepository.getById(clubId)).thenReturn(club);
+        when(clubManagerRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(true);
+        when(clubCategoryRepository.getById(request.clubCategoryId())).thenReturn(newCategory);
+        when(clubLikeRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(true);
+        when(clubRecruitmentSubscriptionRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(true);
+
+        // when
+        ClubResponse response = clubService.updateClub(clubId, request, studentId);
+
+        // then (club 객체 상태 검증)
+        assertThat(club.getName()).isEqualTo(request.name());
+        assertThat(club.getImageUrl()).isEqualTo(request.imageUrl());
+        assertThat(club.getClubCategory()).isEqualTo(newCategory);
+        assertThat(club.getLocation()).isEqualTo(request.location());
+        assertThat(club.getDescription()).isEqualTo(request.description());
+        assertThat(club.getIsLikeHidden()).isEqualTo(request.isLikeHidden());
+
+        // then (response 상태 검증)
+        assertThat(response.name()).isEqualTo(request.name());
+        assertThat(response.imageUrl()).isEqualTo(request.imageUrl());
+        assertThat(response.category()).isEqualTo(newCategory.getName());
+        assertThat(response.location()).isEqualTo(request.location());
+        assertThat(response.description()).isEqualTo(request.description());
+        assertThat(response.isLikeHidden()).isEqualTo(request.isLikeHidden());
+        assertThat(response.isLiked()).isTrue();
+        assertThat(response.isRecruitSubscribed()).isTrue();
+        assertThat(response.manager()).isTrue();
+        assertThat(response.instagram()).contains(request.instagram());
+        assertThat(response.googleForm()).contains(request.googleForm());
+        assertThat(response.openChat()).contains(request.openChat());
+        assertThat(response.phoneNumber()).contains(request.phoneNumber());
+    }
+
+    @Test
+    void 동아리_관리자가_아닌_유저가_정보_수정_요청을_보낸_경우_예외를_발생한다() {
+        // given
+        ClubUpdateRequest request = new ClubUpdateRequest(
+            "Updated BCSD",
+            "https://bcsdlab.com/static/img/new_logo.png",
+            2,
+            "2공학관",
+            "즐겁게 일하고 열심히 노는 IT 특성화 동아리",
+            "https://www.instagram.com/bcsdlab/",
+            "https://forms.gle/example",
+            "https://open.kakao.com/example",
+            "01012345678",
+            false
+        );
+
+        Integer clubId = 1;
+        Integer studentId = 1;
+        Club club = ClubFixture.활성화_BCSD_동아리();
+
+        when(clubRepository.getById(clubId)).thenReturn(club);
+        when(clubManagerRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(false);
+
+        // when / then
+        assertThatThrownBy(() -> clubService.updateClub(clubId, request, studentId))
+            .isInstanceOf(AuthorizationException.class)
+            .hasMessageContaining("권한이 없습니다.");
+    }
+
+    @Test
+    void 동아리_관리자가_동아리_소개_수정_요청을_보낸_경우_정상적으로_처리한다() {
+        // given
+        ClubIntroductionUpdateRequest request = new ClubIntroductionUpdateRequest("수정된 동아리 소개 문자열");
+
+        Integer clubId = 1;
+        Integer studentId = 1;
+        Club club = ClubFixture.활성화_BCSD_동아리();
+
+        when(clubRepository.getById(clubId)).thenReturn(club);
+        when(clubManagerRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(true);
+        when(clubLikeRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(true);
+        when(clubRecruitmentSubscriptionRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(true);
+
+        // when
+        ClubResponse response = clubService.updateClubIntroduction(clubId, request, studentId);
+
+        // then
+        assertThat(response.introduction()).isEqualTo(request.introduction());
+    }
+
+    @Test
+    void 동아리_관리자가_아닌_유저가_동아리_소개_수정_요청을_보낸_경우_예외를_발생한다() {
+        // given
+        ClubIntroductionUpdateRequest request = new ClubIntroductionUpdateRequest("수정된 동아리 소개 문자열");
+
+        Integer clubId = 1;
+        Integer studentId = 1;
+        Club club = ClubFixture.활성화_BCSD_동아리();
+
+        when(clubRepository.getById(clubId)).thenReturn(club);
+        when(clubManagerRepository.existsByClubIdAndUserId(clubId, studentId)).thenReturn(false);
+
+        // when / then
+        assertThatThrownBy(() -> clubService.updateClubIntroduction(clubId, request, studentId))
+            .isInstanceOf(AuthorizationException.class)
+            .hasMessageContaining("권한이 없습니다.");
     }
 }
 
