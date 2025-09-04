586ec590bcaae88ce02936ea9b0976feb1c87a32
신관규 <soundbar91@gmail.com>
2025-09-04 20:26:28 +0900

    fix: 결제 API 스웨거 그룹화 분리

--- src/main/java/in/koreatech/koin/global/config/SwaggerGroupConfig.java
+++ src/main/java/in/koreatech/koin/global/config/SwaggerGroupConfig.java
@@ -28,7 +28,7 @@ public class SwaggerGroupConfig {
     public GroupedOpenApi adminApi() {
         return createGroupedOpenApi(
             "1. Admin API",
-            new String[] { "in.koreatech.koin.admin" });
+            new String[] {"in.koreatech.koin.admin"});
     }
 
     @Bean
@@ -79,14 +79,14 @@ public class SwaggerGroupConfig {
     public GroupedOpenApi abtestApi() {
         return createGroupedOpenApi(
             "5. abTest API",
-            new String[] { "in.koreatech.koin.admin.abtest" });
+            new String[] {"in.koreatech.koin.admin.abtest"});
     }
 
     @Bean
     public GroupedOpenApi socketApi() {
         return createGroupedOpenApi(
             "6. chat API",
-            new String[] { "in.koreatech.koin.domain.community.lostitem.chatroom" });
+            new String[] {"in.koreatech.koin.domain.community.lostitem.chatroom"});
     }
 
     @Bean
@@ -110,6 +110,14 @@ public class SwaggerGroupConfig {
             "8. Business Order API",
             new String[] {
                 "in.koreatech.koin.domain.order",
+            });
+    }
+
+    @Bean
+    public GroupedOpenApi paymentApi() {
+        return createGroupedOpenApi(
+            "9. Payment API",
+            new String[] {
                 "in.koreatech.koin.domain.payment",
             });
     }
