e6029a563f16b110c7072c68c74984b97c154ce3
DHkimgit <dukekim001@naver.com>
2025-09-05 15:52:32 +0900

    feat: AddressValidator 추상화 & 주석 수정

--- src/main/java/in/koreatech/koin/domain/address/service/RoadNameAddressValidator.java
+++ src/main/java/in/koreatech/koin/domain/address/service/RoadNameAddressValidator.java
@@ -12,13 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class RoadNameAddressValidator {
+public class RoadNameAddressValidator implements AddressValidator {
 
     private final AddressClient roadNameAddressClient;
 
     /**
      * 외부 주소 검색 API를 호출하여 주소의 유효성을 검증한다. 외부 API 오류 발생시 예외를 반환하지 않고 검증을 통과한 것으로 처리한다.
-     * @param address 주소
+     * @param address 주소(상세 주소를 제외한 기본 주소. 예: 충청남도 천안시 동남구 병천면 충절로 1600)
      * @throws CustomException 주소 검색 결과가 0개 이하일 경우 유효하지 않은 주소로 판단한다.
      */
     public void validateAddress(String address) {
