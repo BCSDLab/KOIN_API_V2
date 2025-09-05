2612f7c4521d12689e19b176dd3b8ece6d796582
신관규 <soundbar91@gmail.com>
2025-09-05 22:43:45 +0900

    feat: order_delivery 테이블 추가

--- src/main/java/in/koreatech/koin/domain/order/model/OrderDelivery.java
+++ src/main/java/in/koreatech/koin/domain/order/model/OrderDelivery.java
@@ -2,6 +2,9 @@ package in.koreatech.koin.domain.order.model;
 
 import static lombok.AccessLevel.PROTECTED;
 
+import java.time.LocalDateTime;
+
+import in.koreatech.koin.common.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -20,11 +23,11 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "order_delivery")
 @NoArgsConstructor(access = PROTECTED)
-public class OrderDelivery {
+public class OrderDelivery extends BaseEntity {
 
     @Id
     @Column(name = "order_id", nullable = false, updatable = false)
-    private String id;
+    private Integer id;
 
     @MapsId
     @OneToOne
@@ -52,20 +55,35 @@ public class OrderDelivery {
     @Column(name = "provide_cutlery", nullable = false, updatable = false, columnDefinition = "TINYINT(1)")
     private Boolean provideCutlery;
 
+    @Column(name = "dispatched_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime dispatchedAt;
+
+    @Column(name = "completed_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime completedAt;
+
+    @Column(name = "estimated_arrival_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime estimatedArrivalAt;
+
     @Builder
-    public OrderDelivery(
+    private OrderDelivery(
         Order order,
         String address,
         String toOwner,
         String toRider,
+        Integer deliveryTip,
         Boolean provideCutlery,
-        Integer deliveryTip
+        LocalDateTime dispatchedAt,
+        LocalDateTime completedAt,
+        LocalDateTime estimatedArrivalAt
     ) {
         this.order = order;
         this.address = address;
         this.toOwner = toOwner;
         this.toRider = toRider;
-        this.provideCutlery = provideCutlery;
         this.deliveryTip = deliveryTip;
+        this.provideCutlery = provideCutlery;
+        this.dispatchedAt = dispatchedAt;
+        this.completedAt = completedAt;
+        this.estimatedArrivalAt = estimatedArrivalAt;
     }
 }
---
+++ src/main/resources/db/migration/V219__add_order_delivery_v2.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `koin`.`order_delivery`
+(
+    `order_id`                  INT UNSIGNED    NOT NULL                COMMENT '주문 ID',
+    `address`                   TEXT            NOT NULL                COMMENT '배달 주소',
+    `address_detail`            TEXT            NULL                    COMMENT '배달 상세 주소',
+    `to_owner`                  VARCHAR(50)     NOT NULL                COMMENT '사장님 전달 메시지',
+    `to_rider`                  VARCHAR(50)     NOT NULL                COMMENT '라이더 전달 메시지',
+    `delivery_tip`              INT UNSIGNED    NOT NULL                COMMENT '배달비',
+    `provide_cutlery`           TINYINT(1)      NOT NULL    DEFAULT 0   COMMENT '수저, 포크 수령 여부',
+    `dispatched_at`             TIMESTAMP       NULL                    COMMENT '배달 출발 일시',
+    `completed_at`              TIMESTAMP       NULL                    COMMENT '배달 완료 일시',
+    `estimated_arrival_at`      TIMESTAMP       NULL                    COMMENT '배달 완료 예상 일시',
+    `created_at`                TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP COMMENT '생성 일시',
+    `updated_at`                TIMESTAMP       NOT NULL    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정 일시',
+    PRIMARY KEY (order_id),
+    CONSTRAINT `fk_order_delivery_order` FOREIGN KEY (`order_id`) REFERENCES `koin`.`order` (`id`)
+);
