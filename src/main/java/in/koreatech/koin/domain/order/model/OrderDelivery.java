d2dda56ec7cdedbb11d80ca17965830c2f88e11a
신관규 <soundbar91@gmail.com>
2025-09-07 03:36:14 +0900

    fix: 주문 & 결제 테이블 수정 (#1931)
    * feat: order drop flyway 추가
    
    * feat: order 테이블 추가
    
    * feat: order_delivery 테이블 추가
    
    * feat: order_takeout 테이블 추가
    
    * feat: order_menu 테이블 추가
    
    * feat: order_menu_option 테이블 추가
    
    * feat: payment 테이블 추가
    
    * feat: payment_cancel 테이블 추가
    
    * fix: OrderTakeOut id 자료형 변경
    
    * feat: address_detail 추가
    
    * fix: 테이블 변경에 따른 주문 메뉴 저장 로직 수정
    
    * fix: orderMenu 연간관계 수정
    
    * fix: toOrder 메소드 수정
    
    * fix: flyway 수정
    
    * feat: campus_delivery_address 테이블 address 주소 추가
    
    * fix: addressValidator 인터페이스로 변경
    
    * fix: order 생성 로직 수정
    
    * fix: payment 응답값 수정

--- src/main/java/in/koreatech/koin/domain/order/model/OrderDelivery.java
+++ src/main/java/in/koreatech/koin/domain/order/model/OrderDelivery.java
@@ -2,6 +2,9 @@ package in.koreatech.koin.domain.order.model;
 
 import static lombok.AccessLevel.PROTECTED;
 
+import java.time.LocalDateTime;
+
+import in.koreatech.koin.common.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -18,13 +21,13 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
-@Table(name = "order_delivery")
+@Table(name = "order_delivery_v2")
 @NoArgsConstructor(access = PROTECTED)
-public class OrderDelivery {
+public class OrderDelivery extends BaseEntity {
 
     @Id
     @Column(name = "order_id", nullable = false, updatable = false)
-    private String id;
+    private Integer id;
 
     @MapsId
     @OneToOne
@@ -32,9 +35,11 @@ public class OrderDelivery {
     private Order order;
 
     @NotBlank
-    @Size(max = 100)
-    @Column(name = "address", length = 100, nullable = false, updatable = false)
+    @Column(name = "address", length = 100, nullable = false, updatable = false, columnDefinition = "TEXT")
     private String address;
+
+    @Column(name = "address_detail", columnDefinition = "TEXT")
+    private String addressDetail;
 
     @Size(max = 50)
     @Column(name = "to_owner", length = 50, updatable = false)
@@ -52,20 +57,37 @@ public class OrderDelivery {
     @Column(name = "provide_cutlery", nullable = false, updatable = false, columnDefinition = "TINYINT(1)")
     private Boolean provideCutlery;
 
+    @Column(name = "dispatched_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime dispatchedAt;
+
+    @Column(name = "completed_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime completedAt;
+
+    @Column(name = "estimated_arrival_at", columnDefinition = "TIMESTAMP")
+    private LocalDateTime estimatedArrivalAt;
+
     @Builder
-    public OrderDelivery(
+    private OrderDelivery(
         Order order,
         String address,
+        String addressDetail,
         String toOwner,
         String toRider,
+        Integer deliveryTip,
         Boolean provideCutlery,
-        Integer deliveryTip
+        LocalDateTime dispatchedAt,
+        LocalDateTime completedAt,
+        LocalDateTime estimatedArrivalAt
     ) {
         this.order = order;
         this.address = address;
+        this.addressDetail = addressDetail;
         this.toOwner = toOwner;
         this.toRider = toRider;
-        this.provideCutlery = provideCutlery;
         this.deliveryTip = deliveryTip;
+        this.provideCutlery = provideCutlery;
+        this.dispatchedAt = dispatchedAt;
+        this.completedAt = completedAt;
+        this.estimatedArrivalAt = estimatedArrivalAt;
     }
 }
--- src/main/java/in/koreatech/koin/domain/payment/gateway/toss/TossPaymentGatewayService.java
+++ src/main/java/in/koreatech/koin/domain/payment/gateway/toss/TossPaymentGatewayService.java
@@ -1,8 +1,12 @@
 package in.koreatech.koin.domain.payment.gateway.toss;
 
 import static in.koreatech.koin.domain.payment.gateway.pg.dto.PaymentGatewayCancelResponse.CancelInfo;
+import static in.koreatech.koin.global.code.ApiResponseCode.PAYMENT_CONFIRM_ERROR;
 
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import in.koreatech.koin.domain.payment.gateway.pg.PaymentGatewayService;
 import in.koreatech.koin.domain.payment.gateway.pg.PgOrderIdGenerator;
@@ -10,6 +14,7 @@ import in.koreatech.koin.domain.payment.gateway.pg.dto.PaymentGatewayCancelRespo
 import in.koreatech.koin.domain.payment.gateway.pg.dto.PaymentGatewayConfirmResponse;
 import in.koreatech.koin.domain.payment.gateway.toss.dto.response.TossPaymentCancelResponse;
 import in.koreatech.koin.domain.payment.gateway.toss.dto.response.TossPaymentConfirmResponse;
+import in.koreatech.koin.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -18,20 +23,29 @@ public class TossPaymentGatewayService implements PaymentGatewayService {
 
     private final TossPaymentClient tossPaymentClient;
     private final PgOrderIdGenerator pgOrderIdGenerator;
+    private final ObjectMapper objectMapper;
 
     public PaymentGatewayConfirmResponse confirmPayment(String paymentKey, String pgOrderId, Integer amount) {
-        TossPaymentConfirmResponse tossPaymentConfirmResponse = tossPaymentClient.requestConfirm(paymentKey, pgOrderId,
-            amount);
+        try {
+            TossPaymentConfirmResponse tossPaymentConfirmResponse = tossPaymentClient.requestConfirm(paymentKey, pgOrderId,
+                amount);
 
-        return new PaymentGatewayConfirmResponse(
-            tossPaymentConfirmResponse.paymentKey(),
-            tossPaymentConfirmResponse.totalAmount(),
-            tossPaymentConfirmResponse.orderId(),
-            tossPaymentConfirmResponse.status(),
-            tossPaymentConfirmResponse.method(),
-            tossPaymentConfirmResponse.requestedAt(),
-            tossPaymentConfirmResponse.approvedAt()
-        );
+            return new PaymentGatewayConfirmResponse(
+                tossPaymentConfirmResponse.paymentKey(),
+                tossPaymentConfirmResponse.totalAmount(),
+                tossPaymentConfirmResponse.orderName(),
+                tossPaymentConfirmResponse.orderId(),
+                tossPaymentConfirmResponse.status(),
+                tossPaymentConfirmResponse.method(),
+                tossPaymentConfirmResponse.easyPay().provider(),
+                tossPaymentConfirmResponse.requestedAt(),
+                tossPaymentConfirmResponse.approvedAt(),
+                objectMapper.writeValueAsString(tossPaymentConfirmResponse)
+            );
+        } catch (JsonProcessingException e) {
+            // TODO. 결제 롤백 로직 구현
+            throw CustomException.of(PAYMENT_CONFIRM_ERROR);
+        }
     }
 
     public PaymentGatewayCancelResponse cancelPayment(String paymentKey, String cancelReason, String idempotencyKey) {
