357d2fec339fbca2eeb7b85c82687a299dca9e96
dldb-chamchi <dladkfl49@gmail.com>
2025-09-07 21:51:06 +0900

    feat: orderStatus를 통해 특정 order 조회

--- src/main/java/in/koreatech/koin/domain/order/repository/OrderRepository.java
+++ src/main/java/in/koreatech/koin/domain/order/repository/OrderRepository.java
@@ -1,10 +1,48 @@
 package in.koreatech.koin.domain.order.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import in.koreatech.koin.domain.order.model.Order;
 
 public interface OrderRepository extends Repository<Order, String> {
 
     void save(Order order);
+
+    @Query("""
+        SELECT DISTINCT o
+        FROM Order o
+        JOIN FETCH o.orderableShop os
+        JOIN FETCH os.shop s
+        LEFT JOIN FETCH o.orderDelivery od
+        LEFT JOIN FETCH o.orderTakeout ot
+        WHERE o.user.id = :userId
+          AND o.status NOT IN (
+            in.koreatech.koin.domain.order.model.OrderStatus.DELIVERED,
+            in.koreatech.koin.domain.order.model.OrderStatus.CANCELED
+          )
+        ORDER BY o.createdAt DESC
+    """)
+    List<Order> findOrderWithStatus(@Param("userId") Integer userId);
+
+    @Query("""
+    SELECT DISTINCT o
+    FROM Order o
+    JOIN FETCH o.orderableShop os
+    JOIN FETCH os.shop s
+    LEFT JOIN FETCH o.orderDelivery od
+    LEFT JOIN FETCH o.orderTakeout ot
+    WHERE o.user.id = :userId
+      AND o.status NOT IN (
+        in.koreatech.koin.domain.order.model.OrderStatus.DELIVERED,
+        in.koreatech.koin.domain.order.model.OrderStatus.CANCELED
+      )
+    ORDER BY o.createdAt DESC
+""")
+    List<Order> findOrderWithStatus(@Param("userId") Integer userId,
+        Pageable pageable);
 }
