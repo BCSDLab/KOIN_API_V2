0a86055bf01ceaed605207f0c9f44649657bfab4
신관규 <soundbar91@gmail.com>
2025-09-07 21:24:04 +0900

    feat: 주문 변경 테스트 API 명세서 작성

---
+++ src/main/java/in/koreatech/koin/domain/order/order/controller/OrderTestController.java
@@ -0,0 +1,26 @@
+package in.koreatech.koin.domain.order.order.controller;
+
+import static in.koreatech.koin.domain.user.model.UserType.STUDENT;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import in.koreatech.koin.domain.order.model.OrderStatus;
+import in.koreatech.koin.global.auth.Auth;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Profile("dev")
+@Tag(name = "(Normal) Order: 주문 테스트", description = "주문 테스트 API")
+public interface OrderTestController {
+
+    @Operation(summary = "주문 상태 변경 테스트")
+    @PutMapping("/order/{id}/status")
+    ResponseEntity<Void> updateOrderStatus(
+        @PathVariable(name = "id") Integer id,
+        @Auth(permit = {STUDENT}) Integer userId,
+        OrderStatus orderStatus
+    );
+}
