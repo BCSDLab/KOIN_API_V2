2e76f13c1f84bf94908141f238c2ae29e4d2d442
신관규 <soundbar91@gmail.com>
2025-09-07 21:55:25 +0900

    docs: 주문 상태 변경 테스트 API 설명 작성

--- src/main/java/in/koreatech/koin/domain/order/order/controller/OrderTestApi.java
+++ src/main/java/in/koreatech/koin/domain/order/order/controller/OrderTestApi.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 import in.koreatech.koin.domain.order.model.OrderStatus;
 import in.koreatech.koin.global.auth.Auth;
+import in.koreatech.koin.global.code.ApiResponseCode;
+import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -16,7 +18,22 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "(Normal) Order: 주문 테스트", description = "주문 테스트 API")
 public interface OrderTestApi {
 
-    @Operation(summary = "주문 상태 변경 테스트")
+    @ApiResponseCodes(
+        {
+            ApiResponseCode.NOT_FOUND_ORDER,
+            ApiResponseCode.NOT_FOUND_USER,
+            ApiResponseCode.FORBIDDEN_ORDER,
+        }
+    )
+    @Operation(
+        summary = "주문 상태 변경 테스트",
+        description = """
+            - orderStatus가 DELIVERED이면서 변경할 주문이 배달인 경우에는 배달 완료로 업데이트가 됩니다.
+            - orderStatus가 PACKAGED이면서 변경할 주문이 배달인 경우에는 포장 완료로 업데이트가 됩니다.
+            - 나머지 orderStatus에 대해서는 아무런 처리가 되지 않습니다.
+            - 결제 취소의 경우 결제 취소 API를 사용해주세요.
+            """
+    )
     @PutMapping("/order/{orderId}/status")
     ResponseEntity<Void> updateOrderStatus(
         @PathVariable(name = "orderId") Integer orderId,
