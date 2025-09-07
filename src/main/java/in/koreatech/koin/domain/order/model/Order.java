b3068515ec3b0d0303020a81476ccd079bebe4db
신관규 <soundbar91@gmail.com>
2025-09-07 21:51:24 +0900

    feat: 주문 상태 변경 테스트 API 구현

--- src/main/java/in/koreatech/koin/domain/order/model/Order.java
+++ src/main/java/in/koreatech/koin/domain/order/model/Order.java
@@ -1,6 +1,6 @@
 package in.koreatech.koin.domain.order.model;
 
-import static in.koreatech.koin.domain.order.model.OrderStatus.CANCELED;
+import static in.koreatech.koin.domain.order.model.OrderStatus.*;
 import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.LAZY;
@@ -177,5 +177,13 @@ public class Order extends BaseEntity {
         this.status = CANCELED;
         this.canceledReason = cancelReason;
         this.canceledAt = LocalDateTime.now();
+    }
+
+    public void deliveryComplete() {
+        this.status = DELIVERED;
+    }
+
+    public void takeoutComplete() {
+        this.status = PACKAGED;
     }
 }
