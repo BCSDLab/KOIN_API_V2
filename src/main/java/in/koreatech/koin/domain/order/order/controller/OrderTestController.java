9cf99107e4d0786c2d8e5c82111100caf314871c
신관규 <soundbar91@gmail.com>
2025-09-07 21:57:06 +0900

    feat: Profile 추가

--- src/main/java/in/koreatech/koin/domain/order/order/controller/OrderTestController.java
+++ src/main/java/in/koreatech/koin/domain/order/order/controller/OrderTestController.java
@@ -2,6 +2,7 @@ package in.koreatech.koin.domain.order.order.controller;
 
 import static in.koreatech.koin.domain.user.model.UserType.STUDENT;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,6 +13,7 @@ import in.koreatech.koin.domain.order.order.service.OrderTestService;
 import in.koreatech.koin.global.auth.Auth;
 import lombok.RequiredArgsConstructor;
 
+@Profile("dev")
 @RestController
 @RequiredArgsConstructor
 public class OrderTestController implements OrderTestApi {
