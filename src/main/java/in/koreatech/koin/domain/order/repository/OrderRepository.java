72f4b6771f515a21d1587c4715afa7ad0e7689d1
신관규 <soundbar91@gmail.com>
2025-09-08 01:08:02 +0900

    feat: 주문 내역 리스트 조회 API 추가 (#1942)
    * feat: 주문 내역 조회 API 명세서 작성
    
    * fix: 주문 내역 조회 API 명세서 수정
    
    * feat: GET /order 구현
    
    * fix: OrderStatusCriteria getPredicate 수정
    
    * fix: Projections 오류 수정
    
    * docs: GET /order 설명 작성
    
    * fix: OrderStatusCriteria 기본값 수정
    
    * feat: orderSearchCriteria 추가
    
    * feat: 주문 상태 변경 테스트 API 추가 (#1944)
    
    * feat: 주문 변경 테스트 API 명세서 작성
    
    * feat: 주문 상태 변경 테스트 API 구현
    
    * docs: 주문 상태 변경 테스트 API 설명 작성
    
    * chore: 조건문 수정
    
    * feat: Profile 추가

--- src/main/java/in/koreatech/koin/domain/order/repository/OrderRepository.java
+++ src/main/java/in/koreatech/koin/domain/order/repository/OrderRepository.java
@@ -1,10 +1,22 @@
 package in.koreatech.koin.domain.order.repository;
 
+import static in.koreatech.koin.global.code.ApiResponseCode.NOT_FOUND_ORDER;
+
+import java.util.Optional;
+
 import org.springframework.data.repository.Repository;
 
 import in.koreatech.koin.domain.order.model.Order;
+import in.koreatech.koin.global.exception.CustomException;
 
-public interface OrderRepository extends Repository<Order, String> {
+public interface OrderRepository extends Repository<Order, Integer> {
 
     void save(Order order);
+
+    Optional<Order> findById(Integer orderId);
+
+    default Order getById(Integer orderId) {
+        return findById(orderId)
+            .orElseThrow(() -> CustomException.of(NOT_FOUND_ORDER));
+    }
 }
