d78c106eb4822a5d891457479faae72eda75970b
신관규 <soundbar91@gmail.com>
2025-09-08 01:02:40 +0900

    feat: 주문 상태 변경 테스트 API 추가 (#1944)
    * feat: 주문 변경 테스트 API 명세서 작성
    
    * feat: 주문 상태 변경 테스트 API 구현
    
    * docs: 주문 상태 변경 테스트 API 설명 작성
    
    * chore: 조건문 수정
    
    * feat: Profile 추가

---
+++ src/main/java/in/koreatech/koin/domain/order/order/service/OrderTestService.java
@@ -0,0 +1,43 @@
+package in.koreatech.koin.domain.order.order.service;
+
+import static in.koreatech.koin.domain.order.model.OrderStatus.DELIVERED;
+import static in.koreatech.koin.domain.order.model.OrderStatus.PACKAGED;
+import static in.koreatech.koin.global.code.ApiResponseCode.FORBIDDEN_ORDER;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import in.koreatech.koin.domain.order.model.Order;
+import in.koreatech.koin.domain.order.model.OrderDelivery;
+import in.koreatech.koin.domain.order.model.OrderStatus;
+import in.koreatech.koin.domain.order.model.OrderTakeout;
+import in.koreatech.koin.domain.order.repository.OrderRepository;
+import in.koreatech.koin.domain.user.model.User;
+import in.koreatech.koin.domain.user.repository.UserRepository;
+import in.koreatech.koin.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+
+@Profile("dev")
+@Service
+@RequiredArgsConstructor
+public class OrderTestService {
+
+    private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
+
+    public void updateOrderStatus(Integer orderId, Integer userId, OrderStatus orderStatus) {
+        User user = userRepository.getById(userId);
+        Order order = orderRepository.getById(orderId);
+        if (!order.getUser().getId().equals(user.getId())) {
+            throw CustomException.of(FORBIDDEN_ORDER);
+        }
+
+        if (orderStatus.equals(DELIVERED) && order.getStatus().equals(DELIVERED)) {
+            OrderDelivery orderDelivery = order.getOrderDelivery();
+            orderDelivery.deliveryComplete();
+        } else if (orderStatus.equals(PACKAGED) && order.getStatus().equals(DELIVERED)) {
+            OrderTakeout orderTakeout = order.getOrderTakeout();
+            orderTakeout.takeoutComplete();
+        }
+    }
+}
