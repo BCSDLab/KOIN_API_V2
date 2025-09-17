37bc122118ce0df37dcb6c49e7dd0db7962728ab
박종범 <84450816+asa9874@users.noreply.github.com>
2025-09-17 18:41:34 +0900

    fix: CreateReviewRequest Content @NotNull 검증제거 및 "" 문자열로 변환추가

--- src/main/java/in/koreatech/koin/domain/shop/dto/review/request/CreateReviewRequest.java
+++ src/main/java/in/koreatech/koin/domain/shop/dto/review/request/CreateReviewRequest.java
@@ -27,7 +27,6 @@ public record CreateReviewRequest(
     Integer rating,
 
     @Schema(example = "정말 맛있어요~!", description = "리뷰 내용", requiredMode = REQUIRED)
-    @NotNull
     String content,
 
     @Schema(example = """
