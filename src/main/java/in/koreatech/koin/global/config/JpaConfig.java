dfd7d30ba268bd012424d6e408958897a44dfbd8
신관규 <soundbar91@gmail.com>
2025-10-03 18:22:49 +0900

    feat: JpaRepository 커스텀 어노테이션 추가

--- src/main/java/in/koreatech/koin/global/config/JpaConfig.java
+++ src/main/java/in/koreatech/koin/global/config/JpaConfig.java
@@ -1,11 +1,22 @@
 package in.koreatech.koin.global.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import in.koreatech.koin.global.config.repository.JpaRepository;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(
+    includeFilters = @ComponentScan.Filter(
+        type = FilterType.ANNOTATION,
+        value = JpaRepository.class
+    )
+)
 @Profile("!test")
 public class JpaConfig {
 
