b83e98c9838e12dd7e184ea84eb31d76a21dace5
신관규 <soundbar91@gmail.com>
2025-10-06 17:19:28 +0900

    fix: JpaConfig 설정 파일 통합

--- src/main/java/in/koreatech/koin/config/repository/JpaConfig.java
+++ src/main/java/in/koreatech/koin/config/repository/JpaConfig.java
@@ -1,11 +1,24 @@
 package in.koreatech.koin.config.repository;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaAuditing
+@EnableJpaRepositories(
+    basePackages = {
+        "in.koreatech.koin.admin",
+        "in.koreatech.koin.domain"
+    },
+    includeFilters = @ComponentScan.Filter(
+        type = FilterType.ANNOTATION,
+        value = JpaRepository.class
+    )
+)
 @Profile("!test")
 public class JpaConfig {
 
