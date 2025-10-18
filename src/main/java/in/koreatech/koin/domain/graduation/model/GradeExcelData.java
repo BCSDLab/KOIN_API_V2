75860f1a823c476cb1b1aaa7df301832b16953f7
신관규 <soundbar91@gmail.com>
2025-10-18 17:22:40 +0900

    refactor: getKoinSemester 메소드 리펙토링

--- src/main/java/in/koreatech/koin/domain/graduation/model/GradeExcelData.java
+++ src/main/java/in/koreatech/koin/domain/graduation/model/GradeExcelData.java
@@ -1,5 +1,9 @@
 package in.koreatech.koin.domain.graduation.model;
 
+import static in.koreatech.koin.global.code.ApiResponseCode.INVALID_SEMESTER_REGEX;
+
+import in.koreatech.koin.global.exception.CustomException;
+
 public record GradeExcelData(
     String year,
     String semester,
@@ -17,6 +21,10 @@ public record GradeExcelData(
     private static final String FAIL = "F";
     private static final String UNSATISFACTORY = "U";
     private static final String TOTAL = "합 계";
+    private static final String FIRST_SEMESTER = "1";
+    private static final String SECOND_SEMESTER = "2";
+    private static final String SUMMER_SEMESTER = "동계";
+    private static final String WINER_SEMESTER = "여름";
 
     public boolean isSkipRow() {
         return classTitle.equals(MIDDLE_TOTAL) ||
@@ -29,11 +37,11 @@ public record GradeExcelData(
     }
 
     public String getKoinSemester() {
-        if (semester.equals("1") || semester.equals("2")) {
-            return year + semester;
-        } else if (semester.equals("동계")) {
-            return year + "-" + "겨울";
-        } else
-            return year + "-" + "여름";
+        return switch (semester) {
+            case FIRST_SEMESTER, SECOND_SEMESTER -> year + semester;
+            case WINER_SEMESTER -> year + "-" + "겨울";
+            case SUMMER_SEMESTER -> year + "-" + "여름";
+            default -> throw CustomException.of(INVALID_SEMESTER_REGEX);
+        };
     }
 }
