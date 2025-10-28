b67f462a1c78f21f1056bcbfbe08c6109303c985
박종범 <84450816+asa9874@users.noreply.github.com>
2025-10-28 20:09:12 +0900

    refact: @NotNULL 추가

--- src/main/java/in/koreatech/koin/domain/shoptoOrderable/model/ShopToOrderable.java
+++ src/main/java/in/koreatech/koin/domain/shoptoOrderable/model/ShopToOrderable.java
@@ -15,6 +15,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
@@ -28,53 +29,62 @@ import lombok.NoArgsConstructor;
 @Table(name = "shop_to_orderable")
 @NoArgsConstructor(access = PROTECTED)
 public class ShopToOrderable extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "shop_id", nullable = false)
     private Shop shop;
 
-    @Column(name="minimum_order_amount", nullable = false)
+    @NotNull
+    @Column(name = "minimum_order_amount", nullable = false)
     private Integer minimumOrderAmount;
 
-    @Column(name="takeout" ,nullable = false)
-    private Boolean takeout;
+    @Column(name = "takeout", nullable = false)
+    private Boolean takeout = false;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
-    @Column(name="delivery_option", nullable = false)
+    @Column(name = "delivery_option", nullable = false)
     private ShopToOrderableDeliveryOption deliveryOption;
 
-    @Column(name="campus_delivery_tip", nullable = false)
-    private Integer campusDeliveryTip;
+    @Column(name = "campus_delivery_tip", nullable = false)
+    private Integer campusDeliveryTip = 0;
 
-    @Column(name="outside_delivery_tip", nullable = false)
-    private Integer outsideDeliveryTip;
+    @Column(name = "outside_delivery_tip", nullable = false)
+    private Integer outsideDeliveryTip = 0;
 
+    @NotNull
     @Column(name = "business_license_url", nullable = false)
     private String businessLicenseUrl;
 
+    @NotNull
     @Column(name = "business_certificate_url", nullable = false)
     private String businessCertificateUrl;
 
+    @NotNull
     @Column(name = "bank_copy_url", nullable = false)
     private String bankCopyUrl;
 
+    @NotNull
     @Size(max = 10)
     @Column(name = "bank", length = 10, nullable = false)
     private String bank;
 
+    @NotNull
     @Size(max = 20)
     @Column(name = "account_number", length = 20, nullable = false)
     private String accountNumber;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "request_status", nullable = false)
-    private ShopToOrderableRequestStatus requestStatus;
+    private ShopToOrderableRequestStatus requestStatus = ShopToOrderableRequestStatus.PENDING;
 
     @Column(name = "approved_at", columnDefinition = "TIMESTAMP")
-    private LocalDateTime approvedAt;
+    private LocalDateTime approvedAt = null;
 
     @Builder
     public ShopToOrderable(
@@ -96,7 +106,6 @@ public class ShopToOrderable extends BaseEntity {
         this.deliveryOption = deliveryOption;
         this.campusDeliveryTip = campusDeliveryTip;
         this.outsideDeliveryTip = outsideDeliveryTip;
-        this.requestStatus = ShopToOrderableRequestStatus.PENDING;
         this.businessLicenseUrl = businessLicenseUrl;
         this.businessCertificateUrl = businessCertificateUrl;
         this.bankCopyUrl = bankCopyUrl;
