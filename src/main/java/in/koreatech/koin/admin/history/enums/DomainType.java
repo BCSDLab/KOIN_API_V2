6b39313451427845faca206cc567c5dbf687b545
신관규 <soundbar91@gmail.com>
2025-11-02 16:32:54 +0900

    refactor: DomainType 리펙토링

--- src/main/java/in/koreatech/koin/admin/history/enums/DomainType.java
+++ src/main/java/in/koreatech/koin/admin/history/enums/DomainType.java
@@ -6,37 +6,37 @@ import lombok.Getter;
 public enum DomainType {
     MEMBERS("Member", "BCSDLAB 회원"),
     TRACKS("Track", "트랙"),
-    TECHSTACKS("TechStack", "기술 스택"),
-    NOTICE("Notice", "코인 공지"),
-    VERSION("Version", "버전관리"),
+    TECH_STACKS("TechStack", "기술 스택"),
+    NOTICES("Notice", "코인 공지"),
+    VERSIONS("Version", "버전관리"),
 
     CATEGORIES("Categories", "카테고리"),
 
-    BENEFIT("Benefit", "혜택"),
-    BENEFITCATEGORIES("Benefit Categories", "혜택 카테고리"),
+    BENEFITS("Benefit", "혜택"),
+    BENEFIT_CATEGORIES("Benefit Categories", "혜택 카테고리"),
 
     SHOPS("Shop", "상점"),
-    SHOPSCATEGORIES("Shop Categories", "상점 카테고리"),
+    SHOPS_CATEGORIES("Shop Categories", "상점 카테고리"),
 
     MENUS("Menu", "메뉴"),
-    MENUSCATEGORIES("Menu Categroies", "메뉴 카테고리"),
+    MENUS_CATEGORIES("Menu Categories", "메뉴 카테고리"),
 
     REVIEWS("Review", "리뷰"),
 
-    ABTEST("Abtest", "AB 테스트"),
+    ABTESTS("Abtest", "AB 테스트"),
 
     LANDS("Land", "복덕방"),
-    COOPSHOP("CoopShop", "생협 매장"),
+    COOP_SHOPS("CoopShop", "생협 매장"),
 
     USERS("User", "회원"),
-    STUDENT("Student", "학생"),
-    OWNER("Owner", "사장님"),
-    ADMIN("Admin", "어드민"),
+    STUDENTS("Student", "학생"),
+    OWNERS("Owner", "사장님"),
+    ADMINS("Admin", "어드민"),
 
     BANNERS("Banner", "배너"),
     BANNER_CATEGORIES("Banner Categories", "배너 카테고리"),
 
-    CLUB("Clubs", "동아리")
+    CLUBS("Clubs", "동아리")
     ;
 
     private final String value;
