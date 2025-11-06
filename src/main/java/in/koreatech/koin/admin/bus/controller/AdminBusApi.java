508523f9598b16c3635fa67ce26f2d63de735d19
신관규 <soundbar91@gmail.com>
2025-11-06 21:30:22 +0900

    feat: AdminBusApi 인터페이스 추가

---
+++ src/main/java/in/koreatech/koin/admin/bus/controller/AdminBusApi.java
@@ -0,0 +1,7 @@
+package in.koreatech.koin.admin.bus.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "(Admin) Bus: 버스", description = "어드민 버스 정보를 관리한다.")
+public interface AdminBusApi {
+}
