4071b2b51227996308f780ce5b6cc97c699775f8
신관규 <soundbar91@gmail.com>
2025-11-07 21:57:18 +0900

    fix: 어드민 권한 받도록 수정

--- src/main/java/in/koreatech/koin/admin/bus/commuting/controller/AdminCommutingBusApi.java
+++ src/main/java/in/koreatech/koin/admin/bus/commuting/controller/AdminCommutingBusApi.java
@@ -1,5 +1,7 @@
 package in.koreatech.koin.admin.bus.commuting.controller;
 
+import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+
 import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import in.koreatech.koin.admin.bus.commuting.dto.AdminCommutingBusResponse;
+import in.koreatech.koin.global.auth.Auth;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "(Admin) Commuting Bus: 등하교 버스", description = "어드민 등하교 버스 정보를 관리한다.")
@@ -15,7 +18,7 @@ public interface AdminCommutingBusApi {
 
     @PostMapping("/admin/bus/commuting/excel")
     ResponseEntity<AdminCommutingBusResponse> parseCommutingBusExcel(
-        @RequestParam(name = "commuting_bus_excel_file") MultipartFile commutingBusExcelFile
-        // @Auth(permit = {ADMIN}) Integer adminId
+        @RequestParam(name = "commuting_bus_excel_file") MultipartFile commutingBusExcelFile,
+        @Auth(permit = {ADMIN}) Integer adminId
     ) throws IOException;
 }
--- src/main/java/in/koreatech/koin/admin/bus/commuting/controller/AdminCommutingBusController.java
+++ src/main/java/in/koreatech/koin/admin/bus/commuting/controller/AdminCommutingBusController.java
@@ -1,5 +1,7 @@
 package in.koreatech.koin.admin.bus.commuting.controller;
 
+import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+
 import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
@@ -10,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import in.koreatech.koin.admin.bus.commuting.dto.AdminCommutingBusResponse;
 import in.koreatech.koin.admin.bus.commuting.service.AdminCommutingBusExcelService;
+import in.koreatech.koin.global.auth.Auth;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -20,8 +23,8 @@ public class AdminCommutingBusController implements AdminCommutingBusApi {
 
     @PostMapping("/admin/bus/commuting/excel")
     public ResponseEntity<AdminCommutingBusResponse> parseCommutingBusExcel(
-        @RequestParam(name = "commuting_bus_excel_file") MultipartFile commutingBusExcelFile
-        // @Auth(permit = {ADMIN}) Integer adminId
+        @RequestParam(name = "commuting_bus_excel_file") MultipartFile commutingBusExcelFile,
+        @Auth(permit = {ADMIN}) Integer adminId
     ) throws IOException {
         AdminCommutingBusResponse response = adminCommutingBusExcelService.parseCommutingBusExcel(
             commutingBusExcelFile);
