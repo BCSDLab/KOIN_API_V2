82788cf389df96cfb6cfe76fe673c27ae16e418c
신관규 <soundbar91@gmail.com>
2025-11-07 22:54:50 +0900

    feat: 스웨거 에러코드 추가

--- src/main/java/in/koreatech/koin/admin/bus/commuting/controller/AdminCommutingBusApi.java
+++ src/main/java/in/koreatech/koin/admin/bus/commuting/controller/AdminCommutingBusApi.java
@@ -1,6 +1,7 @@
 package in.koreatech.koin.admin.bus.commuting.controller;
 
 import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+import static in.koreatech.koin.global.code.ApiResponseCode.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -12,11 +13,18 @@ import org.springframework.web.multipart.MultipartFile;
 
 import in.koreatech.koin.admin.bus.commuting.dto.AdminCommutingBusResponse;
 import in.koreatech.koin.global.auth.Auth;
+import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "(Admin) Commuting Bus: 등하교 버스", description = "어드민 등하교 버스 정보를 관리한다.")
 public interface AdminCommutingBusApi {
 
+    @ApiResponseCodes({
+        OK,
+        INVALID_SHUTTLE_ROUTE_TYPE,
+        INVALID_NODE_INFO_START_POINT,
+        INVALID_NODE_INFO_END_POINT,
+    })
     @PostMapping("/admin/bus/commuting/excel")
     ResponseEntity<List<AdminCommutingBusResponse>> parseCommutingBusExcel(
         @RequestParam(name = "commuting_bus_excel_file") MultipartFile commutingBusExcelFile,
