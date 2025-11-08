9b11cdc9bf90542f174e563fc21f0dcfe6a3322b
신관규 <soundbar91@gmail.com>
2025-11-08 22:33:48 +0900

    fix: DomainType value 필드 삭제

--- src/main/java/in/koreatech/koin/admin/history/enums/DomainType.java
+++ src/main/java/in/koreatech/koin/admin/history/enums/DomainType.java
@@ -4,50 +4,48 @@ import lombok.Getter;
 
 @Getter
 public enum DomainType {
-    MEMBERS("Member", "BCSDLAB 회원"),
-    TRACKS("Track", "트랙"),
-    TECH_STACKS("TechStack", "기술 스택"),
-    NOTICES("Notice", "코인 공지"),
-    VERSIONS("Version", "버전관리"),
+    MEMBERS("BCSDLAB 회원"),
+    TRACKS("트랙"),
+    TECH_STACKS("기술 스택"),
+    NOTICES("코인 공지"),
+    VERSIONS("버전관리"),
 
-    CATEGORIES("Categories", "카테고리"),
+    CATEGORIES("카테고리"),
 
-    BENEFITS("Benefit", "혜택"),
-    BENEFIT_CATEGORIES("Benefit Categories", "혜택 카테고리"),
+    BENEFITS("혜택"),
+    BENEFIT_CATEGORIES("혜택 카테고리"),
 
-    SHOPS("Shop", "상점"),
-    SHOPS_CATEGORIES("Shop Categories", "상점 카테고리"),
+    SHOPS("상점"),
+    SHOPS_CATEGORIES("상점 카테고리"),
 
-    MENUS("Menu", "메뉴"),
-    MENUS_CATEGORIES("Menu Categories", "메뉴 카테고리"),
+    MENUS("메뉴"),
+    MENUS_CATEGORIES("메뉴 카테고리"),
 
-    REVIEWS("Review", "리뷰"),
+    REVIEWS("리뷰"),
 
-    ABTESTS("Abtest", "AB 테스트"),
+    ABTESTS("AB 테스트"),
 
-    LANDS("Land", "복덕방"),
-    COOP_SHOPS("CoopShop", "생협 매장"),
+    LANDS("복덕방"),
+    COOP_SHOPS("생협 매장"),
 
-    USERS("User", "회원"),
-    STUDENTS("Student", "학생"),
-    OWNERS("Owner", "사장님"),
-    ADMINS("Admin", "어드민"),
+    USERS("회원"),
+    STUDENTS("학생"),
+    OWNERS("사장님"),
+    ADMINS("어드민"),
 
-    BANNERS("Banner", "배너"),
-    BANNER_CATEGORIES("Banner Categories", "배너 카테고리"),
+    BANNERS("배너"),
+    BANNER_CATEGORIES("배너 카테고리"),
 
-    CLUBS("Clubs", "동아리"),
+    CLUBS("동아리"),
 
-    KEYWORDS("Keyword", "키워드"),
+    KEYWORDS("키워드"),
 
-    LOST_ITEMS("LostItem", "분실물")
+    LOST_ITEMS("분실물")
     ;
 
-    private final String value;
     private final String description;
 
-    DomainType(String value, String description) {
-        this.value = value;
+    DomainType(String description) {
         this.description = description;
     }
 }
