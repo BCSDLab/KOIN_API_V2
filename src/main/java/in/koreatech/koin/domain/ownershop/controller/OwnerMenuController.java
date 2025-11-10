54a070f4cabe88166d376393a83e9cbf72ca90e2
박종범 <84450816+asa9874@users.noreply.github.com>
2025-11-10 14:22:02 +0900

    refact : @Controller -> @RestController 변경

--- src/main/java/in/koreatech/koin/domain/ownershop/controller/OwnerMenuController.java
+++ src/main/java/in/koreatech/koin/domain/ownershop/controller/OwnerMenuController.java
@@ -13,9 +13,9 @@ import in.koreatech.koin.domain.shop.dto.menu.response.ShopMenuResponse;
 import in.koreatech.koin.global.auth.Auth;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,8 +23,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class OwnerMenuController implements OwnerMenuApi {
 
