abdeb946f5e48c142d8744f9783b72ddf40dfb12
신관규 <soundbar91@gmail.com>
2025-11-11 22:38:27 +0900

    docs: 에러코드 명세 추가

--- src/main/java/in/koreatech/koin/admin/semester/controller/AdminSemesterApi.java
+++ src/main/java/in/koreatech/koin/admin/semester/controller/AdminSemesterApi.java
@@ -1,6 +1,7 @@
 package in.koreatech.koin.admin.semester.controller;
 
 import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+import static in.koreatech.koin.global.code.ApiResponseCode.*;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import in.koreatech.koin.admin.semester.dto.AdminSemesterCreateRequest;
 import in.koreatech.koin.global.auth.Auth;
+import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -15,6 +17,13 @@ import jakarta.validation.Valid;
 @Tag(name = "(ADMIN) Semester : 학기", description = "관리자 권한으로 학기 정보를 관리한다")
 public interface AdminSemesterApi {
 
+    @ApiResponseCodes({
+        OK,
+        INVALID_SEMESTER_FORMAT,
+        DUPLICATE_SEMESTER,
+        INVALID_START_DATE_AFTER_END_DATE,
+        OVERLAPPING_SEMESTER_DATE_RANGE
+    })
     @Operation(summary = "(ADMIN) 생협 학기 생성")
     @PostMapping("/admin/coopshop/semesters")
     ResponseEntity<Void> createCoopshopSemester(
