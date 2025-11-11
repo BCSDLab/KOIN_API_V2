b038b0914684ebd2605a0da47add25420ec9a06f
이동훈 <2dh2@naver.com>
2025-11-11 23:46:25 +0900

    fix: ApiResponseCodes로 에러 코드 명세 수정

--- src/main/java/in/koreatech/koin/admin/bus/shuttle/controller/AdminShuttleBusTimeTableApi.java
+++ src/main/java/in/koreatech/koin/admin/bus/shuttle/controller/AdminShuttleBusTimeTableApi.java
@@ -1,6 +1,7 @@
 package in.koreatech.koin.admin.bus.shuttle.controller;
 
 import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+import static in.koreatech.koin.global.code.ApiResponseCode.*;
 
 import java.util.List;
 
@@ -12,25 +13,20 @@ import org.springframework.web.multipart.MultipartFile;
 
 import in.koreatech.koin.admin.bus.shuttle.dto.response.AdminShuttleBusTimeTableResponse;
 import in.koreatech.koin.global.auth.Auth;
+import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "(Admin) ShuttleBusTimeTable: 셔틀 버스 시간표", description = "셔틀 버스의 시간표를 관리한다.")
 @RequestMapping("/admin/bus/shuttle")
 public interface AdminShuttleBusTimeTableApi {
 
-    @ApiResponses(
-        value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true)))
-        }
-    )
+    @ApiResponseCodes({
+        OK,
+        INVALID_EXCEL_FILE_TYPE,
+        INVALID_EXCEL_ROW,
+        INVALID_EXCEL_COL
+    })
     @Operation(summary = "엑셀 파일을 업로드하여 파싱된 데이터를 미리보기 한다.")
     @PostMapping("/preview")
     ResponseEntity<List<AdminShuttleBusTimeTableResponse>> previewShuttleBusTimeTable(
