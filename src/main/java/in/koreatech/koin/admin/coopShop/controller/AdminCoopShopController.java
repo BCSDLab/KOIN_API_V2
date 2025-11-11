b24347340a24b258e4fab357ff8bcde680dab7b8
강인화 <rkddlsghk1998@gmail.com>
2025-11-11 21:35:31 +0900

    chore: 파라미터 이름 수정
    케밥케이스로 수정

--- src/main/java/in/koreatech/koin/admin/coopShop/controller/AdminCoopShopController.java
+++ src/main/java/in/koreatech/koin/admin/coopShop/controller/AdminCoopShopController.java
@@ -50,7 +50,7 @@ public class AdminCoopShopController implements AdminCoopShopApi {
     @PostMapping(value = "/timetable/excel", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<AdminCoopShopsResponse> parseExcel(
         @Auth(permit = {ADMIN}) Integer adminId,
-        @RequestParam("coop_shop_excel") MultipartFile file
+        @RequestParam("coop-shop-excel") MultipartFile file
     ) {
         AdminCoopShopsResponse data = adminCoopShopExcelService.parse(file);
         return ResponseEntity.ok(data);
