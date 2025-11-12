360da3a847a5976a5e1e8d0b70966e7e074a2053
이동훈 <2dh2@naver.com>
2025-11-12 13:23:14 +0900

    feat: 요청 DTO 생성 및 명세 작성

---
+++ src/main/java/in/koreatech/koin/admin/bus/shuttle/dto/request/AdminShuttleBusUpdateRequest.java
@@ -0,0 +1,67 @@
+package in.koreatech.koin.admin.bus.shuttle.dto.request;
+
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+import java.util.List;
+
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+@JsonNaming(SnakeCaseStrategy.class)
+public record AdminShuttleBusUpdateRequest(
+    @Schema(description = "지역 이름", example = "천안・아산", requiredMode = REQUIRED)
+    @NotBlank(message = "지역 이름이 비어있습니다.")
+    String region,
+
+    @Schema(description = "노선 형태", example = "순환", requiredMode = REQUIRED)
+    @NotBlank(message = "노선 형태가 비어있습니다.")
+    String routeType,
+
+    @Schema(description = "노선 이름", example = "천안 셔틀", requiredMode = REQUIRED)
+    @NotBlank(message = "노선 이름이 비어있습니다.")
+    String routeName,
+
+    @Schema(description = "노선 세부 이름", example = "토요일, 일요일", requiredMode = NOT_REQUIRED)
+    String subName,
+
+    @Schema(description = "정류소 정보 리스트", requiredMode = REQUIRED)
+    @NotBlank(message = "정류소 정보 리스트가 비어있습니다.")
+    List<InnerNodeInfo> nodeInfo,
+
+    @Schema(description = "회차 정보 리스트", requiredMode = REQUIRED)
+    @NotBlank(message = "회차 정보 리스트가 비어있습니다.")
+    List<InnerRouteInfo> routeInfo
+) {
+
+    @JsonNaming(SnakeCaseStrategy.class)
+    public record InnerNodeInfo(
+        @Schema(description = "정류소 이름", example = "천안역", requiredMode = REQUIRED)
+        @NotBlank(message = "정류소 이름이 비어있습니다.")
+        String name,
+
+        @Schema(description = "정류소 세부 이름", example = "학화호두과자 앞", requiredMode = NOT_REQUIRED)
+        String detail
+    ) {
+
+    }
+
+    @JsonNaming(SnakeCaseStrategy.class)
+    public record InnerRouteInfo(
+        @Schema(description = "회차 이름", example = "1회", requiredMode = REQUIRED)
+        @NotBlank(message = "회차 이름이 비어있습니다.")
+        String name,
+
+        @Schema(description = "회차 세부 이름", example = "(청주역→본교)", requiredMode = NOT_REQUIRED)
+        String detail,
+
+        @Schema(description = "각 정류소 별 도착 시간", example = "[\"11:10\", null, \"11:35\"]", requiredMode = REQUIRED)
+        @NotBlank(message = "도착 시간 리스트가 비어있습니다.")
+        List<String> arrivalTime
+    ) {
+
+    }
+}
