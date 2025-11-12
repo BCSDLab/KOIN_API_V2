9b29f16a17a923568ab6a390e694761d17c5d2df
이동훈 <2dh2@naver.com>
2025-11-12 13:51:55 +0900

    feat: 엔티티 매핑

--- src/main/java/in/koreatech/koin/admin/bus/shuttle/model/ShuttleBusTimeTable.java
+++ src/main/java/in/koreatech/koin/admin/bus/shuttle/model/ShuttleBusTimeTable.java
@@ -2,35 +2,60 @@ package in.koreatech.koin.admin.bus.shuttle.model;
 
 import java.util.List;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @Getter
+@Document(collection = "shuttlebus-timetables")
 public class ShuttleBusTimeTable {
 
-    private List<NodeInfo> nodeInfos;
-    private List<RouteInfo> routeInfos;
+    @Id
+    private String id;
+
+    @Field(name = "semester_type")
+    private String semesterType;
+
+    @Field(name = "region")
     private Region region;
-    private RouteName routeName;
-    private RouteType routeType;
+
+    @Field(name = "routeType")
+    private String routeType;
+
+    @Field(name = "routeName")
+    private String routeName;
+
+    @Field(name = "subName")
+    private String subName;
+
+    @Field(name = "node_info")
+    private List<NodeInfo> nodeInfos;
+
+    @Field(name = "route_info")
+    private List<RouteInfo> routeInfos;
 
     public static ShuttleBusTimeTable from(
         List<NodeInfo> nodeInfos,
         List<RouteInfo> routeInfos,
         Region region,
         RouteName routeName,
+        SubName subName,
         RouteType routeType
     ) {
         return ShuttleBusTimeTable.builder()
             .nodeInfos(nodeInfos)
             .routeInfos(routeInfos)
             .region(region)
-            .routeName(routeName)
-            .routeType(routeType)
+            .routeName(routeName.getName())
+            .subName(subName.getName())
+            .routeType(routeType.getValue())
             .build();
     }
 }
