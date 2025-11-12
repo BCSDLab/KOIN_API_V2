d07c4f2a2333d9b44ac91c875d17a79654db27ad
신관규 <soundbar91@gmail.com>
2025-11-12 19:55:07 +0900

    refactor: CoopSemester 유효성 검증 메소드 이동

--- src/main/java/in/koreatech/koin/admin/semester/service/AdminCoopShopSemesterService.java
+++ src/main/java/in/koreatech/koin/admin/semester/service/AdminCoopShopSemesterService.java
@@ -1,15 +1,16 @@
 package in.koreatech.koin.admin.semester.service;
 
-import static in.koreatech.koin.global.code.ApiResponseCode.*;
+import static in.koreatech.koin.global.code.ApiResponseCode.DUPLICATE_SEMESTER;
+import static in.koreatech.koin.global.code.ApiResponseCode.OVERLAPPING_SEMESTER_DATE_RANGE;
 
 import java.time.LocalDate;
-import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import in.koreatech.koin.admin.semester.dto.AdminSemesterCreateRequest;
 import in.koreatech.koin.admin.semester.repository.AdminCoopShopSemesterRepository;
+import in.koreatech.koin.domain.coopshop.model.CoopSemester;
 import in.koreatech.koin.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 
@@ -18,35 +19,20 @@ import lombok.RequiredArgsConstructor;
 @Transactional(readOnly = true)
 public class AdminCoopShopSemesterService {
 
-    private static final Pattern SEMESTER_PATTERN = Pattern.compile("^\\d{2}-.+$");
-
     private final AdminCoopShopSemesterRepository adminCoopShopSemesterRepository;
 
     @Transactional
     public void createCoopshopSemester(AdminSemesterCreateRequest request) {
-        validateSemesterFormat(request.semester());
         validateDuplicateSemester(request.semester());
-        validateDateRange(request.fromDate(), request.toDate());
         validateOverlappingDateRange(request.fromDate(), request.toDate());
+        CoopSemester coopSemester = CoopSemester.of(request.semester(), request.fromDate(), request.toDate());
 
-        adminCoopShopSemesterRepository.save(request.toEntity());
-    }
-
-    private void validateSemesterFormat(String semester) {
-        if (!SEMESTER_PATTERN.matcher(semester).matches()) {
-            throw CustomException.of(INVALID_SEMESTER_FORMAT);
-        }
+        adminCoopShopSemesterRepository.save(coopSemester);
     }
 
     private void validateDuplicateSemester(String semester) {
         if (adminCoopShopSemesterRepository.findBySemester(semester).isPresent()) {
             throw CustomException.of(DUPLICATE_SEMESTER);
-        }
-    }
-
-    private void validateDateRange(LocalDate fromDate, LocalDate toDate) {
-        if (fromDate.isAfter(toDate)) {
-            throw CustomException.of(INVALID_START_DATE_AFTER_END_DATE);
         }
     }
 
