f38c934c86ea1ad64d1f0ee250d4f622729e258e
이동훈 <2dh2@naver.com>
2025-11-12 12:55:10 +0900

    feat: API 명세 작성

--- src/main/java/in/koreatech/koin/admin/bus/shuttle/controller/AdminShuttleBusTimeTableApi.java
+++ src/main/java/in/koreatech/koin/admin/bus/shuttle/controller/AdminShuttleBusTimeTableApi.java
@@ -7,15 +7,19 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import in.koreatech.koin.admin.bus.shuttle.dto.response.AdminShuttleBusTimeTableResponse;
+import in.koreatech.koin.admin.bus.shuttle.model.SemesterType;
 import in.koreatech.koin.global.auth.Auth;
 import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @Tag(name = "(Admin) ShuttleBusTimeTable: 셔틀 버스 시간표", description = "셔틀 버스의 시간표를 관리한다.")
 @RequestMapping("/admin/bus/shuttle/timetable")
@@ -32,5 +36,16 @@ public interface AdminShuttleBusTimeTableApi {
     ResponseEntity<List<AdminShuttleBusTimeTableResponse>> previewShuttleBusTimeTable(
         @Auth(permit = {ADMIN}) Integer adminId,
         @RequestParam(name = "shuttle-bus-time-table") MultipartFile file
+    );
+
+    @ApiResponseCodes({
+        OK
+    })
+    @Operation(summary = "셔틀 버스 시간표를 업데이트한다.")
+    @PutMapping
+    ResponseEntity<List<AdminShuttleBusTimeTableResponse>> updateShuttleBusTimeTable(
+        @RequestParam(name = "semester_type") SemesterType semesterType,
+        @Valid @RequestBody AdminShuttleBusUpdateRequest requst,
+        @Auth(permit = {ADMIN}) Integer adminId
     );
 }
