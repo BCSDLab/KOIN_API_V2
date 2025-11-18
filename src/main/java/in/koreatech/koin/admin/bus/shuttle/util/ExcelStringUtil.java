d95df9f09ad7a4f3a248c33200c1ab6a93679976
이동훈 <2dh2@naver.com>
2025-11-18 15:13:42 +0900

    refactor: 이름, 세부 이름 파싱 로직 분리

--- src/main/java/in/koreatech/koin/admin/bus/shuttle/util/ExcelStringUtil.java
+++ src/main/java/in/koreatech/koin/admin/bus/shuttle/util/ExcelStringUtil.java
@@ -12,25 +12,4 @@ public class ExcelStringUtil {
 
         return value != null ? value.trim() : "";
     }
-
-    public static String extractDetailFromBrackets(String str) {
-        int openIdx = str.indexOf('(');
-        int closeIdx = str.indexOf(')');
-
-        if (openIdx != -1 && closeIdx != -1 && closeIdx > openIdx) {
-            return str.substring(openIdx + 1, closeIdx).trim();
-        }
-
-        return null;
-    }
-
-    public static String extractNameWithoutBrackets(String str) {
-        int openIdx = str.indexOf('(');
-
-        if (openIdx != -1) {
-            return str.substring(0, openIdx).trim();
-        }
-
-        return str.trim();
-    }
 }
