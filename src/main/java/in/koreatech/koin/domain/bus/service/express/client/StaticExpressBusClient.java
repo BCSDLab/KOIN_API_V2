bc1091f912bf59da7a4eae924bfc2900e7f59cee
신관규 <soundbar91@gmail.com>
2025-11-30 22:14:40 +0900

    fix: 코리아텍 -> 천안 대성 시간표 수정

--- src/main/java/in/koreatech/koin/domain/bus/service/express/client/StaticExpressBusClient.java
+++ src/main/java/in/koreatech/koin/domain/bus/service/express/client/StaticExpressBusClient.java
@@ -58,12 +58,17 @@ public class StaticExpressBusClient extends ExpressBusClient {
     private List<ExpressBusCacheInfo> getKoreatechToTerminal() {
         return List.of(
             new ExpressBusCacheInfo(LocalTime.of(8, 35), LocalTime.of(8, 55), 1900),
+            new ExpressBusCacheInfo(LocalTime.of(9, 35), LocalTime.of(9, 55), 1900),
             new ExpressBusCacheInfo(LocalTime.of(10, 35), LocalTime.of(10, 55), 1900),
+            new ExpressBusCacheInfo(LocalTime.of(11, 5), LocalTime.of(11, 25), 1900),
             new ExpressBusCacheInfo(LocalTime.of(11, 35), LocalTime.of(11, 55), 1900),
             new ExpressBusCacheInfo(LocalTime.of(13, 35), LocalTime.of(13, 55), 1900),
             new ExpressBusCacheInfo(LocalTime.of(14, 35), LocalTime.of(14, 55), 1900),
+            new ExpressBusCacheInfo(LocalTime.of(15, 5), LocalTime.of(15, 25), 1900),
+            new ExpressBusCacheInfo(LocalTime.of(15, 35), LocalTime.of(15, 55), 1900),
             new ExpressBusCacheInfo(LocalTime.of(16, 35), LocalTime.of(16, 55), 1900),
             new ExpressBusCacheInfo(LocalTime.of(17, 35), LocalTime.of(17, 55), 1900),
+            new ExpressBusCacheInfo(LocalTime.of(18, 55), LocalTime.of(19, 15), 1900),
             new ExpressBusCacheInfo(LocalTime.of(19, 35), LocalTime.of(19, 55), 1900),
             new ExpressBusCacheInfo(LocalTime.of(21, 5), LocalTime.of(21, 25), 1900),
             new ExpressBusCacheInfo(LocalTime.of(22, 5), LocalTime.of(22, 25), 1900)
