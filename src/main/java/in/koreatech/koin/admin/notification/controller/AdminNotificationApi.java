f38d98480f33e5ea97435500845bdf7e80bc699f
신관규 <soundbar91@gmail.com>
2026-01-10 23:53:51 +0900

    docs: POST /admin/notification/send 에러 코드 명시

--- src/main/java/in/koreatech/koin/admin/notification/controller/AdminNotificationApi.java
+++ src/main/java/in/koreatech/koin/admin/notification/controller/AdminNotificationApi.java
@@ -1,6 +1,8 @@
 package in.koreatech.koin.admin.notification.controller;
 
 import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+import static in.koreatech.koin.global.code.ApiResponseCode.INVALID_DETAIL_SUBSCRIBE_TYPE;
+import static in.koreatech.koin.global.code.ApiResponseCode.OK;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import in.koreatech.koin.admin.notification.dto.AdminNotificationRequest;
 import in.koreatech.koin.global.auth.Auth;
+import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -17,6 +20,10 @@ import jakarta.validation.Valid;
 @RequestMapping("/admin/notification")
 public interface AdminNotificationApi {
 
+    @ApiResponseCodes(value = {
+        OK,
+        INVALID_DETAIL_SUBSCRIBE_TYPE
+    })
     @Operation(description = "푸시 알림 전송")
     @PostMapping("/send")
     ResponseEntity<Void> sendNotification(
