7d67814deb5f6918c8fc5b7111c179cedc5c8d6c
신관규 <soundbar91@gmail.com>
2026-02-28 00:36:52 +0900

    feat: 신설 전공/학부 추가 (#2159)
    * feat: 신설 전공/학부 추가
    
    * chore: 코드 포멧팅
    
    * feat: 신규 전공/학부 추가

--- src/main/java/in/koreatech/koin/domain/student/model/StudentDepartment.java
+++ src/main/java/in/koreatech/koin/domain/student/model/StudentDepartment.java
@@ -15,6 +15,11 @@ public enum StudentDepartment {
     INDUSTRIAL("산업경영학부"),
     EMPLOYMENT("고용서비스정책학과"),
     APPLIED_CHEMICAL("응용화학공학부"),
+    SEMICONDUCTOR_DISPLAY_ENGINEERING("반도체·디스플레이공학과"),
+    FUTURE_CONVERGENCE("미래융합학부"),
+    ENGINEERING_CONVERGENCE_AUTONOMOUS_MAJOR("공학융합자율전공"),
+    ICT_CONVERGENCE_AUTONOMOUS_MAJOR("ICT융합자율전공"),
+    SOCIAL_CONVERGENCE_AUTONOMOUS_MAJOR("사회융합자율전공"),
     ;
 
     private final String value;
