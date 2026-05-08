ec1927f778c5fe965e188c3081eb6222ba4be19d
신관규 <soundbar91@gmail.com>
2026-05-09 05:39:16 +0900

    fix: 미사용 메소드 삭제

--- src/main/java/in/koreatech/koin/domain/notification/model/NotificationSubscribe.java
+++ src/main/java/in/koreatech/koin/domain/notification/model/NotificationSubscribe.java
@@ -57,8 +57,4 @@ public class NotificationSubscribe extends BaseEntity {
         this.detailType = detailType;
         this.user = user;
     }
-
-    public boolean hasDeviceToken() {
-        return user.hasDeviceToken();
-    }
 }
--- src/main/java/in/koreatech/koin/domain/user/model/User.java
+++ src/main/java/in/koreatech/koin/domain/user/model/User.java
@@ -254,8 +254,4 @@ public class User extends BaseEntity {
         }
         return "익명 사용자";
     }
-
-    public boolean hasDeviceToken() {
-        return StringUtils.hasText(this.deviceToken);
-    }
 }
