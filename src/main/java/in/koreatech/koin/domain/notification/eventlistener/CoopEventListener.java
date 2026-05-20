9c4b99222097be26b939df5023e0eb1402362348
신관규 <soundbar91@gmail.com>
2026-05-20 20:01:12 +0900

    fix: CoopEventListener onDiningImageUploadRequest 메소드 간소화

--- src/main/java/in/koreatech/koin/domain/notification/eventlistener/CoopEventListener.java
+++ src/main/java/in/koreatech/koin/domain/notification/eventlistener/CoopEventListener.java
@@ -1,18 +1,12 @@
 package in.koreatech.koin.domain.notification.eventlistener;
 
-import static in.koreatech.koin.common.model.MobileAppPath.DINING;
-import static in.koreatech.koin.domain.notification.model.NotificationSubscribeType.DINING_IMAGE_UPLOAD;
-
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 import in.koreatech.koin.common.event.DiningImageUploadEvent;
 import in.koreatech.koin.common.event.DiningSoldOutEvent;
-import in.koreatech.koin.domain.notification.model.NotificationFactory;
-import in.koreatech.koin.domain.notification.repository.NotificationSubscribeRepository;
 import in.koreatech.koin.domain.notification.service.CoopNotificationService;
-import in.koreatech.koin.domain.notification.service.NotificationService;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -21,9 +15,6 @@ import lombok.RequiredArgsConstructor;
 public class CoopEventListener { // TODO : 리팩터링 필요 (비즈니스로직 제거 및 알림 책임만 갖도록)
 
     private final CoopNotificationService coopNotificationService;
-    private final NotificationService notificationService;
-    private final NotificationSubscribeRepository notificationSubscribeRepository;
-    private final NotificationFactory notificationFactory;
 
     @TransactionalEventListener
     public void onDiningSoldOutRequest(DiningSoldOutEvent event) {
@@ -32,16 +23,6 @@ public class CoopEventListener { // TODO : 리팩터링 필요 (비즈니스로�
 
     @TransactionalEventListener
     public void onDiningImageUploadRequest(DiningImageUploadEvent event) {
-        var notifications = notificationSubscribeRepository
-            .findAllBySubscribeTypeAndDetailTypeIsNull(DINING_IMAGE_UPLOAD).stream()
-            .filter(subscribe -> subscribe.getUser().getDeviceToken() != null)
-            .map(subscribe -> notificationFactory.generateDiningImageUploadNotification(
-                DINING,
-                event.id(),
-                event.imageUrl(),
-                subscribe.getUser()
-            )).toList();
-
-        notificationService.pushNotifications(notifications);
+        coopNotificationService.onDiningImageUploadRequest(event.id(), event.imageUrl());
     }
 }
