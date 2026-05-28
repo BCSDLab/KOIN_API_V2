8380d02a9afd57582f3c048dc6eaff0b806a9d4b
이동훈 <2dh2@naver.com>
2026-05-28 13:40:33 +0900

    fix: 날씨 API 응답 문서화 방식 변경
    - 레거시 Swagger 응답 정의 대신 공통 ApiResponseCodes 애너테이션을 사용하도록 변경
    - 성공 및 외부 API 오류 응답 코드가 공통 문서화 흐름을 따르도록 정리
    - 불필요한 Swagger 응답 관련 import를 제거해 컨트롤러 문서화 방식을 단순화

--- src/main/java/in/koreatech/koin/domain/weather/controller/WeatherApi.java
+++ src/main/java/in/koreatech/koin/domain/weather/controller/WeatherApi.java
@@ -1,34 +1,23 @@
 package in.koreatech.koin.domain.weather.controller;
 
+import static in.koreatech.koin.global.code.ApiResponseCode.EXTERNAL_API_ERROR;
+import static in.koreatech.koin.global.code.ApiResponseCode.OK;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import in.koreatech.koin.domain.weather.dto.WeatherResponse;
+import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "(Normal) Weather: 날씨", description = "병천 날씨 정보를 조회한다")
 public interface WeatherApi {
 
-    @ApiResponses(
-        value = {
-            @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", examples =
-            @ExampleObject(
-                value = """
-                    {
-                      "temperature": 21,
-                      "weather": "맑음"
-                    }
-                    """
-            ))),
-            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(hidden = true))),
-        }
-    )
+    @ApiResponseCodes({
+        OK,
+        EXTERNAL_API_ERROR
+    })
     @Operation(summary = "병천 날씨 조회")
     @GetMapping("/weather")
     ResponseEntity<WeatherResponse> getWeather();
