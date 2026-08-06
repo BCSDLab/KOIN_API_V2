cc2171cfcfeab95935a91f86066b1a2bdee213ca
신관규 <soundbar91@gmail.com>
2026-08-06 14:16:43 +0900

    refactor: 강의 API 응답 코드 명세 변경

--- src/main/java/in/koreatech/koin/admin/lecture/controller/AdminLectureApi.java
+++ src/main/java/in/koreatech/koin/admin/lecture/controller/AdminLectureApi.java
@@ -2,6 +2,13 @@ package in.koreatech.koin.admin.lecture.controller;
 
 import static in.koreatech.koin.admin.history.enums.DomainType.LECTURES;
 import static in.koreatech.koin.domain.user.model.UserType.ADMIN;
+import static in.koreatech.koin.global.code.ApiResponseCode.DUPLICATE_LECTURE;
+import static in.koreatech.koin.global.code.ApiResponseCode.FORBIDDEN_ADMIN;
+import static in.koreatech.koin.global.code.ApiResponseCode.INVALID_REQUEST_BODY;
+import static in.koreatech.koin.global.code.ApiResponseCode.INVALID_SEMESTER_FORMAT;
+import static in.koreatech.koin.global.code.ApiResponseCode.NOT_FOUND_SEMESTER;
+import static in.koreatech.koin.global.code.ApiResponseCode.OK;
+import static in.koreatech.koin.global.code.ApiResponseCode.UNAUTHORIZED_USER;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,11 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import in.koreatech.koin.admin.history.aop.AdminActivityLogging;
 import in.koreatech.koin.admin.lecture.dto.AdminLectureCreateRequest;
 import in.koreatech.koin.global.auth.Auth;
+import in.koreatech.koin.global.code.ApiResponseCodes;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -22,17 +26,16 @@ import jakarta.validation.Valid;
 @Tag(name = "(Admin) Lecture: 강의", description = "관리자 권한으로 강의를 관리한다")
 public interface AdminLectureApi {
 
-    @ApiResponses(
-        value = {
-            @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(hidden = true))),
-            @ApiResponse(responseCode = "409", content = @Content(schema = @Schema(hidden = true))),
-        }
-    )
-    @Operation(summary = "강의 생성")
+    @ApiResponseCodes({
+        OK,
+        INVALID_REQUEST_BODY,
+        UNAUTHORIZED_USER,
+        FORBIDDEN_ADMIN,
+        INVALID_SEMESTER_FORMAT,
+        NOT_FOUND_SEMESTER,
+        DUPLICATE_LECTURE
+    })
+    @Operation(summary = "강의 일괄 생성")
     @SecurityRequirement(name = "Jwt Authentication")
     @PostMapping("/admin/lectures")
     @AdminActivityLogging(domain = LECTURES)
