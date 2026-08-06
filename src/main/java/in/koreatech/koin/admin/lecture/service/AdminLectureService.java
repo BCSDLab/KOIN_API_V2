137f3dea1aad74029aa7ea1f7ae384a0d058c060
신관규 <soundbar91@gmail.com>
2026-08-06 14:19:16 +0900

    refactor: 관리자 학기 저장소 분리

--- src/main/java/in/koreatech/koin/admin/lecture/service/AdminLectureService.java
+++ src/main/java/in/koreatech/koin/admin/lecture/service/AdminLectureService.java
@@ -1,7 +1,6 @@
 package in.koreatech.koin.admin.lecture.service;
 
 import static in.koreatech.koin.global.code.ApiResponseCode.DUPLICATE_LECTURE;
-import static in.koreatech.koin.global.code.ApiResponseCode.NOT_FOUND_SEMESTER;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -12,9 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import in.koreatech.koin.admin.lecture.dto.AdminLectureCreateRequest;
 import in.koreatech.koin.admin.lecture.dto.AdminLectureCreateRequest.LectureRequest;
 import in.koreatech.koin.admin.lecture.repository.AdminLectureRepository;
+import in.koreatech.koin.admin.lecture.repository.AdminSemesterRepository;
 import in.koreatech.koin.domain.timetable.model.Semester;
 import in.koreatech.koin.domain.timetableV3.model.Term;
-import in.koreatech.koin.domain.timetableV3.repository.SemesterRepositoryV3;
 import in.koreatech.koin.global.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 
@@ -24,16 +23,12 @@ import lombok.RequiredArgsConstructor;
 public class AdminLectureService {
 
     private final AdminLectureRepository adminLectureRepository;
-    private final SemesterRepositoryV3 semesterRepositoryV3;
+    private final AdminSemesterRepository adminSemesterRepository;
 
     @Transactional
     public void createLectures(AdminLectureCreateRequest request) {
         Term term = Term.fromDescription(request.term());
-        Semester semester = semesterRepositoryV3.findByYearAndTerm(request.year(), term)
-            .orElseThrow(() -> CustomException.of(
-                NOT_FOUND_SEMESTER,
-                "year: " + request.year() + ", term: " + request.term()
-            ));
+        Semester semester = adminSemesterRepository.getByYearAndTerm(request.year(), term);
 
         Set<LectureKey> lectureKeys = new HashSet<>();
         for (LectureRequest lecture : request.lectures()) {
