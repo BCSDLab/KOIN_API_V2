0f134c8540f4af6b2ed8748ec8ead00b23657ca9
dnjswldnd-3513 <zheld3513@gmail.com>
2026-08-29 21:02:52 +0900

    feat: 채팅방 타입/상태 enum 추가 (ChatRoomType, ChatRoomStatus)

---
+++ src/main/java/in/koreatech/koin/domain/teamrecruitment/model/enums/ChatRoomStatus.java
@@ -0,0 +1,6 @@
+package in.koreatech.koin.domain.teamrecruitment.model.enums;
+
+public enum ChatRoomStatus {
+    ACTIVE,
+    READ_ONLY
+}
---
+++ src/main/java/in/koreatech/koin/domain/teamrecruitment/model/enums/ChatRoomType.java
@@ -0,0 +1,6 @@
+package in.koreatech.koin.domain.teamrecruitment.model.enums;
+
+public enum ChatRoomType {
+    TEAM,
+    DIRECT
+}
