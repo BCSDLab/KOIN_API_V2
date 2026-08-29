771b397d23ca7675cfcc913ed5af5e829f4b4b7c
dnjswldnd-3513 <zheld3513@gmail.com>
2026-08-29 22:57:46 +0900

    refactor: 팀원 모집 중복 엔티티, Repository, Flyway 마이그레이션 제거
    - domain.team.recruitment로 통합된 엔티티 중복 파일 삭제
      (ChatRoom, ChatMessage, ChatRoomMember, Notification)
    - 중복 enum 삭제 (ChatRoomType, ChatRoomStatus, NotificationTargetType 등)
    - 중복 Repository 삭제 (Chat, Notification 관련 4개)
    - V5, V6 Flyway 마이그레이션 중복 파일 삭제

--- src/main/java/in/koreatech/koin/domain/teamrecruitment/model/enums/ChatRoomType.java
+++
@@ -1,6 +0,0 @@
-package in.koreatech.koin.domain.teamrecruitment.model.enums;
-
-public enum ChatRoomType {
-    TEAM,
-    DIRECT
-}
--- src/main/java/in/koreatech/koin/domain/teamrecruitment/model/enums/NotificationTargetType.java
+++
@@ -1,8 +0,0 @@
-package in.koreatech.koin.domain.teamrecruitment.model.enums;
-
-public enum NotificationTargetType {
-    APPLICANT_MANAGEMENT,
-    CHAT_ROOM,
-    MY_APPLICATIONS,
-    NONE
-}
