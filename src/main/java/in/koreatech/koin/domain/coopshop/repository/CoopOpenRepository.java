5dd25067d4e97a7faf2d822cb394a59e245fd8aa
dnjswldnd-3513 <zheld3513@gmail.com>
2026-09-18 09:40:26 +0900

    refactor: 사용하지 않는 CoopOpenRepository 죽은 코드 제거

--- src/main/java/in/koreatech/koin/domain/coopshop/repository/CoopOpenRepository.java
+++ src/main/java/in/koreatech/koin/domain/coopshop/repository/CoopOpenRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.repository.Repository;
 
-import in.koreatech.koin.domain.coopshop.exception.CoopOpenNotFoundException;
 import in.koreatech.koin.domain.coopshop.model.CoopOpen;
 import in.koreatech.koin.domain.coopshop.model.CoopShop;
 import in.koreatech.koin.domain.coopshop.model.DayType;
@@ -12,11 +11,4 @@ import in.koreatech.koin.domain.coopshop.model.DayType;
 public interface CoopOpenRepository extends Repository<CoopOpen, Integer> {
 
     Optional<CoopOpen> findByCoopShopAndTypeAndDayOfWeek(CoopShop coopShop, String type, DayType dayOfWeek);
-
-    default CoopOpen getByCoopShopAndTypeAndDayOfWeek(CoopShop coopShop, String type, DayType dayOfWeek) {
-        return findByCoopShopAndTypeAndDayOfWeek(coopShop, type, dayOfWeek)
-            .orElseThrow(() -> CoopOpenNotFoundException.withDetail(
-                String.format("coopShop: %s, type: %s, day of week: %s", coopShop, type, dayOfWeek.getDay())
-            ));
-    }
 }
