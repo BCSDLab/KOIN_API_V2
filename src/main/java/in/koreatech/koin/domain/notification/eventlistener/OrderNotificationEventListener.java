f159e38a70b1bcfdbdfa5d9d59f36e4cbbefc313
신관규 <soundbar91@gmail.com>
2026-09-23 20:13:36 +0900

    refactor: 주문 알림 리스너의 예외 처리 제거 (#2455)

--- src/main/java/in/koreatech/koin/domain/notification/eventlistener/OrderNotificationEventListener.java
+++ src/main/java/in/koreatech/koin/domain/notification/eventlistener/OrderNotificationEventListener.java
@@ -8,9 +8,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 import in.koreatech.koin.domain.notification.service.OrderNotificationService;
 import in.koreatech.koin.domain.order.order.event.OrderStatusChangedEvent;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrderNotificationEventListener {
@@ -20,11 +18,6 @@ public class OrderNotificationEventListener {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void onOrderStatusChanged(OrderStatusChangedEvent event) {
-        try {
-            orderNotificationService.pushNotification(event);
-        } catch (Exception e) {
-            log.error("주문 상태 알림 처리 실패. orderId={}, userId={}, status={}",
-                event.orderId(), event.userId(), event.status(), e);
-        }
+        orderNotificationService.pushNotification(event);
     }
 }
